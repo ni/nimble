070d6f00a9bd6806334c0fb12b440b50f04449fc
Nicolas Webster <22551874+scotia673@users.noreply.github.com>
2021-06-29 08:24:33 -0500

    register nimble-text-field

--- packages/nimble-components/src/index.ts
+++ packages/nimble-components/src/index.ts
@@ -1,12 +1,14 @@
 // TODO: Is exporting Foundation still necessary with the updated API's?
 // export * from "@microsoft/fast-element";
-import { DesignSystem } from "@microsoft/fast-foundation";
-import { nimbleDesignSystemProvider } from  "./design-system-provider/nimble-design-system";
-import { nimbleButton } from "./button/nimble-button";
+import { DesignSystem } from '@microsoft/fast-foundation';
+import { nimbleDesignSystemProvider } from  './design-system-provider/nimble-design-system';
+import { nimbleButton } from './button/nimble-button';
+import { nimbleTextField } from './text-field/index';
 
 const allComponents = {
     nimbleDesignSystemProvider,
-    nimbleButton
+    nimbleButton,
+    nimbleTextField
 }
 
 export const nimbleDesignSystem = DesignSystem.getOrCreate().withPrefix('nimble').register(
--- packages/nimble-components/src/text-field/index.ts
+++ packages/nimble-components/src/text-field/index.ts
@@ -2,9 +2,7 @@ import { TextField as FoundationTextField, textFieldTemplate as template } from 
 import { textFieldStyles as styles } from '@microsoft/fast-components';
 
 export const nimbleTextField = FoundationTextField.compose({
-    // TODO figure out where 'fast-' prefix is being added in fast components. Looks like withPrefix is used, see:
-    // https://github.com/microsoft/fast/blob/5e302d3593279a7ccdb354099aa9c30f82169e14/packages/web-components/fast-foundation/src/design-system/design-system.ts#L253
-    baseName: 'nimble-text-field',
+    baseName: 'text-field',
     template,
     styles,
     shadowOptions: {
