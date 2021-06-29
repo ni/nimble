b5a3ce8ecedfbe76dfdbb294e2081267f98801e4
Nicolas Webster <22551874+scotia673@users.noreply.github.com>
2021-06-29 18:04:00 -0500

    fix tests

--- angular-workspace/projects/example-client-app/src/app/app.component.spec.ts
+++ angular-workspace/projects/example-client-app/src/app/app.component.spec.ts
@@ -1,31 +1,28 @@
 import { TestBed } from '@angular/core/testing';
+import { NimbleTextFieldModule } from '@ni/nimble-angular';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [
+                AppComponent
+            ],
+            imports: [
+                NimbleTextFieldModule
+            ]
+        }).compileComponents();
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-  it(`should have as title 'example-client-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('example-client-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('example-client-app app is running!');
-  });
+    it(`should have as title 'example-client-app'`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app.title).toEqual('example-client-app');
+    });
 });
