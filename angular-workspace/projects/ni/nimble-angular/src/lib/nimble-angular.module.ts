17f16572d3fe395bfb6003642ec3f2d82fe6e829
Nicolas Webster <22551874+scotia673@users.noreply.github.com>
2021-06-29 12:29:18 -0500

    Angular infrastructure (#4)

---
+++ angular-workspace/projects/ni/nimble-angular/src/lib/nimble-angular.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NimbleAngularComponent } from './nimble-angular.component';
+
+
+
+@NgModule({
+  declarations: [
+    NimbleAngularComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    NimbleAngularComponent
+  ]
+})
+export class NimbleAngularModule { }
---
+++ angular-workspace/projects/ni/nimble-angular/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of nimble-angular
+ */
+
+export * from './lib/nimble-angular.service';
+export * from './lib/nimble-angular.component';
+export * from './lib/nimble-angular.module';
