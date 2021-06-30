4eaecbf8abe00af09166027be85ef5b631f97a0e
jattasNI <jattasNI@users.noreply.github.com>
2021-06-30 14:11:38 -0500

    Fix lint error

--- packages/nimble-components/src/number-field/index.ts
+++ packages/nimble-components/src/number-field/index.ts
@@ -14,6 +14,6 @@ const nimbleNumberField = NumberField.compose<NumberFieldOptions>({
     stepUpGlyph: `
         <span class="step-up-glyph" part="step-up-glyph"></span>
     `,
-})
+});
 
 DesignSystem.getOrCreate().withPrefix('nimble').register(nimbleNumberField());
