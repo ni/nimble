9b24cc69c9714617ec3ea1a1a2d23dc551e51f60
jattasNI <jattasNI@users.noreply.github.com>
2021-06-30 12:28:43 -0500

    Fix syntax error parsing HTML files

--- angular-workspace/.eslintrc.js
+++ angular-workspace/.eslintrc.js
@@ -1,15 +1,24 @@
 module.exports = {
-    extends: [
-        '@ni/eslint-config/typescript',
-        '@ni/eslint-config/typescript-requiring-type-checking'
-    ],
-    plugins: [
-        "@angular-eslint"
-    ],
-    rules: {
-        // *-default-export lines can be deleted once a fix for this issue is published and uptaken
-        // https://github.com/ni/javascript-styleguide/issues/39
-        'import/prefer-default-export': 'off',
-        'import/no-default-export': 'error'
-    }
+    overrides: [{
+        files: ['*.ts'],
+        extends: [
+            '@ni/eslint-config/typescript',
+            '@ni/eslint-config/typescript-requiring-type-checking'
+        ],
+        rules: {
+            // *-default-export lines can be deleted once a fix for this issue is published and uptaken
+            // https://github.com/ni/javascript-styleguide/issues/39
+            'import/prefer-default-export': 'off',
+            'import/no-default-export': 'error'
+        }
+    },
+    {
+        files: ['*.html'],
+        extends: [
+            // Should extend '@ni/eslint-config/angular-template' once a package is published which exports it
+            'plugin:@angular-eslint/template/recommended'
+        ],
+        rules: {
+        }
+    }]
 };
