43a99dc62e3532a3ff88f2b4cfd0ca1073eb55cd
jattasNI <jattasNI@users.noreply.github.com>
2021-06-30 11:46:20 -0500

    Fix definition for angular rules not found

--- angular-workspace/.eslintrc.js
+++ angular-workspace/.eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
         '@ni/eslint-config/typescript',
         '@ni/eslint-config/typescript-requiring-type-checking'
     ],
+    plugins: [
+        "@angular-eslint"
+    ],
     rules: {
         // *-default-export lines can be deleted once a fix for this issue is published and uptaken
         // https://github.com/ni/javascript-styleguide/issues/39
