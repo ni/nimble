8641bb327976d784a40183e589c663397f8ee40a
jattasNI <jattasNI@users.noreply.github.com>
2021-06-30 09:39:19 -0500

    Fix default export errors with new config

--- packages/nimble-components/.eslintrc.js
+++ packages/nimble-components/.eslintrc.js
@@ -5,5 +5,11 @@ module.exports = {
     ],
     parserOptions: {
         project: './tsconfig.json'
+    },
+    rules: {
+        // *-default-export lines can be deleted once a fix for this issue is published and uptaken
+        // https://github.com/ni/javascript-styleguide/issues/39
+        'import/prefer-default-export': 'off',
+        'import/no-default-export': 'error'
     }
 };
