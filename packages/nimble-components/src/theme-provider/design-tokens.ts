9d9a1c14a9e84371cb2f3dfca5a4d07c4b307a52
kevinflanagaNI <kevin.flanagan@ni.com>
2021-06-30 17:14:20 -0500

    updating tokens

--- packages/nimble-components/src/theme-provider/design-tokens.ts
+++ packages/nimble-components/src/theme-provider/design-tokens.ts
@@ -1,33 +1,29 @@
-import { DesignToken } from "@microsoft/fast-foundation";
-import { NimbleTheme } from './themes';
+import { DesignToken } from '@microsoft/fast-foundation';
 import hexRgb from 'hex-rgb';
 import {
     Black91,
     Black15,
     Brand100,
-    Brand15,
+    Selection100,
     BodyFamily,
     OverlineCapsFamily
 } from '@ni/nimble-tokens/dist/styledictionary/js/tokens';
+import { NimbleTheme } from './themes';
 
-function rgbString(hexValue: string) {
-    const {red, green, blue} = hexRgb(hexValue);
+function rgbString(hexValue: string): string {
+    const { red, green, blue } = hexRgb(hexValue);
     return `${red}, ${green}, ${blue}`;
 }
 
 const { create } = DesignToken;
 
-export const theme = create<NimbleTheme>({ name: 'theme', cssCustomPropertyName: null }).withDefault(NimbleTheme.light);
+export const theme = create<NimbleTheme>({ name: 'theme', cssCustomPropertyName: null }).withDefault(NimbleTheme.Light);
 
-//Color Tokens
-export const fillColorSelected = create<string>("fill-color-hover").withDefault(Brand15);
-export const outlineColor = create<string>("outline-color").withDefault((element: HTMLElement)=>{
-    return theme.getValueFor(element) === NimbleTheme.light ? Black91 : Black15;
-});
-export const outlineColorRgb = create<string>("outline-color-rgb").withDefault((element: HTMLElement)=>{
-    return theme.getValueFor(element) === NimbleTheme.light ? rgbString(Black91) : rgbString(Black15);
-});
-export const outlineColorHover = create<string>("outline-color-hover").withDefault(Brand100);
+// Color Tokens
+export const fillColorSelected = create<string>('fill-color-hover').withDefault(Selection100);
+export const outlineColor = create<string>('outline-color').withDefault((element: HTMLElement) => ((theme.getValueFor(element) === NimbleTheme.Light ? Black91 : Black15)) as string);
+export const outlineColorRgb = create<string>('outline-color-rgb').withDefault((element: HTMLElement) => (rgbString(theme.getValueFor(element) === NimbleTheme.Light ? Black91 : Black15)));
+export const outlineColorHover = create<string>('outline-color-hover').withDefault(Brand100);
 
 // Component Sizing Tokens
 export const controlHeight = create<string>('control-height').withDefault('30px');
@@ -41,4 +37,4 @@ export const labelFontFamily = create<string>('label-font-family').withDefault(O
 export const labelFontSize = create<string>('label-font-size').withDefault('11px');
 
 // Font Color Tokens
-export const fontColor = create<string>('label-font-color').withDefault((element: HTMLElement) => (theme.getValueFor(element) === NimbleTheme.light ? Black91 : Black15));
+export const fontColor = create<string>('label-font-color').withDefault((element: HTMLElement) => ((theme.getValueFor(element) === NimbleTheme.Light ? Black91 : Black15)) as string);
--- packages/nimble-components/src/theme-provider/themes.ts
+++ packages/nimble-components/src/theme-provider/themes.ts
@@ -1,4 +1,4 @@
 export enum NimbleTheme {
-    light = 'LIGHT',
-    dark = 'DARK'
+    Light = 'LIGHT',
+    Dark = 'DARK'
 }
