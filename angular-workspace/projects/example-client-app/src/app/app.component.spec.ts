dba96faf6613ef8b9099bd0235f428aeac941e52
jattasNI <jattasNI@users.noreply.github.com>
2021-06-30 11:42:04 -0500

    Auto fix errors in example-client-app

--- angular-workspace/projects/example-client-app/src/app/app.component.spec.ts
+++ angular-workspace/projects/example-client-app/src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'example-client-app'`, () => {
+    it('should have as title \'example-client-app\'', () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
         expect(app.title).toEqual('example-client-app');
