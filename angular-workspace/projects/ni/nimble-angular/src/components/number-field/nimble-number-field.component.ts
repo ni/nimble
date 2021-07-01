4f49fc3469dd1112e0e208fbcd752ee69f159218
Jonathan Meyer <jonathan.meyer@ni.com>
2021-07-01 15:45:42 -0500

    NumberField with nimble icons.

---
+++ angular-workspace/projects/ni/nimble-angular/src/components/number-field/nimble-number-field.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'nimble-angular-number-field',
+    templateUrl: './nimble-number-field.component.html',
+    styleUrls: ['./nimble-number-field.component.scss']
+})
+export class NimbleNumberFieldComponent { }
