8a329d07cdee4914ab0bd7b6cd40f412de35cd45
Nicolas Webster <22551874+scotia673@users.noreply.github.com>
2021-07-06 17:19:59 -0500

    add placeholder to text field

--- angular-workspace/projects/ni/nimble-angular/src/components/text-field/nimble-text-field.component.ts
+++ angular-workspace/projects/ni/nimble-angular/src/components/text-field/nimble-text-field.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'nimble-angular-text-field',
     templateUrl: './nimble-text-field.component.html',
     styleUrls: ['./nimble-text-field.component.scss']
 })
-export class NimbleTextFieldComponent { }
+export class NimbleTextFieldComponent {
+    @Input() public placeholder = '';
+}
