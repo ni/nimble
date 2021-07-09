c0a24f84ef16baabd7b629008b18a420fabec537
Fred Visser <fred.visser@ni.com>
2021-07-09 16:17:35 -0500

    add theme switching support

--- packages/nimble-components/.storybook/preview.js
+++ packages/nimble-components/.storybook/preview.js
@@ -2,8 +2,11 @@
 
 // FAST;
 
+import { addDecorator } from "@storybook/html";
+
 import "@ni/nimble-tokens/dist-manual/nimble-icon-font-thin.css";
 import "@ni/nimble-tokens/dist-manual/fonts.css";
+import "@ni/nimble-components/src/theme-provider/index";
 
 export const parameters = {
   layout: "fullscreen",
@@ -28,3 +31,36 @@ export const parameters = {
     ]
   }
 };
+
+export const globalTypes = {
+  theme: {
+    name: "Theme",
+    description: "Global theme for components",
+    defaultValue: "light",
+    toolbar: {
+      icon: "circlehollow",
+      // array of plain string values for theme.
+      items: ["clear", "LIGHT", "DARK"]
+    }
+  }
+};
+
+const storyAsString = (story, theme) =>
+  `<nimble-theme-provider theme="${theme}">${story}</nimble-theme-provider>`;
+
+const storyAsNode = (story, theme) => {
+  const wrapper = document.createElement("nimble-theme-provider");
+  // wrapper.attributes = [theme]; TODO - develop with real component
+  wrapper.appendChild(story);
+  return wrapper;
+};
+
+addDecorator((story, context) => {
+  const tale = story();
+  const theme = context?.globals?.theme || "LIGHT";
+  console.log(tale);
+  console.log(theme);
+  return typeof tale === "string"
+    ? storyAsString(tale, theme)
+    : storyAsNode(tale, theme);
+});
