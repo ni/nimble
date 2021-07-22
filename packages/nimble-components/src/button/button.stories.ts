5151d839db58bd4f4bbeded36ccf9a71ab9095e2
Fred Visser <fred.visser@ni.com>
2021-07-22 15:21:03 -0500

    adding args to existing stories

--- packages/nimble-components/src/button/button.stories.ts
+++ packages/nimble-components/src/button/button.stories.ts
@@ -10,13 +10,32 @@ export default {
             artboardUrl:
         'https://xd.adobe.com/view/6fdbdacb-b624-4d2c-950e-a58f3fd7ebac-3a88/screen/aeff5951-0982-4f82-a471-a9e5a0675a63/specs'
         }
+    },
+    argTypes: {
+        appearance: {
+            options: Object.values(ButtonAppearance),
+            control: { type: 'radio' }
+        }
+    },
+    render: ({
+        label,
+        appearance
+    }: {
+        label: string,
+        appearance: string
+    }): string => `<nimble-button appearance="${appearance}">${label}</nimble-button>`,
+    args: {
+        label: 'Ghost Button',
+        appearance: 'ghost'
     }
 };
 
-const buttonTemplate = `
-    <nimble-button>Button</nimble-button>
-    <nimble-button appearance="${ButtonAppearance.Ghost}">Ghost button</nimble-button>
-    <nimble-button appearance="${ButtonAppearance.Block}">Block button</nimble-button>
-`;
-
-export const button = (): string => buttonTemplate;
+export const outlineButton = {
+    args: { label: 'Outline Button', appearance: ButtonAppearance.Outline }
+};
+export const ghostButton = {
+    args: { label: 'Ghost Button', appearance: ButtonAppearance.Ghost }
+};
+export const blockButton = {
+    args: { label: 'Block Button', appearance: ButtonAppearance.Block }
+};
--- packages/nimble-components/src/text-field/text-field.stories.ts
+++ packages/nimble-components/src/text-field/text-field.stories.ts
@@ -9,9 +9,31 @@ export default {
             artboardUrl:
         'https://xd.adobe.com/view/2a5a7401-925e-4fcc-9230-39d8e3c56729-7f09/screen/bd1931c3-ab16-4f62-9249-7be138831280/specs/'
         }
+    },
+    render: ({
+        label,
+        type,
+        value
+    }: {
+        label: string,
+        type: string,
+        value: string
+    }): string => `<nimble-text-field placeholder='${label}' type='${type}' value='${value}'>${label}</nimble-text-field>`,
+    args: {
+        label: 'default label',
+        type: 'text',
+        value: ''
     }
 };
 
-const template = '<nimble-text-field>Text Field</nimble-text-field>';
+export const textField = {
+    args: { label: 'Text Field' }
+};
 
-export const textField = (): string => template;
+export const passwordField = {
+    args: { label: 'Password Field', type: 'password' }
+};
+
+export const passwordFieldWithValue = {
+    args: { label: 'Password Field', type: 'password', value: 'password' }
+};
