44a1cbb4eba248a2a8b0373e91d3d257865d3ccf
Fred Visser <fred.visser@ni.com>
2021-07-22 23:42:31 -0500

    add action support

--- packages/nimble-components/src/button/button.stories.ts
+++ packages/nimble-components/src/button/button.stories.ts
@@ -9,6 +9,9 @@ export default {
         design: {
             artboardUrl:
         'https://xd.adobe.com/view/6fdbdacb-b624-4d2c-950e-a58f3fd7ebac-3a88/screen/aeff5951-0982-4f82-a471-a9e5a0675a63/specs'
+        },
+        actions: {
+            handles: ['click']
         }
     },
     argTypes: {
--- packages/nimble-components/src/text-field/text-field.stories.ts
+++ packages/nimble-components/src/text-field/text-field.stories.ts
@@ -8,6 +8,9 @@ export default {
         design: {
             artboardUrl:
         'https://xd.adobe.com/view/2a5a7401-925e-4fcc-9230-39d8e3c56729-7f09/screen/bd1931c3-ab16-4f62-9249-7be138831280/specs/'
+        },
+        actions: {
+            handles: ['change', 'input']
         }
     },
     render: ({
