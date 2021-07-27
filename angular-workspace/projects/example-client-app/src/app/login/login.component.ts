243d4b221ca5eebba03c1fd1c5587447da776a1c
Nicolas Webster <22551874+scotia673@users.noreply.github.com>
2021-07-27 14:00:46 -0500

    login route

---
+++ angular-workspace/projects/example-client-app/src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NimbleTheme } from '@ni/nimble-components/dist/esm/theme-provider/themes';
+
+@Component({
+    selector: 'nimble-example-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+    public theme: NimbleTheme = NimbleTheme.Light;
+    public onoff = 'off';
+    public themes = NimbleTheme;
+    private toggle = true;
+
+    public switchThemes(): void {
+        this.toggle = !this.toggle;
+        this.theme = this.toggle ? NimbleTheme.Light : NimbleTheme.Dark;
+        this.onoff = this.toggle ? 'off' : 'on';
+    }
+}
