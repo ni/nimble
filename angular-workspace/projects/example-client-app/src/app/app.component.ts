cbce4a975e55808e89c1c6e5795ff7af1214db84
Nicolas Webster <22551874+scotia673@users.noreply.github.com>
2021-07-28 13:58:13 -0500

    Update example app for NI Tech Demo + extend text-field to support password mode (#42)
    * Update demo app and add support for password text field

--- angular-workspace/projects/example-client-app/src/app/app.component.ts
+++ angular-workspace/projects/example-client-app/src/app/app.component.ts
@@ -1,22 +1,8 @@
 import { Component } from '@angular/core';
-import { NimbleTheme } from '@ni/nimble-components/dist/esm/theme-provider/themes';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    public title = 'example-client-app';
-
-    public theme: NimbleTheme = NimbleTheme.Light;
-    public onoff = 'off';
-    public themes = NimbleTheme;
-    private toggle = true;
-
-    public onClick(): void {
-        this.toggle = !this.toggle;
-        this.theme = this.toggle ? NimbleTheme.Light : NimbleTheme.Dark;
-        this.onoff = this.toggle ? 'off' : 'on';
-    }
-}
+export class AppComponent { }
--- angular-workspace/projects/example-client-app/src/app/app.module.ts
+++ angular-workspace/projects/example-client-app/src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NimbleTextFieldModule, NimbleNumberFieldModule } from '@ni/nimble-angular';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        LoginComponent
     ],
     imports: [
         BrowserModule,
         NimbleTextFieldModule,
-        NimbleNumberFieldModule
+        NimbleNumberFieldModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: '/login', pathMatch: 'full' },
+            { path: 'login', component: LoginComponent }
+        ])
     ],
     providers: [],
     bootstrap: [AppComponent],
