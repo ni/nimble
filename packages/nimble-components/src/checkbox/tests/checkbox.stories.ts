4cfbe238c4b23f99c37fd04edb5c097d9264d959
Lukas Kall <lukas.kall@ni.com>
2021-08-03 12:32:04 +0200

    Add actions and design spec

--- packages/nimble-components/src/checkbox/tests/checkbox.stories.ts
+++ packages/nimble-components/src/checkbox/tests/checkbox.stories.ts
@@ -1,4 +1,5 @@
 import type { Meta, Story } from '@storybook/html';
+import { withXD } from 'storybook-addon-xd-designs';
 import '../index';
 
 interface CheckboxArgs {
@@ -9,6 +10,16 @@ interface CheckboxArgs {
 
 const metadata: Meta<CheckboxArgs> = {
     title: 'Checkbox',
+    decorators: [withXD],
+    parameters: {
+        design: {
+            artboardUrl:
+                'https://xd.adobe.com/view/8ce280ab-1559-4961-945c-182955c7780b-d9b1/screen/3698340b-8162-4e5d-bf7a-20194612b3a7/specs'
+        },
+        actions: {
+            handles: ['change']
+        }
+    },
     args: {
         label: 'Checkbox label',
         checked: false,
