cedb9676f4d87902c3db749b49389efa560ea910
Fred Visser <fred.visser@ni.com>
2021-08-09 15:17:16 -0500

    Standarize storybook typing (#71)
    🤨 Rationale
    
    @LukasKall noted that there are Story and Meta types that we should use within *.stories.ts files, and types are great.
    
    👩‍💻 Implementation
    
    We're using the simpler (but up-and-coming) CSF 3.0 format, so I've updated to Storybook 6.4 alpha (to get the updated types for CSF changes - i.e. the render parameter), and refactored all stories to be consistent with the Story and Meta types and the CSF 3.0.
    
    🧪 Testing
    
    Reviewed Storybook locally - no issues with component rendering or plugins
    Ran build and test locally - no issues
    ✅ Checklist
    
    [ X] I have updated the project documentation to reflect my changes or determined no changes are needed.

--- packages/nimble-components/src/number-field/tests/number-field.stories.ts
+++ packages/nimble-components/src/number-field/tests/number-field.stories.ts
@@ -1,7 +1,13 @@
+import type { Story, Meta } from '@storybook/html';
 import { withXD } from 'storybook-addon-xd-designs';
 import '../index';
 
-export default {
+interface NumberFieldArgs {
+    label: string;
+    value: number;
+}
+
+const metadata: Meta<NumberFieldArgs> = {
     title: 'Number Field',
     decorators: [withXD],
     parameters: {
@@ -13,17 +19,19 @@ export default {
             handles: ['change', 'input']
         }
     },
-    render: ({ label, value }: { label: string, value: string }): string => `<nimble-number-field id='user' placeholder='${label}' value='${value}'>${label}</nimble-number-field>`,
+    render: ({ label, value }: NumberFieldArgs): string => `<nimble-number-field id='user' placeholder='${label}' value='${value}'>${label}</nimble-number-field>`,
     args: {
         label: 'default label',
-        value: 'number'
+        value: undefined
     }
 };
 
-export const numberField = {
+export default metadata;
+
+export const numberField: Story<NumberFieldArgs> = {
     args: { label: 'Number Field' }
 };
 
-export const numberFieldWithValue = {
+export const numberFieldWithValue: Story<NumberFieldArgs> = {
     args: { label: 'Number Field', value: 1234567 }
 };
