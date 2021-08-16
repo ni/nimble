b8dac7bd41680f92abad089d599f06d2c0cec767
Fred Visser <fred.visser@ni.com>
2021-08-16 09:35:49 -0500

    update DSP from XD library

--- packages/nimble-tokens/dist/styledictionary/js/tokens.js
+++ packages/nimble-tokens/dist/styledictionary/js/tokens.js
@@ -1,40 +1,49 @@
 /**
  * Do not edit directly
- * Generated on Tue, 20 Jul 2021 19:35:33 GMT
+ * Generated on Mon, 16 Aug 2021 14:33:54 GMT
  */
 
-export const Fail100Dark = "#ff4663";
-export const Fail100Light = "#c4000c";
+export const SlLegacyBlue = "#009ff5";
+export const Information100DarkUi = "#a46eff";
+export const Information100 = "#804ad9";
+export const Warning100DarkUi = "#ff6900";
+export const Pass100LightUi = "#009921";
+export const Pass100DarkUi = "#00c12b";
+export const WarningTypographyLightUi = "#d94100";
+export const PassTypographyLightUi = "#00801c";
+export const Fail100LightUi = "#c4000c";
+export const Fail100DarkUi = "#ff4663";
 export const Black75 = "#818386";
-export const Enterprise = "#074023";
-export const Fail85 = "#cf1a37";
-export const Selection100 = "#00b888";
-export const Brand85 = "#26a97c";
-export const Black88 = "#252526";
 export const Black15 = "#f1f1f2";
 export const Black7 = "#f5f5f5";
-export const Black85 = "#363738";
-export const Black = "#000000";
-export const Warning100 = "#ff4b00";
-export const Black91 = "#161617";
 export const White = "#ffffff";
-export const Black95 = "#0e0f0f";
-export const Pass100 = "#00c12b";
-export const Fail100 = "#c4000c";
-export const Brand100 = "#009b65";
+export const Black88 = "#252526";
+export const Black85 = "#363738";
 export const Black80 = "#505153";
-export const Header1Family = "Noto Serif";
-export const Header1Weight = "Regular";
-export const SubtitleFamily = "Source Sans Pro";
-export const SubtitleWeight = "Regular";
+export const Black = "#000000";
+export const Black95 = "#0e0f0f";
+export const Black91 = "#161617";
+export const Enterprise = "#074023";
+export const Selection100 = "#00b888";
+export const Brand85 = "#26a97c";
+export const Brand100 = "#009b65";
+export const Fail85 = "#cf1a37";
+export const RgbNiSky = "#3cb4e7";
+export const Header2Family = "Source Sans Pro";
+export const Header2Weight = "Light";
 export const OverlineCapitalizedFamily = "Space Mono";
 export const OverlineCapitalizedWeight = "Regular";
 export const BodyFamily = "Source Sans Pro";
 export const BodyWeight = "Regular";
 export const Header0Family = "Noto Serif";
 export const Header0Weight = "Regular";
-export const Header1Size = "24rem";
-export const SubtitleSize = "12rem";
-export const OverlineCapitalizedSize = "12rem";
+export const SubHeaderFamily = "Source Sans Pro";
+export const SubHeaderWeight = "Regular";
+export const Header1Family = "Noto Serif";
+export const Header1Weight = "Regular";
+export const Header2Size = "22rem";
+export const OverlineCapitalizedSize = "11rem";
 export const BodySize = "14rem";
 export const Header0Size = "36rem";
+export const SubHeaderSize = "12rem";
+export const Header1Size = "24rem";
