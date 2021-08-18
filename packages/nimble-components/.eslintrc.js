8c2a53cf9eb2c8f7eecf11ab9590bf0b14e2dad1
Fred Visser <fred.visser@ni.com>
2021-08-18 09:58:54 -0500

    revert formatting change

--- packages/nimble-components/.eslintrc.js
+++ packages/nimble-components/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     root: true,
-    extends: ['@ni'],
+    extends: '@ni',
     parserOptions: {
         ecmaVersion: 2020
     },
@@ -10,20 +10,15 @@ module.exports = {
         'node_modules',
         'dist'
     ],
-    overrides: [
-        {
-            files: ['.storybook/**'],
-            env: {
-                browser: true
-            },
-            rules: {
-                // Storybook files will not be in published package and are allowed to use devDependencies
-                'import/no-extraneous-dependencies': [
-                    'error',
-                    { devDependencies: true }
-                ],
-                'import/no-default-export': 'off'
-            }
+    overrides: [{
+        files: ['.storybook/**'],
+        env: {
+            browser: true
+        },
+        rules: {
+            // Storybook files will not be in published package and are allowed to use devDependencies
+            'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+            'import/no-default-export': 'off'
         }
-    ]
+    }]
 };
