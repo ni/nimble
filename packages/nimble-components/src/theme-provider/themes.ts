6ffc2b3fc4b14e438c1f0f01bf2febd18d431288
Fred Visser <fred.visser@ni.com>
2021-08-18 16:45:37 -0500

    minor comment

--- packages/nimble-components/src/theme-provider/themes.ts
+++ packages/nimble-components/src/theme-provider/themes.ts
@@ -2,5 +2,5 @@ export enum NimbleTheme {
     Light = 'light',
     Dark = 'dark',
     Color = 'color',
-    LegacyBlue = 'legacyBlue'
+    LegacyBlue = 'legacy-blue' // ⛔️ only for use within SystemLink apps that haven't been updated to brand-aligned controls
 }
