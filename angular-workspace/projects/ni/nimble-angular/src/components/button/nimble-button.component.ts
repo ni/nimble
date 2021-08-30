18949b51a5322a96354ee409796a0bcab7139094
Jonathan Meyer <jonathan.meyer@ni.com>
2021-08-30 16:41:04 -0500

    Update nimble-button.component.ts
    Adding docs.

--- angular-workspace/projects/ni/nimble-angular/src/components/button/nimble-button.component.ts
+++ angular-workspace/projects/ni/nimble-angular/src/components/button/nimble-button.component.ts
@@ -1,11 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { ButtonAppearance } from '@ni/nimble-components/dist/esm/button/types';
 
+/**
+ * This component provides a simple wrapper for the nimble button component. See:
+ * https://github.com/ni/nimble/blob/main/packages/nimble-components/src/button/index.ts
+ */
 @Component({
     selector: 'nimble-angular-button',
     templateUrl: './nimble-button.component.html'
 })
 export class NimbleButtonComponent {
+    /** Represents the disabled state of the component. */
     @Input() public disabled = false;
+
+    /**  Helps control the appearance of the button with respect to its theme. See the following link
+    *    for the set of valid states:
+    *    https://github.com/ni/nimble/blob/main/packages/nimble-components/src/button/types.ts */
     @Input() public appearance = ButtonAppearance.Outline;
 }
