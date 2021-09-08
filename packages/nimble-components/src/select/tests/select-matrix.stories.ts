d6cbed462bb496faf6dd1c59db4c16fddf915f02
Brian Henry <brianehenry@gmail.com>
2021-09-08 12:36:43 -0500

    Updated XD links

--- packages/nimble-components/src/select/tests/select-matrix.stories.ts
+++ packages/nimble-components/src/select/tests/select-matrix.stories.ts
@@ -13,7 +13,7 @@ const metadata: Meta = {
     parameters: {
         design: {
             artboardUrl:
-                'https://xd.adobe.com/view/8ce280ab-1559-4961-945c-182955c7780b-d9b1/screen/4ec38f56-6100-411f-8993-fde767635c44/'
+                'https://xd.adobe.com/view/8ce280ab-1559-4961-945c-182955c7780b-d9b1/screen/6ec70d21-9a59-40cd-a8f4-45cfeed9e01e/specs'
         }
     }
 };
--- packages/nimble-components/src/select/tests/select-opened-matrix.stories.ts
+++ packages/nimble-components/src/select/tests/select-opened-matrix.stories.ts
@@ -9,7 +9,7 @@ const metadata: Meta = {
     parameters: {
         design: {
             artboardUrl:
-                'https://xd.adobe.com/view/8ce280ab-1559-4961-945c-182955c7780b-d9b1/screen/4ec38f56-6100-411f-8993-fde767635c44/'
+                'https://xd.adobe.com/view/8ce280ab-1559-4961-945c-182955c7780b-d9b1/screen/c098395e-30f8-4bd4-b8c5-394326b59919/specs'
         }
     }
 };
