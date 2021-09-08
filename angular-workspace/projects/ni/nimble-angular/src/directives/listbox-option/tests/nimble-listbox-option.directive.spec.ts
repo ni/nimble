111981c2b962c6005823eacc6b771bfff513028e
Brian Henry <brianehenry@gmail.com>
2021-09-08 14:23:49 -0500

    Deleted directive tests, they are useless

--- angular-workspace/projects/ni/nimble-angular/src/directives/listbox-option/tests/nimble-listbox-option.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NimbleListboxOptionDirective } from '../nimble-listbox-option.directive';
-
-describe('NimbleListboxOptionDirective', () => {
-    it('should create an instance', () => {
-        const directive = new NimbleListboxOptionDirective();
-        expect(directive).toBeTruthy();
-    });
-});
--- angular-workspace/projects/ni/nimble-angular/src/directives/select/tests/nimble-select.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NimbleSelectDirective } from '../nimble-select.directive';
-
-describe('NimbleSelectDirective', () => {
-    it('should create an instance', () => {
-        const directive = new NimbleSelectDirective();
-        expect(directive).toBeTruthy();
-    });
-});
