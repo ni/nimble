43fcfd659fbf62eb1a1bcaacbdc2699f2ed5e31e
Brian Henry <brianehenry@gmail.com>
2021-09-08 12:30:12 -0500

    Removed copied control value accessor and added custom implementation. Added workaround for initial value bug

--- angular-workspace/projects/ni/nimble-angular/src/directives/select/nimble-select.directive.ts
+++ angular-workspace/projects/ni/nimble-angular/src/directives/select/nimble-select.directive.ts
@@ -1,8 +1,54 @@
-import { Directive, Input } from '@angular/core';
+import { Directive, ElementRef, forwardRef, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Select } from '@ni/nimble-components/dist/esm/select';
 
 @Directive({
-    selector: 'nimble-select'
+    selector: 'nimble-select',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => NimbleSelectDirective),
+            multi: true
+        }
+    ]
 })
-export class NimbleSelectDirective {
+export class NimbleSelectDirective implements ControlValueAccessor {
     @Input() public disabled: boolean;
+
+    public constructor(private elementRef: ElementRef<Select>, private renderer2: Renderer2) { }
+
+    writeValue(value: any): void {
+        this.renderer2.setProperty(this.elementRef.nativeElement, 'value', value);
+
+        // Workaround for https://github.com/microsoft/fast/issues/5139
+        // The Angular value binding sets the initial value before the FAST select has initialized its options property.
+        // We set the selected attribute on the appropriate option element to make sure the initial value is set correctly.
+        if (this.elementRef.nativeElement.options.length === 0 && value !== null) {
+            const matchingOption = this.elementRef.nativeElement.querySelector(`nimble-listbox-option[value=${value}]`);
+            if (matchingOption !== null) {
+                this.renderer2.setAttribute(matchingOption, 'selected', '');
+            }
+        }
+    }
+
+    registerOnChange(fn: any): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any): void {
+        this.onTouched = fn;
+    }
+    
+    setDisabledState?(isDisabled: boolean): void {
+        this.renderer2.setProperty(this.elementRef.nativeElement, 'disabled', isDisabled);
+    }
+
+    @HostListener('change')
+    changeListener() {
+        this.onChange(this.elementRef.nativeElement.value);
+        this.onTouched();
+    }
+
+    private onTouched = (): void => {};
+    private onChange = (value: string): void => {};
 }
--- packages/nimble-components/src/select/index.ts
+++ packages/nimble-components/src/select/index.ts
@@ -5,8 +5,14 @@ import {
     selectTemplate as template
 } from '@microsoft/fast-foundation';
 import { downArrow16X16 } from '@ni/nimble-tokens/dist-icons-esm/nimble-icons-inline';
-
 import { styles } from './styles';
+
+/**
+ * A nimble-styled HTML number input
+ */
+type Select = FoundationSelect;
+
+export type { Select };
 
 const nimbleSelect = FoundationSelect.compose<SelectOptions>({
     baseName: 'select',
