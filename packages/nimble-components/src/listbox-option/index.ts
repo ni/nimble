e6fe1dd1db3d0c2268d8e5f9e85a938f0db52300
Brian Henry <brianehenry@gmail.com>
2021-09-09 15:45:58 -0500

    Export listbox type

--- packages/nimble-components/src/listbox-option/index.ts
+++ packages/nimble-components/src/listbox-option/index.ts
@@ -3,8 +3,14 @@ import {
     ListboxOption as FoundationListboxOption,
     listboxOptionTemplate as template
 } from '@microsoft/fast-foundation';
-
 import { styles } from './styles';
+
+/**
+ * A nimble-styled HTML listbox option
+ */
+type ListboxOption = FoundationListboxOption;
+
+export type { ListboxOption };
 
 const nimbleListboxOption = FoundationListboxOption.compose({
     baseName: 'listbox-option',
