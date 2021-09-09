5628b9e7b144e6870031e5705b9db7896510e43c
Nicolas Webster <22551874+scotia673@users.noreply.github.com>
2021-09-09 10:32:01 -0500

    Upgrade fast-foundation and leverage end slot support for text-field (#108)
    ## 🤨 Rationale
    
    When we first added the invalid state styling for the text-field, the end slot did not support using a default value - something for which I filed an issue to the fast project (issue [here](https://github.com/microsoft/fast/issues/5116). They submitted a fix for this that became available in the latest version of fast-foundation.
    
    ## 👩‍💻 Implementation
    
    Upgraded fast-foundation package and updated text-field implementation to use the new end slot default value.

--- packages/nimble-components/src/text-field/index.ts
+++ packages/nimble-components/src/text-field/index.ts
@@ -2,6 +2,7 @@ import { attr } from '@microsoft/fast-element';
 import {
     DesignSystem,
     TextField as FoundationTextField,
+    TextFieldOptions,
     textFieldTemplate as template
 } from '@microsoft/fast-foundation';
 import { alarmActive16X16 } from '@ni/nimble-tokens/dist-icons-esm/nimble-icons-inline';
@@ -28,20 +29,17 @@ class TextField extends FoundationTextField {
         if (!this.invalid) {
             this.invalid = false;
         }
-
-        // https://github.com/microsoft/fast/issues/5116
-        // Will hopefully be able to set this to the end slot in compose in the future
-        this.end.innerHTML = `${alarmActive16X16.data}`;
     }
 }
 
-const nimbleTextField = TextField.compose({
+const nimbleTextField = TextField.compose<TextFieldOptions>({
     baseName: 'text-field',
     template,
     styles,
     shadowOptions: {
         delegatesFocus: true
-    }
+    },
+    end: `${alarmActive16X16.data}`
 });
 
 DesignSystem.getOrCreate().withPrefix('nimble').register(nimbleTextField());
