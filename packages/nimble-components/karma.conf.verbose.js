7a245f44dc55bd2eb6efef9cfce3d33d59c1451c
jattasNI <jattasNI@users.noreply.github.com>
2021-09-10 13:31:25 -0500

    Prettier

--- packages/nimble-components/karma.conf.verbose.js
+++ packages/nimble-components/karma.conf.verbose.js
@@ -7,11 +7,8 @@ const originalConfigFunction = require('./karma.conf');
 module.exports = config => {
     originalConfigFunction(config);
     const options = {
-        plugins: [
-            ...config.plugins,
-            'karma-spec-reporter'
-        ],
-        reporters: [...config.reporters, 'spec'],
+        plugins: [...config.plugins, 'karma-spec-reporter'],
+        reporters: [...config.reporters, 'spec']
     };
 
     config.set(options);
