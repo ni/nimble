108001245242f59986d09c4c2e75467fb573c8fa
Lukas Kall <lukas.kall@ni.com>
2021-09-14 13:31:14 +0200

    Simple styling with active and focus indicator

--- packages/nimble-components/src/tab-panel/styles.ts
+++ packages/nimble-components/src/tab-panel/styles.ts
@@ -1,4 +1,16 @@
 import { css } from '@microsoft/fast-element';
+import { display } from '@microsoft/fast-foundation';
+import {
+    contentFontColor,
+    contentFontSize,
+    fontFamily
+} from '../theme-provider/design-tokens';
 
 export const styles = css`
+    ${display('block')} :host {
+        box-sizing: border-box;
+        font-family: ${fontFamily};
+        font-size: ${contentFontSize};
+        color: ${contentFontColor};
+    }
 `;
--- packages/nimble-components/src/tab/styles.ts
+++ packages/nimble-components/src/tab/styles.ts
@@ -1,4 +1,39 @@
 import { css } from '@microsoft/fast-element';
+import { display } from '@microsoft/fast-foundation';
+import {
+    borderColorHover,
+    borderWidth,
+    contentFontColor,
+    contentFontSize,
+    controlHeight,
+    fillColorHover,
+    fontFamily,
+    standardPadding
+} from '../theme-provider/design-tokens';
+import { focusVisible } from '../utilities/style/focus';
 
 export const styles = css`
+    ${display('inline-flex')} :host {
+        box-sizing: border-box;
+        font-family: ${fontFamily};
+        font-size: ${contentFontSize};
+        height: ${controlHeight};
+        padding: calc(${standardPadding} / 2) ${standardPadding}
+            calc(${standardPadding} / 2 - ${borderWidth});
+        color: ${contentFontColor};
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        ${/* Separate focus indicator from active indicator */ ''}
+        border-bottom: transparent ${borderWidth} solid;
+    }
+
+    :host(:hover) {
+        background-color: ${fillColorHover};
+    }
+
+    :host(${focusVisible}) {
+        outline: none;
+        box-shadow: 0 calc(${borderWidth} * -1) ${borderColorHover} inset;
+    }
 `;
