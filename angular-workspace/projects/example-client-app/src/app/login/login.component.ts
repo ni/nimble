036a4535fb0076815bec3e8915dc072e18f7cf9e
Jonathan Meyer <26874831+atmgrifter00@users.noreply.github.com>
2021-09-14 17:43:07 -0500

    Angular button directive (#119)
    * Adding button Angular directive
    
    - Introducing Angular button directive
    - Updating Login app to change appearance of button.
    
    * Change files
    
    * Update @ni-nimble-angular-86caefcb-b798-4c4d-841b-e801f91b9c3f.json
    
    Change to prerelease.
    
    * Fixing bad merge.
    
    I lost a change to login.component.ts that was needed (and also introduced an incorrect change into the html).
    
    * Wasn't using the directive in the login app.
    
    Needed to import the directive module into the login app. Also fixing an incorrect binding to the disabled attribute.
    
    Co-authored-by: Fred Visser <1458528+fredvisser@users.noreply.github.com>

--- angular-workspace/projects/example-client-app/src/app/login/login.component.ts
+++ angular-workspace/projects/example-client-app/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ButtonAppearance } from '@ni/nimble-components/dist/esm/button/types';
 import { NimbleTheme } from '@ni/nimble-components/dist/esm/theme-provider/themes';
 
 @Component({
@@ -11,6 +12,7 @@ export class LoginComponent {
     public loginForm: FormGroup;
     public theme: NimbleTheme = NimbleTheme.Light;
     public themes = NimbleTheme;
+    public buttonAppearance = ButtonAppearance;
 
     public constructor(private readonly formBuilder: FormBuilder) {
         this.loginForm = this.formBuilder.group({
---
+++ angular-workspace/projects/ni/nimble-angular/src/directives/button/nimble-button-directive.ts
@@ -0,0 +1,10 @@
+import { Directive, HostBinding, Input } from '@angular/core';
+import { ButtonAppearance } from '@ni/nimble-components/dist/esm/button/types';
+
+@Directive({
+    selector: 'nimble-button'
+})
+export class NimbleButtonDirective {
+    @HostBinding('disabled') @Input() public disabled: boolean;
+    @HostBinding('appearance') @Input() public appearance: ButtonAppearance;
+}
