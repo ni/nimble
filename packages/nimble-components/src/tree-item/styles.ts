d52e59f0066bd131655abe02a82fa1f7c7adfdcc
Kevin Flanagan <kevin.flanagan@ni.com>
2021-09-16 11:26:48 -0500

    nesting indentation

--- packages/nimble-components/src/tree-item/styles.ts
+++ packages/nimble-components/src/tree-item/styles.ts
@@ -13,7 +13,8 @@ import {
     borderColorHover,
     fillColorSelected,
     contentFontSize,
-    fillColorHover
+    fillColorHover,
+    fillColorSelectedHover
 } from '../theme-provider/design-tokens';
 
 const ltr = css`
@@ -49,126 +50,128 @@ const rtl = css`
 export const styles: (
     context: ElementDefinitionContext,
     definition: TreeItemOptions
-) => ElementStyles = (
-    context: ElementDefinitionContext,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    definition: TreeItemOptions
-) => css`
-        ${display('block')} :host {
-            contain: content;
-            position: relative;
-            outline: none;
-            color: ${contentFontColor};
-            background: ${applicationBackgroundColor};
-            cursor: pointer;
-            font-family: ${fontFamily};
-            --tree-item-nested-width: 0;
-        }
-        :host(:focus) > .positioning-region {
-            outline: none;
-        }
-        :host(:focus) .content-region {
-            outline: none;
-        }
-        .positioning-region {
-            display: flex;
-            position: relative;
-            box-sizing: border-box;
-            height: 40px;
-        }
-        .positioning-region:hover {
-            background: ${fillColorHover};
-        }
-        .content-region {
-            display: inline-flex;
-            align-items: center;
-            white-space: nowrap;
-            width: 100%;
-            height: 16px;
-            padding: 12px;
-            font-size: ${contentFontSize};
-        }
-        .items {
-            display: none;
-            font-size: ${contentFontSize};
-        }
-        .expand-collapse-button {
-            background: none;
-            border: none;
-            outline: none;
-            width: 16px;
-            height: 16px;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            cursor: pointer;
-            margin-left: 10px;
-            margin-right: 18px;
-        }
-        .expand-collapse-button svg {
-            width: 16px;
-            height: 16px;
-            transition: transform 0.2s ease-in;
-            pointer-events: none;
-            fill: currentcolor;
-        }
-        .start,
-        .end {
-            display: flex;
-            fill: currentcolor;
-        }
-
-        ::slotted(svg) {
-            width: 16px;
-            height: 16px;
-        }
-        .start {
-            margin-inline-end: 16px;
-        }
-        .end {
-            margin-inline-start: 16px;
-        }
-        :host([expanded]) > .items {
-            display: block;
-        }
-        :host([disabled]) .content-region {
-            opacity: 0.5;
-            cursor: not-allowed;
-        }
-        :host(.nested) .content-region {
-            position: relative;
-            margin-inline-start: 16px;
-        }
-        :host(.nested) .expand-collapse-button {
-            position: absolute;
-        }
-        :host([selected]) {
-            background: ${fillColorSelected};
-        }
-        :host([selected])::after {
-            /* The background needs to be calculated based on the selected background state
-            for this control. We currently have no way of changing that, so setting to
-            accent-foreground-rest for the time being */
-            background: ${borderColorHover};
-            border-radius: 0px;
-            content: '';
-            display: block;
-            position: absolute;
-            top: 0px;
-            width: 2px;
-            height: 40px;
-        }
-        ::slotted(${context.tagFor(TreeItem)}) {
-            --tree-item-nested-width: 1em;
-            --expand-collapse-button-nested-width: 15px;
-        }
-    `.withBehaviors(new DirectionalStyleSheetBehavior(ltr, rtl));
-
-// this block controlled indenting the nested items, it might end up being a useful trick
-/* .positioning-region::before {
+) => ElementStyles = (context: ElementDefinitionContext) =>
+    css`
+    ${display("block")} :host {
+        contain: content;
+        position: relative;
+        outline: none;
+        color: ${contentFontColor};
+        background: ${applicationBackgroundColor};
+        cursor: pointer;
+        font-family: ${fontFamily};
+        --tree-item-nested-width: 0;
+    }
+    :host(:focus) > .positioning-region {
+        outline: none;
+    }
+    :host(:focus) .content-region {
+        outline: none;
+    }
+    .positioning-region {
+        display: flex;
+        position: relative;
+        box-sizing: border-box;
+        height: 32px;
+    }
+    .positioning-region::before {
         content: "";
         display: block;
         width: var(--tree-item-nested-width);
         flex-shrink: 0;
-    } */
+    }
+    .content-region {
+        display: inline-flex;
+        align-items: center;
+        white-space: nowrap;
+        width: 100%;
+        padding: 12px;
+        margin-inline-start: 16px;
+        font-size: ${contentFontSize};
+        user-select: none;
+    }
+
+    .items {
+        display: none;
+        /*  for reasons unknown this controls the nested indentation, make it larger or shorter by changing the px value
+            it must minimally contain arithmetic with an em and a px value
+        */
+        font-size: calc(1em + 20px);
+    }
+    .expand-collapse-button {
+        background: none;
+        border: none;
+        outline: none;
+        width: 16px;
+        height: 16px;
+        padding: 0px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        margin-left: 10px;
+        margin-right: 18px;
+    }
+    .expand-collapse-button svg {
+        width: 16px;
+        height: 16px;
+        transition: transform 0.2s ease-in;
+        pointer-events: none;
+        fill: currentcolor;
+    }
+    .start,
+    .end {
+        display: flex;
+        fill: currentcolor;
+    }
+    
+    ::slotted(svg) {
+        width: 16px;
+        height: 16px;
+    }
+    .start {
+        margin-inline-start: 16px;
+        margin-inline-end: 16px;
+    }
+    .end {
+        margin-inline-start: 16px;
+    }
+    :host([expanded]) > .items {
+        display: block;
+    }
+    :host([disabled]) .content-region {
+        opacity: 0.5;
+        cursor: not-allowed;
+    }
+    :host(.nested) .content-region {
+        position: relative;
+        margin-inline-start: 16px;
+    }
+    :host(.nested) .expand-collapse-button {
+        position: absolute;
+    }
+    .positioning-region:hover {
+        background: ${fillColorHover};
+    }
+    :host([selected]) .positioning-region {
+        background: ${fillColorSelected};
+    }
+    :host([selected]) .positioning-region:hover {
+        background: ${fillColorSelectedHover};
+    }
+    :host([selected])::after {
+        /* this controls the side border */
+        background: ${borderColorHover};
+        border-radius: 0px;
+        content: '';
+        display: block;
+        position: absolute;
+        top: 0px;
+        width: 2px;
+        height: 32px;
+    }
+    ::slotted(${context.tagFor(TreeItem)}) {
+        --tree-item-nested-width: 1em;
+        --expand-collapse-button-nested-width: -15px;
+    }
+    `.withBehaviors(new DirectionalStyleSheetBehavior(ltr, rtl));
