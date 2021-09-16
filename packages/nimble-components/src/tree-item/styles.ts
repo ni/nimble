d82c3d5f9a15a8626bbf9bf00ccb2d76b978d2d7
Kevin Flanagan <kevin.flanagan@ni.com>
2021-09-16 14:16:07 -0500

    lint & pretty

--- packages/nimble-components/src/tree-item/styles.ts
+++ packages/nimble-components/src/tree-item/styles.ts
@@ -50,129 +50,128 @@ const rtl = css`
 export const styles: (
     context: ElementDefinitionContext,
     definition: TreeItemOptions
-) => ElementStyles = (context: ElementDefinitionContext) =>
-    css`
-    ${display("block")} :host {
-        contain: content;
-        position: relative;
-        outline: none;
-        color: ${contentFontColor};
-        cursor: pointer;
-        font-family: ${fontFamily};
-        --tree-item-nested-width: 0;
-    }
-    :host(:focus) > .positioning-region {
-        outline: none;
-    }
-    :host(:focus) .content-region {
-        outline: none;
-    }
-    .positioning-region {
-        display: flex;
-        position: relative;
-        box-sizing: border-box;
-        height: 32px;
-    }
-    .positioning-region::before {
-        content: "";
-        display: block;
-        width: var(--tree-item-nested-width);
-        flex-shrink: 0;
-    }
-    .content-region {
-        display: inline-flex;
-        align-items: center;
-        white-space: nowrap;
-        width: 100%;
-        padding-left: 10px;
-        font-size: ${contentFontSize};
-        user-select: none;
-    }
-    /* this rule keeps children without an icon text aligned with parents */
-    .content-region span {
-        width: 16px;
-    }
-    .items {
-        display: none;
-        /*  this controls the nested indentation (by affecting .positioning-region::before)
+) => ElementStyles = (context: ElementDefinitionContext) => css`
+        ${display('block')} :host {
+            contain: content;
+            position: relative;
+            outline: none;
+            color: ${contentFontColor};
+            cursor: pointer;
+            font-family: ${fontFamily};
+            --tree-item-nested-width: 0;
+        }
+        :host(:focus) > .positioning-region {
+            outline: none;
+        }
+        :host(:focus) .content-region {
+            outline: none;
+        }
+        .positioning-region {
+            display: flex;
+            position: relative;
+            box-sizing: border-box;
+            height: 32px;
+        }
+        .positioning-region::before {
+            content: '';
+            display: block;
+            width: var(--tree-item-nested-width);
+            flex-shrink: 0;
+        }
+        .content-region {
+            display: inline-flex;
+            align-items: center;
+            white-space: nowrap;
+            width: 100%;
+            padding-left: 10px;
+            font-size: ${contentFontSize};
+            user-select: none;
+        }
+        /* this rule keeps children without an icon text aligned with parents */
+        .content-region span {
+            width: 16px;
+        }
+        .items {
+            display: none;
+            /*  this controls the nested indentation (by affecting .positioning-region::before)
             it must minimally contain arithmetic with an em and a px value
             make it larger or shorter by changing the px value
         */
-        font-size: calc(1em + 32px);
-    }
-    .expand-collapse-button {
-        background: none;
-        border: none;
-        outline: none;
-        width: 16px;
-        height: 16px;
-        padding: 0px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        cursor: pointer;
-        margin-left: 10px;
-    }
-    .expand-collapse-button svg {
-        width: 16px;
-        height: 16px;
-        transition: transform 0.2s ease-in;
-        pointer-events: none;
-        fill: currentcolor;
-    }
-    .start,
-    .end {
-        display: flex;
-        fill: currentcolor;
-    }
-    
-    ::slotted(svg) {
-        width: 16px;
-        height: 16px;
-    }
-    .start {
-        margin-inline-start: 16px;
-        margin-inline-end: 16px;
-    }
-    .end {
-        margin-inline-start: 16px;
-    }
-    :host([expanded]) > .items {
-        display: block;
-    }
-    :host([disabled]) .content-region {
-        opacity: 0.5;
-        cursor: not-allowed;
-    }
-    :host(.nested) .content-region {
-        position: relative;
-        margin-inline-start: 16px;
-    }
-    :host(.nested) .expand-collapse-button {
-        position: absolute;
-    }
-    .positioning-region:hover {
-        background: ${fillColorHover};
-    }
-    :host([selected]) .positioning-region {
-        background: ${fillColorSelected};
-    }
-    :host([selected]) .positioning-region:hover {
-        background: ${fillColorSelectedHover};
-    }
-    /* this controls the side border */
-    :host([selected])::after {
-        background: ${borderColorHover};
-        border-radius: 0px;
-        content: '';
-        display: block;
-        position: absolute;
-        top: 0px;
-        width: 2px;
-        height: 32px;
-    }
-    ::slotted(${context.tagFor(TreeItem)}) {
-        --tree-item-nested-width: 1em;
-        --expand-collapse-button-nested-width: -16px;
-    }
+            font-size: calc(1em + 32px);
+        }
+        .expand-collapse-button {
+            background: none;
+            border: none;
+            outline: none;
+            width: 16px;
+            height: 16px;
+            padding: 0px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            cursor: pointer;
+            margin-left: 10px;
+        }
+        .expand-collapse-button svg {
+            width: 16px;
+            height: 16px;
+            transition: transform 0.2s ease-in;
+            pointer-events: none;
+            fill: currentcolor;
+        }
+        .start,
+        .end {
+            display: flex;
+            fill: currentcolor;
+        }
+
+        ::slotted(svg) {
+            width: 16px;
+            height: 16px;
+        }
+        .start {
+            margin-inline-start: 16px;
+            margin-inline-end: 16px;
+        }
+        .end {
+            margin-inline-start: 16px;
+        }
+        :host([expanded]) > .items {
+            display: block;
+        }
+        :host([disabled]) .content-region {
+            opacity: 0.5;
+            cursor: not-allowed;
+        }
+        :host(.nested) .content-region {
+            position: relative;
+            margin-inline-start: 16px;
+        }
+        :host(.nested) .expand-collapse-button {
+            position: absolute;
+        }
+        .positioning-region:hover {
+            background: ${fillColorHover};
+        }
+        :host([selected]) .positioning-region {
+            background: ${fillColorSelected};
+        }
+        :host([selected]) .positioning-region:hover {
+            background: ${fillColorSelectedHover};
+        }
+        /* this controls the side border */
+        :host([selected])::after {
+            background: ${borderColorHover};
+            border-radius: 0px;
+            content: '';
+            display: block;
+            position: absolute;
+            top: 0px;
+            width: 2px;
+            height: 32px;
+        }
+        ::slotted(${context.tagFor(TreeItem)}) {
+            --tree-item-nested-width: 1em;
+            --expand-collapse-button-nested-width: -16px;
+        }
     `.withBehaviors(new DirectionalStyleSheetBehavior(ltr, rtl));
