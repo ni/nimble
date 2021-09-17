5c9f69166939c2d6ac7984569d9cc5c66928b610
Fred Visser <1458528+fredvisser@users.noreply.github.com>
2021-09-17 13:42:24 -0500

    update tree-view story

--- packages/nimble-components/src/tree-view/tests/tree-view.stories.ts
+++ packages/nimble-components/src/tree-view/tests/tree-view.stories.ts
@@ -2,9 +2,12 @@ import type { Story, Meta } from '@storybook/html';
 import { withXD } from 'storybook-addon-xd-designs';
 import '../index';
 import '../../tree-item/index';
+import {
+    slHeader32X32,
+    chart40X40
+} from '@ni/nimble-tokens/dist-icons-esm/nimble-icons-inline';
 
 interface TreeArgs {
-    disabled: boolean;
     options: ItemArgs[];
 }
 
@@ -12,6 +15,7 @@ interface ItemArgs {
     label: string;
     value: string;
     disabled: boolean;
+    icon: boolean;
 }
 
 const metadata: Meta<TreeArgs> = {
@@ -25,29 +29,26 @@ const metadata: Meta<TreeArgs> = {
             handles: ['change']
         }
     },
-    render: ({ disabled, options }: TreeArgs): string => `
-        <nimble-tree-view
-            ${disabled ? 'disabled' : ''}
-        >
-            ${options
-        .map(
-            option => `<nimble-tree-item value="${option.value}" ${
-                option.disabled ? 'disabled' : ''
-            }>${option.label}</nimble-tree-item>\n`
-        )
+    // prettier-ignore
+    render: ({ options }: TreeArgs): string => `
+        <nimble-tree-view>
+        ${options.map(option => `
+            <nimble-tree-item value="${option.value}" ${option.disabled ? 'disabled' : ''}>
+                ${option.icon ? `<svg slot="start">${slHeader32X32.data}</svg>` : ''}
+                ${option.label}
+                <nimble-tree-item slot="item">Nested Item 1</nimble-tree-item>
+                <nimble-tree-item slot="item"><svg slot="start">${chart40X40.data}</svg>Nested Item 2</nimble-tree-item>
+                <nimble-tree-item slot="item"><svg slot="start">${chart40X40.data}</svg>Nested Item 3</nimble-tree-item>
+            </nimble-tree-item>\n`)
         .join('')}
         </nimble-tree-view>
 `,
     args: {
-        disabled: false,
         options: [
-            { label: 'Option 1', value: '1', disabled: false },
-            { label: 'Option 2', value: '2', disabled: true },
-            {
-                label: 'Option 3<nimble-tree-item slot="item">Nested Option 3</nimble-tree-item>',
-                value: '3',
-                disabled: false
-            }
+            { label: 'Option 1', value: '1', disabled: false, icon: true },
+            { label: 'Option 2', value: '2', disabled: true, icon: true },
+            { label: 'Option 3', value: '3', disabled: false, icon: true },
+            { label: 'Option 4', value: '3', disabled: false, icon: false }
         ]
     }
 };
@@ -55,16 +56,3 @@ const metadata: Meta<TreeArgs> = {
 export default metadata;
 
 export const treeView: Story<TreeArgs> = {};
-
-export const test = {
-    render: (): string => `
-        <nimble-tree-view>
-            <nimble-tree-item>Root node one</nimble-tree-item>
-            <nimble-tree-item>Root node two</nimble-tree-item>
-            <nimble-tree-item>Root node two and a half
-                <nimble-tree-item slot="item">Nested node one</nimble-tree-item>
-                <nimble-tree-item slot="item">Nested node two</nimble-tree-item>
-            </nimble-tree-item>
-            <nimble-tree-item>Root node three</nimble-tree-item>
-        </nimble-tree-view>`
-};
