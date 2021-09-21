9489d198b9edbfcd5f23a800c356666d03da9377
Jonathan Meyer <26874831+atmgrifter00@users.noreply.github.com>
2021-09-21 11:45:41 -0500

    Reverting changes to nimble button directive.

--- angular-workspace/projects/ni/nimble-angular/src/directives/button/nimble-button-directive.ts
+++ angular-workspace/projects/ni/nimble-angular/src/directives/button/nimble-button-directive.ts
@@ -1,6 +1,5 @@
 import { Directive, HostBinding, Input } from '@angular/core';
 import { ButtonAppearance } from '@ni/nimble-components/dist/esm/button/types';
-import { NimbleIcon } from '@ni/nimble-components/dist/esm/icons/types';
 
 @Directive({
     selector: 'nimble-button'
@@ -8,5 +7,4 @@ import { NimbleIcon } from '@ni/nimble-components/dist/esm/icons/types';
 export class NimbleButtonDirective {
     @HostBinding('disabled') @Input() public disabled: boolean;
     @HostBinding('appearance') @Input() public appearance: ButtonAppearance;
-    @HostBinding('icon') @Input() public icon: string | NimbleIcon;
 }
