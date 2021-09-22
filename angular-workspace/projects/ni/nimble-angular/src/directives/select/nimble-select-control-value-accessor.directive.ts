4733a0f930f8e1a47f7ba3caa0a7c463be829f89
Brian Henry <5454342+brianehenry@users.noreply.github.com>
2021-09-22 18:14:06 -0500

    Select control value accessor (#120)
    ## 🤨 Rationale
    
    [User Story 1627432: Review ControlValueAccessor support in existing components](https://ni.visualstudio.com/DevCentral/_workitems/edit/1627432)
    
    When integrating Nimble elements with Angular, we want to provide first-class form integration, similar to native HTML inputs and Material controls. 
    
    ## 👩‍💻 Implementation
    
    Implemented a new ControlValueAccessor for the nimble-select control, which extends Angular's [SelectControlValueAccessor](https://github.com/angular/angular/blob/master/packages/forms/src/directives/select_control_value_accessor.ts). This new CVA supports binding to a `ngValue` property on `nimble-listbox-option` elements, which allows for binding to any type of value (objects, enums, etc) instead of just strings. There is also a `compareWith` property on the `nimble-select` which can be used with `ngValue` options to implement custom value equality checking.
    
    ## 🧪 Testing
    
    Wrote tests covering the `nimble-select`'s value binding as well as the `nimble-listbox-option`'s `value` and `ngValue` inputs.

---
+++ angular-workspace/projects/ni/nimble-angular/src/directives/select/nimble-select-control-value-accessor.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, SelectControlValueAccessor } from '@angular/forms';
+
+/**
+ * Extension of Angular's SelectControlValueAccessor to target the Nimble select control.
+ *
+ * @see NimbleSelectOptionDirective
+ */
+@Directive({
+    selector:
+      'nimble-select:not([multiple])[formControlName],nimble-select:not([multiple])[formControl],nimble-select:not([multiple])[ngModel]',
+    host: { '(change)': 'onChange($event.target.value)', '(blur)': 'onTouched()' },
+    providers: [{
+        provide: NG_VALUE_ACCESSOR,
+        useExisting: forwardRef(() => NimbleSelectControlValueAccessor),
+        multi: true
+    }]
+})
+export class NimbleSelectControlValueAccessor extends SelectControlValueAccessor {
+}
