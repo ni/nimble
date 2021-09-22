5537a377ae210063d19f9b6c59bdc3ed42fb1c9c
Brian Henry <5454342+brianehenry@users.noreply.github.com>
2021-09-22 16:47:32 -0500

    Moved workarounds for listbox option to Nimble element from directive. Combined option directives

--- angular-workspace/projects/ni/nimble-angular/src/directives/select/nimble-select.module.ts
+++ angular-workspace/projects/ni/nimble-angular/src/directives/select/nimble-select.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NimbleSelectDirective } from './nimble-select.directive';
 import { NimbleSelectControlValueAccessor } from './nimble-select-control-value-accessor.directive';
-import { NimbleSelectOptionDirective } from './nimble-select-option.directive';
 
 import '@ni/nimble-components/dist/esm/select';
 
 @NgModule({
-    declarations: [NimbleSelectDirective, NimbleSelectControlValueAccessor, NimbleSelectOptionDirective],
+    declarations: [NimbleSelectDirective, NimbleSelectControlValueAccessor],
     imports: [CommonModule],
-    exports: [NimbleSelectDirective, NimbleSelectControlValueAccessor, NimbleSelectOptionDirective]
+    exports: [NimbleSelectDirective, NimbleSelectControlValueAccessor]
 })
 export class NimbleSelectModule { }
--- packages/nimble-components/src/listbox-option/index.ts
+++ packages/nimble-components/src/listbox-option/index.ts
@@ -5,14 +5,38 @@ import {
 } from '@microsoft/fast-foundation';
 import { styles } from './styles';
 
+export type { ListboxOption };
+
 /**
  * A nimble-styled HTML listbox option
  */
-type ListboxOption = FoundationListboxOption;
+class ListboxOption extends FoundationListboxOption {
+    public get value(): string {
+        return super.value;
+    }
 
-export type { ListboxOption };
+    public set value(value: string) {
+        // Coerce value to string
+        super.value = `${value}`;
 
-const nimbleListboxOption = FoundationListboxOption.compose({
+        if (this.$fastController.isConnected) {
+            this.reflectValueAttributeIfUnset();
+        }
+    }
+
+    public override connectedCallback(): void {
+        super.connectedCallback();
+        this.reflectValueAttributeIfUnset();
+    }
+
+    private reflectValueAttributeIfUnset(): void {
+        if (typeof this.value === 'string' && this.value.length !== 0 && this.getAttribute('value') === null) {
+            this.setAttribute('value', this.value);
+        }
+    }
+}
+
+const nimbleListboxOption = ListboxOption.compose({
     baseName: 'listbox-option',
     template,
     styles
