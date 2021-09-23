b23e4f133d9d1a84cab2649be1ea1c7faa5560b8
Brian Henry <5454342+brianehenry@users.noreply.github.com>
2021-09-23 10:36:19 -0500

    Updated tests

--- angular-workspace/projects/ni/nimble-angular/src/directives/text-field/tests/nimble-text-field-control-value-accessor.spec.ts
+++ angular-workspace/projects/ni/nimble-angular/src/directives/text-field/tests/nimble-text-field-control-value-accessor.spec.ts
@@ -1,68 +1,137 @@
+// eslint-disable-next-line max-classes-per-file
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { NimbleTextFieldModule } from '../nimble-text-field.module';
 import { TextField } from '../../../../../../../../packages/nimble-components/dist/esm/text-field';
-
-async function waitAsync(): Promise<void> {
-    await new Promise(window.requestAnimationFrame);
-}
+import { waitAnimationFrame, waitMicrotask } from '../../../async-test-utilities';
 
 function setTextFieldValue(textField: TextField, value: string): void {
     textField.value = value;
     textField.dispatchEvent(new Event('input'));
+    textField.dispatchEvent(new Event('change'));
 }
 
 describe('Nimble text field control value accessor', () => {
-    @Component({
-        template: `
-            <nimble-text-field #textField [(ngModel)]="value"></nimble-text-field>
-         `
-    })
-    class TestHostComponent {
-        @ViewChild('textField', { static: true }) public textField: ElementRef<TextField>;
+    describe('using [(ngModel)] binding', () => {
+        @Component({
+            template: `
+                <nimble-text-field #textField [(ngModel)]="value" [disabled]="fieldDisabled"></nimble-text-field>
+             `
+        })
+        class TestHostComponent {
+            @ViewChild('textField', { static: true }) public textField: ElementRef<TextField>;
 
-        public readonly initialValue = 'initial value';
-        public value = this.initialValue;
-    }
+            public readonly initialValue = 'initial value';
+            public value = this.initialValue;
+            public fieldDisabled = false;
+        }
 
-    let textField: TextField;
-    let fixture: ComponentFixture<TestHostComponent>;
-    let testHostComponent: TestHostComponent;
+        let textField: TextField;
+        let fixture: ComponentFixture<TestHostComponent>;
+        let testHostComponent: TestHostComponent;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [TestHostComponent],
-            imports: [NimbleTextFieldModule, FormsModule]
-        }).compileComponents();
+        beforeEach(async () => {
+            await TestBed.configureTestingModule({
+                declarations: [TestHostComponent],
+                imports: [NimbleTextFieldModule, FormsModule]
+            }).compileComponents();
+        });
+
+        beforeEach(async () => {
+            fixture = TestBed.createComponent(TestHostComponent);
+            testHostComponent = fixture.componentInstance;
+            textField = testHostComponent.textField.nativeElement;
+            fixture.detectChanges();
+        });
+
+        it('sets correct initial selected value', () => {
+            expect(testHostComponent.value).toBe(testHostComponent.initialValue);
+            expect(textField.value).toBe(testHostComponent.initialValue);
+        });
+
+        it('updates selected value when bound property is changed', async () => {
+            const newValue = 'new value';
+            testHostComponent.value = newValue;
+            fixture.detectChanges();
+            await waitMicrotask();
+
+            expect(textField.value).toBe(newValue);
+        });
+
+        it('updates bound property when selected value is changed', async () => {
+            const newValue = 'new value';
+            setTextFieldValue(textField, newValue);
+            fixture.detectChanges();
+
+            expect(testHostComponent.value).toBe(newValue);
+        });
+
+        it('sets "disabled" attribute with value of bound property', async () => {
+            testHostComponent.fieldDisabled = true;
+            fixture.detectChanges();
+            await waitAnimationFrame();
+
+            expect(textField.getAttribute('disabled')).toBe('');
+            expect(textField.disabled).toBe(true);
+        });
     });
 
-    beforeEach(async () => {
-        fixture = TestBed.createComponent(TestHostComponent);
-        testHostComponent = fixture.componentInstance;
-        textField = testHostComponent.textField.nativeElement;
-        fixture.detectChanges();
-    });
+    describe('using [value] binding', () => {
+        @Component({
+            template: `
+                <nimble-text-field #textField [value]="value" (change)="valueChanged()"></nimble-text-field>
+             `
+        })
+        class TestHostComponent {
+            @ViewChild('textField', { static: true }) public textField: ElementRef<TextField>;
 
-    it('should set correct initial selected value', async () => {
-        expect(testHostComponent.value).toBe(testHostComponent.initialValue);
-        expect(textField.value).toBe(testHostComponent.initialValue);
-    });
+            public readonly initialValue = 'initial value';
+            public value = this.initialValue;
 
-    it('should update selected value when bound property is changed', async () => {
-        const newValue = 'new value';
-        testHostComponent.value = newValue;
-        fixture.detectChanges();
-        await waitAsync();
+            public valueChanged(): void {
+                this.value = this.textField.nativeElement.value;
+            }
+        }
 
-        expect(textField.value).toBe(newValue);
-    });
+        let textField: TextField;
+        let fixture: ComponentFixture<TestHostComponent>;
+        let testHostComponent: TestHostComponent;
 
-    it('should update bound property when selected value is changed', async () => {
-        const newValue = 'new value';
-        setTextFieldValue(textField, newValue);
-        fixture.detectChanges();
+        beforeEach(async () => {
+            await TestBed.configureTestingModule({
+                declarations: [TestHostComponent],
+                imports: [NimbleTextFieldModule, FormsModule]
+            }).compileComponents();
+        });
 
-        expect(testHostComponent.value).toBe(newValue);
+        beforeEach(async () => {
+            fixture = TestBed.createComponent(TestHostComponent);
+            testHostComponent = fixture.componentInstance;
+            textField = testHostComponent.textField.nativeElement;
+            fixture.detectChanges();
+        });
+
+        it('should set correct initial selected value', () => {
+            expect(testHostComponent.value).toBe(testHostComponent.initialValue);
+            expect(textField.value).toBe(testHostComponent.initialValue);
+        });
+
+        it('should update selected value when bound property is changed', async () => {
+            const newValue = 'new value';
+            testHostComponent.value = newValue;
+            fixture.detectChanges();
+            await waitMicrotask();
+
+            expect(textField.value).toBe(newValue);
+        });
+
+        it('should update bound property when selected value is changed', async () => {
+            const newValue = 'new value';
+            setTextFieldValue(textField, newValue);
+            fixture.detectChanges();
+
+            expect(testHostComponent.value).toBe(newValue);
+        });
     });
 });
