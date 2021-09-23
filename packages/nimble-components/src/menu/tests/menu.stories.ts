71bdcbf2ed94944a45cfe5c53a5b3d01e09ba8d8
Michael Brandt <35616474+michaeldbrandt@users.noreply.github.com>
2021-09-23 14:45:34 -0500

    Removed icons from the story, since they will be implemented later.

--- packages/nimble-components/src/menu/tests/menu.stories.ts
+++ packages/nimble-components/src/menu/tests/menu.stories.ts
@@ -2,7 +2,6 @@ import type { Story, Meta } from '@storybook/html';
 import { withXD } from 'storybook-addon-xd-designs';
 import '../index';
 import '../../menu-item/index';
-import { admin16X16 } from '@ni/nimble-tokens/dist-icons-esm/nimble-icons-inline';
 
 interface MenuArgs {
     options: ItemArgs[];
@@ -11,7 +10,6 @@ interface MenuArgs {
 interface ItemArgs {
     text: string;
     disabled: boolean;
-    icon: boolean;
 }
 
 const metadata: Meta = {
@@ -25,19 +23,10 @@ const metadata: Meta = {
             handles: ['change']
         }
     },
+    // prettier-ignore
     render: ({ options }: MenuArgs): string => `
-        <nimble-menu>
-        ${options
-        .map(
-            option => `
-            <nimble-menu-item${option.disabled ? 'disabled' : ''}>${
-    option.icon
-        ? `<svg slot="start">${admin16X16.data}</svg>`
-        : ''
-}${option.text}</nimble-menu-item>
-            `
-        )
-        .join('')}
+        <nimble-menu>${options.map(option => `
+            <nimble-menu-item ${option.disabled ? 'disabled' : ''}>${option.text}</nimble-menu-item>`).join('')}
         </nimble-menu>
 `,
     args: {
@@ -45,17 +34,14 @@ const metadata: Meta = {
             {
                 text: 'Item 1',
                 disabled: false,
-                icon: false
             },
             {
                 text: 'Item 2',
                 disabled: false,
-                icon: false
             },
             {
                 text: 'Item 3',
                 disabled: false,
-                icon: false
             }
         ]
     }
