ad9b5a2f6855dfa281355b8a3ce37066263c8da5
rajsite <rajsite@users.noreply.github.com>
2021-09-24 12:45:09 -0500

    Switch from ViewTemplate to html template string

--- packages/nimble-components/src/tree-view/tests/tree.spec.ts
+++ packages/nimble-components/src/tree-view/tests/tree.spec.ts
@@ -1,4 +1,4 @@
-import { DOM, ViewTemplate } from '@microsoft/fast-element';
+import { DOM, html, ref } from '@microsoft/fast-element';
 import type { TreeView, TreeItem } from '@microsoft/fast-foundation';
 import { notebook16X16 } from '@ni/nimble-tokens/dist-icons-esm/nimble-icons-inline';
 import { fixture, Fixture } from '../../tests/utilities/fixture';
@@ -6,84 +6,78 @@ import { clickElement } from '../../tests/utilities/component-test-helpers';
 import '..';
 import '../../tree-item';
 
-async function setup(): Promise<Fixture<TreeView>> {
+class Model {
+    public root: TreeItem;
+    public leaf1: TreeItem;
+    public leaf2: TreeItem;
+    public leafWithIcon: TreeItem;
+}
+
+async function setup(source: Model): Promise<Fixture<TreeView>> {
     return fixture<TreeView>(
-        new ViewTemplate(
-            `<nimble-tree-view>
-                <nimble-tree-item class="root">Root1
-                    <nimble-tree-item class="sub-root">SubRoot
-                        <nimble-tree-item class="leaf1">Leaf 1</nimble-tree-item>
-                    </nimble-tree-item>
-                    <nimble-tree-item class="leaf2">Leaf 2</nimble-tree-item>
-                    <nimble-tree-item class="leafWithIcon"><svg slot="start">${notebook16X16.data}</svg>Leaf 1</nimble-tree-item>
+        // prettier-ignore
+        html<Model>`
+        <nimble-tree-view>
+            <nimble-tree-item ${ref('root')}>Root1
+                <nimble-tree-item>SubRoot
+                    <nimble-tree-item ${ref('leaf1')}>Leaf 1</nimble-tree-item>
                 </nimble-tree-item>
-            </nimble-tree-view>`,
-            []
-        )
+                <nimble-tree-item ${ref('leaf2')}>Leaf 2</nimble-tree-item>
+                <nimble-tree-item ${ref('leafWithIcon')}><svg slot="start">${notebook16X16.data}</svg>Leaf 1</nimble-tree-item>
+            </nimble-tree-item>
+        </nimble-tree-view>`,
+        { source }
     );
 }
 
 describe('TreeView', () => {
-    it('root should not be selected after bring clicked, but should be expanded', async () => {
-        const { element, connect, disconnect } = await setup();
+    let element: TreeView;
+    let connect: () => Promise<void>;
+    let disconnect: () => Promise<void>;
+    let model: Model;
+
+    beforeEach(async () => {
+        model = new Model();
+        ({ element, connect, disconnect } = await setup(model));
         await connect();
         await DOM.nextUpdate();
-        const rootItem = element.querySelector<TreeItem>('.root')!;
+    });
 
-        await clickElement(rootItem);
-        expect(rootItem?.hasAttribute('selected')).toBe(false);
-        expect(rootItem?.hasAttribute('expanded')).toBe(true);
-
-        await clickElement(rootItem);
-        expect(rootItem?.hasAttribute('selected')).toBe(false);
-        expect(rootItem?.hasAttribute('expanded')).toBe(false);
-
+    afterEach(async () => {
         await disconnect();
+    });
+
+    it('root should not be selected after bring clicked, but should be expanded', async () => {
+        await clickElement(model.root);
+        expect(model.root.hasAttribute('selected')).toBe(false);
+        expect(model.root.hasAttribute('expanded')).toBe(true);
+
+        await clickElement(model.root);
+        expect(model.root.hasAttribute('selected')).toBe(false);
+        expect(model.root.hasAttribute('expanded')).toBe(false);
     });
 
     it('leaf should stay selected after parent is expanded\\collapsed', async () => {
-        const { element, connect, disconnect } = await setup();
-        await connect();
-        await DOM.nextUpdate();
-        const rootItem = element.querySelector<TreeItem>('.root')!;
-        const leafItem = element.querySelector<TreeItem>('.leaf2')!;
+        await clickElement(model.root); // expand root
+        await clickElement(model.leaf2); // select leaf
+        expect(model.leaf2.hasAttribute('selected')).toBe(true);
 
-        await clickElement(rootItem); // expand root
-        await clickElement(leafItem); // select leaf
-        expect(leafItem?.hasAttribute('selected')).toBe(true);
-
-        await clickElement(rootItem); // collapse root
-        expect(leafItem?.hasAttribute('selected')).toBe(true);
-
-        await disconnect();
+        await clickElement(model.root); // collapse root
+        expect(model.leaf2.hasAttribute('selected')).toBe(true);
     });
 
     it('when group is clicked, the TreeView currentSelected state does not change', async () => {
-        const { element, connect, disconnect } = await setup();
-        await connect();
-        await DOM.nextUpdate();
-        const rootItem = element.querySelector<TreeItem>('.root')!;
-        const leafItem = element.querySelector<TreeItem>('.leaf2')!;
-
-        await clickElement(leafItem); // select leaf
-        await clickElement(rootItem); // collapse root
-        expect(element.currentSelected).toBe(leafItem);
-
-        await disconnect();
+        await clickElement(model.leaf2); // select leaf
+        await clickElement(model.root); // collapse root
+        expect(element.currentSelected).toBe(model.leaf2);
     });
 
     it('when glyph is clicked tree group is expanded', async () => {
-        const { element, connect, disconnect } = await setup();
-        await connect();
-        await DOM.nextUpdate();
-        const rootItem = element.querySelector<TreeItem>('.root')!;
-        const rootExpandButton = rootItem?.shadowRoot?.querySelector<HTMLElement>(
+        const rootExpandButton = model.root.shadowRoot?.querySelector<HTMLElement>(
             '.expand-collapse-button'
         );
 
         await clickElement(rootExpandButton!);
-        expect(rootItem?.hasAttribute('expanded')).toBe(true);
-
-        await disconnect();
+        expect(model.root.hasAttribute('expanded')).toBe(true);
     });
 });
