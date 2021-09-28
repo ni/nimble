4d2b7a3ade98172b61f229718018a71c956d8acf
jattasNI <jattasNI@users.noreply.github.com>
2021-09-28 14:28:37 -0500

    Empty nimble button wrapper

---
+++ packages/nimble-blazor/NimbleBlazor/Button/Button.Razor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace NimbleBlazor
+{
+    /// <summary>
+    /// The "code behind" for the Nimble Button.
+    /// </summary>
+    public partial class Button : ComponentBase
+    {
+        /// <summary>
+        /// The text displayed on the button
+        /// </summary>
+        [Parameter]
+        public string Text { get; set; }
+
+        /// <summary>
+        /// The event fired when the button is clicked
+        /// </summary>
+        [Parameter]
+        public EventCallback ButtonClicked { get; set; }
+    }
+}
