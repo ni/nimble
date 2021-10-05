0050ffb181c492c32101994812658272f17a7b22
Michael Brandt <35616474+michaeldbrandt@users.noreply.github.com>
2021-10-05 11:30:24 -0500

    Lint fixes

--- packages/nimble-components/src/menu/tests/menu-matrix.stories.ts
+++ packages/nimble-components/src/menu/tests/menu-matrix.stories.ts
@@ -1,8 +1,6 @@
 import type { Story, Meta } from '@storybook/html';
 import { withXD } from 'storybook-addon-xd-designs';
-import {
-    matrixThemeWrapper,
-} from '../../tests/utilities/theme-test-helpers';
+import { matrixThemeWrapper } from '../../tests/utilities/theme-test-helpers';
 import '../index';
 
 const metadata: Meta = {
@@ -12,8 +10,8 @@ const metadata: Meta = {
         design: {
             artboardUrl:
                 'https://xd.adobe.com/view/8ce280ab-1559-4961-945c-182955c7780b-d9b1/screen/6ec70d21-9a59-40cd-a8f4-45cfeed9e01e'
-        },
-    },
+        }
+    }
 };
 
 export default metadata;
