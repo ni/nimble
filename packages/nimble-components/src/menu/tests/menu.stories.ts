21390e492299590e5d8d20910b83ddd95b5c11ce
Michael Brandt <35616474+michaeldbrandt@users.noreply.github.com>
2021-10-05 12:44:22 -0500

    Fixed XD links

--- packages/nimble-components/src/menu/tests/menu.stories.ts
+++ packages/nimble-components/src/menu/tests/menu.stories.ts
@@ -18,7 +18,7 @@ const metadata: Meta = {
     parameters: {
         design: {
             artboardUrl:
-                'https://xd.adobe.com/view/8ce280ab-1559-4961-945c-182955c7780b-d9b1/screen/6ec70d21-9a59-40cd-a8f4-45cfeed9e01e'
+                'https://xd.adobe.com/view/8ce280ab-1559-4961-945c-182955c7780b-d9b1/screen/c098395e-30f8-4bd4-b8c5-394326b59919/specs'
         },
         actions: {
             handles: ['change']
