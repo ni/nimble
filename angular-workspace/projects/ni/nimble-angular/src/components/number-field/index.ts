3cd79fe0ca9f76d133957ad16260b43ebb874f20
Brian Henry <5454342+brianehenry@users.noreply.github.com>
2021-10-05 10:15:38 -0500

    Implemented number field CVA and directive, and removed old wrapper component

--- angular-workspace/projects/ni/nimble-angular/src/components/number-field/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './nimble-number-field.module';
-export * from './nimble-number-field.component';
---
+++ angular-workspace/projects/ni/nimble-angular/src/directives/number-field/tests/nimble-number-field-control-value-accessor.spec.ts
@@ -0,0 +1,137 @@
+// eslint-disable-next-line max-classes-per-file
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { NumberField } from '@ni/nimble-components/dist/esm/number-field';
+import { NimbleNumberFieldModule } from '../nimble-number-field.module';
+import { waitAnimationFrame, waitMicrotask } from '../../../async-test-utilities';
+
+function setNumberFieldValue(numberField: NumberField, value: number): void {
+    numberField.value = value.toString();
+    numberField.dispatchEvent(new Event('input'));
+    numberField.dispatchEvent(new Event('change'));
+}
+
+describe('Nimble number field control value accessor', () => {
+    describe('using [(ngModel)] binding', () => {
+        @Component({
+            template: `
+                <nimble-number-field #numberField [(ngModel)]="value" [disabled]="fieldDisabled" [min]="0"></nimble-number-field>
+             `
+        })
+        class TestHostComponent {
+            @ViewChild('numberField', { static: true }) public numberField: ElementRef<NumberField>;
+
+            public readonly initialValue = 123;
+            public value = this.initialValue;
+            public fieldDisabled = false;
+        }
+
+        let numberField: NumberField;
+        let fixture: ComponentFixture<TestHostComponent>;
+        let testHostComponent: TestHostComponent;
+
+        beforeEach(async () => {
+            await TestBed.configureTestingModule({
+                declarations: [TestHostComponent],
+                imports: [NimbleNumberFieldModule, FormsModule]
+            }).compileComponents();
+        });
+
+        beforeEach(async () => {
+            fixture = TestBed.createComponent(TestHostComponent);
+            testHostComponent = fixture.componentInstance;
+            numberField = testHostComponent.numberField.nativeElement;
+            fixture.detectChanges();
+        });
+
+        it('sets correct initial selected value', () => {
+            expect(testHostComponent.value).toBe(testHostComponent.initialValue);
+            expect(parseFloat(numberField.value)).toBe(testHostComponent.initialValue);
+        });
+
+        it('updates selected value when bound property is changed', async () => {
+            const newValue = 1;
+            testHostComponent.value = newValue;
+            fixture.detectChanges();
+            await waitMicrotask();
+
+            expect(parseFloat(numberField.value)).toBe(newValue);
+        });
+
+        it('updates bound property when selected value is changed', async () => {
+            const newValue = 1;
+            setNumberFieldValue(numberField, newValue);
+            fixture.detectChanges();
+
+            expect(testHostComponent.value).toBe(newValue);
+        });
+
+        it('sets "disabled" attribute with value of bound property', async () => {
+            testHostComponent.fieldDisabled = true;
+            fixture.detectChanges();
+            await waitAnimationFrame();
+
+            expect(numberField.getAttribute('disabled')).toBe('');
+            expect(numberField.disabled).toBe(true);
+        });
+    });
+
+    describe('using [value] binding', () => {
+        @Component({
+            template: `
+                <nimble-number-field #numberField [value]="value" (change)="valueChanged()"></nimble-number-field>
+             `
+        })
+        class TestHostComponent {
+            @ViewChild('numberField', { static: true }) public numberField: ElementRef<NumberField>;
+
+            public readonly initialValue = 123;
+            public value = this.initialValue;
+
+            public valueChanged(): void {
+                this.value = parseFloat(this.numberField.nativeElement.value);
+            }
+        }
+
+        let numberField: NumberField;
+        let fixture: ComponentFixture<TestHostComponent>;
+        let testHostComponent: TestHostComponent;
+
+        beforeEach(async () => {
+            await TestBed.configureTestingModule({
+                declarations: [TestHostComponent],
+                imports: [NimbleNumberFieldModule, FormsModule]
+            }).compileComponents();
+        });
+
+        beforeEach(async () => {
+            fixture = TestBed.createComponent(TestHostComponent);
+            testHostComponent = fixture.componentInstance;
+            numberField = testHostComponent.numberField.nativeElement;
+            fixture.detectChanges();
+        });
+
+        it('should set correct initial selected value', () => {
+            expect(testHostComponent.value).toBe(testHostComponent.initialValue);
+            expect(parseFloat(numberField.value)).toBe(testHostComponent.initialValue);
+        });
+
+        it('should update selected value when bound property is changed', async () => {
+            const newValue = 1;
+            testHostComponent.value = newValue;
+            fixture.detectChanges();
+            await waitMicrotask();
+
+            expect(parseFloat(numberField.value)).toBe(newValue);
+        });
+
+        it('should update bound property when selected value is changed', async () => {
+            const newValue = 1;
+            setNumberFieldValue(numberField, newValue);
+            fixture.detectChanges();
+
+            expect(testHostComponent.value).toBe(newValue);
+        });
+    });
+});
