db669e9de5166260c994aaeb4b1ed9bdfb60bcd9
jattasNI <jattasNI@users.noreply.github.com>
2021-10-06 11:24:22 -0500

    Rename button module and directive

--- angular-workspace/projects/ni/nimble-angular/src/directives/button/nimble-button.module.ts
+++ angular-workspace/projects/ni/nimble-angular/src/directives/button/nimble-button.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NimbleButtonDirective } from './nimble-button-directive';
+import { NimbleButtonDirective } from './nimble-button.directive';
 
 @NgModule({
     declarations: [NimbleButtonDirective],
