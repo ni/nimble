a93568fe69e1d283ceb129a37151190b1d4c892e
Michael Brandt <35616474+michaeldbrandt@users.noreply.github.com>
2021-10-06 10:29:22 -0500

    Short documentation added

--- packages/nimble-components/src/menu-item/index.ts
+++ packages/nimble-components/src/menu-item/index.ts
@@ -7,8 +7,20 @@ import {
 
 import { styles } from './styles';
 
+/**
+ * A nimble-styled menu-item
+ */
 export { FoundationMenuItem as MenuItem };
 
+/**
+ * A function that returns a nimble-menu-item registration for configuring the component with a DesignSystem.
+ * Implements {@link @microsoft/fast-foundation#menuItemTemplate}
+ *
+ * @public
+ * @remarks
+ * Generates HTML Element: \<nimble-menu-item\>
+ *
+ */
 export const nimbleMenuItem = FoundationMenuItem.compose<MenuItemOptions>({
     baseName: 'menu-item',
     template,
