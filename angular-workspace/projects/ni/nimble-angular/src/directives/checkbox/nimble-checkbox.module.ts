b07524e1c6f40c218a43beb39c3b59f3c9bd2676
jattasNI <jattasNI@users.noreply.github.com>
2021-10-07 15:16:48 -0500

    Rename checkbox module/directive to match others

--- angular-workspace/projects/ni/nimble-angular/src/directives/checkbox/nimble-checkbox.module.ts
+++ angular-workspace/projects/ni/nimble-angular/src/directives/checkbox/nimble-checkbox.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NimbleCheckboxDirective } from './nimble-checkbox-directive';
+import { NimbleCheckboxDirective } from './nimble-checkbox.directive';
 import { NimbleCheckboxControlValueAccessorDirective } from './nimble-checkbox-control-value-accessor.directive';
 
 import '@ni/nimble-components/dist/esm/checkbox';
