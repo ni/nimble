ceff5878e90e42fbc9231df1534328c72bf7ce83
Michael Brandt <35616474+michaeldbrandt@users.noreply.github.com>
2021-10-12 13:04:05 -0500

    Added listener to indent items based on icon slot contents.

--- packages/nimble-components/src/menu/index.ts
+++ packages/nimble-components/src/menu/index.ts
@@ -1,15 +1,50 @@
+import { Notifier, Observable } from '@microsoft/fast-element';
 import {
     DesignSystem,
     Menu as FoundationMenu,
-    menuTemplate as template
+    menuTemplate as template,
 } from '@microsoft/fast-foundation';
+import { MenuItem } from '../menu-item';
 
 import { styles } from './styles';
 
 /**
  * A nimble-styled menu
  */
-export { FoundationMenu as Menu };
+export class Menu extends FoundationMenu {
+    private notifier: Notifier;
+
+    private readonly itemChangeHandler = {
+        handleChange(source: Menu): void {
+            const menuItems = Array.from(source.children).filter(source.isNimbleMenuItem);
+
+            const anyItemHasIcon = menuItems.reduce((accum: boolean, current: MenuItem) => {
+                return accum || current.icon.assignedNodes().length > 0;
+            }, false);
+
+            if (anyItemHasIcon) {
+                menuItems.forEach((item: MenuItem) => {
+                    item.classList.add('nimble-indent');
+                });
+            }
+        }
+    };
+
+    public connectedCallback(): void {
+        super.connectedCallback();
+        this.notifier = Observable.getNotifier(this);
+        this.notifier.subscribe(this.itemChangeHandler, 'items');
+    }
+
+    public disconnectedCallback(): void {
+        super.disconnectedCallback();
+        this.notifier.unsubscribe(this.itemChangeHandler, 'items');
+    }
+
+    private readonly isNimbleMenuItem = (el: Element): el is HTMLElement => {
+        return (el instanceof MenuItem);
+    };
+}
 
 /**
  * A function that returns a nimble-menu registration for configuring the component with a DesignSystem.
@@ -20,7 +55,7 @@ export { FoundationMenu as Menu };
  * Generates HTML Element: \<nimble-menu\>
  *
  */
-export const nimbleMenu = FoundationMenu.compose({
+export const nimbleMenu = Menu.compose({
     baseName: 'menu',
     template,
     styles
