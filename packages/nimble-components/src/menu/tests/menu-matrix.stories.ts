84cdcb7e5458766db81ab08debf438fc78c60635
Michael Brandt <35616474+michaeldbrandt@users.noreply.github.com>
2021-10-13 14:17:18 -0500

    added a matrix dimension for the icon, to demo how it indents better

--- packages/nimble-components/src/menu/tests/menu-matrix.stories.ts
+++ packages/nimble-components/src/menu/tests/menu-matrix.stories.ts
@@ -1,4 +1,4 @@
-import { html, ViewTemplate } from '@microsoft/fast-element';
+import { html, ViewTemplate, when } from '@microsoft/fast-element';
 import { admin16X16 } from '@ni/nimble-tokens/dist-icons-esm/nimble-icons-inline';
 import type { Story, Meta } from '@storybook/html';
 import { withXD } from 'storybook-addon-xd-designs';
@@ -8,6 +8,11 @@ import {
     themeWrapper
 } from '../../tests/utilities/theme-test-helpers';
 import '../index';
+
+/* TODO: there is an IconState helper in '../../tests/utilities/theme-test-helpers'
+when the other components are updated to use the start slot then we should use that helper instead. */
+type MenuIconState = boolean;
+const menuIconStates: MenuIconState[] = [false, true];
 
 const metadata: Meta = {
     title: 'Tests/Menu',
@@ -23,16 +28,19 @@ const metadata: Meta = {
 
 export default metadata;
 
+// TODO: this is a little confusing to look at.  Once we have headers they can be used to indicate what each menu is demonstrating
 // prettier-ignore
-const component = (): ViewTemplate => html`
+const component = (
+    icon: MenuIconState,
+): ViewTemplate => html`
     <nimble-menu>
         <nimble-menu-item>Item 1</nimble-menu-item>
         <hr>
         <nimble-menu-item disabled>Item 2</nimble-menu-item>
-        <nimble-menu-item><div slot="start">${admin16X16.data}</div>Item 3</nimble-menu-item>
+        <nimble-menu-item>${when(() => icon, html`<div slot="start">${admin16X16.data}</div>`)}Item 3</nimble-menu-item>
     </nimble-menu>
 `;
 
 export const menuThemeMatrix: Story = createRenderer(
-    themeWrapper(createMatrix(component))
+    themeWrapper(createMatrix(component, [menuIconStates]))
 );
