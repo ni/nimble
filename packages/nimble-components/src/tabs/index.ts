cd2b97ee18439e133dd316730a721ea5df6cfc41
rajsite <rajsite@users.noreply.github.com>
2021-10-14 15:52:22 -0500

    Align tabs with icon slot decision

--- packages/nimble-components/src/tabs/index.ts
+++ packages/nimble-components/src/tabs/index.ts
@@ -1,4 +1,3 @@
-import { html } from '@microsoft/fast-element';
 import {
     DesignSystem,
     Tabs,
@@ -7,15 +6,10 @@ import {
 } from '@microsoft/fast-foundation';
 import { styles } from './styles';
 
-export * from '../tab';
-export * from '../tab-panel';
-export * from '../tabs-toolbar';
-
 const nimbleTabs = Tabs.compose<TabsOptions>({
     baseName: 'tabs',
     template,
-    styles,
-    end: html`<slot name="toolbar"></slot>`
+    styles
 });
 
 DesignSystem.getOrCreate().withPrefix('nimble').register(nimbleTabs());
