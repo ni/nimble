6c6392ae4016d6ebbbd14e43955a2d65dc33201d
Michael Brandt <35616474+michaeldbrandt@users.noreply.github.com>
2021-10-20 16:40:46 -0500

    Added a hidden binding to the menu to make hiding/showing the menu easier.

--- angular-workspace/projects/ni/nimble-angular/src/directives/menu/nimble-menu.directive.ts
+++ angular-workspace/projects/ni/nimble-angular/src/directives/menu/nimble-menu.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 /**
  * Directive to provide Angular integration for the menu.
  */
@@ -6,4 +6,5 @@ import { Directive } from '@angular/core';
     selector: 'nimble-menu'
 })
 export class NimbleMenuDirective {
+    @HostBinding('hidden') @Input() public hidden: boolean;
 }
