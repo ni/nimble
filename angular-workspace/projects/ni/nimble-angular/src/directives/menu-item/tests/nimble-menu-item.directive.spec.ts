17facfc5beb3174ae2aa19b4bcd6d7434ab01e1c
Michael Brandt <35616474+michaeldbrandt@users.noreply.github.com>
2021-10-25 09:44:57 -0500

    Got rid of uneccessary tests.
    https://github.com/ni/nimble/blob/main/angular-workspace/projects/ni/nimble-angular/CONTRIBUTING.md#testing
    "Any other custom behavior implemented in the directives (lifecycle hooks, new properties) should be tested.
    However, it shouldn't be necessary to test properties which only use a simple HostBinding decorator.
    "

--- angular-workspace/projects/ni/nimble-angular/src/directives/menu-item/tests/nimble-menu-item.directive.spec.ts
+++ angular-workspace/projects/ni/nimble-angular/src/directives/menu-item/tests/nimble-menu-item.directive.spec.ts
@@ -1,7 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import type { MenuItem } from '@ni/nimble-components/dist/esm/menu-item';
-import { NimbleMenuModule } from '../../menu';
+import { TestBed } from '@angular/core/testing';
 import { NimbleMenuItemModule } from '..';
 
 describe('Nimble menu item', () => {
@@ -13,62 +10,5 @@ describe('Nimble menu item', () => {
 
     it('custom element is defined', () => {
         expect(customElements.get('nimble-menu-item')).not.toBeUndefined();
-    });
-});
-
-describe('Nimble menu item directive (using 2-way binding)', () => {
-    @Component({
-        template: `
-            <nimble-menu>
-                <nimble-menu-item #item1 [(disabled)]="item1Disabled">
-                    Item 1
-                </nimble-menu-item>
-                <nimble-menu-item #item2 [(disabled)]="item2Disabled">
-                    Item 2
-                </nimble-menu-item>
-            </nimble-menu>
-         `
-    })
-    class TestHostComponent {
-        @ViewChild('item1', { static: true }) public item1: ElementRef<MenuItem>;
-        @ViewChild('item2', { static: true }) public item2: ElementRef<MenuItem>;
-        public item1Disabled = true;
-        public item2Disabled = false;
-    }
-
-    let fixture: ComponentFixture<TestHostComponent>;
-    let testHostComponent: TestHostComponent;
-    let item1Element: MenuItem;
-    let item2Element: MenuItem;
-
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [TestHostComponent],
-            imports: [NimbleMenuModule, NimbleMenuItemModule]
-        }).compileComponents();
-    });
-
-    beforeEach(() => {
-        fixture = TestBed.createComponent(TestHostComponent);
-        testHostComponent = fixture.componentInstance;
-        item1Element = testHostComponent.item1.nativeElement;
-        item2Element = testHostComponent.item2.nativeElement;
-        fixture.detectChanges();
-    });
-
-    it('the TreeItem DOM element reflects correct initial state set via NimbleMenuItemDirective', () => {
-        expect(item1Element.disabled).toBe(true);
-        expect(item2Element.disabled).toBe(false);
-    });
-
-    describe('when directive properties change, the menu item DOM element is updated', () => {
-        it('for disabled', () => {
-            testHostComponent.item1Disabled = false;
-            testHostComponent.item2Disabled = true;
-            fixture.detectChanges();
-
-            expect(item1Element.disabled).toBe(false);
-            expect(item2Element.disabled).toBe(true);
-        });
     });
 });
