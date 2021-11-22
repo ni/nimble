e39813b97320d65ca311fa80d5c32984bf155290
Milan Raj <rajsite@users.noreply.github.com>
2021-11-22 12:52:21 -0600

    Angular integration cleanup 2 (#204)
    # Pull Request
    
    ## 🤨 Rationale
    
    Continues on https://github.com/ni/nimble/pull/203
    
    - Disables beachball npm package publishing in CI (keeps package versioning and tagging)
    - Moves all npm package publishing to separate workflow
    
    ## 👩‍💻 Implementation
    
    See above.
    
    ## 🧪 Testing
    
    Doing it live.
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or determined no changes are needed. Will follow-up after.

--- angular-workspace/projects/ni/nimble-angular/beachball.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    // Beachball manages versioning and tagging of the npm package but not publishing to npm.
-    // Angular packages are built into the dist/ folder and need to be published from there instead.
-    publish: false
-};
