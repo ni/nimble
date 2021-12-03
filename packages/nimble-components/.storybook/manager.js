ea84945ce9639d614a0ed675e4bdac369c0b36d3
Fred Visser <1458528+fredvisser@users.noreply.github.com>
2021-12-03 15:05:03 -0600

    add favicon

--- packages/nimble-components/.storybook/manager.js
+++ packages/nimble-components/.storybook/manager.js
@@ -1,5 +1,11 @@
 import { addons } from '@storybook/addons';
+import favicon from './nimble-logo-icon.svg';
 import theme from './theme';
+
+const link = document.createElement('link');
+link.setAttribute('rel', 'shortcut icon');
+link.setAttribute('href', favicon);
+document.head.appendChild(link);
 
 addons.setConfig({
     enableShortcuts: false,
