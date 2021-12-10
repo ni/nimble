dbd451f306504727667afaecb3a540d9a755d04d
Brian Henry <5454342+brianehenry@users.noreply.github.com>
2021-12-10 16:04:55 -0600

    Re-export DOM.processUpdates() from nimble-components

--- packages/nimble-components/src/testing/dom-next-update.ts
+++ packages/nimble-components/src/testing/dom-next-update.ts
@@ -4,3 +4,5 @@ import { DOM } from '@microsoft/fast-element';
  * Allows test code to wait for the render update cycle of the components.
  */
 export const domNextUpdate = async (): Promise<void> => DOM.nextUpdate();
+
+export const processUpdates = (): void => DOM.processUpdates();
