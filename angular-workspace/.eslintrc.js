af8b1294073262a6065a3d2666350d24c7a6b2d0
jattasNI <jattasNI@users.noreply.github.com>
2021-12-16 16:20:54 -0600

    Remove rules that moved to styleguide

--- angular-workspace/.eslintrc.js
+++ angular-workspace/.eslintrc.js
@@ -7,10 +7,6 @@ module.exports = {
             '@ni/eslint-config-typescript/requiring-type-checking'
         ],
         rules: {
-            // *-default-export lines can be deleted once a fix for this issue is published and uptaken
-            // https://github.com/ni/javascript-styleguide/issues/39
-            'import/prefer-default-export': 'off',
-            'import/no-default-export': 'error',
             'no-restricted-imports': ['error', {
                 patterns:
                 [{
--- packages/nimble-components/src/.eslintrc.js
+++ packages/nimble-components/src/.eslintrc.js
@@ -26,11 +26,6 @@ module.exports = {
             { contexts: ['ClassDeclaration'] }
         ],
 
-        // *-default-export lines can be deleted once a fix for this issue is published and uptaken
-        // https://github.com/ni/javascript-styleguide/issues/39
-        'import/prefer-default-export': 'off',
-        'import/no-default-export': 'error',
-
         // Rules enabled due to strictNullChecks
         '@typescript-eslint/no-non-null-assertion': 'off',
 
