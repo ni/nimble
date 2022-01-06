09fdfa518ef5ae44aa5129a1c20cbdcde0dcc8ac
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2022-01-06 17:35:45 -0600

    Don't export nimbleDrawer variable

--- packages/nimble-components/src/drawer/index.ts
+++ packages/nimble-components/src/drawer/index.ts
@@ -234,7 +234,7 @@ class Drawer extends FoundationDialog {
     }
 }
 
-export const nimbleDrawer = Drawer.compose({
+const nimbleDrawer = Drawer.compose({
     baseName: 'drawer',
     template,
     styles
