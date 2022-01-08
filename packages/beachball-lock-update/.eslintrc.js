c70b0596914b918a540253d2d2e464d4c7948b1f
rajsite <rajsite@users.noreply.github.com>
2022-01-07 18:25:04 -0600

    Add README and test

--- packages/beachball-lock-update/.eslintrc.js
+++ packages/beachball-lock-update/.eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
         '@ni/eslint-config-javascript'
     ],
     rules: {
-        'no-console': 'off',
-        'import/extensions': 'off'
+        'no-console': 'off'
     }
 };
--- packages/beachball-lock-update/spec/basic.spec.mjs
+++ packages/beachball-lock-update/spec/basic.spec.mjs
@@ -2,13 +2,55 @@ import tempy from 'tempy';
 import fs from 'fs';
 import { createPostbump } from '../src/index.js';
 
-describe('hello world', () => {
-    it('hi', async () => {
-        const lockFile = tempy.writeSync('{}');
+describe('beachball lock update', () => {
+    it('processes package references', async () => {
+        const lockFile = tempy.writeSync(JSON.stringify({
+            packages: {
+                'a-package': {
+                    dependencies: {
+                        'my-package': '^1.0.0'
+                    }
+                }
+            }
+        }));
         const postbump = createPostbump(lockFile);
-        postbump('', '', '');
+        const packageName = 'my-package';
+        const packageVersion = '2.0.0';
+        postbump('', packageName, packageVersion);
         const result = fs.readFileSync(lockFile, { encoding: 'utf-8' });
-        const resultNormalized = JSON.stringify(JSON.parse(result));
-        expect(resultNormalized).toBe('{}');
+        const resultNormalized = JSON.parse(result);
+        expect(resultNormalized).toEqual({
+            packages: {
+                'a-package': {
+                    dependencies: {
+                        'my-package': '^2.0.0'
+                    }
+                }
+            }
+        });
+    });
+    it('processes package definitions', async () => {
+        const lockFile = tempy.writeSync(JSON.stringify({
+            packages: {
+                'package-folder/my-package': {
+                    name: 'my-package',
+                    version: '1.0.0'
+                }
+            }
+        }));
+        const postbump = createPostbump(lockFile);
+        const packageName = 'my-package';
+        const packageVersion = '2.0.0';
+        postbump('', packageName, packageVersion);
+        const result = fs.readFileSync(lockFile, { encoding: 'utf-8' });
+        const resultNormalized = JSON.parse(result);
+        expect(resultNormalized).toEqual({
+            packages: {
+                'package-folder/my-package': {
+                    name: 'my-package',
+                    version: '2.0.0'
+                }
+            }
+        });
     });
 });
