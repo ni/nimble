03bca9bc6f224b4ccc55998779bbc86511471bb4
Milan Raj <rajsite@users.noreply.github.com>
2022-01-11 15:12:03 -0600

    Create the @ni/beachball-lock-update package (#248)
    # Pull Request
    
    ## 🤨 Rationale
    
    As discussed in https://github.com/ni/javascript-styleguide/pull/77#discussion_r780432943 a package was made to share the workaround script used for bumping package-lock.json files in npm 7 monorepos managed by beachball.
    
    The package is listed as a beta version. Was thinking we can move it to release after it is end-to-end tested with an external repo, ie [@ni/javascript-style-guide](https://github.com/ni/javascript-styleguide).
    
    ## 👩‍💻 Implementation
    
    - Moved the script into it's own package and added a README
    - Tweaked the console logging to be more descriptive for build logs
    
    ## 🧪 Testing
    
    - Added a very basic sanity test in jasmine
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or determined no changes are needed.

--- beachball.config.js
+++ beachball.config.js
@@ -1,73 +1,9 @@
 const path = require('path');
-const fs = require('fs');
-const os = require('os');
-
-// Workaround for the following issues:
-// Beachball issue: https://github.com/microsoft/beachball/issues/525
-// NPM issue 1: https://github.com/npm/cli/issues/3756
-// NPM issue 2: https://github.com/npm/cli/issues/3757
-
-/**
- * Returns a beachball postbump hook implementation that bumps package
- * references specified in the root package-lock.json file during
- * beachball package version bumps.
- */
-const postbump = (_packagePath, packageName, packageVersion) => {
-    console.log(`Updating lockfile for package ${packageName} to version ${packageVersion}`);
-    const lockPath = path.resolve(__dirname, './package-lock.json');
-
-    console.log(`Lockfile to update: ${lockPath}`);
-    const lockFile = JSON.parse(fs.readFileSync(lockPath));
-
-    const isObject = function (obj) {
-        return typeof obj === 'object' && obj !== null;
-    };
-    const recurseProperties = (currentValue, path) => {
-        if (Array.isArray(currentValue)) {
-            currentValue.forEach((value, i) => recurseProperties(value, [...path, `[${i}]`]));
-        } else if (isObject(currentValue)) {
-            const keys = Object.keys(currentValue);
-            keys.forEach(key => {
-                const value = currentValue[key];
-                if (typeof value === 'string') {
-                    if (key === packageName) {
-                        // "@awesome/package": "^1.2.3-beta.4"
-                        // Note: Uses a hard coded semantic range specifier
-                        const update = `^${packageVersion}`;
-                        console.log(`Updating lockfile path (${[...path, key].join(' > ')}) to value ${update}.`);
-                        currentValue[key] = update;
-                    } else if (key === 'version' && currentValue.name === packageName) {
-                        // "name": "@awesome/package",
-                        // "version": "1.2.3-beta.4"
-                        const update = packageVersion;
-                        console.log(`Updating lockfile path (${[...path, key].join(' > ')}) to value ${update}.`);
-                        currentValue[key] = update;
-                    } else {
-                        // don't care about other string values
-                        return;
-                    }
-                } else {
-                    recurseProperties(currentValue[key], [...path, key]);
-                }
-            })
-        } else {
-            // Don't care about primitives at this point
-            // Only care about string keys on an object
-            return;
-        }
-    };
-    recurseProperties(lockFile, []);
-
-    console.log(`Serializing and writing lockfile for package ${packageName}`);
-    const updatedLockFileJSON = JSON.stringify(lockFile, null, 2) + '\n';
-    updatedLockFileJSONNewLines = updatedLockFileJSON.split('\n').join(os.EOL);
-    fs.writeFileSync(lockPath, updatedLockFileJSONNewLines);
-
-    console.log(`Updated lockfile for package ${packageName}`);
-};
+const lockPath = path.resolve(__dirname, './package-lock.json');
+const { createPostbump } = require('@ni/beachball-lock-update');
 
 module.exports = {
     hooks: {
-        postbump
+        postbump: createPostbump(lockPath)
     }
 };
