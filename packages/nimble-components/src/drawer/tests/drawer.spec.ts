e8fc47ffadd0f26f5c4b2d09773578fac404ef20
Malcolm Smith <20709258+msmithNI@users.noreply.github.com>
2022-01-12 21:08:41 -0600

    Tests

--- packages/nimble-components/src/drawer/tests/drawer.spec.ts
+++ packages/nimble-components/src/drawer/tests/drawer.spec.ts
@@ -4,23 +4,29 @@ import '..';
 import type { Drawer } from '..';
 import { DrawerState } from '../types';
 
-async function setup(): Promise<Fixture<Drawer>> {
-    return fixture<Drawer>(html` <nimble-drawer> </nimble-drawer>`);
+async function createFixture(modal: boolean): Promise<Fixture<Drawer>> {
+    return fixture<Drawer>(
+        html`<nimble-drawer modal="${modal}">
+            <textarea></textarea>
+        </nimble-drawer>`
+    );
 }
 
 describe('Drawer', () => {
+    let drawerFixture: Fixture<Drawer>;
     let element: Drawer;
-    let connect: () => Promise<void>;
-    let disconnect: () => Promise<void>;
+    let focusableChild: HTMLTextAreaElement;
 
-    beforeEach(async () => {
-        ({ element, connect, disconnect } = await setup());
-        await connect();
+    async function setup(modal = true): Promise<void> {
+        drawerFixture = await createFixture(modal);
+        await drawerFixture.connect();
         await DOM.nextUpdate();
-    });
+        element = drawerFixture.element;
+        focusableChild = element.firstElementChild as HTMLTextAreaElement;
+    }
 
     afterEach(async () => {
-        await disconnect();
+        await drawerFixture.disconnect();
     });
 
     async function waitForDrawerAnimationsCompleted(): Promise<void> {
@@ -32,12 +38,56 @@ describe('Drawer', () => {
         await DOM.nextUpdate();
     }
 
-    it('element is hidden and does not trap focus by default', async () => {
+    it('element is hidden by default', async () => {
+        await setup();
         expect(element.hidden).toBe(true);
-        expect(element.trapFocus).toBe(false);
+    });
+
+    describe('element trapFocus default matches modal', () => {
+        [true, false].forEach(modal => {
+            it(`when modal is initialized as ${modal.toString()}`, async () => {
+                await setup(modal);
+                expect(element.trapFocus).toBe(modal);
+            });
+        });
+    });
+
+    it('first focusable element in modal drawer is focused when drawer is opened', async () => {
+        await setup(true);
+        element.state = DrawerState.Opened;
+        await DOM.nextUpdate();
+        expect(document.activeElement).toBe(focusableChild);
+    });
+
+    it('non-modal drawer does not take focus when opened', async () => {
+        await setup(false);
+        element.state = DrawerState.Opened;
+        await DOM.nextUpdate();
+        expect(
+            document.activeElement === null
+                || !element.contains(document.activeElement)
+        ).toBe(true);
+    });
+
+    it('drawer trapFocus (focusing behavior) updates when modal property is changed', async () => {
+        await setup(true);
+        element.modal = false;
+        element.state = DrawerState.Opened;
+        await DOM.nextUpdate();
+        expect(
+            document.activeElement === null
+                || !element.contains(document.activeElement)
+        ).toBe(true);
+        element.state = DrawerState.Closed;
+        await DOM.nextUpdate();
+        element.modal = true;
+        element.state = DrawerState.Opened;
+        await DOM.nextUpdate();
+        expect(document.activeElement).toBe(focusableChild);
     });
 
     it('state=opening animates to shown/state=opened, and state=closing animates to hidden/state=closed', async () => {
+        await setup();
         element.state = DrawerState.Opening;
         await waitForDrawerAnimationsCompleted();
         expect(element.hidden).toBe(false);
@@ -49,6 +99,7 @@ describe('Drawer', () => {
     });
 
     it('show() is equivalent to state=opening, and hide() is equivalent to state=closing', async () => {
+        await setup();
         element.show();
         await waitForDrawerAnimationsCompleted();
         expect(element.hidden).toBe(false);
@@ -60,6 +111,7 @@ describe('Drawer', () => {
     });
 
     it('showing then hiding element (without waiting for slide-in animation to finish) works correctly (element ends as hidden)', async () => {
+        await setup();
         element.show();
         await DOM.nextUpdate();
         element.hide();
@@ -69,6 +121,7 @@ describe('Drawer', () => {
     });
 
     it('updates to "state" property fire the "state-change" event', async () => {
+        await setup();
         const stateChange = jasmine.createSpy();
         element.addEventListener('state-change', stateChange);
         element.state = DrawerState.Opened;
