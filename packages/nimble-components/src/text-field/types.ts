2ed436493d0be7cdd9f63a58cb049a8fe0b6dca3
jattasNI <jattasNI@users.noreply.github.com>
2022-01-21 11:56:01 -0600

    Add text field appearance types (and docs)

--- packages/nimble-components/src/text-field/types.ts
+++ packages/nimble-components/src/text-field/types.ts
@@ -1,9 +1,30 @@
 import { TextFieldType } from '@microsoft/fast-foundation';
 
+/**
+ * Values for the 'type' attribute of the text field.
+ */
 export type TextFieldTypeAttribute =
     | 'email'
     | 'password'
     | 'tel'
     | 'text'
     | 'url';
+
+/**
+ * Values for the 'type' property of the text field.
+ */
 export { TextFieldType };
+
+/**
+ * Values for the 'appearance' attribute of the text field.
+ */
+export type TextFieldAppearanceAttribute = 'underline' | 'outline' | 'block';
+
+/**
+ * Values for the 'appearance' property of the text field
+ */
+export enum TextFieldAppearance {
+    Underline = 'underline',
+    Outline = 'outline',
+    Block = 'block'
+}
