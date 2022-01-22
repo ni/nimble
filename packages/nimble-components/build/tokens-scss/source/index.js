6a277f90fbf115e0f2c6ca8ae62c870a87892a3a
rajsite <rajsite@users.noreply.github.com>
2022-01-21 18:06:05 -0600

    Split tokens and tokens-internal to separate files

--- packages/nimble-components/build/tokens-scss/source/index.js
+++ packages/nimble-components/build/tokens-scss/source/index.js
@@ -5,22 +5,34 @@ import { comments } from '../../../dist/esm/theme-provider/design-token-comments
 const fs = require('fs');
 const path = require('path');
 
-const tokensPath = path.resolve(__dirname, '../../../dist/tokens.scss');
+const tokensFilePath = path.resolve(__dirname, '../../../dist/tokens.scss');
 
-const fileHeader = `
+const tokensFileHeader = `
 // Nimble Tokens SCSS
 // Used to integrate theme aware design tokens in an application.
 // Requires using a <nimble-theme-provider> in the page.
 // For more information see https://github.com/ni/nimble/tree/main/packages/nimble-components#theming
+
+@import './tokens-internal.scss';
 `;
 
-const fileTokenContents = Object.entries(tokenNames)
+const tokensFileContents = Object.entries(tokenNames)
     .map(([exportName, tokenName]) => `
 ${comments[exportName] === '' ? '' : `/// ${comments[exportName]}`}
 ${scssPropertyFromTokenName(tokenName)}: var(${scssInternalPropertyFromTokenName(tokenName)});
 `);
 
-const fileInternalTokenContents = Object.entries(tokenNames)
+const tokensFile = [tokensFileHeader, ...tokensFileContents].join('');
+
+const tokensInternalFilePath = path.resolve(__dirname, '../../../dist/tokens-internal.scss');
+
+const tokensInternalFileHeader = `
+// Nimble Internal Tokens SCSS
+// Used by Nimble Tokens SCSS
+// For more information see https://github.com/ni/nimble/tree/main/packages/nimble-components#theming
+`;
+
+const tokensInternalFileContents = Object.entries(tokenNames)
     .map(([_, tokenName]) => `
 /// Internal property for ${scssPropertyFromTokenName(tokenName)}.
 /// Not intended for general use. Used to override a token value (potentially making in no longer theme-aware).
@@ -28,8 +40,15 @@ const fileInternalTokenContents = Object.entries(tokenNames)
 ${scssInternalPropertyFromTokenName(tokenName)}: ${cssPropertyFromTokenName(tokenName)};
 `);
 
-const file = [fileHeader, ...fileTokenContents, ...fileInternalTokenContents].join('');
-console.log(`tokens.scss path to write to: ${tokensPath}`);
+const tokensInternalFile = [tokensInternalFileHeader, ...tokensInternalFileContents].join('');
+
+console.log(`tokens path to write to: ${tokensFilePath}`);
+console.log(`tokens internal path to write to: ${tokensInternalFilePath}`);
+
 console.log('Writing tokens file');
-fs.writeFileSync(tokensPath, file, { encoding: 'utf-8' });
+fs.writeFileSync(tokensFilePath, tokensFile, { encoding: 'utf-8' });
 console.log('Done writing tokens file');
+
+console.log('Writing tokens internal file');
+fs.writeFileSync(tokensInternalFilePath, tokensInternalFile, { encoding: 'utf-8' });
+console.log('Done writing tokens internal file');
