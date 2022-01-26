5a88f1abdb8feb5ccc0f430570e0f0736e6e4c20
rajsite <rajsite@users.noreply.github.com>
2022-01-26 12:25:58 -0600

    Change property format to config option

--- packages/nimble-components/src/theme-provider/tests/tokens.stories.ts
+++ packages/nimble-components/src/theme-provider/tests/tokens.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/html';
 import { withXD } from 'storybook-addon-xd-designs';
-import { html, repeat } from '@microsoft/fast-element';
+import { html, repeat, when } from '@microsoft/fast-element';
 import { createRenderer } from '../../utilities/tests/storybook';
 import '..';
 import {
@@ -27,6 +27,15 @@ tokenNameKeys.sort((a, b) => a.localeCompare(b));
 
 const overviewText = 'Design Tokens to use in applications. See the <a href="https://github.com/ni/nimble/tree/main/packages/nimble-components">nimble-components</a> readme for more information.';
 
+enum PropertyFormat {
+    SCSS = 'SCSS',
+    CSS = 'CSS'
+}
+
+interface TokenArgs {
+    propertyFormat: PropertyFormat;
+}
+
 const metadata: Meta = {
     title: 'Tokens/Property Names',
     decorators: [withXD],
@@ -45,11 +54,21 @@ const metadata: Meta = {
 export default metadata;
 
 // prettier-ignore
-export const propertyNames: StoryObj = {
+export const propertyNames: StoryObj<TokenArgs> = {
     parameters: {
         controls: { hideNoControlsWarning: true }
     },
-    render: createRenderer(html`
+    args: {
+        propertyFormat: PropertyFormat.SCSS
+    },
+    argTypes: {
+        propertyFormat: {
+            options: Object.values(PropertyFormat),
+            control: { type: 'radio' },
+            name: 'Property Format'
+        }
+    },
+    render: createRenderer(html<TokenArgs>`
         <style>
             table {
                 font-family: var(${fontFamily.cssCustomProperty});
@@ -68,17 +87,22 @@ export const propertyNames: StoryObj = {
         <table>
             <thead>
                 <tr>
-                    <th>SCSS Property</th>
+                    <th>${x => x.propertyFormat} Property</th>
                     <th>Description</th>
-                    <th>CSS Property</th>
                 </tr>
             </thead>
             <tbody>
-            ${repeat(() => tokenNameKeys, html<TokenName>`
+            ${repeat(() => tokenNameKeys, html<TokenName, TokenArgs>`
                 <tr>
-                    <td>${x => scssPropertyFromTokenName(tokenNames[x])}</td>
+                    <td>
+                        ${when((_, c) => (c.parent as TokenArgs).propertyFormat === PropertyFormat.CSS, html<TokenName>`
+                            ${x => cssPropertyFromTokenName(tokenNames[x])}
+                        `)}
+                        ${when((_, c) => (c.parent as TokenArgs).propertyFormat === PropertyFormat.SCSS, html<TokenName>`
+                            ${x => scssPropertyFromTokenName(tokenNames[x])}
+                        `)}
+                    </td>
                     <td>${x => comments[x]}</td>
-                    <td>${x => cssPropertyFromTokenName(tokenNames[x])}</td>
                 </tr>
             `)}
             </tbody>
