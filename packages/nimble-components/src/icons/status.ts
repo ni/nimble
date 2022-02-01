c01f1056543f1a3323974fc04cdbceb5a16f066a
Christine Spangler <christine.spangler@ni.com>
2022-02-01 15:19:57 -0600

    add status icons to nimble-components

---
+++ packages/nimble-components/src/icons/status.ts
@@ -0,0 +1,19 @@
+import { statusHeader16X16 } from '@ni/nimble-tokens/dist-icons-esm/nimble-icons-inline';
+import { Icon, registerIcon } from '../icon-base';
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'nimble-status-icon': StatusIcon;
+    }
+}
+
+/**
+ *  The icon component for the 'status' icon
+ */
+export class StatusIcon extends Icon {
+    public constructor() {
+        super(statusHeader16X16);
+    }
+}
+
+registerIcon('status-icon', StatusIcon);
