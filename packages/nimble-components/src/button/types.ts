119f2e24691e61af596e26c975cc873e2d7e9932
mollykreis <20542556+mollykreis@users.noreply.github.com>
2022-02-08 16:15:23 -0600

    apply import/export format to button types

--- packages/nimble-components/src/button/types.ts
+++ packages/nimble-components/src/button/types.ts
@@ -3,9 +3,8 @@
  * @public
  */
 import type { Button } from '@microsoft/fast-foundation';
-import type { ButtonAppearanceAttribute } from '../patterns/button/types';
-import { ButtonAppearance } from '../patterns/button/types';
+
+export type { ButtonAppearanceAttribute } from '../patterns/button/types';
+export { ButtonAppearance } from '../patterns/button/types';
 
 export type ButtonType = Button['type'];
-export type { ButtonAppearanceAttribute };
-export { ButtonAppearance };
