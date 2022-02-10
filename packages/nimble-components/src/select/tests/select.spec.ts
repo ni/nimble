ca41f2c9272b52aa0efc34c2f23c43f48920f8c1
rajsite <rajsite@users.noreply.github.com>
2022-02-09 18:17:54 -0600

    Update to fast foundation 2.28.0

--- packages/nimble-components/src/select/tests/select.spec.ts
+++ packages/nimble-components/src/select/tests/select.spec.ts
@@ -38,6 +38,8 @@ describe('Select', () => {
         await disconnect();
     });
 
+    // fast-foundationversions >= 2.29.0 will error
+    // See https://github.com/microsoft/fast/issues/5586
     it('should respect "open" and "position" attributes when both set', async () => {
         const position = 'above';
         const { element, connect, disconnect } = await setup(position, true);
