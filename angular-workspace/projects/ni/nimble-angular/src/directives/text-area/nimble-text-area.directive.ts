002c08d5750ab8a5ac83b27227a156e102eba80a
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2022-02-15 16:47:07 -0600

    Add all input properties in text area API

--- angular-workspace/projects/ni/nimble-angular/src/directives/text-area/nimble-text-area.directive.ts
+++ angular-workspace/projects/ni/nimble-angular/src/directives/text-area/nimble-text-area.directive.ts
@@ -1,6 +1,7 @@
 import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 import type { TextArea } from '@ni/nimble-components/dist/esm/text-area';
-import { BooleanValueOrAttribute, toBooleanProperty } from '../utilities/template-value-helpers';
+import type { TextAreaAppearance, TextAreaAppearanceAttribute, TextAreaResize, TextAreaResizeAttribute } from '@ni/nimble-components/dist/esm/text-area/types';
+import { BooleanValueOrAttribute, NumberValueOrAttribute, toBooleanProperty, toNumberProperty } from '../utilities/template-value-helpers';
 
 export type { TextArea };
 
@@ -11,6 +12,14 @@ export type { TextArea };
     selector: 'nimble-text-area'
 })
 export class NimbleTextAreaDirective {
+    public get appearance(): TextAreaAppearance {
+        return this.elementRef.nativeElement.appearance;
+    }
+
+    @Input() public set appearance(value: TextAreaAppearance | TextAreaAppearanceAttribute) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'appearance', value);
+    }
+
     public get readOnly(): boolean {
         return this.elementRef.nativeElement.readOnly;
     }
@@ -22,12 +31,95 @@ export class NimbleTextAreaDirective {
         this.renderer.setProperty(this.elementRef.nativeElement, 'readOnly', toBooleanProperty(value));
     }
 
-    public get type(): TextAreaType {
-        return this.elementRef.nativeElement.type;
+    public get resize(): TextAreaResize {
+        return this.elementRef.nativeElement.resize;
     }
 
-    @Input() public set type(value: TextFieldType | TextFieldTypeAttribute) {
-        this.renderer.setProperty(this.elementRef.nativeElement, 'type', value);
+    @Input() public set resize(value: TextAreaResize | TextAreaResizeAttribute) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'resize', value);
+    }
+
+    public get autofocus(): boolean {
+        return this.elementRef.nativeElement.autofocus;
+    }
+
+    @Input() public set autofocus(value: BooleanValueOrAttribute) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'autofocus', toBooleanProperty(value));
+    }
+
+    public get disabled(): boolean {
+        return this.elementRef.nativeElement.disabled;
+    }
+
+    @Input() public set disabled(value: BooleanValueOrAttribute) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'disabled', toBooleanProperty(value));
+    }
+
+    public get required(): boolean {
+        return this.elementRef.nativeElement.required;
+    }
+
+    @Input() public set required(value: BooleanValueOrAttribute) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'required', toBooleanProperty(value));
+    }
+
+    public get formId(): string {
+        return this.elementRef.nativeElement.formId;
+    }
+
+    // formId property maps to the form attribute
+    // See: https://github.com/microsoft/fast/blob/46bb6d9aab2c37105f4434db3795e176c2354a4f/packages/web-components/fast-foundation/src/text-area/text-area.ts#L63
+    // eslint-disable-next-line @angular-eslint/no-input-rename
+    @Input('form') public set formId(value: string) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'formId', value);
+    }
+
+    public get maxlength(): number {
+        return this.elementRef.nativeElement.maxlength;
+    }
+
+    @Input() public set maxlength(value: NumberValueOrAttribute) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'maxlength', toNumberProperty(value));
+    }
+
+    public get minlength(): number {
+        return this.elementRef.nativeElement.minlength;
+    }
+
+    @Input() public set minlength(value: NumberValueOrAttribute) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'minlength', toNumberProperty(value));
+    }
+
+    public get placeholder(): string {
+        return this.elementRef.nativeElement.placeholder;
+    }
+
+    @Input() public set placeholder(value: string) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'placeholder', value);
+    }
+
+    public get cols(): number {
+        return this.elementRef.nativeElement.cols;
+    }
+
+    @Input() public set cols(value: NumberValueOrAttribute) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'cols', toNumberProperty(value));
+    }
+
+    public get rows(): number {
+        return this.elementRef.nativeElement.rows;
+    }
+
+    @Input() public set rows(value: NumberValueOrAttribute) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'rows', toNumberProperty(value));
+    }
+
+    public get spellcheck(): boolean {
+        return this.elementRef.nativeElement.spellcheck;
+    }
+
+    @Input() public set spellcheck(value: BooleanValueOrAttribute) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'spellcheck', toBooleanProperty(value));
     }
 
     public constructor(private readonly renderer: Renderer2, private readonly elementRef: ElementRef<TextArea>) {}
