9a79fce89a57125a2da8c4a2db6bb5a4a39c71c8
Fred Visser <1458528+fredvisser@users.noreply.github.com>
2022-02-17 08:16:12 -0600

    Update packages/nimble-components/src/theme-provider/design-token-comments.ts
    Co-authored-by: Jesse Attas <jattasNI@users.noreply.github.com>

--- packages/nimble-components/src/theme-provider/design-token-comments.ts
+++ packages/nimble-components/src/theme-provider/design-token-comments.ts
@@ -33,7 +33,7 @@ export const comments: { readonly [key in TokenName]: string } = {
     labelHeight: 'Standard label height for components',
     borderWidth: 'e.g. Checkbox border width',
     iconSize: 'Standard layout height for all icons',
-    groupHeader1TextTransform: 'e.g. uppercase',
+    groupHeader1TextTransform: 'CSS text-transform string to use for headers',
     drawerWidth: 'TODO: delete when able',
     smallDelay:
         'Elements with small transition areas, such as icons and selection controls, have short durations.',
