6872d1878c110d0f0a0669c09cfcec3200204661
Brian Henry <5454342+brianehenry@users.noreply.github.com>
2022-02-24 09:13:43 -0700

    Icon component code generation (#375)
    # Pull Request
    
    ## 🤨 Rationale
    
    Fixes #282
    
    Currently, Nimble components for icons are hand-written. This requires quite a bit of copy-paste, and won't scale as we add more icons to our library. 
    
    ## 👩‍💻 Implementation
    
    * Implemented code generation for Nimble icons
      * Codegen is implemented via a NodeJS script which imports icons from nimble-tokens
      * The script is pre-processed with rollup to allow using ES6 imports and NodeJS `require` statements together (inspired by @rajsite's `generate-scss` script)
      * The script generates a Nimble component file for each icon, and generates a `all-icons.ts` file which exports all icons.
      * Icon element names and class names are based on the exported icon names from `nimble-tokens`. This means that many icons are renamed by this change. 
    * Deleted old icons
    * Updated storybook with new icon names
    
    ## 🧪 Testing
    
    Verified icons appearance in storybook.

---
+++ packages/nimble-components/build/generate-icons/source/index.js
@@ -0,0 +1,84 @@
+/**
+ * Build script for generating Nimble components for icons.
+ *
+ * Iterates through icons provided by nimble-tokens, and generates a Nimble component for each in
+ * src/icons. Also generates an all-icons barrel file.
+ */
+
+import * as icons from '@ni/nimble-tokens/dist-icons-esm/nimble-icons-inline';
+
+const fs = require('fs');
+const path = require('path');
+
+const trimSizeFromName = text => {
+    // Remove dimensions from icon name, e.g. "add16X16" -> "add"
+    return text.replace(/\d+X\d+$/, '');
+};
+
+const camelToPascalCase = text => {
+    return text.substring(0, 1).toUpperCase() + text.substring(1);
+};
+
+const camelToKebabCase = text => {
+    // Adapted from https://stackoverflow.com/a/67243723
+    return text.replace(/[A-Z]+(?![a-z])|[A-Z]/g, (substring, offset) => (offset !== 0 ? '-' : '') + substring.toLowerCase());
+};
+
+const generatedFilePrefix = `// AUTO-GENERATED FILE - DO NOT EDIT DIRECTLY
+// See generation source in nimble-components/build/generate-icons\n`;
+
+const iconsDirectory = path.resolve(__dirname, '../../../src/icons');
+
+if (fs.existsSync(iconsDirectory)) {
+    console.log(`Deleting existing icons directory "${iconsDirectory}"`);
+    fs.rmSync(iconsDirectory, { recursive: true });
+    console.log('Finished deleting existing icons directory');
+}
+console.log(`Creating icons directory "${iconsDirectory}"`);
+fs.mkdirSync(iconsDirectory);
+console.log('Finished creating icons directory');
+
+console.log('Writing icon component files');
+let allIconsFileContents = `${generatedFilePrefix}\n`;
+let fileCount = 0;
+for (const key of Object.keys(icons)) {
+    const svgName = key; // e.g. "arrowExpanderLeft16X16"
+    const iconName = trimSizeFromName(key); // e.g. "arrowExpanderLeft"
+    const fileName = camelToKebabCase(iconName); // e.g. "arrow-expander-left";
+    const elementName = `${camelToKebabCase(iconName)}-icon`; // e.g. "arrow-expander-left-icon"
+    const className = `${camelToPascalCase(iconName)}Icon`; // e.g. "ArrowExpanderLeftIcon"
+
+    const componentFileContents = `${generatedFilePrefix}
+import { ${svgName} } from '@ni/nimble-tokens/dist-icons-esm/nimble-icons-inline';
+import { Icon, registerIcon } from '../icon-base';
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'nimble-${elementName}': ${className};
+    }
+}
+
+/**
+ * The icon component for the '${iconName}' icon
+ */
+export class ${className} extends Icon {
+    public constructor() {
+        super(${svgName});
+    }
+}
+
+registerIcon('${elementName}', ${className});
+`;
+
+    const filePath = path.resolve(iconsDirectory, `${fileName}.ts`);
+    fs.writeFileSync(filePath, componentFileContents, { encoding: 'utf-8' });
+    fileCount += 1;
+
+    allIconsFileContents = allIconsFileContents.concat(`export { ${className} } from './${fileName}';\n`);
+}
+console.log(`Finshed writing ${fileCount} icon component files`);
+
+const allIconsFilePath = path.resolve(iconsDirectory, 'all-icons.ts');
+console.log('Writing all-icons file');
+fs.writeFileSync(allIconsFilePath, allIconsFileContents, { encoding: 'utf-8' });
+console.log('Finished writing all-icons file');
