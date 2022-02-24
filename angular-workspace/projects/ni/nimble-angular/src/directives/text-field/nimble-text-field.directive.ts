6296c65fa16f70b5ffe76a600d5011201b6bd460
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2022-02-24 15:04:39 -0600

    Add explanation for lint suppression

--- angular-workspace/projects/ni/nimble-angular/src/directives/text-field/nimble-text-field.directive.ts
+++ angular-workspace/projects/ni/nimble-angular/src/directives/text-field/nimble-text-field.directive.ts
@@ -37,6 +37,7 @@ export class NimbleTextFieldDirective {
         return this.elementRef.nativeElement.errorText;
     }
 
+    // Renaming because property should have camel casing, but attribute should not
     // eslint-disable-next-line @angular-eslint/no-input-rename
     @Input('error-text') public set errorText(value: string) {
         this.renderer.setProperty(this.elementRef.nativeElement, 'errorText', value);
