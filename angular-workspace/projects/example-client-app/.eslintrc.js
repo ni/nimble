ni/nimble

commit 4010f167415c7bbf258f8b48a70abcc9f5c1ac1c

Fix nimble-angular package contents (#159)
## 🤨 Rationale

The nimble-angular NPM package currently [doesn't include any build output](https://unpkg.com/browse/@ni/nimble-angular@1.0.0-beta.83/).

## 👩‍💻 Implementation

* Added `prepack` script to `nimble-angular` project to copy build output into the project directory, so that it can be included in the packed package.
* Updated package.json to include the copied build output in the package

## 🧪 Testing

Ran `npm pack` locally and confirmed that the package included the expected files.

angular-workspace/projects/ni/nimble-angular/.eslintrc.js
@@ -1,75 +1,93 @@
 module.exports = {
-  extends: '../../../.eslintrc.js',
-  ignorePatterns: [
-    '!**/*'
-  ],
-  plugins: ['jsdoc'],
-  overrides: [
-    {
-      files: [
-        '*.ts'
-      ],
-      parserOptions: {
-        project: [
-          './tsconfig.lib.json',
-          './tsconfig.spec.json'
-        ],
-        tsconfigRootDir: __dirname,
-        createDefaultProgram: true
-      },
-      rules: {
-        // Require non-empty JSDoc comment on classes
-        'jsdoc/require-jsdoc': [
-          'error',
-          {
-              publicOnly: false,
-              require: {
-                  ClassDeclaration: true,
-                  FunctionDeclaration: false
-              }
-          }
-        ],
-        'jsdoc/require-description': [
-          'error', 
-          { contexts: ['ClassDeclaration']}
-        ],
-      }
-    },
-    {
-        // Don't require class docs on modules (they're trivial) or tests (not public API)
-        files: [
-        '*.module.ts', '*.spec.ts'
-      ], 
-      rules: {            
-        'jsdoc/require-jsdoc': 'off',
-        'jsdoc/require-description': 'off'
-      }
-    },
-    {
-      files: [
-        '*.spec.ts'
-      ],
-      rules: {
-        // No value in requiring a selector for components that are only used within a test file
-        "@angular-eslint/use-component-selector": "off",
-        // Multiple component definitions in a test file is acceptable
-        "max-classes-per-file": "off"
-      }
-    },
-    {
-      // disable failing rule in one autogenerated file
-      files: [
-        'src/test.ts'
-      ],
-      rules: {
-        "import/no-extraneous-dependencies": "off"
-      }
-    },
-    {
-      files: [
-        '*.html'
-      ],
-      rules: {}
-    }
-  ]
-}
+    extends: '../../../.eslintrc.js',
+    ignorePatterns: [
+        '!**/*',
+        // Files temporarily copied to project directory during pack
+        'bundles/**',
+        'directives/**',
+        'esm2015/**',
+        'fesm2015/**',
+        'ni-nimble-angular.d.ts',
+        'public-api.d.ts'
+    ],
+    plugins: ['jsdoc'],
+    overrides: [
+        {
+            files: [
+                '*.ts'
+            ],
+            parserOptions: {
+                project: [
+                    './tsconfig.lib.json',
+                    './tsconfig.spec.json'
+                ],
+                tsconfigRootDir: __dirname,
+                createDefaultProgram: true
+            },
+            rules: {
+                // Require non-empty JSDoc comment on classes
+                'jsdoc/require-jsdoc': [
+                    'error',
+                    {
+                        publicOnly: false,
+                        require: {
+                            ClassDeclaration: true,
+                            FunctionDeclaration: false
+                        }
+                    }
+                ],
+                'jsdoc/require-description': [
+                    'error',
+                    { contexts: ['ClassDeclaration'] }
+                ],
+            }
+        },
+        {
+            // Don't require class docs on modules (they're trivial) or tests (not public API)
+            files: [
+                '*.module.ts', '*.spec.ts'
+            ],
+            rules: {
+                'jsdoc/require-jsdoc': 'off',
+                'jsdoc/require-description': 'off'
+            }
+        },
+        {
+            files: [
+                '*.spec.ts'
+            ],
+            rules: {
+                // No value in requiring a selector for components that are only used within a test file
+                '@angular-eslint/use-component-selector': 'off',
+                // Multiple component definitions in a test file is acceptable
+                'max-classes-per-file': 'off'
+            }
+        },
+        {
+            // disable failing rule in one autogenerated file
+            files: [
+                'src/test.ts'
+            ],
+            rules: {
+                'import/no-extraneous-dependencies': 'off'
+            }
+        },
+        {
+            files: [
+                '*.html'
+            ],
+            rules: {}
+        },
+        {
+            files: [
+                'build/*.js'
+            ],
+            rules: {
+                // Logging in build scripts is useful
+                'no-console': 'off',
+                // Okay to use dev dependencies in build scripts
+                'import/no-extraneous-dependencies': 'off'
+            }
+        }
+    ]
+};

angular-workspace/.eslintrc.js
@@ -11,10 +11,13 @@ module.exports = {
             // https://github.com/ni/javascript-styleguide/issues/39
             'import/prefer-default-export': 'off',
             'import/no-default-export': 'error',
-            'no-restricted-imports': ['error', { patterns: 
-                [{ group: ['@microsoft/fast-*'], 
-                message: 'Do not directly use underlying libraries of nimble. Instead rely on or add to exports of nimble packages.' 
-            }]}]
+            'no-restricted-imports': ['error', {
+                patterns:
+                [{
+                    group: ['@microsoft/fast-*'],
+                    message: 'Do not directly use underlying libraries of nimble. Instead rely on or add to exports of nimble packages.'
+                }]
+            }]
         }
     },
     {
@@ -27,5 +30,10 @@ module.exports = {
             // example-client-app is just for demo purposes and isn't anticipated to be localized
             '@angular-eslint/template/i18n': 'off'
         }
+    }, {
+        files: ['*.js'],
+        extends: [
+            '@ni/eslint-config-javascript'
+        ],
     }]
 };

.github/workflows/main.yml
@@ -32,13 +32,15 @@ jobs:
 
       # Build
       - run: npm run build
+
+      # Pack
       - run: npm run pack
       - uses: actions/upload-artifact@v2
         with:
           name: packages
           path: |
             packages/**/*.tgz
-            angular-workspace/dist/**/*.tgz
+            angular-workspace/**/*.tgz
           if-no-files-found: error
 
       # Lint

angular-workspace/projects/ni/nimble-angular/.gitignore
@@ -1,0 +1,7 @@
+# Files temporarily copied to project directory during pack
+/bundles
+/directives
+/esm2015
+/fesm2015
+/ni-nimble-angular.d.ts
+/public-api.d.ts

angular-workspace/projects/ni/nimble-angular/build/copyBuildOutputForPack.js
@@ -1,0 +1,63 @@
+const glob = require('glob');
+const cpFile = require('cp-file');
+const path = require('path');
+const fs = require('fs');
+
+/**
+ * We want to publish out of the project directory, not the build output (dist) directory. Angular
+ * seems to encourage publishing out of the dist directory, by copying the package.json and README
+ * to that directory. But the Beachball tooling only publishes packages that are tracked by git
+ * (which the dist directory is not). So instead, we copy the build output to the project directory
+ * before packing so that it can be included.
+ */
+
+const sourceDir = path.resolve(__dirname, '../../../../dist/ni/nimble-angular');
+const sources = [
+    'bundles/**',
+    'directives/**',
+    'esm2015/**',
+    'fesm2015/**',
+    'ni-nimble-angular.d.ts',
+    'public-api.d.ts'
+];
+const destinationDir = path.resolve(__dirname, '../');
+
+const deleteCopiedFiles = () => {
+    console.log('Deleting existing pack files');
+    for (const source of sources) {
+        const filesToDelete = glob.sync(path.join(destinationDir, source), { nodir: true });
+        for (const fileToDelete of filesToDelete) {
+            fs.unlinkSync(fileToDelete);
+        }
+    }
+};
+
+const copyFiles = () => {
+    console.log('Copying new pack files');
+    for (const source of sources) {
+        const filesToCopy = glob.sync(path.join(sourceDir, source), { nodir: true });
+        for (const fileToCopy of filesToCopy) {
+            const relativeSource = path.relative(sourceDir, fileToCopy);
+            cpFile.sync(fileToCopy, path.join(destinationDir, relativeSource));
+        }
+    }
+};
+
+const arg = process.argv[2] || '';
+const clean = arg.toLowerCase() === 'clean';
+
+if (clean) {
+    console.log('*** Clean nimble-angular build output after pack ***');
+    console.log(`Cleaning copied files from: ${destinationDir}`);
+    deleteCopiedFiles();
+    console.log('Completed cleaning files after pack');
+    console.log();
+} else {
+    console.log('*** Copy nimble-angular build output for pack ***');
+    console.log(`Source directory: ${sourceDir}`);
+    console.log(`Destination directory: ${destinationDir}`);
+    deleteCopiedFiles();
+    copyFiles();
+    console.log('Completed copying files for pack');
+    console.log();
+}

commit 3db55d7a1e15ad1ca548316a2fe9788720664449

Fix angular library linting (#385)
# Pull Request

## 🤨 Rationale

Fixes #379

It turns out that I broke this in #159 when I incorrectly added `ignorePatterns` for directories to the ESLint config without a leading `/`.

## 👩‍💻 Implementation

* Removed the `ingorePatterns` that were preventing any source from being linted (these aren't necessary after #203 anyway)
* Enabled linting for `*.js` files in nimble-angular
* Updated config for `import/no-extraneous-dependencies` rule to point to correct package.json file
* Fixed lint errors

## 🧪 Testing

Verified that all types of source files seem to be linted. Linting passes.

angular-workspace/projects/example-client-app/.eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-  extends: '../../.eslintrc.js',
-  ignorePatterns: [
-    '!**/*'
-  ],
-  overrides: [
-    {
-      files: [
-        '*.ts'
-      ],
-      parserOptions: {
-        project: [
-          './tsconfig.app.json',
-          './tsconfig.spec.json'
-        ],
-        tsconfigRootDir: __dirname,
-        createDefaultProgram: true
-      },
-      rules: {
-        'no-restricted-imports': ['error', {
-          patterns:
+    extends: '../../.eslintrc.js',
+    ignorePatterns: [
+        '!**/*'
+    ],
+    overrides: [
+        {
+            files: [
+                '*.ts'
+            ],
+            parserOptions: {
+                project: [
+                    './tsconfig.app.json',
+                    './tsconfig.spec.json'
+                ],
+                tsconfigRootDir: __dirname,
+                createDefaultProgram: true
+            },
+            rules: {
+                'no-restricted-imports': ['error', {
+                    patterns:
           [{
               group: ['@microsoft/fast-*'],
               message: 'Do not directly use underlying libraries of nimble. Instead rely on or add to exports of nimble packages.'
@@ -26,18 +26,18 @@ module.exports = {
               group: ['@ni/nimble-components'],
               message: 'Client Angular applications should not have to directly depend on nimble-components.'
           }]
-      }],
-      '@angular-eslint/component-selector': [
-        "error",
-        { "type": "element", "prefix": "example", "style": "kebab-case" }
-      ]
-      }
-    },
-    {
-      files: [
-        '*.html'
-      ],
-      rules: {}
-    }
-  ]
+                }],
+                '@angular-eslint/component-selector': [
+                    'error',
+                    { type: 'element', prefix: 'example', style: 'kebab-case' }
+                ]
+            }
+        },
+        {
+            files: [
+                '*.html'
+            ],
+            rules: {}
+        }
+    ]
 };

angular-workspace/projects/ni/nimble-angular/.eslintrc.js
@@ -1,14 +1,10 @@
+const path = require('path');
+
 module.exports = {
     extends: '../../../.eslintrc.js',
     ignorePatterns: [
         '!**/*',
-        // Files temporarily copied to project directory during pack
-        'bundles/**',
-        'directives/**',
-        'esm2015/**',
-        'fesm2015/**',
-        'ni-nimble-angular.d.ts',
-        'public-api.d.ts'
+        '**/dist'
     ],
     plugins: ['jsdoc'],
     overrides: [
@@ -43,6 +39,16 @@ module.exports = {
             }
         },
         {
+            files: [
+                '*.js',
+                '*.ts'
+            ],
+            rules: {
+                // Use package.json from angular-workspace root
+                'import/no-extraneous-dependencies': ['error', { packageDir: path.resolve(__dirname, '../../../') }]
+            }
+        },
+        {
             // Don't require class docs on modules (they're trivial) or tests (not public API)
             files: [
                 '*.module.ts', '*.spec.ts'
@@ -73,9 +79,7 @@ module.exports = {
             ],
             rules: {
                 // Logging in build scripts is useful
-                'no-console': 'off',
-                // Okay to use dev dependencies in build scripts
-                'import/no-extraneous-dependencies': 'off'
+                'no-console': 'off'
             }
         }
     ]

angular-workspace/projects/ni/nimble-angular/karma.conf.js
@@ -1,46 +1,52 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-process.env.CHROME_BIN = require('puppeteer').executablePath()
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+const karmaJasmine = require('karma-jasmine');
+const karmaChromeLauncher = require('karma-chrome-launcher');
+const karmaJasmineHtmlReporter = require('karma-jasmine-html-reporter');
+const karmaCoverage = require('karma-coverage');
+const karmaAngular = require('@angular-devkit/build-angular/plugins/karma');
+const path = require('path');
 
-module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
-    coverageReporter: {
-      dir: require('path').join(__dirname, '../../../coverage/ni/nimble-angular'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['ChromeHeadless'],
-    singleRun: false,
-    restartOnFileChange: true
-  });
+module.exports = config => {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        plugins: [
+            karmaJasmine,
+            karmaChromeLauncher,
+            karmaJasmineHtmlReporter,
+            karmaCoverage,
+            karmaAngular
+        ],
+        client: {
+            jasmine: {
+                // you can add configuration options for Jasmine here
+                // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+                // for example, you can disable the random execution with `random: false`
+                // or set a specific seed with `seed: 4321`
+            },
+            clearContext: false // leave Jasmine Spec Runner output visible in browser
+        },
+        jasmineHtmlReporter: {
+            suppressAll: true // removes the duplicated traces
+        },
+        coverageReporter: {
+            dir: path.join(__dirname, '../../../coverage/ni/nimble-angular'),
+            subdir: '.',
+            reporters: [
+                { type: 'html' },
+                { type: 'text-summary' }
+            ]
+        },
+        reporters: ['progress', 'kjhtml'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['ChromeHeadless'],
+        singleRun: false,
+        restartOnFileChange: true
+    });
 };

angular-workspace/projects/ni/nimble-angular/src/directives/toggle-button/nimble-toggle-button.directive.ts
@@ -1,7 +1,6 @@
 import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 import type { ToggleButton } from '@ni/nimble-components/dist/esm/toggle-button';
-import type { ButtonAppearanceAttribute } from '@ni/nimble-components/dist/esm/toggle-button/types';
-import type { ButtonAppearance } from '@ni/nimble-components/dist/esm/toggle-button/types';
+import type { ButtonAppearance, ButtonAppearanceAttribute } from '@ni/nimble-components/dist/esm/toggle-button/types';
 import { BooleanValueOrAttribute, toBooleanProperty } from '../utilities/template-value-helpers';
 
 export type { ToggleButton };

angular-workspace/projects/ni/nimble-angular/src/directives/select/tests/nimble-select-control-value-accessor.directive.spec.ts
@@ -37,8 +37,8 @@ describe('Nimble select control value accessor', () => {
 
             public selectDisabled = false;
 
-            public compareWith(option1: { name: string, value: number }, option2: { name: string, value: number }): boolean {
-                return option1 && option2 && option1.value === option2.value;
+            public compareWith(option1: { name: string, value: number } | null, option2: { name: string, value: number } | null): boolean {
+                return !!option1 && !!option2 && option1.value === option2.value;
             }
         }
 