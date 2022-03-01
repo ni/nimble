cf0ebf7f1c460afc2f942748623e225dd0c3f27a
Fred Visser <1458528+fredvisser@users.noreply.github.com>
2022-03-01 10:01:13 -0600

    Update slash icon, update from XD, and and rename a few tokens (#389)
    🤨 Rationale
    
    Replaces temporary forward-slash icon with proper icon
    Replaces Enterprise green with ForestGreen
    Replaces Selection100 with DigitalGreenLight
    Replaces Banner_Fail_100_DarkUI_ with Banner_Fail_100_DarkUI
    Adds DigitalGreenDark, PowerGreen, and Black22
    Rename fillColorSelected to fillSelectedColor to match token naming schema.
    Rename fill-selected-hover-color to fill-hover-selected-color to match token naming schema.
    👩‍💻 Implementation
    
    Followed CONTRIBUTING docs to import new icon.
    Used XD plugin to import latest Nimble_Components doc
    
    🧪 Testing
    
    Local builds and Storybook review
    
    ✅ Checklist
    
     I have updated the project documentation to reflect my changes or determined no changes are needed.

--- packages/nimble-components/src/theme-provider/design-tokens.ts
+++ packages/nimble-components/src/theme-provider/design-tokens.ts
@@ -9,8 +9,8 @@ import {
     Black75,
     Black80,
     White,
-    Enterprise,
-    Selection100,
+    ForestGreen,
+    DigitalGreenLight,
     Fail100LightUi,
     SmallDelay,
     MediumDelay,
@@ -94,27 +94,27 @@ export const actionRgbPartialColor = DesignToken.create<string>(
 
 export const applicationBackgroundColor = DesignToken.create<string>(
     styleNameFromTokenName(tokenNames.applicationBackgroundColor)
-).withDefault((element: HTMLElement) => getColorForTheme(element, White, Black85, Enterprise));
+).withDefault((element: HTMLElement) => getColorForTheme(element, White, Black85, ForestGreen));
 
 export const headerBackgroundColor = DesignToken.create<string>(
     styleNameFromTokenName(tokenNames.headerBackgroundColor)
-).withDefault((element: HTMLElement) => getColorForTheme(element, Black7, Black80, Enterprise));
+).withDefault((element: HTMLElement) => getColorForTheme(element, Black7, Black80, ForestGreen));
 
 export const sectionBackgroundColor = DesignToken.create<string>(
     styleNameFromTokenName(tokenNames.sectionBackgroundColor)
-).withDefault((element: HTMLElement) => getColorForTheme(element, Black30, Black91, Enterprise));
+).withDefault((element: HTMLElement) => getColorForTheme(element, Black30, Black91, ForestGreen));
 
-export const fillColorSelected = DesignToken.create<string>(
-    styleNameFromTokenName(tokenNames.fillColorSelected)
-).withDefault((element: HTMLElement) => hexToRgbaCssColor(getFillColorSelectedForTheme(element), 0.3));
+export const fillSelectedColor = DesignToken.create<string>(
+    styleNameFromTokenName(tokenNames.fillSelectedColor)
+).withDefault((element: HTMLElement) => hexToRgbaCssColor(getfillSelectedColorForTheme(element), 0.3));
 
 export const fillSelectedRgbPartialColor = DesignToken.create<string>(
     styleNameFromTokenName(tokenNames.fillSelectedRgbPartialColor)
-).withDefault((element: HTMLElement) => hexToRgbPartial(getFillColorSelectedForTheme(element)));
+).withDefault((element: HTMLElement) => hexToRgbPartial(getfillSelectedColorForTheme(element)));
 
 export const fillHoverSelectedColor = DesignToken.create<string>(
     styleNameFromTokenName(tokenNames.fillHoverSelectedColor)
-).withDefault((element: HTMLElement) => hexToRgbaCssColor(getFillColorSelectedForTheme(element), 0.15));
+).withDefault((element: HTMLElement) => hexToRgbaCssColor(getfillSelectedColorForTheme(element), 0.15));
 
 export const fillHoverColor = DesignToken.create<string>(
     styleNameFromTokenName(tokenNames.fillHoverColor)
@@ -142,7 +142,7 @@ export const passColor = DesignToken.create<string>(
 
 export const borderHoverColor = DesignToken.create<string>(
     styleNameFromTokenName(tokenNames.borderHoverColor)
-).withDefault((element: HTMLElement) => getColorForTheme(element, Selection100, Selection100, White));
+).withDefault((element: HTMLElement) => getColorForTheme(element, DigitalGreenLight, DigitalGreenLight, White));
 
 // Component Color Tokens
 export const iconColor = DesignToken.create<string>(
@@ -435,8 +435,13 @@ function getDefaultFontColorForTheme(element: HTMLElement): string {
     return getColorForTheme(element, Black91, Black15, White);
 }
 
-function getFillColorSelectedForTheme(element: HTMLElement): string {
-    return getColorForTheme(element, Selection100, Selection100, White);
+function getfillSelectedColorForTheme(element: HTMLElement): string {
+    return getColorForTheme(
+        element,
+        DigitalGreenLight,
+        DigitalGreenLight,
+        White
+    );
 }
 
 function getfillHoverColorForTheme(element: HTMLElement): string {
