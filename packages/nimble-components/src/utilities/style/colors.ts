cd4469b2894e90e453a6ea0e15154dd6cb83a45c
Malcolm Smith <20709258+msmithNI@users.noreply.github.com>
2022-03-04 14:18:55 -0600

    Tweak JSDoc

--- packages/nimble-components/src/utilities/style/colors.ts
+++ packages/nimble-components/src/utilities/style/colors.ts
@@ -2,6 +2,7 @@ import hexRgb from 'hex-rgb';
 
 /**
  * Convert a hexadecimal color string to an RGBA CSS color string
+ * Example: 'ff0102' or '#ff0102' to 'rgba(255, 1, 2, 1)'
  * @param hexValue Hex color (with or without a starting '#')
  * @param alpha CSS alpha value between 0 (transparent) and 1 (opaque)
  * @returns An rgba()-formatted CSS color string
