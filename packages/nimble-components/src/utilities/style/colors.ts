cb9fa958d62660d764c0ff3466a47bcc127d81f2
Malcolm Smith <20709258+msmithNI@users.noreply.github.com>
2022-03-04 14:15:55 -0600

    Add JSDoc for color function

--- packages/nimble-components/src/utilities/style/colors.ts
+++ packages/nimble-components/src/utilities/style/colors.ts
@@ -1,5 +1,11 @@
 import hexRgb from 'hex-rgb';
 
+/**
+ * Convert a hexadecimal color string to an RGBA CSS color string
+ * @param hexValue Hex color (with or without a starting '#')
+ * @param alpha CSS alpha value between 0 (transparent) and 1 (opaque)
+ * @returns An rgba()-formatted CSS color string
+ */
 export function hexToRgbaCssColor(hexValue: string, alpha: number): string {
     const { red, green, blue } = hexRgb(hexValue);
     return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
