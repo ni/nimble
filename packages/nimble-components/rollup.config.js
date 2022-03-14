95e2c2bf3c0d5bd99cd558791f8d5ddd6a9c22b3
Brian Henry <5454342+brianehenry@users.noreply.github.com>
2022-03-14 16:22:49 -0500

    Implementation

---
+++ packages/nimble-components/rollup.config.js
@@ -0,0 +1,58 @@
+import commonJS from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import filesize from 'rollup-plugin-filesize';
+import { terser } from 'rollup-plugin-terser';
+import transformTaggedTemplate from 'rollup-plugin-transform-tagged-template';
+import typescript from 'rollup-plugin-typescript2';
+import {
+    transformCSSFragment,
+    transformHTMLFragment,
+} from './build/transform-fragments';
+
+const parserOptions = {
+    sourceType: 'module',
+};
+
+// eslint-disable-next-line import/no-default-export
+export default [
+    {
+        context: 'this',
+        input: 'src/index-rollup.ts',
+        output: [
+            {
+                file: 'dist/nimble-components.js',
+                format: 'esm',
+            },
+            {
+                file: 'dist/nimble-components.min.js',
+                format: 'esm',
+                plugins: [terser()],
+            },
+        ],
+        plugins: [
+            resolve(),
+            commonJS(),
+            typescript({
+                tsconfigOverride: {
+                    compilerOptions: {
+                        declaration: false,
+                    },
+                },
+            }),
+            transformTaggedTemplate({
+                tagsToProcess: ['css'],
+                transformer: transformCSSFragment,
+                parserOptions,
+            }),
+            transformTaggedTemplate({
+                tagsToProcess: ['html'],
+                transformer: transformHTMLFragment,
+                parserOptions,
+            }),
+            filesize({
+                showMinifiedSize: false,
+                showBrotliSize: true,
+            }),
+        ],
+    },
+];
