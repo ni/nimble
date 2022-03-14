e6b8e894d5a83dfbf3ba26ab34d52e4614767cc9
mollykreis <20542556+mollykreis@users.noreply.github.com>
2022-03-14 12:47:27 -0500

    transparent block button on hover

--- packages/nimble-components/src/patterns/button/styles.ts
+++ packages/nimble-components/src/patterns/button/styles.ts
@@ -191,13 +191,17 @@ export const styles = css`
                 }
 
                 .control:hover {
-                    background-color: rgba(${borderRgbPartialColor}, 0.1);
+                    background-color: transparent;
                     border-color: ${borderHoverColor};
                 }
 
                 .control${focusVisible} {
                     background-color: rgba(${borderRgbPartialColor}, 0.1);
                     border-color: ${borderHoverColor};
+                }
+
+                .control:hover${focusVisible} {
+                    background-color: transparent;
                 }
 
                 .control:active {
