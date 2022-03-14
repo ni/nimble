9f367f69590374c042bfdcb07105b4d104db3975
Brian Henry <5454342+brianehenry@users.noreply.github.com>
2022-03-14 10:33:31 -0600

    Switch component (#424)
    🤨 Rationale
    
    #387
    
    👩‍💻 Implementation
    
    Created nimble-switch in nimble-components
    Styled switch to match visual design spec
    Created custom template to enable visual design
    Has unchecked and checked message slots on either side of the switch visual
    Has an additional sub-element in the checked indicator to represent checked state
    🧪 Testing
    
    Copied switch tests from fast-foundation to make sure that our custom template has good coverage.
    Created storybook stories for switch, including a matrix story and a hidden story.
    ✅ Checklist
    
     I have updated the project documentation to reflect my changes or determined no changes are needed.

---
+++ packages/nimble-components/src/switch/tests/switch-matrix.stories.ts
@@ -0,0 +1,67 @@
+import type { Meta, Story } from '@storybook/html';
+import { withXD } from 'storybook-addon-xd-designs';
+import { html, ViewTemplate, when } from '@microsoft/fast-element';
+import {
+    disabledStates,
+    DisabledState,
+    createMatrix,
+    themeWrapper
+} from '../../utilities/tests/matrix';
+import { createRenderer } from '../../utilities/tests/storybook';
+import '..';
+import { hiddenWrapper } from '../../utilities/tests/hidden';
+
+const metadata: Meta = {
+    title: 'Tests/Switch',
+    decorators: [withXD],
+    parameters: {
+        design: {
+            artboardUrl:
+                'https://xd.adobe.com/view/33ffad4a-eb2c-4241-b8c5-ebfff1faf6f6-66ac/screen/3698340b-8162-4e5d-bf7a-20194612b3a7/specs/'
+        },
+        controls: { hideNoControlsWarning: true },
+        a11y: { disabled: true }
+    }
+};
+
+export default metadata;
+
+type CheckedState = [string, boolean];
+const checkedStates: CheckedState[] = [
+    ['Checked', true],
+    ['Unchecked', false]
+];
+
+type MessagesState = [string, boolean];
+const messagesStates: MessagesState[] = [
+    ['With Messages', true],
+    ['Without Messages', false]
+];
+
+// prettier-ignore
+const component = (
+    [checkedName, checked]: CheckedState,
+    [disabledName, disabled]: DisabledState,
+    [messagesName, messages]: MessagesState
+): ViewTemplate => html`
+    <nimble-switch
+        ?disabled=${() => disabled}
+        ?checked=${() => checked}
+        style="margin-right: 8px; margin-bottom: 8px;">
+            ${() => `${checkedName} Switch ${disabledName} ${messagesName}`}
+            ${when(() => messages, html`<span slot="checked-message">On</span><span slot="unchecked-message">Off</span>`)}
+    </nimble-switch>
+`;
+
+export const switchThemeMatrix: Story = createRenderer(
+    themeWrapper(
+        createMatrix(component, [checkedStates, disabledStates, messagesStates])
+    )
+);
+
+// prettier-ignore
+export const hiddenSwitch: Story = createRenderer(
+    hiddenWrapper(
+        html`<nimble-switch hidden>Hidden Switch</nimble-switch>`
+    )
+);
---
+++ packages/nimble-components/src/switch/tests/switch.stories.ts
@@ -0,0 +1,64 @@
+import { html, when } from '@microsoft/fast-element';
+import type { Meta, StoryObj } from '@storybook/html';
+import { withXD } from 'storybook-addon-xd-designs';
+import { createRenderer } from '../../utilities/tests/storybook';
+import '..';
+import '../../icons/key';
+
+interface SwitchArgs {
+    label: string;
+    checked: boolean;
+    disabled: boolean;
+    checkedMessage: string;
+    uncheckedMessage: string;
+}
+
+const overviewText = `Per [W3C](https://w3c.github.io/aria-practices/#switch) - A switch is an input widget that
+allows users to choose one of two values: on or off. Switches are similar to checkboxes and toggle buttons, which
+can also serve as binary inputs. One difference, however, is that switches can only be used for binary input while
+checkboxes and toggle buttons allow implementations the option of supporting a third middle state. Checkboxes can
+be checked or not checked and can optionally also allow for a partially checked state. Toggle buttons can be
+pressed or not pressed and can optionally allow for a partially pressed state.`;
+
+const metadata: Meta<SwitchArgs> = {
+    title: 'Switch',
+    decorators: [withXD],
+    parameters: {
+        docs: {
+            description: {
+                component: overviewText
+            }
+        },
+        design: {
+            artboardUrl:
+                'https://xd.adobe.com/view/33ffad4a-eb2c-4241-b8c5-ebfff1faf6f6-66ac/screen/3698340b-8162-4e5d-bf7a-20194612b3a7/specs/'
+        },
+        actions: {
+            handles: ['change']
+        }
+    },
+    // prettier-ignore
+    render: createRenderer(html`
+        <nimble-switch
+            ?checked="${x => x.checked}"
+            ?disabled="${x => x.disabled}"
+        >
+            ${when(x => x.label, html<SwitchArgs>`${x => x.label}`)}
+            ${when(x => x.checkedMessage, html<SwitchArgs>`<span slot="checked-message">${x => x.checkedMessage}</span>`)}
+            ${when(x => x.uncheckedMessage, html<SwitchArgs>`<span slot="unchecked-message">${x => x.uncheckedMessage}</span>`)}
+        </nimble-switch>
+    `),
+    args: {
+        label: 'Switch',
+        checked: true,
+        disabled: false,
+        checkedMessage: 'On',
+        uncheckedMessage: 'Off'
+    }
+};
+
+export default metadata;
+
+export const switchStory: StoryObj<SwitchArgs> = {
+    name: 'Switch'
+};
