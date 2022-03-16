6bb381682f2daf5b2c57e5f7e21576e01e30794c
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2022-03-16 14:40:09 -0500

    Add selection mode to TreeView

---
+++ packages/nimble-blazor/NI.Nimble.Blazor.Components/SelectionMode.cs
@@ -0,0 +1,15 @@
+﻿namespace NI.Nimble.Components;
+
+public enum SelectionMode
+{
+    All,
+    LeavesOnly
+}
+
+internal static class SelectionModeExtensions
+{
+    private static readonly Dictionary<SelectionMode, string> _selectionModeValues =
+        Enum.GetValues<SelectionMode>().ToDictionary(id => id, id => Enum.GetName(id)!.ToLowerInvariant());
+
+    public static string? ToAttributeValue(this SelectionMode? value) => value == null ? null : _selectionModeValues[value.Value];
+}
