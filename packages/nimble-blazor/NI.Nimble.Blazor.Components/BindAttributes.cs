b9f86a5f38418b330dce898d08a43d9dae493fe6
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2022-03-16 14:27:52 -0500

    Copy changes from old initial-blazor-integration branch on top of forked FAST blazor files

---
+++ packages/nimble-blazor/NI.Nimble.Blazor.Components/BindAttributes.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NI.Nimble.Components;
+
+// Checkbox like items
+[BindElement("nimble-checkbox", null, "checked", "onchange")]
+[BindElement("nimble-checkbox", "value", "checked", "onchange")]
+
+[BindElement("nimble-menu-item", null, "checked", "onchange")]
+[BindElement("nimble-menu-item", "value", "checked", "onchange")]
+
+// nimble-select (value)
+[BindElement("nimble-select", null, "value", "onchange")]
+[BindElement("nimble-select", "value", "value", "onchange")]
+
+// Text like inputs
+[BindElement("nimble-text-field", null, "value", "onchange")]
+[BindElement("nimble-text-field", "value", "value", "onchange")]
+
+
+
+public static class BindAttributes
+{
+}
+
---
+++ packages/nimble-blazor/NI.Nimble.Blazor.Components/TextFieldType.cs
@@ -0,0 +1,18 @@
+﻿namespace NI.Nimble.Components;
+
+public enum TextFieldType
+{
+    Text,
+    Email,
+    Password,
+    Tel,
+    Url
+}
+
+internal static class TextFieldTypeExtensions
+{
+    private static readonly Dictionary<TextFieldType, string> _textFieldTypeValues =
+        Enum.GetValues<TextFieldType>().ToDictionary(id => id, id => Enum.GetName(id)!.ToLowerInvariant());
+
+    public static string? ToAttributeValue(this TextFieldType? value) => value == null ? null : _textFieldTypeValues[value.Value];
+}
