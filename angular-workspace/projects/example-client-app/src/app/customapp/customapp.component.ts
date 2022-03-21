8a9e064872ba34f92cac684ee373974839279191
jattasNI <jattasNI@users.noreply.github.com>
2022-03-21 12:29:02 -0500

    Example app

--- angular-workspace/projects/example-client-app/src/app/customapp/customapp.component.ts
+++ angular-workspace/projects/example-client-app/src/app/customapp/customapp.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { DrawerLocation } from '@ni/nimble-angular';
+import { DrawerLocation, NimbleCheckboxDirective } from '@ni/nimble-angular';
 
 @Component({
     selector: 'example-customapp',
@@ -10,4 +10,9 @@ export class CustomAppComponent {
     public drawerLocation: DrawerLocation = DrawerLocation.Right;
     public isDrawerPinned = false;
     public drawerLocations = DrawerLocation;
+    public checkboxState: string;
+    public onCheckBoxValueChange(event: Event): void {
+        const checked = (event.target as unknown as NimbleCheckboxDirective).checked ? '' : 'not ';
+        this.checkboxState = `The checkbox is ${checked}checked`;
+    }
 }
