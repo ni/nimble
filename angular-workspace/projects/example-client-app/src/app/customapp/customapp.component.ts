c28806327440c27eb14def8d91b846df36654247
jattasNI <jattasNI@users.noreply.github.com>
2022-03-22 08:43:25 -0500

    Revert "Example app"
    This reverts commit 8a9e064872ba34f92cac684ee373974839279191.

--- angular-workspace/projects/example-client-app/src/app/customapp/customapp.component.ts
+++ angular-workspace/projects/example-client-app/src/app/customapp/customapp.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { DrawerLocation, NimbleCheckboxDirective } from '@ni/nimble-angular';
+import { DrawerLocation } from '@ni/nimble-angular';
 
 @Component({
     selector: 'example-customapp',
@@ -10,9 +10,4 @@ export class CustomAppComponent {
     public drawerLocation: DrawerLocation = DrawerLocation.Right;
     public isDrawerPinned = false;
     public drawerLocations = DrawerLocation;
-    public checkboxState: string;
-    public onCheckBoxValueChange(event: Event): void {
-        const checked = (event.target as unknown as NimbleCheckboxDirective).checked ? '' : 'not ';
-        this.checkboxState = `The checkbox is ${checked}checked`;
-    }
 }
