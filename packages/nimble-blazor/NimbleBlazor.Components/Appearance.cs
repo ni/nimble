cb8d53a931a75fd120f2ac841f5df292706145ce
m-akinc <7282195+m-akinc@users.noreply.github.com>
2022-03-22 16:16:09 -0500

    Initial blazor integration (#452)
    # Pull Request
    
    ## 🤨 Rationale
    
    Fixes #429
    
    ## 👩‍💻 Implementation
    
    This change updates the code forked from the FAST Blazor repo to represent the Nimble components instead.
    
    We are initially supporting only the component set specified in the spec (https://github.com/ni/nimble/tree/main/specs/nimble-blazor/README.md).
    
    ## 🧪 Testing
    
    Solution builds.
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or determined no changes are needed.

---
+++ packages/nimble-blazor/NimbleBlazor.Components/Appearance.cs
@@ -0,0 +1,17 @@
+﻿namespace NimbleBlazor.Components;
+
+public enum Appearance
+{
+    Block,
+    Ghost,
+    Outline,
+    Underline
+}
+
+internal static class AppearanceExtensions
+{
+    private static readonly Dictionary<Appearance, string> _appearanceValues =
+        Enum.GetValues<Appearance>().ToDictionary(id => id, id => Enum.GetName(id)!.ToLowerInvariant());
+
+    public static string? ToAttributeValue(this Appearance? value) => value == null ? null : _appearanceValues[value.Value];
+}
