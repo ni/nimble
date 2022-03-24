7e641fc43d08e501a668fb7c91208405d5ae8b02
Brian Henry <5454342+brianehenry@users.noreply.github.com>
2022-03-23 20:07:42 -0500

    Components bundled output (#445)
    # Pull Request
    
    ## 🤨 Rationale
    
    Fixes #428 
    
    ## 👩‍💻 Implementation
    
    * Added a build step which uses Rollup to create a dist file which contains all `nimble-components` and dependencies. 
      * Implementation inspired by [similar build in FAST foundation](https://github.com/microsoft/fast/blob/master/packages/web-components/fast-foundation/rollup.config.js).
      * The build step also creates a minified version with the same contents.
    * Created `src/all-components.ts` which registers all the web components in nimble.
    * Copied `transform-fragments.js` helpers from [the FAST repo](https://github.com/microsoft/fast/blob/a8aada16c7b089728a13a6bb7d98b8b928bb2b4f/build/transform-fragments.js). These helpers minify code in `html` and `css` tagged templates.
    * The generated files are `dist/all-components-bundle.js` and `dist/all-components-bundle.min.js`
     * The files are only component registration, not exports.
       * Because the files do not have any exports they are bundled as `iife` instead of as `esm`
       * Because the files are `iife` they can be used in legacy `<script src="">` tags instead of requiring `<script src="" type="module">` which allows them to be used from `file://` urls like by double clicking an html file on disk compared to esm modules which must be served from a web server
    
    ## 🧪 Testing
    
    Verified that both minified and unminified bundled files work via a simple html file on disk.
    
    ## ✅ Checklist
    
    <!--- Review the list and put an x in the boxes that apply or ~~strike through~~ around items that don't (along with an explanation). -->
    
    - [x] I have updated the project documentation to reflect my changes or determined no changes are needed.

---
+++ packages/nimble-components/rollup.config.js
@@ -0,0 +1,54 @@
+import commonJS from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import transformTaggedTemplate from 'rollup-plugin-transform-tagged-template';
+import {
+    transformCSSFragment,
+    transformHTMLFragment
+} from './build/transform-fragments';
+
+const parserOptions = {
+    sourceType: 'module'
+};
+
+// eslint-disable-next-line import/no-default-export
+export default [
+    {
+        input: 'dist/esm/all-components.js',
+        output: {
+            file: 'dist/all-components-bundle.js',
+            format: 'iife',
+            sourcemap: true
+        },
+        plugins: [resolve(), commonJS()]
+    },
+    {
+        input: 'dist/esm/all-components.js',
+        output: {
+            file: 'dist/all-components-bundle.min.js',
+            format: 'iife',
+            sourcemap: true,
+            plugins: [
+                terser({
+                    output: {
+                        semicolons: false
+                    }
+                })
+            ]
+        },
+        plugins: [
+            resolve(),
+            commonJS(),
+            transformTaggedTemplate({
+                tagsToProcess: ['css'],
+                transformer: transformCSSFragment,
+                parserOptions
+            }),
+            transformTaggedTemplate({
+                tagsToProcess: ['html'],
+                transformer: transformHTMLFragment,
+                parserOptions
+            })
+        ]
+    }
+];
