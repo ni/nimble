a5444469218f3b569719d9347b56acd69e3a84db
Fred Visser <1458528+fredvisser@users.noreply.github.com>
2022-03-29 12:07:36 -0500

    add icon tags

--- packages/nimble-components/src/icon-base/icon-metadata.ts
+++ packages/nimble-components/src/icon-base/icon-metadata.ts
@@ -10,149 +10,365 @@ export const iconMetadata: {
     readonly [key in IconName]: IconMetadata | undefined;
 } = {
     /* eslint-disable @typescript-eslint/naming-convention */
-    AddIcon: undefined,
-    ArrowDownRightAndArrowUpLeftIcon: undefined,
+    AddIcon: {
+        tags: ['add-input-field']
+    },
+    ArrowDownRightAndArrowUpLeftIcon: {
+        tags: ['resize', 'collapse']
+    },
     ArrowExpanderDownIcon: undefined,
     ArrowExpanderLeftIcon: undefined,
     ArrowExpanderRightIcon: undefined,
     ArrowExpanderUpIcon: undefined,
-    ArrowLeftFromLineIcon: undefined,
-    ArrowPartialRotateLeftIcon: undefined,
-    ArrowRightToLineIcon: undefined,
-    ArrowRotateRightIcon: undefined,
-    ArrowURotateLeftIcon: undefined,
-    ArrowUpLeftAndArrowDownRightIcon: undefined,
-    ArrowsMaximizeIcon: undefined,
-    ArrowsRepeatIcon: undefined,
-    BarsIcon: undefined,
-    BellIcon: undefined,
-    BellAndCommentIcon: undefined,
-    BellCircleIcon: undefined,
-    BellSolidCircleIcon: undefined,
-    BlockWithRibbonIcon: undefined,
+    ArrowLeftFromLineIcon: {
+        tags: ['logout']
+    },
+    ArrowPartialRotateLeftIcon: {
+        tags: ['reset']
+    },
+    ArrowRightToLineIcon: {
+        tags: ['login']
+    },
+    ArrowRotateRightIcon: {
+        tags: ['refresh']
+    },
+    ArrowURotateLeftIcon: {
+        tags: ['update']
+    },
+    ArrowUpLeftAndArrowDownRightIcon: {
+        tags: ['resize', 'expand']
+    },
+    ArrowsMaximizeIcon: {
+        tags: ['resize', 'fullscreen']
+    },
+    ArrowsRepeatIcon: {
+        tags: ['status', 'looping']
+    },
+    BarsIcon: {
+        tags: ['hamburger-menu']
+    },
+    BellIcon: {
+        tags: ['status', 'alarm']
+    },
+    BellAndCommentIcon: {
+        tags: ['alarm', 'notification']
+    },
+    BellCircleIcon: {
+        tags: ['alarm']
+    },
+    BellSolidCircleIcon: {
+        tags: ['alarm']
+    },
+    BlockWithRibbonIcon: {
+        tags: ['certificate']
+    },
     CalendarIcon: undefined,
-    ChartDiagramIcon: undefined,
-    ChartDiagramChildFocusIcon: undefined,
-    ChartDiagramParentFocusIcon: undefined,
-    ChartDiagramParentFocusTwoChildIcon: undefined,
-    CheckIcon: undefined,
-    CheckDotIcon: undefined,
-    CircleIcon: undefined,
-    CircleBrokenIcon: undefined,
-    CircleCheckIcon: undefined,
-    CirclePartialBrokenIcon: undefined,
-    CircleSlashIcon: undefined,
-    CircleXIcon: undefined,
+    ChartDiagramIcon: {
+        tags: ['oidc']
+    },
+    ChartDiagramChildFocusIcon: {
+        tags: ['managed-systems']
+    },
+    ChartDiagramParentFocusIcon: {
+        tags: ['manager-asset']
+    },
+    ChartDiagramParentFocusTwoChildIcon: {
+        tags: ['assets']
+    },
+    CheckIcon: {
+        tags: ['status', 'alarm-acknowledged']
+    },
+    CheckDotIcon: {
+        tags: ['status', 'done']
+    },
+    CircleIcon: {
+        tags: ['status', 'connected']
+    },
+    CircleBrokenIcon: {
+        tags: ['status', 'disconnected']
+    },
+    CircleCheckIcon: {
+        tags: ['status', 'acknowledged']
+    },
+    CirclePartialBrokenIcon: {
+        tags: ['status', 'partially-connected']
+    },
+    CircleSlashIcon: {
+        tags: ['status', 'blocked']
+    },
+    CircleXIcon: {
+        tags: ['status', 'terminated']
+    },
     ClipboardIcon: undefined,
-    ClockIcon: undefined,
-    ClockCogIcon: undefined,
-    ClockTriangleIcon: undefined,
-    CloneIcon: undefined,
+    ClockIcon: {
+        tags: ['time']
+    },
+    ClockCogIcon: {
+        tags: ['time-settings']
+    },
+    ClockTriangleIcon: {
+        tags: ['status', 'timed-out']
+    },
+    CloneIcon: {
+        tags: ['duplicate']
+    },
     CloudUploadIcon: undefined,
-    CloudWithArrowIcon: undefined,
-    CogIcon: undefined,
-    CogDatabaseIcon: undefined,
-    CogDatabaseInsetIcon: undefined,
-    CogSmallCogIcon: undefined,
-    CogZoomedIcon: undefined,
-    CommentIcon: undefined,
-    ComputerAndMonitorIcon: undefined,
-    CopyIcon: undefined,
-    CopyTextIcon: undefined,
+    CloudWithArrowIcon: {
+        tags: ['cloud-connector']
+    },
+    CogIcon: {
+        tags: ['details', 'settings']
+    },
+    CogDatabaseIcon: {
+        tags: ['admin', 'administration']
+    },
+    CogDatabaseInsetIcon: {
+        tags: ['admin', 'administration']
+    },
+    CogSmallCogIcon: {
+        tags: ['system-manager']
+    },
+    CogZoomedIcon: {
+        tags: ['data-indexing']
+    },
+    CommentIcon: {
+        tags: ['notes', 'alarm-notes', 'speech', 'bubble']
+    },
+    ComputerAndMonitorIcon: {
+        tags: ['devices-and-interfaces']
+    },
+    CopyIcon: {
+        tags: ['clipboard']
+    },
+    CopyTextIcon: {
+        tags: ['clipboard']
+    },
     DashboardBuilderIcon: undefined,
     DashboardBuilderLegendIcon: undefined,
     DashboardBuilderTemplatesIcon: undefined,
     DashboardBuilderTileIcon: undefined,
-    DatabaseIcon: undefined,
-    DatabaseCheckIcon: undefined,
-    DesktopIcon: undefined,
+    DatabaseIcon: {
+        tags: ['measurement-data-analysis']
+    },
+    DatabaseCheckIcon: {
+        tags: ['system-state-manager']
+    },
+    DesktopIcon: {
+        tags: ['monitor']
+    },
     DonutChartIcon: undefined,
-    DotSolidDotStrokeIcon: undefined,
+    DotSolidDotStrokeIcon: {
+        tags: ['status', 'header']
+    },
     DownloadIcon: undefined,
-    ElectronicChipZoomedIcon: undefined,
-    ExclamationMarkIcon: undefined,
-    EyeIcon: undefined,
-    FancyAIcon: undefined,
-    FileIcon: undefined,
-    FileDrawerIcon: undefined,
-    FileSearchIcon: undefined,
+    ElectronicChipZoomedIcon: {
+        tags: ['data-preparation']
+    },
+    ExclamationMarkIcon: {
+        tags: ['error']
+    },
+    EyeIcon: {
+        tags: ['view']
+    },
+    FancyAIcon: {
+        tags: ['tdms-string-channel']
+    },
+    FileIcon: {
+        tags: ['file-tdms']
+    },
+    FileDrawerIcon: {
+        tags: ['box', 'repository-manager']
+    },
+    FileSearchIcon: {
+        tags: ['file-viewer']
+    },
     FilterIcon: undefined,
-    FloppyDiskCheckmarkIcon: undefined,
-    FloppyDiskStarArrowRightIcon: undefined,
-    FloppyDiskThreeDotsIcon: undefined,
-    FolderIcon: undefined,
-    FolderOpenIcon: undefined,
+    FloppyDiskCheckmarkIcon: {
+        tags: ['save', 'no-unsaved-changed']
+    },
+    FloppyDiskStarArrowRightIcon: {
+        tags: ['save', 'autosave']
+    },
+    FloppyDiskThreeDotsIcon: {
+        tags: ['save', 'in-progress']
+    },
+    FolderIcon: {
+        tags: ['ldap']
+    },
+    FolderOpenIcon: {
+        tags: ['browse']
+    },
     ForwardSlashIcon: undefined,
-    FourDotsSquareIcon: undefined,
-    FunctionIcon: undefined,
-    GaugeSimpleIcon: undefined,
-    GridThreeByThreeIcon: undefined,
-    GridTwoByTwoIcon: undefined,
-    HammerIcon: undefined,
-    HashtagIcon: undefined,
+    FourDotsSquareIcon: {
+        tags: ['knurling']
+    },
+    FunctionIcon: {
+        tags: ['data-analyzer']
+    },
+    GaugeSimpleIcon: {
+        tags: ['widget']
+    },
+    GridThreeByThreeIcon: {
+        tags: ['chart']
+    },
+    GridTwoByTwoIcon: {
+        tags: ['custom-applications']
+    },
+    HammerIcon: {
+        tags: ['operation']
+    },
+    HashtagIcon: {
+        tags: ['number']
+    },
     HomeIcon: undefined,
-    HourglassIcon: undefined,
-    IndeterminantCheckboxIcon: undefined,
-    InfoIcon: undefined,
+    HourglassIcon: {
+        tags: ['history', 'timer']
+    },
+    IndeterminantCheckboxIcon: {
+        tags: ['selection']
+    },
+    InfoIcon: {
+        tags: ['']
+    },
     InfoCircleIcon: undefined,
     KeyIcon: {
         tags: ['access-control', 'active-directory']
     },
     LaptopIcon: undefined,
-    LayerGroupIcon: undefined,
-    LightningBoltIcon: undefined,
+    LayerGroupIcon: {
+        tags: ['jobs']
+    },
+    LightningBoltIcon: {
+        tags: ['active-jobs']
+    },
     LinkIcon: undefined,
     LinkCancelIcon: undefined,
-    ListIcon: undefined,
-    ListTreeIcon: undefined,
-    ListTreeDatabaseIcon: undefined,
+    ListIcon: {
+        tags: ['merged-view']
+    },
+    ListTreeIcon: {
+        tags: ['tree-view']
+    },
+    ListTreeDatabaseIcon: {
+        tags: ['measurement-data-analysis']
+    },
     LockIcon: undefined,
-    MagnifyingGlassIcon: undefined,
+    MagnifyingGlassIcon: {
+        tags: ['search']
+    },
     MarkdownIcon: undefined,
-    MinusIcon: undefined,
+    MinusIcon: {
+        tags: ['mixed-checkbox']
+    },
     MinusWideIcon: undefined,
     MobileIcon: undefined,
     NotebookIcon: undefined,
-    PasteIcon: undefined,
-    PencilIcon: undefined,
-    PotWithLidIcon: undefined,
-    QuestionIcon: undefined,
-    RunningArrowIcon: undefined,
-    ServerIcon: undefined,
-    ShareSquareIcon: undefined,
-    ShieldCheckIcon: undefined,
-    ShieldXmarkIcon: undefined,
-    SignalBarsIcon: undefined,
-    SineGraphIcon: undefined,
-    SkipArrowIcon: undefined,
-    SpinnerIcon: undefined,
-    SquareCheckIcon: undefined,
-    SquareTIcon: undefined,
-    TIcon: undefined,
+    PasteIcon: {
+        tags: ['clipboard']
+    },
+    PencilIcon: {
+        tags: ['edit']
+    },
+    PotWithLidIcon: {
+        tags: ['box', 'utilities']
+    },
+    QuestionIcon: {
+        tags: ['help']
+    },
+    RunningArrowIcon: {
+        tags: ['status', 'running']
+    },
+    ServerIcon: {
+        tags: ['analysis', 'automation']
+    },
+    ShareSquareIcon: {
+        tags: ['export']
+    },
+    ShieldCheckIcon: {
+        tags: ['secure']
+    },
+    ShieldXmarkIcon: {
+        tags: ['insecure']
+    },
+    SignalBarsIcon: {
+        tags: ['tdms-channel-group']
+    },
+    SineGraphIcon: {
+        tags: ['graph']
+    },
+    SkipArrowIcon: {
+        tags: ['status', 'skipped']
+    },
+    SpinnerIcon: {
+        tags: ['in-progress']
+    },
+    SquareCheckIcon: {
+        tags: ['test-insights']
+    },
+    SquareTIcon: {
+        tags: ['static-text']
+    },
+    TIcon: {
+        tags: ['text']
+    },
     TabletIcon: undefined,
     TagIcon: undefined,
-    TagsIcon: undefined,
-    TargetCrosshairsIcon: undefined,
-    TargetCrosshairsProgressIcon: undefined,
-    ThreeDotsLineIcon: undefined,
+    TagsIcon: {
+        tags: ['tag-query']
+    },
+    TargetCrosshairsIcon: {
+        tags: ['calibrate']
+    },
+    TargetCrosshairsProgressIcon: {
+        tags: ['calibrate', 'self-calibrate']
+    },
+    ThreeDotsLineIcon: {
+        tags: ['options']
+    },
     ThumbtackIcon: undefined,
     TileSizeIcon: undefined,
-    TimesIcon: undefined,
-    TrashIcon: undefined,
-    TriangleIcon: undefined,
-    TrueFalseRectangleIcon: undefined,
-    UnlinkIcon: undefined,
+    TimesIcon: {
+        tags: ['close']
+    },
+    TrashIcon: {
+        tags: ['delete']
+    },
+    TriangleIcon: {
+        tags: ['status', 'alarm-active']
+    },
+    TrueFalseRectangleIcon: {
+        tags: ['tdms-boolean-channel']
+    },
+    UnlinkIcon: {
+        tags: ['link-broken']
+    },
     UnlockIcon: undefined,
     UploadIcon: undefined,
-    UserIcon: undefined,
-    WatchIcon: undefined,
-    WaveformIcon: undefined,
+    UserIcon: {
+        tags: ['admin', 'account']
+    },
+    WatchIcon: {
+        tags: ['status', 'waiting']
+    },
+    WaveformIcon: {
+        tags: ['tdms-waveform-channel']
+    },
     WebviCustomIcon: undefined,
     WebviHostIcon: undefined,
-    WindowCodeIcon: undefined,
-    WindowTextIcon: undefined,
-    WrenchHammerIcon: undefined,
-    XmarkIcon: undefined,
-    XmarkCheckIcon: undefined
+    WindowCodeIcon: {
+        tags: ['http-api']
+    },
+    WindowTextIcon: {
+        tags: ['manual']
+    },
+    WrenchHammerIcon: {
+        tags: ['status', 'custom']
+    },
+    XmarkIcon: {
+        tags: ['status', 'fail']
+    },
+    XmarkCheckIcon: {
+        tags: ['self-test']
+    }
     /* eslint-enable @typescript-eslint/naming-convention */
 };
