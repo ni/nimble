c8ba478678f4b2591e38b74fb6d9d4a13c11adb9
Jonathan Meyer <26874831+atmgrifter00@users.noreply.github.com>
2022-04-07 12:11:17 -0500

    Change nimble-listbox-option to nimble-list-option

--- packages/nimble-components/src/list-option/tests/list-option.spec.ts
+++ packages/nimble-components/src/list-option/tests/list-option.spec.ts
@@ -2,18 +2,18 @@ import {
     DesignSystem,
     ListboxOption as FoundationListboxOption
 } from '@microsoft/fast-foundation';
-import { ListboxOption } from '..';
+import { ListOption } from '..';
 
 describe('ListboxOption', () => {
     it('should have its tag returned by tagFor(FoundationListboxOption)', () => {
         expect(DesignSystem.tagFor(FoundationListboxOption)).toBe(
-            'nimble-listbox-option'
+            'nimble-list-option'
         );
     });
 
     it('can construct an element instance', () => {
-        expect(document.createElement('nimble-listbox-option')).toBeInstanceOf(
-            ListboxOption
+        expect(document.createElement('nimble-list-option')).toBeInstanceOf(
+            ListOption
         );
     });
 });
