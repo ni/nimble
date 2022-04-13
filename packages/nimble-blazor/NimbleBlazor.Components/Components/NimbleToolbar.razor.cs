31d87b48a99501fca98a6614acbf29e2a70b7465
Jonathan Meyer <26874831+atmgrifter00@users.noreply.github.com>
2022-04-13 15:41:22 -0500

    Add Nimble Toolbar to Blazor (#490)
    As outlined in the [spec](https://github.com/ni/nimble/blob/main/specs/nimble-blazor/README.md) the Nimble Toolbar was identified as a component we should add to the initial set of Blazor components.

---
+++ packages/nimble-blazor/NimbleBlazor.Components/Components/NimbleToolbar.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NimbleBlazor.Components;
+
+public partial class NimbleToolbar
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+}
