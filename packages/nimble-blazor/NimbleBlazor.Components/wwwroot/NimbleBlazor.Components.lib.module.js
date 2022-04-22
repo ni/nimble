a75cb9baa10edff19ea60f6ef1457ebf2ee749bd
Malcolm Smith <20709258+msmithNI@users.noreply.github.com>
2022-04-22 17:39:21 -0500

    Blazor checkbox: Add binding for Indeterminate, fix 2-way binding (nimblecheckedchange event)

---
+++ packages/nimble-blazor/NimbleBlazor.Components/wwwroot/NimbleBlazor.Components.lib.module.js
@@ -0,0 +1,16 @@
+﻿/**
+ * Register the custom event type used for the NimbleCheckbox and NimbleSwitch change events.
+ * Necessary because the control's value property is always just the value 'on', so we need to look
+ * at the checked property to correctly get the value.
+ * @see NimbleCheckbox.razor and NimbleSwitch.razor
+ */
+export function afterStarted(Blazor) {
+    Blazor.registerCustomEventType('nimblecheckedchange', {
+        browserEventName: 'change',
+        createEventArgs: event => {
+            return {
+                checked: event.target.currentChecked
+            };
+        }
+    });
+}
