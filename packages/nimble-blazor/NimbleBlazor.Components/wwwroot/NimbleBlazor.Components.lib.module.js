07b940016e5deacfcc7875e070cfe102c3208c99
Malcolm Smith <20709258+msmithNI@users.noreply.github.com>
2022-04-22 18:02:32 -0500

    Remove UTF8 BOM in file

--- packages/nimble-blazor/NimbleBlazor.Components/wwwroot/NimbleBlazor.Components.lib.module.js
+++ packages/nimble-blazor/NimbleBlazor.Components/wwwroot/NimbleBlazor.Components.lib.module.js
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * Register the custom event type used for the NimbleCheckbox and NimbleSwitch change events.
  * Necessary because the control's value property is always just the value 'on', so we need to look
  * at the checked property to correctly get the value.
