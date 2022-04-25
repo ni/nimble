8812f880475c49b521e0225db2b90588780b6b44
Malcolm Smith <20709258+msmithNI@users.noreply.github.com>
2022-04-25 16:56:04 -0500

    Update comment

--- packages/nimble-blazor/NimbleBlazor.Components/wwwroot/NimbleBlazor.Components.lib.module.js
+++ packages/nimble-blazor/NimbleBlazor.Components/wwwroot/NimbleBlazor.Components.lib.module.js
@@ -1,8 +1,8 @@
 /**
- * Register the custom event type used for the NimbleCheckbox and NimbleSwitch change events.
+ * Register the custom event type used for the change events for NimbleCheckbox/NimbleSwitch/NimbleToggleButton.
  * Necessary because the control's value property is always just the value 'on', so we need to look
  * at the checked property to correctly get the value.
- * @see NimbleCheckbox.razor and NimbleSwitch.razor
+ * @see NimbleCheckbox.razor, NimbleSwitch.razor, NimbleToggleButton.razor
  */
 export function afterStarted(Blazor) {
     Blazor.registerCustomEventType('nimblecheckedchange', {
