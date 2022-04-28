a3c2c26b5bf945f1a3fa46a5d78290f10b8c88f9
Malcolm Smith <20709258+msmithNI@users.noreply.github.com>
2022-04-28 18:07:21 -0500

    Breadcrumb/BreadcrumbItem

---
+++ packages/nimble-blazor/NimbleBlazor.Components/Components/NimbleBreadcrumbItem.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NimbleBlazor.Components;
+
+public partial class NimbleBreadcrumbItem : ComponentBase
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    public IDictionary<string, object>? AdditionalAttributes { get; set; }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "NI1704:Identifiers should be spelled correctly", Justification = "Matching spelling of HTMLAnchorElement.href")]
+    [Parameter]
+    public string? Href { get; set; }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "NI1704:Identifiers should be spelled correctly", Justification = "Matching spelling of HTMLAnchorElement.hreflang")]
+    [Parameter]
+    public string? HrefLang { get; set; }
+
+    [Parameter]
+    public string? Ping { get; set; }
+
+    [Parameter]
+    public string? ReferrerPolicy { get; set; }
+
+    [Parameter]
+    public string? Rel { get; set; }
+
+    [Parameter]
+    public string? Target { get; set; }
+
+    [Parameter]
+    public string? Type { get; set; }
+}
