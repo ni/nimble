8111951db6dbc2584d10e26d3736cc71bbb194fe
mollykreis <20542556+mollykreis@users.noreply.github.com>
2022-04-29 15:15:09 -0500

    group aria attributes with comments

--- packages/nimble-components/src/toggle-button/template.ts
+++ packages/nimble-components/src/toggle-button/template.ts
@@ -19,13 +19,24 @@ ButtonOptions
         @click="${(x, c) => x.clickHandler(c.event as MouseEvent)}"
         class="control ${x => (x.checked ? 'checked' : '')}"
         ?disabled="${x => x.disabled}"
+        ${
+    '' /* Configure aria-disabled, aria-readonly, and aria-pressed based on the
+        toggle button's state to keep the ARIA attributes consistent with the component's
+        state without a client having to configure ARIA attributes directly */
+}
+        aria-disabled="${x => x.disabled}"
+        aria-readonly="${x => x.readOnly}"
+        aria-pressed="${x => x.checked}"
+        ${
+    '' /* Configure all other ARIA attributes based on the aria attributes
+        configured on the toggle button */
+}
         aria-atomic="${x => x.ariaAtomic}"
         aria-busy="${x => x.ariaBusy}"
         aria-controls="${x => x.ariaControls}"
         aria-current="${x => x.ariaCurrent}"
         aria-describedby="${x => x.ariaDescribedby}"
         aria-details="${x => x.ariaDetails}"
-        aria-disabled="${x => x.disabled}"
         aria-errormessage="${x => x.ariaErrormessage}"
         aria-expanded="${x => x.ariaExpanded}"
         aria-flowto="${x => x.ariaFlowto}"
@@ -37,10 +48,8 @@ ButtonOptions
         aria-labelledby="${x => x.ariaLabelledby}"
         aria-live="${x => x.ariaLive}"
         aria-owns="${x => x.ariaOwns}"
-        aria-pressed="${x => x.checked}"
         aria-relevant="${x => x.ariaRelevant}"
         aria-roledescription="${x => x.ariaRoledescription}"
-        aria-readonly="${x => x.readOnly}"
         ${ref('control')}
     >
         ${startSlotTemplate(context, definition)}
