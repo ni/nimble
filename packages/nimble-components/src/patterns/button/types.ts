ababe39dfea6abcded377f3ee25066a3c9d4a96e
mollykreis <20542556+mollykreis@users.noreply.github.com>
2022-05-04 12:31:15 -0500

    add comments to the button interface

--- packages/nimble-components/src/patterns/button/types.ts
+++ packages/nimble-components/src/patterns/button/types.ts
@@ -2,8 +2,21 @@
  * The interface that buttons of various types implement
  */
 export interface IButton {
+    /**
+     * The appearance the button should have.
+     */
     appearance: ButtonAppearance;
+
+    /**
+     * Specify as 'true' to hide the text content of the button. The button will
+     * become square, and the text content will be used as the label of the button
+     * for accessibility purposes.
+     */
     contentHidden: boolean;
+
+    /**
+     * Whether or not the button is disabled.
+     */
     disabled: boolean;
 }
 
