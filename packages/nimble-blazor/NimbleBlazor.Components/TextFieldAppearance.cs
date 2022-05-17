ae9c35fe75d93aa8b3a947759020b8558eb92490
Malcolm Smith <20709258+msmithNI@users.noreply.github.com>
2022-05-17 14:56:35 -0500

    Blazor: Replace single Appearance enum with one per control (#561)
    # Pull Request
    
    ## 🤨 Rationale
    
    Resolves #551
    
    ## 👩‍💻 Implementation
    
    Mostly self-explanatory.
    - Create separate Appearance enums per control type to be consistent with nimble-components / to only allow supported appearance modes per control
    - Update tests that were using invalid appearance modes for their associated controls
    - Add the missing Frameless appearance for text field
    
    ## 🧪 Testing
    
    Ran Blazor tests locally. Ran Blazor example app and tested out TextArea/TextField Appearance (since they're not currently shown in the demo page).
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or determined no changes are needed.

---
+++ packages/nimble-blazor/NimbleBlazor.Components/TextFieldAppearance.cs
@@ -0,0 +1,16 @@
+﻿namespace NimbleBlazor.Components;
+
+public enum TextFieldAppearance
+{
+    Underline,
+    Outline,
+    Block,
+    Frameless
+}
+
+internal static class TextFieldAppearanceExtensions
+{
+    private static readonly Dictionary<TextFieldAppearance, string> _appearanceValues = AttributeHelpers.GetEnumNamesAsKebabCaseValues<TextFieldAppearance>();
+
+    public static string? ToAttributeValue(this TextFieldAppearance? value) => value == null ? null : _appearanceValues[value.Value];
+}
