f856f00e56c51b23592e4f62144b5d4e07c9e30f
mollykreis <20542556+mollykreis@users.noreply.github.com>
2022-05-24 16:40:35 -0500

    Update .eslintrc.js

--- angular-workspace/.eslintrc.js
+++ angular-workspace/.eslintrc.js
@@ -26,6 +26,37 @@ module.exports = {
                 allowNullableNumber: false
             }]
         }
+    }, {
+        files: ['*.spec.ts'],
+        extends: [
+            '@ni/eslint-config-angular',
+            '@ni/eslint-config-typescript/requiring-type-checking'
+        ],
+        rules: {
+            'no-restricted-imports': [
+                'error', {
+                    patterns:
+                    [{
+                        group: ['@microsoft/fast-*'],
+                        message: 'Do not directly use underlying libraries of nimble. Instead rely on or add to exports of nimble packages.'
+                    }, {
+                        group: ['@ni/nimble-components'],
+                        message: 'Client Angular applications should not have to directly depend on nimble-components.'
+                    }]
+                }
+            ],
+
+            // Recommended rules with strict null checks enabled: https://github.com/ni/javascript-styleguide/#strict-null-checks
+            '@typescript-eslint/no-non-null-assertion': 'off',
+            '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+            '@typescript-eslint/no-unnecessary-condition': 'error',
+            '@typescript-eslint/strict-boolean-expressions': ['error', {
+                allowNumber: false,
+                allowNullableBoolean: true,
+                allowNullableString: true,
+                allowNullableNumber: false
+            }]
+        }
     },
     {
         files: ['*.html'],
