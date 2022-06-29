9f3e267b6aae06dc8555687bd77ed40524a9e950
Dakeondrick <60270271+Dakeondrick@users.noreply.github.com>
2022-06-29 17:33:13 -0500

    review feedback

--- angular-workspace/projects/ni/nimble-angular/src/directives/select/nimble-select.directive.ts
+++ angular-workspace/projects/ni/nimble-angular/src/directives/select/nimble-select.directive.ts
@@ -1,5 +1,6 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 import type { Select } from '@ni/nimble-components/dist/esm/select';
+import { DropdownAppearance } from '@ni/nimble-components/dist/esm/patterns/dropdown/types';
 
 export type { Select };
 
@@ -10,4 +11,13 @@ export type { Select };
     selector: 'nimble-select',
 })
 export class NimbleSelectDirective {
+    public get appearance(): DropdownAppearance {
+        return this.elementRef.nativeElement.appearance;
+    }
+
+    @Input() public set appearance(value: DropdownAppearance) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'appearance', value);
+    }
+
+    public constructor(private readonly renderer: Renderer2, private readonly elementRef: ElementRef<Select>) {}
 }
--- packages/nimble-components/src/select/tests/select-matrix.stories.ts
+++ packages/nimble-components/src/select/tests/select-matrix.stories.ts
@@ -1,6 +1,7 @@
 import type { Story, Meta } from '@storybook/html';
 import { withXD } from 'storybook-addon-xd-designs';
 import { html, ViewTemplate } from '@microsoft/fast-element';
+import { pascalCase } from '@microsoft/fast-web-utilities';
 import {
     createMatrixThemeStory,
     createStory
@@ -12,6 +13,7 @@ import {
 import { disabledStates, DisabledState } from '../../utilities/tests/states';
 import { hiddenWrapper } from '../../utilities/tests/hidden';
 import '../../all-components';
+import { DropdownAppearance } from '../../patterns/dropdown/types';
 
 const metadata: Meta = {
     title: 'Tests/Select',
@@ -27,9 +29,21 @@ const metadata: Meta = {
 
 export default metadata;
 
+const appearanceStates = Object.entries(DropdownAppearance).map(
+    ([key, value]) => [pascalCase(key), value]
+);
+
+type AppearanceState = typeof appearanceStates[number];
+
 // prettier-ignore
-const component = ([_, disabled]: DisabledState): ViewTemplate => html`
-    <nimble-select ?disabled="${() => disabled}">
+const component = (
+    [_, disabled]: DisabledState,
+    [_appearanceName, appearance]: AppearanceState
+): ViewTemplate => html`
+    <nimble-select 
+        ?disabled="${() => disabled}"
+        appearance="${() => appearance}"
+    >
         <nimble-list-option value="1">Option 1</nimble-list-option>
         <nimble-list-option value="2" disabled>Option 2</nimble-list-option>
         <nimble-list-option value="3">Option 3</nimble-list-option>
@@ -38,7 +52,7 @@ const component = ([_, disabled]: DisabledState): ViewTemplate => html`
 `;
 
 export const selectThemeMatrix: Story = createMatrixThemeStory(
-    createMatrix(component, [disabledStates])
+    createMatrix(component, [disabledStates, appearanceStates])
 );
 
 export const hiddenSelect: Story = createStory(
