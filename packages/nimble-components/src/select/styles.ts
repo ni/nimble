6d873ae0a916c69972512b329fb52f2bf4fc4e8e
Dakeondrick <60270271+Dakeondrick@users.noreply.github.com>
2022-07-12 16:32:38 -0500

    formatting

--- packages/nimble-components/src/select/styles.ts
+++ packages/nimble-components/src/select/styles.ts
@@ -15,44 +15,44 @@ export const styles = css`
         appearanceBehavior(
             DropdownAppearance.underline,
             css`
-        .control {
-            padding-top: ${borderWidth};
-            padding-left: calc(${borderWidth} + ${standardPadding} / 2);
-            padding-right: ${borderWidth};
-        }
+            .control {
+                padding-top: ${borderWidth};
+                padding-left: calc(${borderWidth} + ${standardPadding} / 2);
+                padding-right: ${borderWidth};
+            }
 
-        :host([disabled]) .control {
-            border-color: rgba(${borderRgbPartialColor}, 0.1);
-        }
-    `
+            :host([disabled]) .control {
+                border-color: rgba(${borderRgbPartialColor}, 0.1);
+            }
+        `
         ),
         appearanceBehavior(
             DropdownAppearance.outline,
             css`
-        .control {
-            border: ${borderWidth} solid rgba(${borderRgbPartialColor}, 0.3);
-        }
+            .control {
+                border: ${borderWidth} solid rgba(${borderRgbPartialColor}, 0.3);
+            }
 
-        :host(.invalid) .errortext {
-            top: calc(${controlHeight} - ${borderWidth});
-        }
-    `
+            :host(.invalid) .errortext {
+                top: calc(${controlHeight} - ${borderWidth});
+            }
+        `
         ),
         appearanceBehavior(
             DropdownAppearance.block,
             css`
-        .control {
-            background-color: rgba(${borderRgbPartialColor}, 0.1);
-            padding-left: calc(${borderWidth} + ${standardPadding} / 2);
-            padding-right: ${borderWidth};
-            padding-bottom: calc(${borderWidth});
-            border-bottom: ${borderWidth}
-                rgba(${borderRgbPartialColor}, 0.07);
-        }
+            .control {
+                background-color: rgba(${borderRgbPartialColor}, 0.1);
+                padding-left: calc(${borderWidth} + ${standardPadding} / 2);
+                padding-right: ${borderWidth};
+                padding-bottom: calc(${borderWidth});
+                border-bottom: ${borderWidth}
+                    rgba(${borderRgbPartialColor}, 0.07);
+            }
 
-        :host([disabled]) .control {
-            background-color: rgba(${borderRgbPartialColor}, 0.07);
-        }
-    `
+            :host([disabled]) .control {
+                background-color: rgba(${borderRgbPartialColor}, 0.07);
+            }
+        `
         )
     );
