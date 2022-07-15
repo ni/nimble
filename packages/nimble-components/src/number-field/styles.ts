4859ea416f4952a1dfdc63157bf8c5c4c8c76293
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2022-07-15 11:36:23 -0500

    Fix weird jitter in number and text fields

--- packages/nimble-components/src/number-field/styles.ts
+++ packages/nimble-components/src/number-field/styles.ts
@@ -32,6 +32,9 @@ export const styles = css`
         color: ${bodyFontColor};
         height: calc(${labelHeight} + ${controlHeight});
         --ni-private-hover-indicator-width: calc(${borderWidth} + 1px);
+        --ni-private-height-within-border: calc(
+            ${controlHeight} - 2 * ${borderWidth}
+        );
     }
 
     :host([disabled]) {
@@ -58,6 +61,7 @@ export const styles = css`
         align-items: center;
         border-radius: 0px;
         border: 0px solid rgba(${borderRgbPartialColor}, 0.3);
+        padding: ${borderWidth};
     }
 
     .root:focus-within {
@@ -118,7 +122,7 @@ export const styles = css`
         font: inherit;
         background: transparent;
         color: inherit;
-        height: ${controlHeight};
+        height: var(--ni-private-height-within-border);
         width: 100%;
         border: none;
         padding: 0px;
@@ -141,15 +145,14 @@ export const styles = css`
         color: ${controlLabelFontColor};
     }
 
-    .control:focus-within::placeholder {
-        opacity: 1;
-    }
-
     .control[disabled]::placeholder {
         color: ${bodyDisabledFontColor};
     }
 
     .controls {
+        ${
+            /* We are using flex `order` to define the visual ordering of the inc/dec buttons and the invalid icon because they are not "interactive" i.e. part of the tab order */ ''
+        }
         display: contents;
     }
 
@@ -175,55 +178,44 @@ export const styles = css`
         padding-right: calc(${standardPadding} / 4);
     }
 `.withBehaviors(
-        appearanceBehavior(
-            NumberFieldAppearance.underline,
-            css`
+                appearanceBehavior(
+                    NumberFieldAppearance.underline,
+                    css`
             .root {
                 border-bottom-width: ${borderWidth};
-                padding-top: ${borderWidth};
-                padding-left: ${borderWidth};
-                padding-right: ${borderWidth};
-            }
-
-            .control {
-                height: calc(${controlHeight} - 2 * ${borderWidth});
+                padding-bottom: 0;
             }
         `
-        ),
-        appearanceBehavior(
-            NumberFieldAppearance.block,
-            css`
+                ),
+                appearanceBehavior(
+                    NumberFieldAppearance.block,
+                    css`
             .root {
                 background-color: rgba(${borderRgbPartialColor}, 0.1);
-                padding-left: ${borderWidth};
-                padding-right: ${borderWidth};
             }
 
             .root:focus-within,
             :host(.invalid) .root {
                 border-bottom-width: ${borderWidth};
+                padding-bottom: 0;
             }
 
-            .root:focus-within .control,
-            :host(.invalid) .control {
-                height: calc(${controlHeight} - ${borderWidth});
+            :host(:hover) .root {
+                padding-bottom: 0;
             }
 
             :host([disabled]) .root {
                 background-color: rgba(${borderRgbPartialColor}, 0.07);
             }
         `
-        ),
-        appearanceBehavior(
-            NumberFieldAppearance.outline,
-            css`
+                ),
+                appearanceBehavior(
+                    NumberFieldAppearance.outline,
+                    css`
             .root {
                 border-width: ${borderWidth};
-            }
-
-            .control {
-                height: calc(${controlHeight} - 2 * ${borderWidth});
+                padding: 0;
             }
         `
-        )
-    );
+                )
+            );
