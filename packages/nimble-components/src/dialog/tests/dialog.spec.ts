e80bb88998b004991cf6e0787d7ec833a9ccff8d
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2022-07-19 15:01:10 -0500

    Add first few tests

---
+++ packages/nimble-components/src/dialog/tests/dialog.spec.ts
@@ -0,0 +1,70 @@
+import {
+    DesignSystem
+} from '@microsoft/fast-foundation';
+import { DOM, html } from '@microsoft/fast-element';
+import { keyEscape } from '@microsoft/fast-web-utilities';
+import { fixture, Fixture } from '../../utilities/tests/fixture';
+import { Dialog } from '..';
+
+async function setup(
+    preventDismiss?: boolean
+): Promise<Fixture<Dialog>> {
+    const viewTemplate = html`
+        <nimble-dialog
+            ${preventDismiss ? 'prevent-dismiss' : ''}
+        >
+        </nimble-dialog>
+    `;
+    return fixture<Dialog>(viewTemplate);
+}
+
+fdescribe('Dialog', () => {
+    it('should have its tag returned by tagFor(Dialog)', () => {
+        expect(DesignSystem.tagFor(Dialog)).toBe('nimble-dialog');
+    });
+
+    it('can construct an element instance', () => {
+        expect(document.createElement('nimble-dialog')).toBeInstanceOf(
+            Dialog
+        );
+    });
+
+    it('should initially be hidden', async () => {
+        const { element, connect, disconnect } = await setup();
+        await connect();
+        expect(getComputedStyle(element.shadowRoot!.querySelector('dialog') as Element).display).toBe('none');
+        await disconnect();
+    });
+
+    it('should be displayed after calling showModal()', async () => {
+        const { element, connect, disconnect } = await setup();
+        await connect();
+        void element.showModal();
+        await DOM.nextUpdate();
+        expect(getComputedStyle(element.shadowRoot!.querySelector('dialog') as Element).display).toBe('block');
+        await disconnect();
+    });
+
+    it('should be hidden after calling close()', async () => {
+        const { element, connect, disconnect } = await setup();
+        await connect();
+        void element.showModal();
+        await DOM.nextUpdate();
+        element.close();
+        await DOM.nextUpdate();
+        expect(getComputedStyle(element.shadowRoot!.querySelector('dialog') as Element).display).toBe('none');
+        await disconnect();
+    });
+
+    it('should be dimissed by ESC', async () => {
+        const { element, connect, disconnect } = await setup();
+        await connect();
+        void element.showModal();
+        await DOM.nextUpdate();
+        const nativeElement = element.shadowRoot!.querySelector('dialog') as Element;
+        nativeElement.dispatchEvent(new KeyboardEvent('keypress', { key: keyEscape }));
+        await DOM.nextUpdate();
+        expect(getComputedStyle(nativeElement).display).toBe('none');
+        await disconnect();
+    });
+});
