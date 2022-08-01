904b8ce52a6163e087c400e08aa524c1df50125c
aidendk <103057880+aidendk@users.noreply.github.com>
2022-08-01 13:29:04 -0500

    updates 8/1

---
+++ packages/nimble-blazor/NimbleBlazor/Components/NimbleTooltip.razor.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components;
+
+namespace NimbleBlazor;
+
+public partial class NimbleTooltip : ComponentBase
+{
+    [Parameter]
+    public string? Anchor { get; set; }
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object>? AdditionalAttributes { get; set; }
+}
