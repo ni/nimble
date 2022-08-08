6718c1ea477392cc965425b86c4a4d8bb83e73f5
aidendk <103057880+aidendk@users.noreply.github.com>
2022-08-08 09:50:50 -0500

    html updates

--- packages/nimble-components/src/tooltip/tests/tooltip.stories.ts
+++ packages/nimble-components/src/tooltip/tests/tooltip.stories.ts
@@ -53,14 +53,14 @@ const metadata: Meta<TooltipArgs> = {
         </style>
         <div class="container">
             <div class="anchorDiv" id="anchor" aria-describedby="ariaAnchor">
-                Text, Button, Icon, etc.
+                Hover here to see tooltip
             </div>
             <nimble-tooltip
                 anchor="anchor"
                 ?visible="${x => x.visible}"
                 delay="${x => x.delay}"
                 auto-update-mode="${x => x.autoUpdateMode}"
-                id="ariaAnchor"
+                id="text-tooltip"
                 class="${x => TooltipStatus[x.state]} ${x => (x.icon ? 'icon-visible' : '')}"
             >
                 ${x => x.tooltip}
@@ -102,7 +102,7 @@ const metadata: Meta<TooltipArgs> = {
 export default metadata;
 
 export const tooltip: StoryObj<TooltipArgs> = {};
-export const highLevelTooltip: StoryObj<TooltipArgs> = {
+export const complexContentTooltip: StoryObj<TooltipArgs> = {
     render: createUserSelectedThemeStory(html<TooltipArgs>`
         <style>
             .container {
@@ -132,17 +132,25 @@ export const highLevelTooltip: StoryObj<TooltipArgs> = {
                 padding-bottom: 5px;
                 padding-top: 5px;
             }
+
+            .title {
+                margin-block: 0em;
+            }
         </style>
         <div class="container">
-            <div class="anchorDiv" id="anchor" aria-describedby="ariaAnchor">
-                High level tooltip
+            <div
+                class="anchorDiv"
+                id="complexAnchor"
+                aria-describedby="ariaAnchor"
+            >
+                Hover here to see complex content tooltip
             </div>
             <nimble-tooltip
-                anchor="anchor"
+                anchor="complexAnchor"
                 ?visible="${x => x.visible}"
                 delay="${x => x.delay}"
                 auto-update-mode="${x => x.autoUpdateMode}"
-                id="ariaAnchor"
+                id="complex-tooltip"
                 class="${x => TooltipStatus[x.state]} ${x => (x.icon ? 'icon-visible' : '')}"
             >
                 <div class="tooltipText">
@@ -151,30 +159,25 @@ export const highLevelTooltip: StoryObj<TooltipArgs> = {
                     <div class="grouping">
                         <div class="grouping-left">
                             <div class="section">
-                                <b class="title">Total Units</b>
-                                <br />
+                                <h4 class="title">Total Units</h4>
                                 <span>2800</span>
                             </div>
                             <div class="section">
-                                <b class="title">Total Good</b>
-                                <br />
+                                <h4 class="title">Total Good</h4>
                                 <span>2519</span>
                             </div>
                             <div class="section">
-                                <b class="title">Total Bad</b>
-                                <br />
+                                <h4 class="title">Total Bad</h4>
                                 <span>281</span>
                             </div>
                         </div>
                         <div class="grouping-right">
                             <div class="section">
-                                <b class="title">Yield</b>
-                                <br />
+                                <h4 class="title">Yield</h4>
                                 <span>89.99%</span>
                             </div>
                             <div class="section">
-                                <b class="title">Classification</b>
-                                <br />
+                                <h4 class="title">Classification</h4>
                                 <span>Bullseye</span>
                             </div>
                         </div>
