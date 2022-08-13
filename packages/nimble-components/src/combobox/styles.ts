1211de2be6dcbad7bb28d47a2b6ef29ba430c881
Jonathan Meyer <26874831+atmgrifter00@users.noreply.github.com>
2022-08-13 07:51:02 -0500

    Handle PR feedback.

--- packages/nimble-components/src/combobox/styles.ts
+++ packages/nimble-components/src/combobox/styles.ts
@@ -65,8 +65,7 @@ export const styles = css`
         color: inherit;
         margin: auto 0;
         width: 100%;
-        font-size: inherit;
-        font-style: inherit;
+        font: inherit;
         height: var(--ni-private-height-within-border);
     }
 
