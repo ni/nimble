a9f15fe9c1be5d611acd1919346f325f478803ab
mollykreis <20542556+mollykreis@users.noreply.github.com>
2022-09-08 12:55:34 -0500

    remove delay

--- packages/nimble-components/src/drawer/tests/drawer-matrix.stories.ts
+++ packages/nimble-components/src/drawer/tests/drawer-matrix.stories.ts
@@ -14,9 +14,6 @@ const metadata: Meta = {
         design: {
             artboardUrl:
                 'https://xd.adobe.com/view/33ffad4a-eb2c-4241-b8c5-ebfff1faf6f6-66ac/screen/730cdeb8-a4b5-4dcc-9fe4-718a75da7aff/specs/'
-        },
-        chromatic: {
-            delay: 1000
         }
     }
 };
