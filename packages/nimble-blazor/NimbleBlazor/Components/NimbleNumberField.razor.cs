51110de555a93c1e01ae41755423a7db747a0f38
Jonathan Meyer <26874831+atmgrifter00@users.noreply.github.com>
2022-09-08 13:23:27 -0500

    Remove APIs not exposed in Angular. Add docs.

--- packages/nimble-blazor/NimbleBlazor/Components/NimbleNumberField.razor.cs
+++ packages/nimble-blazor/NimbleBlazor/Components/NimbleNumberField.razor.cs
@@ -2,44 +2,65 @@
 
 namespace NimbleBlazor;
 
+/// <summary>
+/// The Blazor wrapper for the 'nimble-number-field' web component
+/// </summary>
 public partial class NimbleNumberField : NimbleInputBase<double?>
 {
+    /// <summary>
+    /// Gets or sets the disabled state of the number field.
+    /// </summary>
     [Parameter]
     public bool? Disabled { get; set; }
 
+    /// <summary>
+    /// Gets or sets the readonly state of the number field.
+    /// </summary>
     [Parameter]
     public bool? ReadOnly { get; set; }
 
-    [Parameter]
-    public bool? Required { get; set; }
-
-    [Parameter]
-    public bool? AutoFocus { get; set; }
-
-    [Parameter]
-    public int? Size { get; set; }
-
+    /// <summary>
+    /// Gets or sets the placeholder text of the number field.
+    /// </summary>
     [Parameter]
     public string? Placeholder { get; set; }
 
+    /// <summary>
+    /// Gets or sets the <see cref="NumberFieldAppearance"/> of the number field.
+    /// </summary>
     [Parameter]
     public NumberFieldAppearance? Appearance { get; set; }
 
-    [Parameter]
-    public int? MinLength { get; set; }
-
-    [Parameter]
-    public int? MaxLength { get; set; }
-
+    /// <summary>
+    /// Gets or sets the step value of the number field.
+    /// </summary>
+    /// <remarks>default is 1.0</remarks>
     [Parameter]
     public double Step { get; set; } = 1.0;
 
+    /// <summary>
+    /// Gets or sets the step value of the number field.
+    /// </summary>
+    /// <remarks>default is <see cref="double.MinValue"/></remarks>
     [Parameter]
     public double Min { get; set; } = double.MinValue;
 
+    /// <summary>
+    /// Gets or sets the step value of the number field.
+    /// </summary>
+    /// <remarks>default is <see cref="double.MaxValue"/></remarks>
     [Parameter]
     public double Max { get; set; } = double.MaxValue;
 
+    /// <summary>
+    /// Gets or sets the error text of the number field.
+    /// </summary>
+    [Parameter]
+    public string? ErrorText { get; set; }
+
+    /// <summary>
+    /// Gets or sets the child content to be rendered inside the combobox
+    /// </summary>
     [Parameter]
     public RenderFragment? ChildContent { get; set; }
 }
