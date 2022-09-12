1e46113d2de915d3f8a6201f4a7da6cb1bfaad10
mollykreis <20542556+mollykreis@users.noreply.github.com>
2022-09-12 10:18:54 -0500

    update comment

--- packages/nimble-components/src/patterns/dialog/constants.ts
+++ packages/nimble-components/src/patterns/dialog/constants.ts
@@ -1,6 +1,6 @@
 /**
- * Symbol that is returned as the dialog close reason (from the Promise returned by show()) when
- * the dialog was closed by pressing the ESC key (vs. calling the close() function).
+ * Symbol that is returned as the close reason (from the Promise returned by show()) when
+ * the dialog or drawer was closed by pressing the ESC key (vs. calling the close() function).
  */
 export const USER_DISMISSED: unique symbol = Symbol('user dismissed');
 export type UserDismissed = typeof USER_DISMISSED;
