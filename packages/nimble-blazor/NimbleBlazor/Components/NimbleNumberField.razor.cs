7d7cb6587d27bd3c05e20a94de01127ba8480112
Jonathan Meyer <jonathan.meyer@ni.com>
2022-09-12 16:56:09 -0500

    Blazor number field (#722)
    # Pull Request
    
    ## 🤨 Rationale
    
     Closes #526
    
    ## 👩‍💻 Implementation
    
    Standard Blazor integration. Limited API exposure to what is currently
    offered in Angular.
    
    ## 🧪 Testing
    
    Added standard unit tests.
    
    ## ✅ Checklist
    
    <!--- Review the list and put an x in the boxes that apply or ~~strike
    through~~ around items that don't (along with an explanation). -->
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

---
+++ packages/nimble-blazor/NimbleBlazor/Components/NimbleNumberField.razor.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NimbleBlazor;
+
+/// <summary>
+/// The Blazor wrapper for the 'nimble-number-field' web component
+/// </summary>
+public partial class NimbleNumberField : NimbleInputBase<double?>
+{
+    /// <summary>
+    /// Gets or sets the disabled state of the number field.
+    /// </summary>
+    [Parameter]
+    public bool? Disabled { get; set; }
+
+    /// <summary>
+    /// Gets or sets the readonly state of the number field.
+    /// </summary>
+    [Parameter]
+    public bool? ReadOnly { get; set; }
+
+    /// <summary>
+    /// Gets or sets the placeholder text of the number field.
+    /// </summary>
+    [Parameter]
+    public string? Placeholder { get; set; }
+
+    /// <summary>
+    /// Gets or sets the <see cref="NumberFieldAppearance"/> of the number field.
+    /// </summary>
+    [Parameter]
+    public NumberFieldAppearance? Appearance { get; set; }
+
+    /// <summary>
+    /// Gets or sets the step value of the number field.
+    /// </summary>
+    /// <remarks>default is 1.0</remarks>
+    [Parameter]
+    public double Step { get; set; } = 1.0;
+
+    /// <summary>
+    /// Gets or sets the min value of the number field.
+    /// </summary>
+    /// <remarks>default is <see cref="double.MinValue"/></remarks>
+    [Parameter]
+    public double Min { get; set; } = double.MinValue;
+
+    /// <summary>
+    /// Gets or sets the max value of the number field.
+    /// </summary>
+    /// <remarks>default is <see cref="double.MaxValue"/></remarks>
+    [Parameter]
+    public double Max { get; set; } = double.MaxValue;
+
+    /// <summary>
+    /// Gets or sets the error text of the number field.
+    /// </summary>
+    [Parameter]
+    public string? ErrorText { get; set; }
+
+    /// <summary>
+    /// Gets or sets the child content to be rendered inside the combobox
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+}
---
+++ packages/nimble-blazor/Tests/NimbleBlazor.Tests/Unit/Components/NimbleNumberFieldTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq.Expressions;
+using Bunit;
+using Xunit;
+
+namespace NimbleBlazor.Tests.Unit.Components;
+
+/// <summary>
+/// Tests for <see cref="NimbleTextField"/>
+/// </summary>
+public class NimbleNumberFieldTests
+{
+    [Fact]
+    public void NimbleNumberField_Rendered_HasNumberFieldMarkup()
+    {
+        var context = new TestContext();
+        context.JSInterop.Mode = JSRuntimeMode.Loose;
+        var expectedMarkup = "nimble-number-field";
+
+        var textField = context.RenderComponent<NimbleNumberField>();
+
+        Assert.Contains(expectedMarkup, textField.Markup);
+    }
+
+    [Theory]
+    [InlineData(NumberFieldAppearance.Outline, "outline")]
+    [InlineData(NumberFieldAppearance.Block, "block")]
+    [InlineData(NumberFieldAppearance.Underline, "underline")]
+    public void NumberFieldAppearance_AttributeIsSet(NumberFieldAppearance value, string expectedAttribute)
+    {
+        var numberField = RenderNimbleNumberFieldWithPropertySet(x => x.Appearance, value);
+
+        Assert.Contains(expectedAttribute, numberField.Markup);
+    }
+
+    [Fact]
+    public void NumberFieldStep_AttributeIsSet()
+    {
+        var numberField = RenderNimbleNumberFieldWithPropertySet(x => x.Step, 2.3);
+
+        Assert.Contains("step=\"2.3\"", numberField.Markup);
+    }
+
+    [Fact]
+    public void NumberFieldMin_AttributeIsSet()
+    {
+        var numberField = RenderNimbleNumberFieldWithPropertySet(x => x.Min, 2.3);
+
+        Assert.Contains("min=\"2.3\"", numberField.Markup);
+    }
+
+    [Fact]
+    public void NumberFieldMax_AttributeIsSet()
+    {
+        var numberField = RenderNimbleNumberFieldWithPropertySet(x => x.Max, 2.3);
+
+        Assert.Contains("max=\"2.3\"", numberField.Markup);
+    }
+
+    [Fact]
+    public void NumberFieldPlaceholder_AttributeIsSet()
+    {
+        var numberField = RenderNimbleNumberFieldWithPropertySet(x => x.Placeholder, "enter a value");
+
+        Assert.Contains("placeholder=\"enter a value\"", numberField.Markup);
+    }
+
+    [Fact]
+    public void NumberFieldErrorText_AttributeIsSet()
+    {
+        var numberField = RenderNimbleNumberFieldWithPropertySet(x => x.ErrorText, "bad number");
+
+        Assert.Contains("error-text=\"bad number\"", numberField.Markup);
+    }
+
+    [Fact]
+    public void NumberFieldDisabled_AttributeIsSet()
+    {
+        var numberField = RenderNimbleNumberFieldWithPropertySet(x => x.Disabled, true);
+
+        Assert.Contains("disabled", numberField.Markup);
+    }
+
+    [Fact]
+    public void NumberFieldReadOnly_AttributeIsSet()
+    {
+        var numberField = RenderNimbleNumberFieldWithPropertySet(x => x.ReadOnly, true);
+
+        Assert.Contains("readonly", numberField.Markup);
+    }
+
+    private IRenderedComponent<NimbleNumberField> RenderNimbleNumberFieldWithPropertySet<TProperty>(Expression<Func<NimbleNumberField, TProperty>> propertyGetter, TProperty propertyValue)
+    {
+        var context = new TestContext();
+        context.JSInterop.Mode = JSRuntimeMode.Loose;
+        return context.RenderComponent<NimbleNumberField>(p => p.Add(propertyGetter, propertyValue));
+    }
+}
