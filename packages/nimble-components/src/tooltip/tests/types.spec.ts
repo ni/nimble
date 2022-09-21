ca3148509cd47121b792e2eb0384caa393292330
rajsite <rajsite@users.noreply.github.com>
2022-09-21 12:44:09 -0500

    enum tests

--- packages/nimble-components/src/tooltip/tests/types.spec.ts
+++ packages/nimble-components/src/tooltip/tests/types.spec.ts
@@ -4,7 +4,6 @@ describe('Tooltip type', () => {
     it('TooltipSeverity fails compile if assigning arbitrary string values', () => {
         // @ts-expect-error This expect will fail if the enum-like type is missing "as const"
         const severity: TooltipSeverity = 'hello';
-        // @ts-expect-error This expect will fail if the enum-like type is missing "as const"
-        expect(severity).toEqual('hello');
+        expect(severity!).toEqual('hello');
     });
 });
