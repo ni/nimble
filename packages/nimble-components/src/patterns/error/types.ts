5c613b867b6b034ea4ae96e562be5070be38b353
Milan Raj <rajsite@users.noreply.github.com>
2022-09-22 15:30:19 -0500

    Update packages/nimble-components/src/patterns/error/types.ts
    Co-authored-by: Jesse Attas <jattasNI@users.noreply.github.com>

--- packages/nimble-components/src/patterns/error/types.ts
+++ packages/nimble-components/src/patterns/error/types.ts
@@ -1,5 +1,5 @@
 /**
- * This interface should be used by components that want to leverage the error pattern
+ * This interface should be used by components that want to leverage the error pattern. The error pattern conditionally adds error text to the template when an error is configured to be visible.
  */
 export interface ErrorPattern {
     /**
