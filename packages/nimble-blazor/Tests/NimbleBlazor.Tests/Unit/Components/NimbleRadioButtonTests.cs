662c1db1b6c39da6427f901069d4e6d65ba97f63
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2022-09-26 11:18:53 -0500

    Add RadioButton attribute tests

--- packages/nimble-blazor/Tests/NimbleBlazor.Tests/Unit/Components/NimbleRadioButtonTests.cs
+++ packages/nimble-blazor/Tests/NimbleBlazor.Tests/Unit/Components/NimbleRadioButtonTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq.Expressions;
 using Bunit;
 using Xunit;
 
@@ -18,5 +20,44 @@ public class NimbleRadioButtonTests
         var radioButton = context.RenderComponent<NimbleRadioButton>();
 
         Assert.Contains(expectedMarkup, radioButton.Markup);
+    }
+
+    [Fact]
+    public void NimbleRadioButtonCurrentValue_AttributeIsSet()
+    {
+        var radioButton = RenderNimbleRadioButtonWithPropertySet(x => x.CurrentValue, "foo");
+
+        Assert.Contains("current-value", radioButton.Markup);
+    }
+
+    [Fact]
+    public void NimbleRadioButtonDisabled_AttributeIsSet()
+    {
+        var radioButton = RenderNimbleRadioButtonWithPropertySet(x => x.Disabled, true);
+
+        Assert.Contains("disabled", radioButton.Markup);
+    }
+
+    [Fact]
+    public void NimbleRadioButtonName_AttributeIsSet()
+    {
+        var radioButton = RenderNimbleRadioButtonWithPropertySet(x => x.Name, "buttons");
+
+        Assert.Contains("name", radioButton.Markup);
+    }
+
+    [Fact]
+    public void NimbleRadioButtonChecked_AttributeIsSet()
+    {
+        var radioButton = RenderNimbleRadioButtonWithPropertySet(x => x.Checked, true);
+
+        Assert.Contains("checked", radioButton.Markup);
+    }
+
+    private IRenderedComponent<NimbleRadioButton> RenderNimbleRadioButtonWithPropertySet<TProperty>(Expression<Func<NimbleRadioButton, TProperty>> propertyGetter, TProperty propertyValue)
+    {
+        var context = new TestContext();
+        context.JSInterop.Mode = JSRuntimeMode.Loose;
+        return context.RenderComponent<NimbleRadioButton>(p => p.Add(propertyGetter, propertyValue));
     }
 }
