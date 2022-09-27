d51e129e8f82e28501da5312271752d567e58340
Jesse Attas <jattasNI@users.noreply.github.com>
2022-09-27 09:49:59 -0500

    Remove async from test

--- angular-workspace/projects/example-client-app/src/app/app.component.spec.ts
+++ angular-workspace/projects/example-client-app/src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
         });
     });
 
-    it('should create the app', async () => {
+    it('should create the app', () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
         expect(app).toBeTruthy();
