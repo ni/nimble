737c70d8f6ce4b26a8a6baf0b54ebfdd26710370
rajsite <rajsite@users.noreply.github.com>
2022-10-27 20:13:30 -0500

    whitespace

--- examples/vanilla/counter/finish/.eslintrc.cjs
+++ examples/vanilla/counter/finish/.eslintrc.cjs
@@ -6,8 +6,6 @@ module.exports = {
     ignorePatterns: ['*.js', '*.cjs'],
     parserOptions: {
         project: ['./tsconfig.json'],
-        // Resolve the project relative to this file's directory.
-        // https://stackoverflow.com/questions/64933543/parsing-error-cannot-read-file-tsconfig-json-eslint
         tsconfigRootDir: __dirname
     },
     rules: {
