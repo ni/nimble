1b33d43412aad2a893ccb4a8fccb3112a780f0f1
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2022-11-21 14:10:25 -0600

    Make separate stories for different appearances

--- packages/nimble-components/src/anchor/tests/anchor.stories.ts
+++ packages/nimble-components/src/anchor/tests/anchor.stories.ts
@@ -29,12 +29,7 @@ const metadata: Meta<AnchorArgs> = {
                 'https://xd.adobe.com/view/33ffad4a-eb2c-4241-b8c5-ebfff1faf6f6-66ac/screen/bfadf499-caf5-4ca0-9814-e777fbae0d46'
         },
         actions: {}
-    }
-};
-
-export default metadata;
-
-export const anchor: StoryObj<AnchorArgs> = {
+    },
     // prettier-ignore
     render: createUserSelectedThemeStory(html`
         Click on the <nimble-anchor
@@ -49,29 +44,86 @@ export const anchor: StoryObj<AnchorArgs> = {
             `)}
             ${x => x.label}
         </nimble-anchor> to navigate.
-`),
+    `),
     argTypes: {
+        href: {
+            description:
+                'In addition to `href`, all other attributes of `<a>` are also supported, e.g. `ping`, `target`, `type`, etc.'
+        },
         appearance: {
             options: Object.values(AnchorAppearance),
-            control: { type: 'radio' }
+            control: { type: 'radio' },
+            description:
+                'Use `inline-text` for anchors that are embedded within a block of text.'
         },
         appearanceVariant: {
             name: 'appearance-variant',
             options: Object.values(AnchorAppearanceVariant),
             control: { type: 'radio' },
+            description:
+                `
+\`prominent\` applies only to \`text\` and \`inline-text\` appearances. \`primary\` applies only to \`outline\` and \`block\`.
+
+This attribute has no effect on anchors with a \`ghost\` appearance.
+
+<details>
+    <summary>Prominent Usage</summary>
+    Make an anchor prominent to draw extra attention to it.
+</details>
+<details>
+    <summary>Primary Usage</summary>
+    Make a button primary to distinguish it visibly for one of the following reasons:
+    <ul>
+        <li>to indicate the action that allows the user to accomplish their most common or important goal</li>
+        <li>to indicate the action that allows the user to complete their task</li>
+    </ul>
+</details>`
         },
         icon: {
             description:
                 'When including an icon, set `slot="start"` on the icon to ensure proper styling.'
         },
+        contentHidden: {
+            name: 'content-hidden',
+        }
     },
     args: {
         label: 'link',
         href: 'http://nimble.ni.dev',
-        appearance: 'text',
+        appearance: AnchorAppearance.text,
         appearanceVariant: undefined,
         icon: false,
         contentHidden: false,
         disabled: false
+    }
+};
+
+export default metadata;
+
+export const textAnchor: StoryObj<AnchorArgs> = {
+    args: { appearance: AnchorAppearance.text }
+};
+
+export const inlineTextAnchor: StoryObj<AnchorArgs> = {
+    args: { appearance: AnchorAppearance.inlineText }
+};
+
+export const outlineAnchor: StoryObj<AnchorArgs> = {
+    args: { appearance: AnchorAppearance.outline }
+};
+
+export const ghostAnchor: StoryObj<AnchorArgs> = {
+    args: { appearance: AnchorAppearance.ghost }
+};
+
+export const blockAnchor: StoryObj<AnchorArgs> = {
+    args: { appearance: AnchorAppearance.block }
+};
+
+export const iconAnchor: StoryObj<AnchorArgs> = {
+    args: {
+        icon: true,
+        contentHidden: true,
+        appearance: AnchorAppearance.ghost
     }
 };
