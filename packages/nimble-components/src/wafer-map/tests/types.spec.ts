22ae01a4c7599bf6bf5e04d02387e4afd99b63ed
elizaolariu <eliza.olariu@ni.com>
2022-11-21 18:12:10 +0200

    deleted tests

--- packages/nimble-components/src/wafer-map/tests/types.spec.ts
+++ packages/nimble-components/src/wafer-map/tests/types.spec.ts
@@ -1,17 +1,9 @@
 import type {
-    WaferMapColorBy,
-    WaferMapDataType,
     WaferMapOrientation,
     WaferMapQuadrant
 } from '../types';
 
 describe('WaferMap type', () => {
-    it('WaferMapColorBy fails compile if assigning arbitrary string values', () => {
-        // @ts-expect-error This expect will fail if the enum-like type is missing "as const"
-        const value: WaferMapColorBy = 'hello';
-        expect(value).toEqual('hello');
-    });
-
     it('WaferMapQuadrant fails compile if assigning arbitrary string values', () => {
         // @ts-expect-error This expect will fail if the enum-like type is missing "as const"
         const value: WaferMapQuadrant = 'hello';
@@ -21,12 +13,6 @@ describe('WaferMap type', () => {
     it('WaferMapOrientation fails compile if assigning arbitrary string values', () => {
         // @ts-expect-error This expect will fail if the enum-like type is missing "as const"
         const value: WaferMapOrientation = 'hello';
-        expect(value).toEqual('hello');
-    });
-
-    it('WaferMapDataType fails compile if assigning arbitrary string values', () => {
-        // @ts-expect-error This expect will fail if the enum-like type is missing "as const"
-        const value: WaferMapDataType = 'hello';
         expect(value).toEqual('hello');
     });
 });
