3c2e8bdb9d5b2e56fde9af19a864310bdb6179e2
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2022-11-22 12:44:51 -0600

    Add endIcon to story and more descriptions

--- packages/nimble-components/src/anchor/tests/anchor.stories.ts
+++ packages/nimble-components/src/anchor/tests/anchor.stories.ts
@@ -11,9 +11,39 @@ interface AnchorArgs {
     appearance: string;
     appearanceVariant: string;
     icon: boolean;
+    endIcon: boolean;
     contentHidden: boolean;
     disabled: boolean;
 }
+
+const appearanceVariantDescription = `
+\`prominent\` applies only to \`text\` and \`inline-text\` appearances. \`primary\` applies only to \`outline\` and \`block\`.
+
+This attribute has no effect on anchors with a \`ghost\` appearance.
+
+<details>
+    <summary>Prominent Usage</summary>
+    Make an anchor prominent to draw extra attention to it.
+</details>
+<details>
+    <summary>Primary Usage</summary>
+    Make a button primary to distinguish it visibly for one of the following reasons:
+    <ul>
+        <li>to indicate the action that allows the user to accomplish their most common or important goal</li>
+        <li>to indicate the action that allows the user to complete their task</li>
+    </ul>
+</details>`;
+
+const endIconDescription = `
+When including an icon after the text content, set \`slot="end"\` on the icon to ensure proper styling.
+
+This icon will be hidden when \`content-hidden\` is set.
+.`;
+
+const contentHiddenDescription = `
+This attribute applies only to \`outline\`, \`ghost\`, and \`block\` appearances.
+
+When set, this attribute hides the text and end icon, leaving only the start icon visible.`;
 
 const metadata: Meta<AnchorArgs> = {
     title: 'Anchor',
@@ -40,7 +70,10 @@ const metadata: Meta<AnchorArgs> = {
             ?disabled=${x => x.disabled}
         >
             ${when(x => x.icon, html`
-                <nimble-icon-key slot="start"></nimble-icon-key>
+                <nimble-icon-link slot="start"></nimble-icon-link>
+            `)}
+            ${when(x => x.endIcon, html`
+                <nimble-icon-arrow-expander-right slot="end"></nimble-icon-arrow-expander-right>
             `)}
             ${x => x.label}
         </nimble-anchor> to navigate.
@@ -60,31 +93,18 @@ const metadata: Meta<AnchorArgs> = {
             name: 'appearance-variant',
             options: Object.values(AnchorAppearanceVariant),
             control: { type: 'radio' },
-            description:
-                `
-\`prominent\` applies only to \`text\` and \`inline-text\` appearances. \`primary\` applies only to \`outline\` and \`block\`.
-
-This attribute has no effect on anchors with a \`ghost\` appearance.
-
-<details>
-    <summary>Prominent Usage</summary>
-    Make an anchor prominent to draw extra attention to it.
-</details>
-<details>
-    <summary>Primary Usage</summary>
-    Make a button primary to distinguish it visibly for one of the following reasons:
-    <ul>
-        <li>to indicate the action that allows the user to accomplish their most common or important goal</li>
-        <li>to indicate the action that allows the user to complete their task</li>
-    </ul>
-</details>`
+            description: appearanceVariantDescription
         },
         icon: {
             description:
                 'When including an icon, set `slot="start"` on the icon to ensure proper styling.'
         },
+        endIcon: {
+            description: endIconDescription
+        },
         contentHidden: {
             name: 'content-hidden',
+            description: contentHiddenDescription
         }
     },
     args: {
@@ -93,6 +113,7 @@ This attribute has no effect on anchors with a \`ghost\` appearance.
         appearance: AnchorAppearance.text,
         appearanceVariant: undefined,
         icon: false,
+        endIcon: false,
         contentHidden: false,
         disabled: false
     }
