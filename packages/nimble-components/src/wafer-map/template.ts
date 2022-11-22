d1d5b8d83f4423a1bfe95c6ebc712ef0a6738d9b
Zoltan Szilagyi <62277788+zszilagy@users.noreply.github.com>
2022-11-22 19:37:04 +0200

    Commented lines for test

--- packages/nimble-components/src/wafer-map/template.ts
+++ packages/nimble-components/src/wafer-map/template.ts
@@ -1,17 +1,15 @@
 import { html } from '@microsoft/fast-element';
 import type { WaferMap } from '.';
-import { html } from '@microsoft/fast-element';
-import type { WaferMap } from '.';
 
-export const template = html<WaferMap>`
-    <div class="waferMapContainer wafermap-default"}>
-        <svg class="svgRoot" width="${x => x.offsetWidth}" heigth="${x => x.offsetHeight}">
-            <g class="zoomContainer">
-                <g transform="translate ${x => x.dataModule?.margin.left}, ${x => x.dataModule?.margin.top}"><g>
-            </g>
-        </svg>
-    </div>
-`;
+// export const template = html<WaferMap>`
+//     <div class="waferMapContainer wafermap-default"}>
+//         <svg class="svgRoot" width="${x => x.offsetWidth}" heigth="${x => x.offsetHeight}">
+//             <g class="zoomContainer">
+//                 <g transform="translate ${x => x.dataModule?.margin.left}, ${x => x.dataModule?.margin.top}"><g>
+//             </g>
+//         </svg>
+//     </div>
+// `;
 // prettier-ignore
 export const template = html<WaferMap>`
     <template>
