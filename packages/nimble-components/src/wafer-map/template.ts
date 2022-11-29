51b038304115290ccae960ddc64f029a49898cb5
DStavilaNI <denis.stavila@ni.com>
2022-11-29 17:30:47 +0200

    applied PR feedback

--- packages/nimble-components/src/wafer-map/template.ts
+++ packages/nimble-components/src/wafer-map/template.ts
@@ -2,6 +2,7 @@ import { html, repeat } from '@microsoft/fast-element';
 import type { WaferMap } from '.';
 import type { WaferMapDie } from './types';
 
+// prettier-ignore
 export const template = html<WaferMap>`
     <div>
         <ul>
