a4ce35df8efd5b806cc0335b30a2c4764d470a8c
mollykreis <20542556+mollykreis@users.noreply.github.com>
2022-12-09 16:49:37 -0600

    Basic TanStack integration with nimble-table (#902)
    # Pull Request
    
    ## 🤨 Rationale
    
    This PR is part of the effort for #840.
    
    This PR pulls in the TanStack dependency to nimble-components and
    updates the `nimble-table` to render basic data by leveraging TanStack.
    
    Things that are temporary in this PR:
    - Determining the set of columns by looking at the keys in the data
    - This will ultimately be determined by column definitions provided to
    the table (likely slotted as children of the table)
    - Turning the data into `string[][]` type for rendering
    - This will ultimately use view templates provided by the column
    definitions
    - The entire template, including styling
    - This will ultimately be updated to have additional `nimble-*` elements
    that make up the table. Some examples may be `nimble-column-header`,
    `nimble-row`, and `nimble-cell`.
    - The template will ultimately need to be setting appropriately ARIA
    attributes
    
    ## 👩‍💻 Implementation
    
    - Add a dependency on the latest version of TanStack
    - Use the TanStack APIs to create a table using the basic
    `getCoreRowModel()` function. Currently the only relevant information
    we're giving TanStack is the data, but as development continues on the
    table, the same patterns can be used for other information, such as
    sorting & grouping state.
    - Create an interface named `TableData` that describes the required type
    of `TData` passed to the table
    - Provide a property on the `nimble-table` that allows setting data,
    which is rendered in the table
    
    ## 🧪 Testing
    
    - Manually tested via storybook
    - Wrote unit tests, primarily focused around being able to update the
    data in the table
    - As much/all of the template is likely to change, I created a page
    object for the table in hopes that it will help us avoid having to
    continually update tests as we iteratively update the template
    - I had to update `karma.conf.js` to get TanStack to work in the unit
    tests
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.
    
    Co-authored-by: Jesse Attas <jattasNI@users.noreply.github.com>
    Co-authored-by: Milan Raj <rajsite@users.noreply.github.com>

--- packages/nimble-components/src/table/tests/table.stories.ts
+++ packages/nimble-components/src/table/tests/table.stories.ts
@@ -3,12 +3,36 @@ import type { Meta, StoryObj } from '@storybook/html';
 import { withXD } from 'storybook-addon-xd-designs';
 import { createUserSelectedThemeStory } from '../../utilities/tests/storybook';
 import '../../all-components';
+import { ExampleDataType } from './types';
 import { bodyFont } from '../../theme-provider/design-tokens';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface TableArgs {}
+interface TableArgs {
+    data: ExampleDataType;
+}
+
+const simpleData = [
+    {
+        myStr: 'my first row',
+        myNum: 5,
+        myBool: true
+    },
+    {
+        myStr: 'my second row',
+        myNum: 15,
+        myBool: false
+    }
+] as const;
+
+const dataSets = {
+    [ExampleDataType.simpleData]: simpleData
+} as const;
 
 const overviewText = 'The `nimble-table` is a component that offers a way to render tabular data in a variety of ways in each column.';
+
+const dataDescription = `\`data\` is a property that is an array of records. A record provides the data that backs a single row in the table.
+Each record is made up of fields, which are key/value pairs. The key in each pair must be of type \`string\`, which is defined by the type
+\`TableFieldName\`. The value in each pair must be of type \`string\`, \`number\`, \`boolean\`, \`Date\`, \`null\`, or \`undefined\`,
+which is defined by the type \`TableFieldValue\`.`;
 
 const metadata: Meta<TableArgs> = {
     title: 'Table',
@@ -33,7 +57,7 @@ const metadata: Meta<TableArgs> = {
             WARNING - The table is still in development and considered
             experimental. It is not recommended for application use.
         </div>
-        <nimble-table>
+        <nimble-table :data=${x => dataSets[x.data]}>
         </nimble-table>
         <style class="code-hide">
             #usage-warning {
@@ -42,7 +66,24 @@ const metadata: Meta<TableArgs> = {
             }
         </style>
     `),
-    args: {}
+    argTypes: {
+        data: {
+            description: dataDescription,
+            table: {
+                defaultValue: { summary: '[]' }
+            },
+            options: [ExampleDataType.simpleData],
+            control: {
+                type: 'radio',
+                labels: {
+                    [ExampleDataType.simpleData]: 'Simple data'
+                }
+            }
+        }
+    },
+    args: {
+        data: ExampleDataType.simpleData
+    }
 };
 
 export default metadata;
