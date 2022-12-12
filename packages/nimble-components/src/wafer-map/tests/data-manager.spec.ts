3715f739a5eb0183c4b244dbbc03c8a28a99a089
Natan Muntean <natan.muntean@ni.com>
2022-12-12 10:39:21 +0200

    changed tests descriptions and clarified naming

--- packages/nimble-components/src/wafer-map/tests/data-manager.spec.ts
+++ packages/nimble-components/src/wafer-map/tests/data-manager.spec.ts
@@ -94,24 +94,24 @@ describe('Data manager', () => {
         }
     });
 
-    it('should have all dies with no opacity from the highlighted list', () => {
+    it('should have all dies with full opacity from the highlighted list', () => {
         const highlightedValues = getHighlightedValues().map(
             value => value + dieLabelsSuffix
         );
-        const diesWithoutOpacity = dataManagerModule.diesRenderInfo.filter(x => x.fillStyle.endsWith(', 1)'));
-        for (const dieRenderInfo of diesWithoutOpacity) {
+        const diesWithFullOpacity = dataManagerModule.diesRenderInfo.filter(x => x.fillStyle.endsWith(',1)'));
+        for (const dieRenderInfo of diesWithFullOpacity) {
             expect(highlightedValues).toContain(dieRenderInfo.text);
         }
     });
 
-    it('should not have any dies with opacity from the highlighted list', () => {
+    it('should not have any dies with partial opacity from the highlighted list', () => {
         const highlightedValues = getHighlightedValues().map(
             value => value + dieLabelsSuffix
         );
-        const diesWithOpacity = dataManagerModule.diesRenderInfo.filter(
-            x => !x.fillStyle.endsWith(', 1)')
+        const diesWithPartialOpacity = dataManagerModule.diesRenderInfo.filter(
+            x => !x.fillStyle.endsWith(',1)')
         );
-        for (const dieRenderInfo of diesWithOpacity) {
+        for (const dieRenderInfo of diesWithPartialOpacity) {
             expect(highlightedValues).not.toContain(dieRenderInfo.text);
         }
     });
