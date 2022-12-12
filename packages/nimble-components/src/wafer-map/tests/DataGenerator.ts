a63d464d2e85503e7dfc998b2d7eda127d60f5ee
DStavilaNI <denis.stavila@ni.com>
2022-12-12 14:38:37 +0200

    Addressed linting issues

--- packages/nimble-components/src/wafer-map/tests/DataGenerator.ts
+++ packages/nimble-components/src/wafer-map/tests/DataGenerator.ts
@@ -1,52 +1,41 @@
-import type { WaferMapDie } from "../types"
-import type { IValueGenerator } from "./ValueGenerator";
+import type { WaferMapDie } from '../types';
+import type { IValueGenerator } from './ValueGenerator';
 
-const valueToString = (value:number) => {return value %1 ? value.toFixed(2):Math.trunc(value).toString()};
+const valueToString = (value: number): string => { return value % 1 ? value.toFixed(2) : Math.trunc(value).toString(); };
 
-export const  generateWaferData = (numDies:number, valueGenerator?: IValueGenerator)=>{
+export const generateWaferData = (numDies: number, valueGenerator?: IValueGenerator): WaferMapDie[] => {
+    const diesSet: WaferMapDie[] = [];
 
-    const diesSet:WaferMapDie[]=[];
-    let count:number = 0;
-
-    if(numDies>0){
-
-        //calculate the equivalent radius of a circle that would contain the <<<<numDies>>>> number of dies
-        const radius = Math.ceil(Math.sqrt(numDies/Math.PI));
+    if (numDies > 0) {
+        // calculate the equivalent radius of a circle that would contain the <<<<numDies>>>> number of dies
+        const radius = Math.ceil(Math.sqrt(numDies / Math.PI));
         const centerX = radius;
-        const centerY =radius;
+        const centerY = radius;
 
-        //Generate dies values
-        for (let i = centerY-radius; i < centerY+radius; i++) {
-            
-            let value:number;
-            let stringValue:string;
+        // Generate dies values
+        for (let i = centerY - radius; i < centerY + radius; i++) {
+            let value: number;
+            let stringValue: string;
 
-            for (let j = centerX; (j-centerX)*(j-centerX) + (i-centerY)*(i-centerY) <= radius*radius; j--) {
-            
-                if(valueGenerator!==undefined){
-                    value = valueGenerator(i,j);
-                } else value = (Math.random()*100);
-                
-                stringValue = valueToString(value);
-            
-                diesSet.push({x:i,y:j,value:stringValue});
+            for (let j = centerX; (j - centerX) * (j - centerX) + (i - centerY) * (i - centerY) <= radius * radius; j--) {
+                if (valueGenerator !== undefined) {
+                    value = valueGenerator(i, j);
+                } else value = (Math.random() * 100);
 
-                count++;
-            };
-            for (let j = centerX+1; (j-centerX)*(j-centerX) + (i-centerY)*(i-centerY) <= radius*radius; j++) {
-                
-                if(valueGenerator!==undefined){
-                    value = valueGenerator(i,j);
-                } else value = (Math.random()*100);
-                
                 stringValue = valueToString(value);
 
-                diesSet.push({x:i,y:j,value:stringValue});
+                diesSet.push({ x: i, y: j, value: stringValue });
+            }
+            for (let j = centerX + 1; (j - centerX) * (j - centerX) + (i - centerY) * (i - centerY) <= radius * radius; j++) {
+                if (valueGenerator !== undefined) {
+                    value = valueGenerator(i, j);
+                } else value = (Math.random() * 100);
 
-                count++;
-            };
-        };
+                stringValue = valueToString(value);
 
-    };
+                diesSet.push({ x: i, y: j, value: stringValue });
+            }
+        }
+    }
     return diesSet;
-}
+};
--- packages/nimble-components/src/wafer-map/tests/ValueGenerator.ts
+++ packages/nimble-components/src/wafer-map/tests/ValueGenerator.ts
@@ -1,15 +1,13 @@
-export interface IValueGenerator {
-    (x: number, y: number): number;
-}
+export type IValueGenerator = (x: number, y: number) => number;
 
-export const goodValueGenerator : IValueGenerator = ()=>{
+export const goodValueGenerator: IValueGenerator = () => {
     const odds = Math.random();
-    if(odds >= 0.25) return Math.random() * (100 - 90) + 90;
-    else return Math.random() * 50;
-}
+    if (odds >= 0.25) return Math.random() * (100 - 90) + 90;
+    return Math.random() * 50;
+};
 
-export const badValueGenerator : IValueGenerator = ()=>{
+export const badValueGenerator: IValueGenerator = () => {
     const odds = Math.random();
-    if(odds <= 0.25) return Math.random() * (100 - 90) + 90;
-    else return Math.random() * 50;
-}
+    if (odds <= 0.25) return Math.random() * (100 - 90) + 90;
+    return Math.random() * 50;
+};
