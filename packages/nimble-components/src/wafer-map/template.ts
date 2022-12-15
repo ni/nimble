413f6a2a67e3ea72ff10aa9a246c7be06fd9e9fa
Zoltan Szilagyi <62277788+zszilagy@users.noreply.github.com>
2022-12-15 21:00:47 +0200

    Re-worked wafer halo

--- packages/nimble-components/src/wafer-map/template.ts
+++ packages/nimble-components/src/wafer-map/template.ts
@@ -5,8 +5,11 @@ export const template = html<WaferMap>`
     <div class="wafer-map-container">
         <svg class="svg-root ${x => x.orientation}">
             <g class="zoom-container" ${ref('zoomContainer')}>               
-                <svg class="circle-base" version="1.1" x="0px" y="0px" viewbox="1 .45 20 21">
-                    <path class="circle-drawing-path" d="m 21 12 a 10 10 330 1 1 0 -1.98 a 1 1 0 0 0 0 2"/>
+                <!-- <svg class="circle-base" version="1.1" x="0px" y="0px" viewbox="0 0 22.07 22.05">
+                    <path class="circle-drawing-path" d="m 22 11.55 a 11 11 330 1 1 0 -1 a 0.5 0.5 0 0 0 0 1"/>
+                </svg> -->
+                <svg class="circle-base" version="1.1" x="0px" y="0px" viewbox="0 0 40 40">
+                    <path class="circle-drawing-path" d="m 40 20.5 a 20 20 330 1 1 0 -1 a 0.5 0.5 0 0 0 0 1"/>
                 </svg>
             </g>
         </svg>
