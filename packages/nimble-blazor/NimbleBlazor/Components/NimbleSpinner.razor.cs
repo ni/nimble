b29888a67848eeeb275d37116f95ec85cc30e8ff
Malcolm Smith <20709258+msmithNI@users.noreply.github.com>
2023-01-06 15:42:02 -0600

    Blazor integration

--- packages/nimble-blazor/NimbleBlazor/Components/NimbleSpinner.razor.cs
+++ packages/nimble-blazor/NimbleBlazor/Components/NimbleSpinner.razor.cs
@@ -4,6 +4,15 @@ namespace NimbleBlazor;
 
 public partial class NimbleSpinner : ComponentBase
 {
+    /// <summary>
+    /// The size of the spinner.
+    /// </summary>
+    [Parameter]
+    public SpinnerSize? Size { get; set; }
+
+    /// <summary>
+    /// Any additional attributes that did not match known properties.
+    /// </summary>
     [Parameter(CaptureUnmatchedValues = true)]
     public IDictionary<string, object>? AdditionalAttributes { get; set; }
 }
---
+++ packages/nimble-blazor/NimbleBlazor/SpinnerSize.cs
@@ -0,0 +1,15 @@
+﻿namespace NimbleBlazor;
+
+public enum SpinnerSize
+{
+    Default,
+    Medium,
+    Large
+}
+
+internal static class SpinnerSizeExtensions
+{
+    private static readonly Dictionary<SpinnerSize, string> _enumValues = AttributeHelpers.GetEnumNamesAsKebabCaseValues<SpinnerSize>();
+
+    public static string? ToAttributeValue(this SpinnerSize? value) => (value == null || value == SpinnerSize.Default) ? null : _enumValues[value.Value];
+}
