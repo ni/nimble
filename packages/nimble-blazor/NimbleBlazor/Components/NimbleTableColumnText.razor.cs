dad19dfd6a7e7f01144b1332724c0a8214a8c105
Jonathan Meyer <26874831+atmgrifter00@users.noreply.github.com>
2023-01-09 11:27:54 -0600

    Adding documentation.

--- packages/nimble-blazor/NimbleBlazor/Components/NimbleTableColumnText.razor.cs
+++ packages/nimble-blazor/NimbleBlazor/Components/NimbleTableColumnText.razor.cs
@@ -5,10 +5,16 @@ namespace NimbleBlazor;
 
 public partial class NimbleTableColumnText : ComponentBase
 {
+    /// <summary>
+    /// Gets or sets the field in the element representing a row of data in a <see cref="NimbleTable{TData}"/>to display
+    /// </summary>
     [Parameter]
     [DisallowNull]
     public string FieldName { get; set; } = null!;
 
+    /// <summary>
+    /// The text to show when no value is available for a particular cell in the column of a <see cref="NimbleTable{TData}"/>
+    /// </summary>
     [Parameter]
     public string? Placeholder { get; set; }
 
