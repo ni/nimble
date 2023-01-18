04257fe9e71f776407b52afafda6d52ca1c3bebf
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-01-18 13:12:58 -0600

    Anchor tabs files

---
+++ angular-workspace/projects/ni/nimble-angular/src/directives/anchor-tabs/nimble-anchor-tabs.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+import type { AnchorTabs } from '@ni/nimble-components/dist/esm/anchor-tabs';
+
+export type { AnchorTabs };
+
+/**
+ * Directive to provide Angular integration for the anchor tabs element.
+ */
+@Directive({
+    selector: 'nimble-anchor-tabs'
+})
+export class NimbleAnchorTabsDirective {
+    public get activeid(): string {
+        return this.elementRef.nativeElement.activeid;
+    }
+
+    @Input() public set activeid(value: string) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'activeid', value);
+    }
+
+    public get activetab(): HTMLElement | undefined {
+        return this.elementRef.nativeElement.activetab;
+    }
+
+    public constructor(private readonly renderer: Renderer2, private readonly elementRef: ElementRef<AnchorTabs>) {}
+}
