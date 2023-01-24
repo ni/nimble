0548a2a07aee2461e94d22ebb0f91a817f2ae088
m-akinc <7282195+m-akinc@users.noreply.github.com>
2023-01-24 16:39:46 -0600

    Anchor tabs Angular integration (#987)
    ## 🤨 Rationale
    
    Provide Angular integration for anchor tabs.
    
    ## 👩‍💻 Implementation
    
    - Directives for both `nimble-anchor-tabs` and `nimble-anchor-tab`
    - Unit tests
    - Added to example app
    - Updated README
    
    ## 🧪 Testing
    
    Unit tests pass and exercised in example app
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

---
+++ angular-workspace/projects/ni/nimble-angular/src/directives/anchor-tab/nimble-anchor-tab.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+import type { AnchorTab } from '@ni/nimble-components/dist/esm/anchor-tab';
+import { NimbleAnchorBaseDirective } from '../anchor-base/nimble-anchor-base.directive';
+import { BooleanValueOrAttribute, toBooleanProperty } from '../utilities/template-value-helpers';
+
+export type { AnchorTab };
+
+/**
+ * Directive to provide Angular integration for the anchor tab.
+ */
+@Directive({
+    selector: 'nimble-anchor-tab'
+})
+export class NimbleAnchorTabDirective extends NimbleAnchorBaseDirective<AnchorTab> {
+    public get disabled(): boolean {
+        return this.elementRef.nativeElement.disabled;
+    }
+
+    @Input() public set disabled(value: BooleanValueOrAttribute) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'disabled', toBooleanProperty(value));
+    }
+
+    public constructor(renderer: Renderer2, elementRef: ElementRef<AnchorTab>) {
+        super(renderer, elementRef);
+    }
+}
