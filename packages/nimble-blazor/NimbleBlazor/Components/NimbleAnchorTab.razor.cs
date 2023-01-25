0671e34c7650008aa063fa0145cee62d4a981dd7
m-akinc <7282195+m-akinc@users.noreply.github.com>
2023-01-25 11:24:46 -0600

    Anchor tabs Blazor support (#989)
    ## 🤨 Rationale
    
    Blazor support for anchor tabs component
    
    ## 👩‍💻 Implementation
    
    - Adds components NimbleAnchorTabs and NimbleAnchorTab
    - A few tests
    - Add to example app
    - Update README
    
    ## 🧪 Testing
    
    Ran tests and exercised example app.
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

---
+++ packages/nimble-blazor/NimbleBlazor/Components/NimbleAnchorTab.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NimbleBlazor;
+
+/// <summary>
+/// A link styled as a tab
+/// </summary>
+public partial class NimbleAnchorTab : ComponentBase
+{
+    /// <summary>
+    /// The child content of the element.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// <summary>
+    /// Whether the tab is disabled (not selectable)
+    /// </summary>
+    [Parameter]
+    public bool? Disabled { get; set; }
+
+    /// <summary>
+    /// Any additional attributes that did not match known properties.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object>? AdditionalAttributes { get; set; }
+}
