a00e17ae5bf12cd771d3fb345c81a55f7e1040cc
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-01-30 10:59:27 -0600

    Create matrix stories

---
+++ packages/nimble-components/src/banner/tests/banner-matrix.stories.ts
@@ -0,0 +1,84 @@
+import type { Story, Meta } from '@storybook/html';
+import { withXD } from 'storybook-addon-xd-designs';
+import { html, ViewTemplate, when } from '@microsoft/fast-element';
+import {
+    createMatrixThemeStory,
+    createStory
+} from '../../utilities/tests/storybook';
+import {
+    createMatrix,
+    sharedMatrixParameters
+} from '../../utilities/tests/matrix';
+import { hiddenWrapper } from '../../utilities/tests/hidden';
+import '../../all-components';
+
+const metadata: Meta = {
+    title: 'Tests/Banner',
+    decorators: [withXD],
+    parameters: {
+        ...sharedMatrixParameters(),
+        design: {
+            artboardUrl:
+                'https://xd.adobe.com/view/33ffad4a-eb2c-4241-b8c5-ebfff1faf6f6-66ac/screen/29c405f7-08ea-48b6-973f-546970b9dbab/specs/'
+        }
+    }
+};
+
+export default metadata;
+
+const severityStates = [
+    ['Neutral', undefined],
+    ['Error', 'error'],
+    ['Warning', 'warning'],
+    ['Info', 'info']
+] as const;
+type SeverityState = typeof severityStates[number];
+
+const actionStates = [
+    ['', false, undefined],
+    ['link', true, undefined],
+    ['ghost button', false, 'ghost'],
+    ['outline button', false, 'outline']
+] as const;
+type ActionState = typeof actionStates[number];
+
+const preventDismissStates = [
+    ['', false],
+    ['prevent dismiss', true]
+] as const;
+type PreventDismissState = typeof preventDismissStates[number];
+
+// prettier-ignore
+const component = (
+    [severityLabel, severity]: SeverityState,
+    [actionLabel, linkVisible, buttonAppearance]: ActionState,
+    [preventDismissLabel, preventDismiss]: PreventDismissState
+): ViewTemplate => html`
+    <nimble-banner
+        open
+        severity="${severity!}"
+        prevent-dismiss="${preventDismiss}"
+    >
+        <span slot="title">${severityLabel} ${actionLabel} ${preventDismissLabel}</span>
+        This is message text.
+        ${when(() => linkVisible, html`
+            <nimble-anchor slot="action" href="#">Nimble anchor</nimble-anchor>
+        `)}
+        ${when(() => buttonAppearance, html`
+            <nimble-button slot="action" appearance="${buttonAppearance!}">Nimble Button</nimble-button>
+        `)}
+    </nimble-banner>
+    <div style="height: 1px"></div>
+`;
+
+export const bannerThemeMatrix: Story = createMatrixThemeStory(
+    createMatrix(component, [severityStates, actionStates, preventDismissStates])
+);
+
+export const hiddenBanner: Story = createStory(
+    hiddenWrapper(
+        html`<nimble-banner hidden>
+            <span slot="title">Hidden banner</span>
+        </nimble-banner>`
+    )
+);
