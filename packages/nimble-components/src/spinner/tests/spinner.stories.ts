ffac90b4a360cc61f01f257078af37dbd97df721
Fred Visser <1458528+fredvisser@users.noreply.github.com>
2023-02-02 15:37:36 -0600

    add comment for test

--- packages/nimble-components/src/spinner/tests/spinner.stories.ts
+++ packages/nimble-components/src/spinner/tests/spinner.stories.ts
@@ -40,7 +40,7 @@ const metadata: Meta<SpinnerArgs> = {
             artboardUrl:
                 'https://xd.adobe.com/view/33ffad4a-eb2c-4241-b8c5-ebfff1faf6f6-66ac/screen/dece308f-79e7-48ec-ab41-011f3376b49b/specs/'
         },
-        chromatic: { pauseAnimationAtEnd: true }
+        chromatic: { pauseAnimationAtEnd: true } // test change
     },
     argTypes: {
         size: {
