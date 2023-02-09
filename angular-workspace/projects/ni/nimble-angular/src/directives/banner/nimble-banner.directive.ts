8ee508a24141fd8e50ba14708e8fb55f46329594
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-02-09 16:01:09 -0600

    Angular wrapper for banner

---
+++ angular-workspace/projects/ni/nimble-angular/src/directives/banner/nimble-banner.directive.ts
@@ -0,0 +1,62 @@
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+import type { Banner } from '@ni/nimble-components/dist/esm/banner';
+import type { BannerSeverity } from '@ni/nimble-components/dist/esm/banner/types';
+import { BooleanValueOrAttribute, toBooleanProperty } from '../utilities/template-value-helpers';
+
+export type { Banner };
+
+/**
+ * Directive to provide Angular integration for the banner.
+ */
+@Directive({
+    selector: 'nimble-banner'
+})
+export class NimbleBannerDirective {
+    public get open(): boolean {
+        return this.elementRef.nativeElement.open;
+    }
+
+    @Input() public set open(value: BooleanValueOrAttribute) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'open', toBooleanProperty(value));
+    }
+
+    public get severity(): BannerSeverity {
+        return this.elementRef.nativeElement.severity;
+    }
+
+    @Input() public set severity(value: BannerSeverity) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'severity', value);
+    }
+
+    public get titleHidden(): boolean {
+        return this.elementRef.nativeElement.titleHidden;
+    }
+
+    // Renaming because property should have camel casing, but attribute should not
+    // eslint-disable-next-line @angular-eslint/no-input-rename
+    @Input('title-hidden') public set titleHidden(value: BooleanValueOrAttribute) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'titleHidden', toBooleanProperty(value));
+    }
+
+    public get preventDismiss(): boolean {
+        return this.elementRef.nativeElement.preventDismiss;
+    }
+
+    // Renaming because property should have camel casing, but attribute should not
+    // eslint-disable-next-line @angular-eslint/no-input-rename
+    @Input('prevent-dismiss') public set preventDismiss(value: BooleanValueOrAttribute) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'preventDismiss', toBooleanProperty(value));
+    }
+
+    public get dismissButtonLabel(): string | undefined {
+        return this.elementRef.nativeElement.dismissButtonLabel;
+    }
+
+    // Renaming because property should have camel casing, but attribute should not
+    // eslint-disable-next-line @angular-eslint/no-input-rename
+    @Input('dismiss-button-label') public set dismissButtonLabel(value: string | undefined) {
+        this.renderer.setProperty(this.elementRef.nativeElement, 'dismissButtonLabel', value);
+    }
+
+    public constructor(private readonly renderer: Renderer2, private readonly elementRef: ElementRef<Banner>) {}
+}
