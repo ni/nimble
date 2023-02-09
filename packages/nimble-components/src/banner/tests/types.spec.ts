a37712695a01da0c98894cca340e674fb2340418
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-02-09 11:03:30 -0600

    Fix build error

--- packages/nimble-components/src/banner/tests/types.spec.ts
+++ packages/nimble-components/src/banner/tests/types.spec.ts
@@ -4,6 +4,6 @@ describe('Banner type', () => {
     it('BannerSeverity fails compile if assigning arbitrary string values', () => {
         // @ts-expect-error This expect will fail if the enum-like type is missing "as const"
         const severity: BannerSeverity = 'hello';
-        expect(severity).toEqual('hello');
+        expect(severity!).toEqual('hello');
     });
 });
