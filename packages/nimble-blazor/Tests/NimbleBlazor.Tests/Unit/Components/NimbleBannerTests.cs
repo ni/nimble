8db8e7d92a402747f5e5db213cb3c4881e3b428f
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-02-10 11:25:07 -0600

    Add tests

---
+++ packages/nimble-blazor/Tests/NimbleBlazor.Tests/Unit/Components/NimbleBannerTests.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq.Expressions;
+using Bunit;
+using Xunit;
+
+namespace NimbleBlazor.Tests.Unit.Components;
+
+/// <summary>
+/// Tests for <see cref="NimbleBanner"/>.
+/// </summary>
+public class NimbleBannerTests
+{
+    [Fact]
+    public void NimbleBanner_Render_HasBannerMarkup()
+    {
+        var context = new TestContext();
+        context.JSInterop.Mode = JSRuntimeMode.Loose;
+        var expectedMarkup = "nimble-banner";
+
+        var banner = context.RenderComponent<NimbleBanner>();
+
+        Assert.Contains(expectedMarkup, banner.Markup);
+    }
+
+    [Theory]
+    [InlineData(BannerSeverity.Default, "<nimble-banner>")]
+    [InlineData(BannerSeverity.Error, "severity=\"error\"")]
+    [InlineData(BannerSeverity.Warning, "severity=\"warning\"")]
+    [InlineData(BannerSeverity.Information, "severity=\"info\"")]
+    public void BannerSeverity_AttributeIsSet(BannerSeverity value, string expectedAttribute)
+    {
+        var banner = RenderWithPropertySet(x => x.Severity, value);
+
+        Assert.Contains(expectedAttribute, banner.Markup);
+    }
+
+    private IRenderedComponent<NimbleBanner> RenderWithPropertySet<TProperty>(Expression<Func<NimbleBanner, TProperty>> propertyGetter, TProperty propertyValue)
+    {
+        var context = new TestContext();
+        context.JSInterop.Mode = JSRuntimeMode.Loose;
+        return context.RenderComponent<NimbleBanner>(p => p.Add(propertyGetter, propertyValue));
+    }
+}
