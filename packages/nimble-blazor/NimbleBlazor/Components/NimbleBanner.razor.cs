9ae72139a7242e0b3ab9a0f4c1f2632506ab5aaa
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-02-10 11:06:14 -0600

    Blazor wrapper for banner

---
+++ packages/nimble-blazor/NimbleBlazor/Components/NimbleBanner.razor.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NimbleBlazor;
+
+
+/// <summary>
+/// A banner component for displaying persistent messages
+/// </summary>
+public partial class NimbleBanner : ComponentBase
+{
+    /// <summary>
+    /// Whether the banner is displayed or not
+    /// </summary>
+    [Parameter]
+    public bool? Open { get; set; }
+
+    /// <summary>
+    /// The severity of the message being displayed
+    /// </summary>
+    [Parameter]
+    public BannerSeverity? Severity { get; set; }
+
+    /// <summary>
+    /// Whether the banner title is displayed or not
+    /// </summary>
+    [Parameter]
+    public bool? TitleHidden { get; set; }
+
+    /// <summary>
+    /// Whether the dismiss button is present or not
+    /// </summary>
+    [Parameter]
+    public bool? PreventDismiss { get; set; }
+
+    /// <summary>
+    /// The (hidden) label applied to the dismiss buton (for accessibility purposes)
+    /// </summary>
+    [Parameter]
+    public string? DismissButtonLabel { get; set; }
+
+    /// <summary>
+    /// The child content of the element.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// <summary>
+    /// Any additional attributes that did not match known properties.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object>? AdditionalAttributes { get; set; }
+}
