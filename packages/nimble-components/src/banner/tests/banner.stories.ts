b1d61179c37584a136fdeaf1f23e34b507261f97
m-akinc <7282195+m-akinc@users.noreply.github.com>
2023-02-13 10:03:32 -0600

    Update packages/nimble-components/src/banner/tests/banner.stories.ts
    Co-authored-by: mollykreis <20542556+mollykreis@users.noreply.github.com>

--- packages/nimble-components/src/banner/tests/banner.stories.ts
+++ packages/nimble-components/src/banner/tests/banner.stories.ts
@@ -46,7 +46,7 @@ const metadata: Meta<BannerArgs> = {
         },
         design: {
             artboardUrl:
-                'https://xd.adobe.com/view/33ffad4a-eb2c-4241-b8c5-ebfff1faf6f6-66ac/screen/7b53bb3e-439b-4f13-9d5f-55adc7da8a2e/specs/'
+                'https://xd.adobe.com/view/33ffad4a-eb2c-4241-b8c5-ebfff1faf6f6-66ac/screen/29c405f7-08ea-48b6-973f-546970b9dbab/specs/'
         },
         actions: {
             handles: ['toggle']
