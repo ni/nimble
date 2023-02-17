8c748c8f5e1b67bb7f2c984f1a91c14f967108c8
m-akinc <7282195+m-akinc@users.noreply.github.com>
2023-02-17 15:10:25 -0600

    Define type for banner toggle event detail (#1065)
    ## 🤨 Rationale
    
    Should define explict type for banner's `toggle` event details. We want
    to re-export this from `nimble-angular`.
    
    ## 👩‍💻 Implementation
    
    Defined new type. Followed example of the menu button's identical event
    details.
    
    ## 🧪 Testing
    
    None
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

--- packages/nimble-components/src/banner/types.ts
+++ packages/nimble-components/src/banner/types.ts
@@ -11,3 +11,12 @@ export const BannerSeverity = {
 
 export type BannerSeverity =
     (typeof BannerSeverity)[keyof typeof BannerSeverity];
+
+/**
+ * The type of the detail associated with the `toggle`
+ * event on the banner.
+ */
+export interface BannerToggleEventDetail {
+    newState: boolean;
+    oldState: boolean;
+}
