8fcdeb884b9be29b92b4e273991fbc96f077d744
mollykreis <20542556+mollykreis@users.noreply.github.com>
2023-02-21 14:15:07 -0600

    Allow table columns to be marked as hidden (#1070)
    # Pull Request
    
    ## 🤨 Rationale
    
    We need to be able to mark columns in a table has hidden so that the
    table can use them for operations, such as sorting, but have those
    hidden columns not be included when the table is rendered.
    
    This is required as part of #874 
    
    ## 👩‍💻 Implementation
    
    - Add a boolean `column-hidden` attribute to the base column class
    - Don't render header or cells for hidden columns
    - Add Blazor support for hidden columns
    - Add Angular support for hidden columns
    
    ## 🧪 Testing
    
    - Manually tested in storybook, Angular, and Blazor
    - Wrote new unit tests
    - Added a hidden column to the table matrix tests
    
    ## ✅ Checklist
    
    <!--- Review the list and put an x in the boxes that apply or ~~strike
    through~~ around items that don't (along with an explanation). -->
    
    - [ ] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

--- packages/nimble-components/src/table/components/row/template.ts
+++ packages/nimble-components/src/table/components/row/template.ts
@@ -8,24 +8,26 @@ import { TableCell } from '../cell';
 export const template = html<TableRow>`
     <template role="row">
         ${repeat(x => x.columnStates, html<ColumnState, TableRow>`
-            <${DesignSystem.tagFor(TableCell)}
-                class="cell"
-                :cellTemplate="${x => x.column.cellTemplate}"
-                :cellStyles="${x => x.column.cellStyles}"
-                :cellState="${x => x.cellState}"
-                ?has-action-menu="${x => !!x.column.actionMenuSlot}"
-                action-menu-label="${x => x.column.actionMenuLabel}"
-                @cell-action-menu-beforetoggle="${(x, c) => c.parent.onCellActionMenuBeforeToggle(c.event as CustomEvent<MenuButtonToggleEventDetail>, x.column)}"
-                @cell-action-menu-toggle="${(x, c) => c.parent.onCellActionMenuToggle(c.event as CustomEvent<MenuButtonToggleEventDetail>, x.column)}"
-            >
+            ${when(x => !x.column.columnHidden, html<ColumnState, TableRow>`
+                <${DesignSystem.tagFor(TableCell)}
+                    class="cell"
+                    :cellTemplate="${x => x.column.cellTemplate}"
+                    :cellStyles="${x => x.column.cellStyles}"
+                    :cellState="${x => x.cellState}"
+                    ?has-action-menu="${x => !!x.column.actionMenuSlot}"
+                    action-menu-label="${x => x.column.actionMenuLabel}"
+                    @cell-action-menu-beforetoggle="${(x, c) => c.parent.onCellActionMenuBeforeToggle(c.event as CustomEvent<MenuButtonToggleEventDetail>, x.column)}"
+                    @cell-action-menu-toggle="${(x, c) => c.parent.onCellActionMenuToggle(c.event as CustomEvent<MenuButtonToggleEventDetail>, x.column)}"
+                >
 
-                ${when((x, c) => ((c.parent as TableRow).currentActionMenuColumn === x.column) && x.column.actionMenuSlot, html<ColumnState, TableRow>`
-                    <slot
-                        name="${x => `row-action-menu-${x.column.actionMenuSlot!}`}"
-                        slot="cellActionMenu"
-                    ></slot>
-                `)}
-            </${DesignSystem.tagFor(TableCell)}>
+                    ${when((x, c) => ((c.parent as TableRow).currentActionMenuColumn === x.column) && x.column.actionMenuSlot, html<ColumnState, TableRow>`
+                        <slot
+                            name="${x => `row-action-menu-${x.column.actionMenuSlot!}`}"
+                            slot="cellActionMenu"
+                        ></slot>
+                    `)}
+                </${DesignSystem.tagFor(TableCell)}>
+            `)}
         `)}
     </template>
 `;
