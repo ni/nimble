0db0b289901763d54b30805777af5c3ace5ee5f7
mollykreis <20542556+mollykreis@users.noreply.github.com>
2023-02-21 16:07:06 -0600

    fix table types tests

--- packages/nimble-components/src/table/tests/types.spec.ts
+++ packages/nimble-components/src/table/tests/types.spec.ts
@@ -7,7 +7,7 @@ describe('Table type', () => {
     it('TableColumnSortDirection fails compile if assigning arbitrary string values', () => {
         // @ts-expect-error This expect will fail if the enum-like type is missing "as const"
         const sortDirection: TableColumnSortDirection = 'hello';
-        expect(sortDirection).toEqual('hello');
+        expect(sortDirection!).toEqual('hello');
     });
 
     it('TableColumnSortOperation fails compile if assigning arbitrary string values', () => {
