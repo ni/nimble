438e2640649c40f55a6bff17632146bb6b78ac4b
rajsite <rajsite@users.noreply.github.com>
2023-02-22 19:14:27 -0600

    Disable theme-provider in angular app temporarily

--- angular-workspace/projects/example-client-app/src/app/app.module.ts
+++ angular-workspace/projects/example-client-app/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { NimbleTextAreaModule, NimbleTextFieldModule, NimbleNumberFieldModule, NimbleSelectModule, NimbleListOptionModule,
-    NimbleButtonModule, NimbleTreeViewModule, NimbleTreeItemModule, NimbleDrawerModule, NimbleThemeProviderModule,
+    NimbleButtonModule, NimbleTreeViewModule, NimbleTreeItemModule, NimbleDrawerModule,
     NimbleTabModule, NimbleTabPanelModule, NimbleTabsModule, NimbleTabsToolbarModule, NimbleMenuModule,
     NimbleMenuItemModule, NimbleCheckboxModule, NimbleToggleButtonModule, NimbleBreadcrumbModule, NimbleBreadcrumbItemModule,
     NimbleIconAddModule, NimbleSwitchModule, NimbleToolbarModule, NimbleMenuButtonModule, NimbleComboboxModule, NimbleTooltipModule,
@@ -24,7 +24,6 @@ import { HeaderComponent } from './header/header.component';
         BrowserModule,
         FormsModule,
         ReactiveFormsModule,
-        NimbleThemeProviderModule,
         NimbleTextAreaModule,
         NimbleTextFieldModule,
         NimbleNumberFieldModule,
