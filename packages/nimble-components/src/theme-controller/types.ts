b220e2a596ccc6e55af2a323f6a22bdb394c56da
rajsite <rajsite@users.noreply.github.com>
2023-02-23 13:41:43 -0600

    Remove unused types

--- packages/nimble-components/src/theme-controller/types.ts
+++
@@ -1,8 +0,0 @@
-import { Theme } from '../theme-provider/types';
-
-export const ThemeControllerTheme = {
-    system: undefined,
-    ...Theme
-} as const;
-export type ThemeControllerTheme =
-    (typeof ThemeControllerTheme)[keyof typeof ThemeControllerTheme];
--- packages/nimble-components/src/theme-provider/types.ts
+++ packages/nimble-components/src/theme-provider/types.ts
@@ -4,10 +4,3 @@ export const Theme = {
     color: 'color'
 } as const;
 export type Theme = (typeof Theme)[keyof typeof Theme];
-
-export const ThemeProviderTheme = {
-    system: undefined,
-    ...Theme
-} as const;
-export type ThemeProviderTheme =
-    (typeof ThemeProviderTheme)[keyof typeof ThemeProviderTheme];
