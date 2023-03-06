1c829593f3effa175ec3a4bd95ef96f1ebcc8e04
Jesse Attas <jattasNI@users.noreply.github.com>
2023-03-06 17:20:09 -0600

    Configure header text

--- packages/nimble-components/src/table-column/tests/table-column.stories.ts
+++ packages/nimble-components/src/table-column/tests/table-column.stories.ts
@@ -13,6 +13,7 @@ interface TableArgs {
     data: ExampleDataType;
     sortedColumns: ExampleSortType;
     idFieldName: undefined;
+    headerText: string;
     validity: undefined;
     checkValidity: undefined;
     tableRef: Table;
@@ -98,12 +99,8 @@ const sortedOptions = {
 const overviewText = `This page contains information about configuring the columns of a \`nimble-table\`. 
 See the **Table** story for information about configuring the table itself.`;
 
-const sortedColumnsDescription = `A column within the table is configured to be sorted by specifying a \`sort-direction\` and a \`sort-index\` on
-it. The \`sort-direction\` indicates the direction to sort (\`ascending\` or \`descending\`), and the \`sort-index\` specifies the sort precedence
-of the column within the set of all sorted columns. Columns within the table will be sorted from lowest \`sort-index\` to highest \`sort-index\`.`;
-
 const metadata: Meta<TableArgs> = {
-    title: 'Table Column',
+    title: 'Table Columns',
     decorators: [withXD],
     parameters: {
         docs: {
@@ -115,45 +112,40 @@ const metadata: Meta<TableArgs> = {
             artboardUrl:
                 'https://xd.adobe.com/view/5b476816-dad1-4671-b20a-efe796631c72-0e14/screen/d389dc1e-da4f-4a63-957b-f8b3cc9591b4/specs/'
         }
-    }
-};
-
-export default metadata;
-
-export const headerContent: StoryObj<TableArgs> = {
+    },
     // prettier-ignore
     render: createUserSelectedThemeStory(html<TableArgs>`
-        ${usageWarning('table')}
-        <${tableTag}
-            ${ref('tableRef')}
-            id-field-name="${() => dataSetIdFieldNames[ExampleDataType.simpleData]}"
-            data-unused="${x => x.updateData(x)}"
+    ${usageWarning('table')}
+    <${tableTag}
+        ${ref('tableRef')}
+        id-field-name="${() => dataSetIdFieldNames[ExampleDataType.simpleData]}"
+        data-unused="${x => x.updateData(x)}"
+    >
+        <${tableColumnTextTag}
+            column-id="first-name-column"
+            field-name="firstName" placeholder="no value"
         >
-            <${tableColumnTextTag}
-                column-id="first-name-column"
-                field-name="firstName" placeholder="no value"
-            >
-                <${iconUserTag} title="First Name"></${iconUserTag}>
-            </${tableColumnTextTag}>
-            <${tableColumnTextTag}
-                column-id="last-name-column"
-                field-name="lastName" placeholder="no value"
-            >
-                Last Name
-            </${tableColumnTextTag}>
-            <${tableColumnTextTag}
-                column-id="favorite-color-column"
-                field-name="favoriteColor" placeholder="no value"
-            >
-                Favorite Color
-            </${tableColumnTextTag}>
-            <${tableColumnTextTag}
-                column-id="quote-column"
-                field-name="quote" placeholder="no value"
-            >
-                Quote
-            </${tableColumnTextTag}>
-        </${tableTag}>
+            First Name
+        </${tableColumnTextTag}>
+        <${tableColumnTextTag}
+            column-id="last-name-column"
+            field-name="lastName" placeholder="no value"
+        >
+            Last Name
+        </${tableColumnTextTag}>
+        <${tableColumnTextTag}
+            column-id="favorite-color-column"
+            field-name="favoriteColor" placeholder="no value"
+        >
+            Favorite Color
+        </${tableColumnTextTag}>
+        <${tableColumnTextTag}
+            column-id="quote-column"
+            field-name="quote" placeholder="no value"
+        >
+            Quote
+        </${tableColumnTextTag}>
+    </${tableTag}>
     `),
     argTypes: {
         tableRef: {
@@ -180,7 +172,107 @@ export const headerContent: StoryObj<TableArgs> = {
     }
 };
 
+export default metadata;
+
+// In the Docs tab, Storybook doesn't render the title of the first story
+// This is a placeholder to get the useful ones to render
+export const columns: StoryObj<TableArgs> = {
+};
+
+export const columnOrder: StoryObj<TableArgs> = {
+};
+
+const headerContentDescription = `The content of each column header comes from whatever is slotted in the column element. 
+If you provide only text content, Nimble will style it, add a \`title\` to show a tooltip when truncated, 
+and set appropriate ARIA attributes. If you provide icon content, you should set your own \`title\` and ARIA attributes for it. 
+Titles should use "Headline Casing" and Nimble will automatically capitalize them for display.`;
+
+export const headerContent: StoryObj<TableArgs> = {
+    parameters: {
+        docs: {
+            description: {
+                story: headerContentDescription
+            }
+        }
+    },
+    // prettier-ignore
+    render: createUserSelectedThemeStory(html<TableArgs>`
+        ${usageWarning('table')}
+        <${tableTag}
+            ${ref('tableRef')}
+            id-field-name="${() => dataSetIdFieldNames[ExampleDataType.simpleData]}"
+            data-unused="${x => x.updateData(x)}"
+        >
+            <${tableColumnTextTag}
+                column-id="first-name-column"
+                field-name="firstName" placeholder="no value"
+            >
+                <${iconUserTag} title="First Name"></${iconUserTag}>
+            </${tableColumnTextTag}>
+            <${tableColumnTextTag}
+                column-id="last-name-column"
+                field-name="lastName" placeholder="no value"
+            >
+                ${x => x.headerText}
+            </${tableColumnTextTag}>
+            <${tableColumnTextTag}
+                column-id="favorite-color-column"
+                field-name="favoriteColor" placeholder="no value"
+            >
+                Favorite Color
+            </${tableColumnTextTag}>
+            <${tableColumnTextTag}
+                column-id="quote-column"
+                field-name="quote" placeholder="no value"
+            >
+                Quote
+            </${tableColumnTextTag}>
+        </${tableTag}>
+    `),
+    argTypes: {
+        headerText: {
+            name: 'Second column header text',
+            description:
+                headerContentDescription
+        },
+        tableRef: {
+            table: {
+                disable: true
+            }
+        },
+        updateData: {
+            table: {
+                disable: true
+            }
+        },
+    },
+    args: {
+        headerText: 'Last Name',
+        tableRef: undefined,
+        updateData: x => {
+            void (async () => {
+                // Safari workaround: the table element instance is made at this point
+                // but doesn't seem to be upgraded to a custom element yet
+                await customElements.whenDefined('nimble-table');
+                x.tableRef.setData(dataSets[ExampleDataType.simpleData]);
+            })();
+        }
+    }
+};
+
+const sortedColumnsDescription = `A column within the table is configured to be sorted by specifying a \`sort-direction\` and a \`sort-index\` on
+it. The \`sort-direction\` indicates the direction to sort (\`ascending\` or \`descending\`), and the \`sort-index\` specifies the sort precedence
+of the column within the set of all sorted columns. Columns within the table will be sorted from lowest \`sort-index\` to highest \`sort-index\`. 
+Sorting is based on the underlying field values in the column, not the rendered values.`;
+
 export const sorting: StoryObj<TableArgs> = {
+    parameters: {
+        docs: {
+            description: {
+                story: sortedColumnsDescription
+            }
+        }
+    },
     // prettier-ignore
     render: createUserSelectedThemeStory(html<TableArgs>`
         ${usageWarning('table')}
@@ -194,7 +286,7 @@ export const sorting: StoryObj<TableArgs> = {
                 field-name="firstName" placeholder="no value"
                 sort-direction="${x => x.getColumnSortData('first-name-column', x).direction}" sort-index="${x => x.getColumnSortData('first-name-column', x).index}"
             >
-                <${iconUserTag} title="First Name"></${iconUserTag}>
+                First Name
             </${tableColumnTextTag}>
             <${tableColumnTextTag}
                 column-id="last-name-column"
