9e6079f5b5d88d7b1f4e410cc19896a97d3ffa68
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-03-07 11:59:21 -0600

    Prettier fix

--- packages/nimble-components/src/table-column/text/template.ts
+++ packages/nimble-components/src/table-column/text/template.ts
@@ -2,10 +2,15 @@ import { html } from '@microsoft/fast-element';
 import type { TableColumnTextCellRecord, TableColumnTextColumnConfig } from '.';
 import type { TableCellState } from '../base/types';
 
-const getCellContent = (cellState: TableCellState<TableColumnTextCellRecord, TableColumnTextColumnConfig>): string => {
-    return (typeof cellState.cellRecord.value === 'string'
+const getCellContent = (
+    cellState: TableCellState<
+    TableColumnTextCellRecord,
+    TableColumnTextColumnConfig
+    >
+): string => {
+    return typeof cellState.cellRecord.value === 'string'
         ? cellState.cellRecord.value
-        : cellState.columnConfig.placeholder);
+        : cellState.columnConfig.placeholder;
 };
 const setTitleWhenOverflow = (span: HTMLElement, title: string): void => {
     if (title && span.offsetWidth < span.scrollWidth) {
@@ -17,7 +22,10 @@ TableCellState<TableColumnTextCellRecord, TableColumnTextColumnConfig>
 >`
     <span
         class="${x => (typeof x.cellRecord.value === 'string' ? '' : 'placeholder')}"
-        @mouseover="${(x, c) => setTitleWhenOverflow(c.event.target as HTMLElement, getCellContent(x))}"
+        @mouseover="${(x, c) => setTitleWhenOverflow(
+        c.event.target as HTMLElement,
+        getCellContent(x)
+    )}"
         @mouseout="${(_x, c) => (c.event.target as HTMLElement).removeAttribute('title')}"
     >
         ${x => getCellContent(x)}
