9b8d16fe2fa5603edf5de7a0c3ad0bcf49f5d7c2
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-03-07 11:56:27 -0600

    Add RouterLink directives and tests and add to exports

--- angular-workspace/projects/ni/nimble-angular/src/directives/anchor-menu-item/nimble-anchor-menu-item.module.ts
+++ angular-workspace/projects/ni/nimble-angular/src/directives/anchor-menu-item/nimble-anchor-menu-item.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NimbleAnchorMenuItemDirective } from './nimble-anchor-menu-item.directive';
 
 import '@ni/nimble-components/dist/esm/anchor-menu-item';
+import { NimbleAnchorMenuItemRouterLinkWithHrefDirective } from './nimble-anchor-menu-item-router-link-with-href.directive';
+import { NimbleAnchorMenuItemRouterLinkDirective } from './nimble-anchor-menu-item-router-link.directive';
 
 @NgModule({
-    declarations: [NimbleAnchorMenuItemDirective],
+    declarations: [NimbleAnchorMenuItemDirective, NimbleAnchorMenuItemRouterLinkDirective, NimbleAnchorMenuItemRouterLinkWithHrefDirective],
     imports: [CommonModule],
-    exports: [NimbleAnchorMenuItemDirective]
+    exports: [NimbleAnchorMenuItemDirective, NimbleAnchorMenuItemRouterLinkDirective, NimbleAnchorMenuItemRouterLinkWithHrefDirective]
 })
 export class NimbleAnchorMenuItemModule { }
