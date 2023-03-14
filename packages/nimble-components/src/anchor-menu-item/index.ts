14a6282858d186986fcc9472adf590ebb49b8d21
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-03-14 13:11:42 -0500

    Simplify instanceof override implementation

--- packages/nimble-components/src/anchor-menu-item/index.ts
+++ packages/nimble-components/src/anchor-menu-item/index.ts
@@ -116,20 +116,16 @@ export const anchorMenuItemTag = DesignSystem.tagFor(AnchorMenuItem);
 // This is a workaround for the fact that FAST's menu uses `instanceof MenuItem`
 // in their logic for indenting menu items. Since our AnchorMenuItem derives from
 // AnchorBase and not FAST's MenuItem, we need to change their MenuItem's definition
-// of `hasInstance` so that it includes our AnchorMenuItem, too. We reimplement
-// `hasInstance FoundationMenuItem` behavior by traversing the prototype chain,
-// and if we don't find a match, we also try `instanceof AnchorMenuItem`
+// of `hasInstance` so that it includes our AnchorMenuItem, too.
 //
 // If/when we change FAST to test for the presence of `startColumnCount` instead
 // of using `instanceof MenuItem`, we can remove this workaround. Here is the
 // PR into FAST: https://github.com/microsoft/fast/pull/6667
+const originalInstanceOf = FoundationMenuItem[Symbol.hasInstance].bind(FoundationMenuItem);
 Object.defineProperty(FoundationMenuItem, Symbol.hasInstance, {
     value(instance: unknown) {
-        for (let prototype: unknown = Object.getPrototypeOf(instance); prototype !== null; prototype = Object.getPrototypeOf(prototype)) {
-            if (prototype === FoundationMenuItem.prototype) {
-                return true;
-            }
-        }
-        return instance instanceof AnchorMenuItem;
+        return (
+            originalInstanceOf(instance) || instance instanceof AnchorMenuItem
+        );
     }
 });
