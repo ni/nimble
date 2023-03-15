19b9692887a48c30eb6f756b923349fb8c57330f
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-03-15 18:24:52 -0500

    Add Angular support

---
+++ angular-workspace/projects/ni/nimble-angular/src/directives/anchor-tree-item/tests/nimble-anchor-button-router-link.directive.spec.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import { processUpdates } from '../../../testing/async-helpers';
+import { NimbleAnchorTreeItemModule } from '../nimble-anchor-tree-item.module';
+
+describe('Nimble anchor button RouterLinkDirective', () => {
+    @Component({
+        template: `
+            <nimble-anchor-tree-item routerLink="page1">
+                Anchor Text
+            </nimble-anchor-tree-item>
+            <router-outlet></router-outlet>
+         `
+    })
+    class TestHostComponent {
+    }
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [TestHostComponent],
+            imports: [
+                NimbleAnchorTreeItemModule,
+                CommonModule,
+                RouterTestingModule.withRoutes([{ path: '', component: TestHostComponent, pathMatch: 'full' }], { useHash: true })
+            ]
+        });
+    });
+
+    afterEach(() => {
+        processUpdates();
+    });
+
+    it('throws an error when used, pointing consumers to nimbleRouterLink', () => {
+        expect(() => {
+            TestBed.createComponent(TestHostComponent);
+        }).toThrowError(/nimbleRouterLink/g);
+    });
+});
