7399f58889144682f11a1a8efb92a07249199fb4
Jonathan Meyer <26874831+atmgrifter00@users.noreply.github.com>
2023-03-16 09:53:54 -0500

    Handling PR feedback.

--- packages/nimble-components/src/table-column/base/tests/table-column.stories.ts
+++ packages/nimble-components/src/table-column/base/tests/table-column.stories.ts
@@ -493,9 +493,10 @@ const fractionalWidthOptions = {
 } as const;
 
 const fractionalWidthDescription = `Some table columns (like \`nimble-table-column-text\`) can be configured to have a fractional width such that its width
-                                    will be proportional to other columns also configured with a fractional width (which is the default). So,
-                                    a column with a \`fractional-width\` set to "2" would be twice as wide as a column with a \`fractional-width\`
-                                    set to "1" (the default).`;
+                                    will be proportional to other columns also configured with a fractional width. So, a column with a \`fractional-width\`
+                                    set to 2 would be twice as wide as a column with a \`fractional-width\` set to 1. The default value for
+                                    \`fractional-width\` is 1. Columns that don't support \`fractional-width\` explicitly, or another API responsible for
+                                    managing the width of the column, will behave as if they have a \`fractional-width\` of 1.`;
 
 const minPixelWidthDescription = `Table columns that support having a \`fractional-width\` can also be configured to have a minimum width such that its width
 will never shrink below the specified pixel width.`;
@@ -538,6 +539,7 @@ export const fractionalWidthColumn: StoryObj<ColumnWidthTableArgs> = {
             </${tableColumnTextTag}>
             <${tableColumnTextTag}
                 field-name="quote"
+                placeholder="${'<pacifier noise>'}"
                 fractional-width="${x => x.getColumnWidthData('quote-column', x)}"
                 min-pixel-width="${x => x.minPixelWidth}"
             >
