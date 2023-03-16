10bc6fc44ddc30f94fb0af5aef2e2f92ba485146
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-03-16 11:15:07 -0500

    Use anchor base directive

--- angular-workspace/projects/ni/nimble-angular/src/directives/anchor-menu-item/nimble-anchor-menu-item.directive.ts
+++ angular-workspace/projects/ni/nimble-angular/src/directives/anchor-menu-item/nimble-anchor-menu-item.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 import type { AnchorMenuItem } from '@ni/nimble-components/dist/esm/anchor-menu-item';
+import { NimbleAnchorBaseDirective } from '../anchor-base/nimble-anchor-base.directive';
 import { BooleanValueOrAttribute, toBooleanProperty } from '../utilities/template-value-helpers';
 
 export type { AnchorMenuItem };
@@ -10,63 +11,7 @@ export type { AnchorMenuItem };
 @Directive({
     selector: 'nimble-anchor-menu-item'
 })
-export class NimbleAnchorMenuItemDirective {
-    public get href(): string | undefined {
-        return this.elementRef.nativeElement.href;
-    }
-
-    @Input() public set href(value: string | undefined) {
-        this.renderer.setProperty(this.elementRef.nativeElement, 'href', value);
-    }
-
-    public get hreflang(): string | undefined {
-        return this.elementRef.nativeElement.hreflang;
-    }
-
-    @Input() public set hreflang(value: string | undefined) {
-        this.renderer.setProperty(this.elementRef.nativeElement, 'hreflang', value);
-    }
-
-    public get ping(): string | undefined {
-        return this.elementRef.nativeElement.ping;
-    }
-
-    @Input() public set ping(value: string | undefined) {
-        this.renderer.setProperty(this.elementRef.nativeElement, 'ping', value);
-    }
-
-    public get referrerpolicy(): string | undefined {
-        return this.elementRef.nativeElement.referrerpolicy;
-    }
-
-    @Input() public set referrerpolicy(value: string | undefined) {
-        this.renderer.setProperty(this.elementRef.nativeElement, 'referrerpolicy', value);
-    }
-
-    public get rel(): string | undefined {
-        return this.elementRef.nativeElement.rel;
-    }
-
-    @Input() public set rel(value: string | undefined) {
-        this.renderer.setProperty(this.elementRef.nativeElement, 'rel', value);
-    }
-
-    public get target(): string | undefined {
-        return this.elementRef.nativeElement.target;
-    }
-
-    @Input() public set target(value: string | undefined) {
-        this.renderer.setProperty(this.elementRef.nativeElement, 'target', value);
-    }
-
-    public get type(): string | undefined {
-        return this.elementRef.nativeElement.type;
-    }
-
-    @Input() public set type(value: string | undefined) {
-        this.renderer.setProperty(this.elementRef.nativeElement, 'type', value);
-    }
-
+export class NimbleAnchorMenuItemDirective extends NimbleAnchorBaseDirective<AnchorMenuItem> {
     public get disabled(): boolean {
         return this.elementRef.nativeElement.disabled;
     }
@@ -75,5 +20,7 @@ export class NimbleAnchorMenuItemDirective {
         this.renderer.setProperty(this.elementRef.nativeElement, 'disabled', toBooleanProperty(value));
     }
 
-    public constructor(private readonly renderer: Renderer2, private readonly elementRef: ElementRef<AnchorMenuItem>) {}
+    public constructor(renderer: Renderer2, elementRef: ElementRef<AnchorMenuItem>) {
+        super(renderer, elementRef);
+    }
 }
