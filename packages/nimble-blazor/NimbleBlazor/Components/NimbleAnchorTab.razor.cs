e1746f201eb7919b8e44ab53caa7eba95f036ffd
m-akinc <7282195+m-akinc@users.noreply.github.com>
2023-03-16 16:45:12 -0500

    Add missing properties to NimbleAnchorTab in Blazor (#1126)
    ## 🤨 Rationale
    
    Somehow I left the seven link properties (href, target, etc) off of the
    Blazor wrapper for the anchor tab. I didn't notice, because the example
    app's anchor tabs still navigated as expected.
    
    ## 👩‍💻 Implementation
    
    Added the seven missing properties and added tests for them. Previously,
    only the anchor tabs (as opposed to anchor tab, singular) component had
    tests.
    
    ## 🧪 Testing
    
    Tests pass
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

--- packages/nimble-blazor/NimbleBlazor/Components/NimbleAnchorTab.razor.cs
+++ packages/nimble-blazor/NimbleBlazor/Components/NimbleAnchorTab.razor.cs
@@ -8,6 +8,48 @@ namespace NimbleBlazor;
 public partial class NimbleAnchorTab : ComponentBase
 {
     /// <summary>
+    /// The URL the hyperlink references. See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a for more information.
+    /// </summary>
+    [Parameter]
+    public string? Href { get; set; }
+
+    /// <summary>
+    /// Hints at the human language of the linked URL. See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a for more information.
+    /// </summary>
+    [Parameter]
+    public string? HrefLang { get; set; }
+
+    /// <summary>
+    /// A space-separated list of URLs. See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a for more information.
+    /// </summary>
+    [Parameter]
+    public string? Ping { get; set; }
+
+    /// <summary>
+    /// How much of the referrer to send when following the link. See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a for more information.
+    /// </summary>
+    [Parameter]
+    public string? ReferrerPolicy { get; set; }
+
+    /// <summary>
+    /// The relationship of the linked URL as space-separated link types. See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a for more information.
+    /// </summary>
+    [Parameter]
+    public string? Rel { get; set; }
+
+    /// <summary>
+    /// Where to display the linked URL, as the name for a browsing context. See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a for more information.
+    /// </summary>
+    [Parameter]
+    public string? Target { get; set; }
+
+    /// <summary>
+    /// Hints at the linked URL's format with a MIME type. See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a for more information.
+    /// </summary>
+    [Parameter]
+    public string? Type { get; set; }
+
+    /// <summary>
     /// The child content of the element.
     /// </summary>
     [Parameter]
