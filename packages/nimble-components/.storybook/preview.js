8e9848fde7b5f663d99bfe39bd6d175104a642eb
Jesse Attas <jattasNI@users.noreply.github.com>
2023-03-24 15:38:22 -0500

    Re-enable theme switcher

--- packages/nimble-components/.storybook/preview.js
+++ packages/nimble-components/.storybook/preview.js
@@ -2,13 +2,13 @@ import { configureActions } from '@storybook/addon-actions';
 import '@ni/nimble-tokens/dist/fonts/css/fonts.css';
 import './preview.css';
 import { transformSource } from './transformSource';
-// import { backgroundStates, defaultBackgroundState } from '../dist/esm/utilities/tests/states';
+import { backgroundStates, defaultBackgroundState } from '../dist/esm/utilities/tests/states';
 
 export const parameters = {
-    // backgrounds: {
-    //     default: defaultBackgroundState.name,
-    //     values: backgroundStates.map(({ name, value }) => ({ name, value }))
-    // },
+    backgrounds: {
+        default: defaultBackgroundState.name,
+        values: backgroundStates.map(({ name, value }) => ({ name, value }))
+    },
     options: {
         storySort: {
             order: ['Getting Started']
