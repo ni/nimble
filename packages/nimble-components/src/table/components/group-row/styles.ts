d8f0c57b84854bce2d9ebe023f5176bd84ae1e78
Jonathan Meyer <26874831+atmgrifter00@users.noreply.github.com>
2023-03-28 16:03:38 -0500

    Formatting.

--- packages/nimble-components/src/table/components/group-row/styles.ts
+++ packages/nimble-components/src/table/components/group-row/styles.ts
@@ -36,7 +36,7 @@ export const styles = css`
 
     :host([expanded='true']) .animating,
     :host([expanded='false']) .animating {
-        transition: .2s ease-in-out;
+        transition: 0.2s ease-in-out;
     }
 
     .group-header-value {
