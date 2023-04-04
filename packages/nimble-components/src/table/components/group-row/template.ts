b827b4dab99a544cc5b0db3fffdd69b7f6003bba
Jonathan Meyer <26874831+atmgrifter00@users.noreply.github.com>
2023-04-04 13:40:42 -0500

    Try to fix storybook issue.

--- packages/nimble-components/src/table/components/group-row/template.ts
+++ packages/nimble-components/src/table/components/group-row/template.ts
@@ -1,18 +1,20 @@
 /* eslint-disable @typescript-eslint/indent */
 import { html, ref } from '@microsoft/fast-element';
 import type { TableGroupRow } from '.';
+import { buttonTag } from '../../../button';
+import { iconArrowExpanderRightTag } from '../../../icons/arrow-expander-right';
 
 // prettier-ignore
 export const template = html<TableGroupRow>`
     <template @click=${x => x.onGroupExpandToggle()} style="--ni-private-table-group-row-indent-level: ${x => x.nestingLevel};">
-        <nimble-button
+        <${buttonTag}
             appearance="ghost"
             content-hidden
             class="expand-collapse-button"
             tabindex="-1"
         >
-            <nimble-icon-arrow-expander-right ${ref('expandIcon')} slot="start" class="expander-icon ${x => x.animationClass}"></nimble-icon-arrow-expander-right>
-        </nimble-button>
+            <${iconArrowExpanderRightTag} ${ref('expandIcon')} slot="start" class="expander-icon ${x => x.animationClass}"></${iconArrowExpanderRightTag}>
+        </${buttonTag}>
         <div class="group-row-header-content">
             ${x => x.groupColumn?.internalGroupHeaderViewTemplate}
             <div class="group-row-child-count">(${x => x.leafItemCount})</span>
