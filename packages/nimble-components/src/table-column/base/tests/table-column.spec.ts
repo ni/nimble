21a32d403be8bfb88bdae0de8faa599bd4f0a8bf
rajsite <rajsite@users.noreply.github.com>
2023-04-10 00:12:07 -0500

    TableColumn not providing ColumnInternals err

--- packages/nimble-components/src/table-column/base/tests/table-column.spec.ts
+++ packages/nimble-components/src/table-column/base/tests/table-column.spec.ts
@@ -1,8 +1,9 @@
 /* eslint-disable max-classes-per-file */
-import { fixture, Fixture } from '../../../utilities/tests/fixture';
+import { customElement } from '@microsoft/fast-element';
+import { fixture, Fixture, uniqueElementName } from '../../../utilities/tests/fixture';
 import { TableColumnEmpty, tableColumnEmptyTag } from './table-column.fixtures';
+import { TableColumn } from '..';
 
-// prettier-ignore
 async function setup(): Promise<Fixture<TableColumnEmpty>> {
     return fixture(tableColumnEmptyTag);
 }
@@ -36,5 +37,31 @@ describe('TableColumn', () => {
         element.columnInternals.pixelWidth = 200;
 
         expect(element.columnInternals.currentPixelWidth).toBe(200);
+    });
+
+    describe('defined with a default constructor', () => {
+        async function waitForGlobalError(): Promise<PromiseRejectionEvent> {
+            return new Promise((_resolve, reject) => {
+                const originalOnError: OnErrorEventHandler = window.onerror;
+                const handler = (event: string | Event): void => {
+                    reject(event);
+                    window.onerror = originalOnError;
+                };
+                window.onerror = handler;
+            });
+        }
+
+        const columnName = uniqueElementName();
+        @customElement({
+            name: columnName
+        })
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        class TestTableColumn extends TableColumn {}
+
+        it('throws when instantiated without ColumnInternalsOptions', async () => {
+            const globalError = waitForGlobalError();
+            document.createElement(columnName);
+            await expectAsync(globalError).toBeRejected();
+        });
     });
 });
