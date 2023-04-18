da9d5918b2982dee9db2347726bf456a21ca1045
Jonathan Meyer <26874831+atmgrifter00@users.noreply.github.com>
2023-04-18 08:50:08 -0500

    Update packages/nimble-components/src/table/components/row/index.ts
    Co-authored-by: mollykreis <20542556+mollykreis@users.noreply.github.com>

--- packages/nimble-components/src/table/components/row/index.ts
+++ packages/nimble-components/src/table/components/row/index.ts
@@ -106,10 +106,7 @@ export class TableRow<
                     columnConfig: {}
                 };
             }
-            let cellIndentLevel = 0;
-            if (i === 0) {
-                cellIndentLevel = this.nestingLevel > 0 ? this.nestingLevel - 1 : 0;
-            }
+            const cellIndentLevel = (i === 0 && this.nestingLevel > 0) ? this.nestingLevel - 1 : 0;
             return { column, cellState, cellIndentLevel };
         });
     }
