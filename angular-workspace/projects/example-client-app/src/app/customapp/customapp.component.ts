b56927365c467310598d6c7e448369f5e8226a84
mollykreis <20542556+mollykreis@users.noreply.github.com>
2023-04-20 08:53:26 -0500

    Table row selection angular (#1186)
    # Pull Request
    
    ## 🤨 Rationale
    
    Add Angular support for row selection in the table, which is part of
    #856.
    
    ## 👩‍💻 Implementation
    
    - Add `selectionMode` to the table's directive
    - Add `getSelectedRowIds()` and `setSelectedRowIds()` to the table's
    directive
    - Update the Angular example app to configure a selection mode of
    `multiple`
    - Update the Angular example app to render with 0 rows in the table
    because there are Lighthouse failures otherwise
    - This is not an ideal solution (nor a long-term solution). However, we
    decided it did not make sense to block adding Angular support for row
    selection on Lighthouse failures, particularly when we already have an
    issue to track accessibility issues with the table -- #1151
    
    ## 🧪 Testing
    
    - Added/ran unit tests
    - Verified table worked correctly in example app
    
    ## ✅ Checklist
    
    <!--- Review the list and put an x in the boxes that apply or ~~strike
    through~~ around items that don't (along with an explanation). -->
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

--- angular-workspace/projects/example-client-app/src/app/customapp/customapp.component.ts
+++ angular-workspace/projects/example-client-app/src/app/customapp/customapp.component.ts
@@ -9,6 +9,7 @@ interface ComboboxItem {
 }
 
 interface SimpleTableRecord extends TableRecord {
+    id: string;
     stringValue1: string;
     stringValue2: string;
 }
@@ -38,14 +39,7 @@ export class CustomAppComponent {
     public activeAnchorTabId = 'a-tab-2';
 
     public readonly tableData$: Observable<SimpleTableRecord[]>;
-    private readonly tableDataSubject = new BehaviorSubject<SimpleTableRecord[]>([
-        { stringValue1: 'hello world', stringValue2: 'more text' },
-        { stringValue1: 'foo', stringValue2: 'bar' },
-        { stringValue1: 'candy', stringValue2: 'bar' },
-        { stringValue1: 'dive', stringValue2: 'bar' },
-        { stringValue1: 're', stringValue2: 'bar' },
-        { stringValue1: 'last row', stringValue2: 'yay!' }
-    ]);
+    private readonly tableDataSubject = new BehaviorSubject<SimpleTableRecord[]>([]);
 
     @ViewChild('dialog', { read: NimbleDialogDirective }) private readonly dialog: NimbleDialogDirective<string>;
     @ViewChild('drawer', { read: NimbleDrawerDirective }) private readonly drawer: NimbleDrawerDirective<string>;
@@ -92,6 +86,7 @@ export class CustomAppComponent {
     public onAddTableRow(): void {
         const tableData = this.tableDataSubject.value;
         tableData.push({
+            id: tableData.length.toString(),
             stringValue1: `new string ${tableData.length}`,
             stringValue2: `bar ${tableData.length}`
         });
