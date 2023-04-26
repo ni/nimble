b7fc09aa1ac25633d5fb5e69c04f737103b02fa3
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-04-26 16:58:03 -0500

    Add link to issue

--- packages/nimble-components/src/select/tests/select.spec.ts
+++ packages/nimble-components/src/select/tests/select.spec.ts
@@ -45,6 +45,7 @@ async function checkFullyInViewport(element: HTMLElement): Promise<boolean> {
             // As of now, passing a document as root is not supported on Safari:
             // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#browser_compatibility
             // If we begin running these tests on Safari, we may need to skip those that use this function.
+            // This issue tracks expanding testing to Safari: https://github.com/ni/nimble/issues/990
             { threshold: 1.0, root: document }
         );
         intersectionObserver.observe(element);
