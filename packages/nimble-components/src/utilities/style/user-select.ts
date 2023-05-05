da707c7e22131d8a093aa7f2c32568c0756e59f2
mollykreis <20542556+mollykreis@users.noreply.github.com>
2023-05-05 14:45:51 -0500

    Update packages/nimble-components/src/utilities/style/user-select.ts
    Co-authored-by: Jesse Attas <jattasNI@users.noreply.github.com>

--- packages/nimble-components/src/utilities/style/user-select.ts
+++ packages/nimble-components/src/utilities/style/user-select.ts
@@ -1,5 +1,9 @@
 import { cssPartial } from '@microsoft/fast-element';
 
+/**
+ * Set user-select: none in a way that works across all supported browsers.
+ * https://developer.mozilla.org/en-US/docs/Web/CSS/user-select#browser_compatibility
+ */
 export const userSelectNone = cssPartial`
     user-select: none;
     -webkit-user-select: none;
