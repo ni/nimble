77deeb6fc9337353f801fade356c0166689aba3d
mollykreis <20542556+mollykreis@users.noreply.github.com>
2023-05-15 12:16:02 -0500

    blazor

--- packages/nimble-blazor/NimbleBlazor/EventHandlers.cs
+++ packages/nimble-blazor/NimbleBlazor/EventHandlers.cs
@@ -38,6 +38,22 @@ public class TableRowSelectionEventArgs : EventArgs
     public IEnumerable<string>? SelectedRecordIds { get; set; }
 }
 
+public class TableColumnConfigurationEventArgs : EventArgs
+{
+    public IEnumerable<TableColumnConfiguration>? Columns { get; set; }
+}
+
+public class TableColumnConfiguration
+{
+    public string? ColumnId { get; set; }
+    public int? SortIndex { get; set; }
+    public TableColumnSortDirection SortDirection { get; set; }
+    public int? GroupIndex { get; set; }
+    public bool Hidden { get; set; }
+    public double FractionalWidth { get; set; }
+    public double? PixelWidth { get; set; }
+}
+
 [EventHandler("onnimbletabsactiveidchange", typeof(TabsChangeEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
 [EventHandler("onnimblecheckedchange", typeof(CheckboxChangeEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
 [EventHandler("onnimblemenubuttontoggle", typeof(MenuButtonToggleEventArgs), enableStopPropagation: true, enablePreventDefault: false)]
@@ -46,6 +62,7 @@ public class TableRowSelectionEventArgs : EventArgs
 [EventHandler("onnimbleactionmenutoggle", typeof(TableActionMenuToggleEventArgs), enableStopPropagation: true, enablePreventDefault: false)]
 [EventHandler("onnimbleactionmenubeforetoggle", typeof(TableActionMenuToggleEventArgs), enableStopPropagation: true, enablePreventDefault: false)]
 [EventHandler("onnimbletablerowselectionchange", typeof(TableRowSelectionEventArgs), enableStopPropagation: true, enablePreventDefault: false)]
+[EventHandler("onnimbletablecolumnconfigurationchange", typeof(TableColumnConfigurationEventArgs), enableStopPropagation: true, enablePreventDefault: false)]
 public static class EventHandlers
 {
 }
