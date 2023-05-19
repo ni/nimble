190064ab6c875848ebc9d4b5470bc065e9ad5558
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-05-19 12:14:00 -0500

    NimbleTableColumnAnchorDirective extends base table column directive

--- angular-workspace/projects/ni/nimble-angular/src/directives/table-column/anchor/nimble-table-column-anchor.directive.ts
+++ angular-workspace/projects/ni/nimble-angular/src/directives/table-column/anchor/nimble-table-column-anchor.directive.ts
@@ -2,6 +2,7 @@ import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 import type { TableColumnAnchor } from '@ni/nimble-components/dist/esm/table-column/anchor';
 import type { AnchorAppearance } from '../../anchor/nimble-anchor.directive';
 import { BooleanValueOrAttribute, NumberValueOrAttribute, toBooleanProperty, toNullableNumberProperty } from '../../utilities/template-value-helpers';
+import { NimbleTableColumnBaseDirective } from '../base/nimble-table-column-base.directive';
 
 export type { TableColumnAnchor };
 
@@ -11,7 +12,7 @@ export type { TableColumnAnchor };
 @Directive({
     selector: 'nimble-table-column-anchor'
 })
-export class NimbleTableColumnAnchorDirective {
+export class NimbleTableColumnAnchorDirective extends NimbleTableColumnBaseDirective<TableColumnAnchor> {
     public get labelFieldName(): string | undefined {
         return this.elementRef.nativeElement.labelFieldName;
     }
@@ -154,5 +155,7 @@ export class NimbleTableColumnAnchorDirective {
         this.renderer.setProperty(this.elementRef.nativeElement, 'groupingDisabled', toBooleanProperty(value));
     }
 
-    public constructor(private readonly renderer: Renderer2, private readonly elementRef: ElementRef<TableColumnAnchor>) {}
+    public constructor(renderer: Renderer2, elementRef: ElementRef<TableColumnAnchor>) {
+        super(renderer, elementRef);
+    }
 }
