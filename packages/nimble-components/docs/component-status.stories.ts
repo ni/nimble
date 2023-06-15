907ca2c6fb76b1f1bc7ebd255bd4d309ee25b989
Jesse Attas <jattasNI@users.noreply.github.com>
2023-06-15 11:05:45 -0500

    Clean up table data

--- packages/nimble-components/docs/component-status.stories.ts
+++ packages/nimble-components/docs/component-status.stories.ts
@@ -156,8 +156,6 @@ const components = [
         componentHref: '/?path=/docs/components-drawer--docs',
         designHref: 'https://xd.adobe.com/view/33ffad4a-eb2c-4241-b8c5-ebfff1faf6f6-66ac/screen/730cdeb8-a4b5-4dcc-9fe4-718a75da7aff',
         designLabel: 'XD',
-        issueHref: undefined,
-        issueLabel: '',
         componentStatus: '✅',
         angularStatus: '✅',
         blazorStatus: '✅',
