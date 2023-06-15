01f08f462ebaca404cdff84e4a6de2944301a78b
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-06-15 10:30:20 -0500

    Trim text content (Firefox fix)

--- packages/nimble-components/src/table-column/base/template.ts
+++ packages/nimble-components/src/table-column/base/template.ts
@@ -8,13 +8,13 @@ export const template = html<TableColumn>`
             ${ref('headerSpan')}
             class="header-content"
             @mouseover="${x => {
-        x.isValidContentAndHasOverflow = !!x.slottedHeaderContent.map(element => element.textContent).join(' ')
+        x.isValidContentAndHasOverflow = !!x.slottedHeaderContent.map(element => element.textContent?.trim()).join(' ')
                     && x.headerSpan.offsetWidth < x.headerSpan.scrollWidth;
     }}"
             @mouseout="${x => {
         x.isValidContentAndHasOverflow = false;
     }}"
-            title=${x => (x.isValidContentAndHasOverflow ? x.slottedHeaderContent.map(element => element.textContent).join(' ') : null)}
+            title=${x => (x.isValidContentAndHasOverflow ? x.slottedHeaderContent.map(element => element.textContent?.trim()).join(' ') : null)}
         >
             <slot ${slotted('slottedHeaderContent')}></slot>
         </span>
