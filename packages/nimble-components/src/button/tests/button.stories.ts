ece550bc254ae5ee2071f6641f1ff30092f3ef8a
Aiden <103057880+aidendk@users.noreply.github.com>
2023-07-06 16:18:08 -0500

    mdx doc update

--- packages/nimble-components/src/button/tests/button.stories.ts
+++ packages/nimble-components/src/button/tests/button.stories.ts
@@ -8,7 +8,7 @@ import {
     appearanceVariantDescription,
     contentHiddenDescription,
     endIconDescription,
-    iconDescription,
+    iconDescription
 } from '../../patterns/button/tests/doc-strings';
 import { buttonTag } from '..';
 import { iconArrowExpanderDownTag } from '../../icons/arrow-expander-down';
@@ -29,50 +29,7 @@ enables users to trigger an action or event, such as submitting a form, opening 
 action, or performing a delete operation. A common convention for informing users that a button launches
 a dialog is to append "…" (ellipsis) to the button label, e.g., "Save as…".
 
-If you want a button that triggers navigation to a URL, use the \`nimble-anchor-button\` instead.
-
-<h4>Sizing</h4>
-<p> 
-    The label button has two sizes: 32px and 24px, while the icon only button has two smaller sizes: 18px and 16px.
-</p>
-<p>
-    32px buttons are the default sized control and should be used first, while 24px buttons should only be used if there's limited space.
-    24px buttons should not be used in combination with 32px buttons.
-
-</p>
-<p>
-    18px buttons are used to nest within controls, as 16px buttons are generally too small. 
-    <br/>Icons are drawn within a 16px space and can sometimes consume the full width or height; this leaves no room for visual interaction bounds.  
-    Both 16px and 18px buttons are too small for touch interactions.  18’s are 23% larger than 16’s and help increase the hit zone for a cursor.
-</p>
-<p>
-    16px buttons are the bare minimum size to use in the event icons need to vertically stack next to a 32px control.  
-    <h5>These should be used very sparingly and only if necessary.</h5>
-</p>
-<h4>Styling / Theme</h4>
-<p>
-    Light UI controls are made of  dark attributes and designed to work against bright, light backgrounds. 
-</p>
-<p>
-    Dark UI controls are made of light attributes and designed to work against bold, dark backgrounds. 
-</p>
-<p>
-    Color UI controls are made of monochromatic attributes and designed to work against bold, medium-dark and dark colored backgrounds.
-</p>
-<h4>-</h4>
-<p>
-    Use the Color UI version for backgrounds with color (e.g. purple, blue). 
-</p>
-<p>
-    See the usage details for more information on button styling / usage.
-</p>
-
-<h4>Accessibility</h4>
-<p> 
-    Please work with your designer and insure you have a 4.5:1 
-    contrast ratio text to background.
-</p>
-`;
+If you want a button that triggers navigation to a URL, use the \`nimble-anchor-button\` instead.`;
 
 const metadata: Meta<ButtonArgs> = {
     title: 'Components/Button',
