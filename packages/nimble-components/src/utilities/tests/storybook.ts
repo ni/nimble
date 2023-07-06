34ab88434ad93e6b30aa93003526eaa84d0a25ae
Aiden <103057880+aidendk@users.noreply.github.com>
2023-07-06 09:44:55 -0500

    Revert Storybook z index

--- packages/nimble-components/src/utilities/tests/storybook.ts
+++ packages/nimble-components/src/utilities/tests/storybook.ts
@@ -119,7 +119,7 @@ export const createMatrixThemeStory = <TSource>(
             ({ theme, value }: BackgroundState) => html`
                 <${themeProviderTag}
                     theme="${theme}">
-                    <div style="background-color: ${value}; padding:20px; position: relative; z-index: 0;">
+                    <div style="background-color: ${value}; padding:20px;">
                         ${viewTemplate}
                     </div>
                 </${themeProviderTag}>
