9ecabb35fbca4f0c233eb2dd0c4adb78027e0add
aidendk <103057880+aidendk@users.noreply.github.com>
2023-07-17 11:56:35 -0500

    Spinner Accent Appearance (#1338)
    # Pull Request
    
    ## 🤨 Rationale
    
    See #1298
    
    <!---
    Provide some background and a description of your work.
    What problem does this change solve?
    
    Include links to issues, work items, or other discussions.
    -->
    
    ## 👩‍💻 Implementation
    
    Implements the `accent` appearance variation of the nimble-spinner in
    Storybook, Blazor, and Angular, and includes spinner docs that more
    closely reflect Brandon's docs in Figma.
    
    <!---
    Describe how the change addresses the problem. Consider factors such as
    complexity, alternative solutions, performance impact, etc.
    
    Consider listing files with important changes or comment on them
    directly in the pull request.
    -->
    
    ## 🧪 Testing
    
    Tested that appearance would not compile with an arbitrary string value
    in types.spec.ts of the spinner/tests directory.
    Appearance testing was also added to the Angular and Blazor spinner
    components,
    
    Spinner matrix tests were updated to include the `accent` appearance.
    
    <!---
    Detail the testing done to ensure this submission meets requirements. 
    
    Include automated/manual test additions or modifications, testing done
    on a local build, private CI run results, and additional testing not
    covered by automatic pull request validation.
    -->
    
    ## ✅ Checklist
    
    <!--- Review the list and put an x in the boxes that apply or ~~strike
    through~~ around items that don't (along with an explanation). -->
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.
    
    ---------
    
    Co-authored-by: Milan Raj <rajsite@users.noreply.github.com>

--- packages/nimble-components/src/spinner/tests/spinner.stories.ts
+++ packages/nimble-components/src/spinner/tests/spinner.stories.ts
@@ -13,6 +13,7 @@ import {
     tokenNames
 } from '../../theme-provider/design-token-names';
 import { spinnerTag } from '..';
+import { SpinnerAppearance } from '../types';
 
 const spinnerSize = {
     small: null,
@@ -22,15 +23,14 @@ const spinnerSize = {
 
 interface SpinnerArgs {
     size: keyof typeof spinnerSize;
+    appearance: keyof typeof SpinnerAppearance;
 }
 
 const overviewText = '<p>The `nimble-spinner` is an animating indicator that can be placed in a particular region of a page to represent '
     + 'loading progress, or an ongoing operation, of an indeterminate / unknown duration.</p>'
-    + '<p>See the `size` argument details for information on customizing the spinner size.</p>';
-
+    + '<p>It has 3 sizes (64px, 32px, and 16px) and 2 appearance types (default and accent).</p>';
 const metadata: Meta<SpinnerArgs> = {
     title: 'Components/Spinner',
-    tags: ['autodocs'],
     parameters: {
         docs: {
             description: {
@@ -42,19 +42,25 @@ const metadata: Meta<SpinnerArgs> = {
         size: {
             description:
                 'Size of the spinner component.<details><summary>Usage details</summary>To customize its size, set its CSS '
-                + '<span style="font-family: monospace;">height</span> to a design token, and its width will automatically match its height:<br/><ul>'
+                + '<span style="font-family: monospace;">height</span> to a design token, and its width will automatically match its height. Each size will also require minimum pixel margins.<br/><ul>'
                 + `<li>For Small (16x16): ${scssPropertySetterMarkdown(
                     tokenNames.spinnerSmallHeight,
                     'height'
-                )}</li>`
+                )}
+                Requires 4px minimum margins.
+                </li>`
                 + `<li>For Medium (32x32): ${scssPropertySetterMarkdown(
                     tokenNames.spinnerMediumHeight,
                     'height'
-                )}</li>`
+                )}
+                Requires 32px minimum margins.
+                </li>`
                 + `<li>For Large (64x64): ${scssPropertySetterMarkdown(
                     tokenNames.spinnerLargeHeight,
                     'height'
-                )}</li></ul></details>`,
+                )}
+                Requires 64px minimum margins.
+                </li></ul></details>`,
             options: Object.keys(spinnerSize),
             table: { defaultValue: { summary: 'Small (16x16)' } },
             control: {
@@ -71,6 +77,20 @@ const metadata: Meta<SpinnerArgs> = {
                     )}`
                 }
             }
+        },
+        appearance: {
+            options: Object.keys(SpinnerAppearance),
+            control: { type: 'radio' },
+            description:
+                'Appearance of the spinner component.<details><summary>Usage details</summary>'
+                + '<ul>'
+                + `<li>For appearance \`default\`:<br/>
+                        Use in product instances.  This version helps avoid any association to status and the idea that this indicator is representing any context outside of an indeterminate wait time.
+                    </li>`
+                + `<li>For appearance \`accent\`:<br/>
+                        Use for promoting NI branding and adding a splash of color in a monochromatic setting.  Common places to use is home screens, licensing, dialogs and any instances where there will not be a conflict with green representing status.<br/>
+                        The green version is not intended to represent “Good” or “Success”.  
+                    </li></ul></details>`
         }
     },
     // Disable animation in Chromatic because it intermittently causes shapshot differences
@@ -78,11 +98,13 @@ const metadata: Meta<SpinnerArgs> = {
     render: createUserSelectedThemeStory(html`
         <${spinnerTag}
             style="${x => spinnerSize[x.size]}; ${isChromatic() ? '--ni-private-spinner-animation-play-state:paused' : ''}"
+            appearance="${x => SpinnerAppearance[x.appearance]}"
         >
         </${spinnerTag}>
     `),
     args: {
-        size: 'small'
+        size: 'small',
+        appearance: 'default'
     }
 };
 
