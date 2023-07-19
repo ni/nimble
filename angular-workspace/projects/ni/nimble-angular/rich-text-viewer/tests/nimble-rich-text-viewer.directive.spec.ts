b1ff7970ac4497b8edf66038fba22e087c1a00f3
SOLITONTECH\vivin.krishna <123377523+vivinkrishna-ni@users.noreply.github.com>
2023-07-19 16:09:56 +0530

    Removed undefined for the default markdown value, expected the empty string

--- angular-workspace/projects/ni/nimble-angular/rich-text-viewer/tests/nimble-rich-text-viewer.directive.spec.ts
+++ angular-workspace/projects/ni/nimble-angular/rich-text-viewer/tests/nimble-rich-text-viewer.directive.spec.ts
@@ -43,8 +43,8 @@ describe('Nimble Rich Text Viewer', () => {
         });
 
         it('has expected defaults for markdown', () => {
-            expect(directive.markdown).toBeUndefined();
-            expect(nativeElement.markdown).toBeUndefined();
+            expect(directive.markdown).toBe('');
+            expect(nativeElement.markdown).toBe('');
         });
     });
 
