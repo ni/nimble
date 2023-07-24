44e7d0d3a06dead27f48a98698aa78d1fd65f9e3
m-akinc <7282195+m-akinc@users.noreply.github.com>
2023-07-24 09:47:43 -0500

    Initial version of date column (#1363)
    ## 🤨 Rationale
    
    Part of #1014 
    
    ## 👩‍💻 Implementation
    
    This is the first, basic version of the column. It does not support any
    configuration. It always formats the datetime in the way that will
    become the default when we later add format configuration. The column
    derives from `TableColumnTextBase`.
    - component defined
    - initial Storybook stories/documentation created
    - unit tests written
    
    ## 🧪 Testing
    
    Tested in Storybook and created unit tests.
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.
    
    ---------
    
    Co-authored-by: Jesse Attas <jattasNI@users.noreply.github.com>

---
+++ packages/nimble-components/src/table-column/date-text/testing/table-column-date-text.pageobject.ts
@@ -0,0 +1,47 @@
+import type { TablePageObject } from '../../../table/testing/table.pageobject';
+import type { TableRecord } from '../../../table/types';
+import { TableColumnDateTextCellView } from '../cell-view';
+
+/**
+ * Page object for date text table column.
+ */
+export class TableColumnDateTextPageObject<T extends TableRecord> {
+    // On Chrome, in a formatted date, the space before AM/PM is a narrow non-breaking space.
+    // For testing consistency across browsers, replace it with a regular space.
+    private readonly narrowNonBreakingSpace = '\u202f';
+
+    public constructor(private readonly tablePageObject: TablePageObject<T>) {}
+
+    public getRenderedCellContent(
+        rowIndex: number,
+        columnIndex: number
+    ): string {
+        this.verifyCellType(rowIndex, columnIndex);
+        return this.tablePageObject
+            .getRenderedCellContent(rowIndex, columnIndex)
+            .replace(this.narrowNonBreakingSpace, ' ');
+    }
+
+    public getRenderedGroupHeaderContent(groupRowIndex: number): string {
+        return this.tablePageObject
+            .getRenderedGroupHeaderContent(groupRowIndex)
+            .replace(this.narrowNonBreakingSpace, ' ');
+    }
+
+    public getCellTitle(rowIndex: number, columnIndex: number): string {
+        this.verifyCellType(rowIndex, columnIndex);
+        return this.tablePageObject
+            .getCellTitle(rowIndex, columnIndex)
+            .replace(this.narrowNonBreakingSpace, ' ');
+    }
+
+    private verifyCellType(rowIndex: number, columnIndex: number): void {
+        const cell = this.tablePageObject.getRenderedCellView(
+            rowIndex,
+            columnIndex
+        );
+        if (!(cell instanceof TableColumnDateTextCellView)) {
+            throw new Error('Cell is not in a date text column');
+        }
+    }
+}
