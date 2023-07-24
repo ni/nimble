776962997b6207113539ef24fe0c097f7291dc01
Aiden <103057880+aidendk@users.noreply.github.com>
2023-07-24 14:55:22 -0500

    Toggle Button Cascade Layers (#663)

--- packages/nimble-components/src/toggle-button/styles.ts
+++ packages/nimble-components/src/toggle-button/styles.ts
@@ -12,79 +12,101 @@ import { styles as buttonStyles } from '../patterns/button/styles';
 export const styles = css`
     ${buttonStyles}
 
-    .control[aria-pressed='true'] {
-        background-color: ${fillSelectedColor};
-        border-color: ${fillSelectedColor};
-        position: relative;
-        transition: box-shadow ${smallDelay} ease-in-out,
-            border-color ${smallDelay} ease-in-out,
-            background-size ${smallDelay} ease-in-out;
-        background-size: 100% 100%;
-        background-repeat: no-repeat;
-        background-position: center;
+    @layer default {
+        .control[aria-pressed='true'] {
+            background-color: transparent;
+            background-image: linear-gradient(
+                ${fillSelectedColor},
+                ${fillSelectedColor}
+            );
+            border-color: ${fillSelectedColor};
+            position: relative;
+            transition: box-shadow ${smallDelay} ease-in-out,
+                border-color ${smallDelay} ease-in-out,
+                background-size ${smallDelay} ease-in-out;
+            background-size: 100% 100%;
+            background-repeat: no-repeat;
+            background-position: center;
+        }
     }
 
-    .control[aria-pressed='true']:hover {
-        border-color: ${borderHoverColor};
-        box-shadow: 0px 0px 0px ${borderWidth} ${borderHoverColor} inset;
-        background-image: linear-gradient(
-            ${fillSelectedColor},
-            ${fillSelectedColor}
-        );
-        background-size: calc(100% - 4px) calc(100% - 4px);
+    @layer hover {
+        .control[aria-pressed='true']:hover {
+            border-color: ${borderHoverColor};
+            box-shadow: 0px 0px 0px ${borderWidth} ${borderHoverColor} inset;
+            background-image: linear-gradient(
+                ${fillSelectedColor},
+                ${fillSelectedColor}
+            );
+            background-size: calc(100% - 4px) calc(100% - 4px);
+        }
     }
 
-    .control[aria-pressed='true']${focusVisible} {
-        background-color: transparent;
-        border-color: ${borderHoverColor};
-        box-shadow: 0px 0px 0px ${borderWidth} ${borderHoverColor} inset;
-        background-image: linear-gradient(
-            ${fillSelectedColor},
-            ${fillSelectedColor}
-        );
-        background-size: calc(100% - 4px) calc(100% - 4px);
+    @layer focusVisible {
+        .control[aria-pressed='true']${focusVisible} {
+            background-color: transparent;
+            border-color: ${borderHoverColor};
+            box-shadow: 0px 0px 0px ${borderWidth} ${borderHoverColor} inset;
+            background-image: linear-gradient(
+                ${fillSelectedColor},
+                ${fillSelectedColor}
+            );
+            background-size: calc(100% - 4px) calc(100% - 4px);
+        }
     }
 
-    .control[aria-pressed='true']:active {
-        box-shadow: none;
-        background-image: linear-gradient(
-            ${fillSelectedColor},
-            ${fillSelectedColor}
-        );
-        background-size: calc(100% - 2px) calc(100% - 2px);
+    @layer active {
+        .control[aria-pressed='true']:active {
+            box-shadow: none;
+            background-image: linear-gradient(
+                ${fillSelectedColor},
+                ${fillSelectedColor}
+            );
+            background-size: calc(100% - 2px) calc(100% - 2px);
+        }
     }
 
-    :host([disabled]) .control[aria-pressed='true'] {
-        border-color: ${fillSelectedColor};
-        background-color: ${fillSelectedColor};
-        background-image: none;
+    @layer disabled {
+        :host([disabled]) .control[aria-pressed='true'] {
+            border-color: ${fillSelectedColor};
+            background-color: ${fillSelectedColor};
+            background-image: none;
+        }
     }
 
-    :host([disabled]) .control[aria-pressed='true']:hover {
-        border-color: ${fillSelectedColor};
-        background-color: ${fillSelectedColor};
-        box-shadow: none;
+    @layer disabled {
+        :host([disabled]) .control[aria-pressed='true']:hover {
+            border-color: ${fillSelectedColor};
+            background-color: ${fillSelectedColor};
+            box-shadow: none;
+        }
     }
 
-    .control[aria-pressed='true']::before {
-        content: '';
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        pointer-events: none;
-        box-sizing: border-box;
-        outline: 0px solid transparent;
-        background-clip: content-box;
-        transition: outline ${smallDelay} ease-in-out;
+    @layer default {
+        .control[aria-pressed='true']::before {
+            content: '';
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            pointer-events: none;
+            box-sizing: border-box;
+            outline: 0px solid transparent;
+            background-clip: content-box;
+            transition: outline ${smallDelay} ease-in-out;
+        }
     }
 
-    .control[aria-pressed='true']${focusVisible}::before {
-        outline: ${borderWidth} solid ${borderHoverColor};
-        outline-offset: -3px;
-        color: transparent;
+    @layer focusVisible {
+        .control[aria-pressed='true']${focusVisible}::before {
+            outline: ${borderWidth} solid ${borderHoverColor};
+            outline-offset: -3px;
+            color: transparent;
+        }
     }
 
-    .control[aria-pressed='true']:active::before {
-        outline: none;
+    @layer active {
+        .control[aria-pressed='true']:active::before {
+            outline: none;
+        }
     }
 `;
