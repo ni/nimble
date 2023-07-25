f197b3ebfbe5212344d44d9c4085c33f2129dfd8
Malcolm Smith <20709258+msmithNI@users.noreply.github.com>
2023-07-25 15:24:36 -0500

    Doc updates

--- packages/nimble-components/src/patterns/button/tests/doc-strings.ts
+++ packages/nimble-components/src/patterns/button/tests/doc-strings.ts
@@ -2,7 +2,7 @@ export const appearanceDescription = `This attribute affects the appearance of t
 
 <details>
     <summary>Appearance Usage</summary>
-    There is a hierarchy to which button appearance should be used.
+    <p>There is a hierarchy to which button appearance should be used.</p>
     <ul>
         <li>For appearance \`ghost\`:
             <br/>This is the default and standard option for buttons. They create an open / airy feel, and fit comfortably in tight spaces.
@@ -27,7 +27,7 @@ export const appearanceVariantDescription = `This attribute has no effect on but
 
 <details>
     <summary>Primary Button Usage</summary>
-    Make a button primary to distinguish it visibly for one of the following reasons:
+    <p>Make a button primary to distinguish it visibly for one of the following reasons:</p>
     <ul>
         <li>to indicate the action that allows the user to accomplish their most common or important goal</li>
         <li>to indicate the action that allows the user to complete their task</li>
@@ -41,7 +41,7 @@ export const iconDescription = `When including an icon, set \`slot="start"\` on 
 
 <details>
     <summary>Icon Usage</summary>
-    Icons should be consistent and only used when necessary.
+    <p>Icons should be consistent and only used when necessary.</p>
     <ul>
         <li>Icon and text buttons: 
             <br/>Use a label plus icon to help reinforce a button's functionality, and increase the visual impact of the action.  
