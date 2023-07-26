646a3abb55f29e926b45481228aff57a394f5b80
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-07-26 12:06:13 -0500

    Fix build error

--- packages/nimble-components/src/icon-base/index.ts
+++ packages/nimble-components/src/icon-base/index.ts
@@ -41,7 +41,7 @@ export class Icon extends FoundationElement {
             return;
         }
         if (this.ariaLabel ?? false) {
-            svg.setAttribute('aria-label', this.ariaLabel);
+            svg.setAttribute('aria-label', this.ariaLabel!);
         } else {
             svg.removeAttribute('aria-label');
         }
