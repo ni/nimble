8ba15c46cf791dce22a09e748e6bc05163a3fcea
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-08-01 15:00:52 -0500

    Feedback

--- packages/nimble-components/src/table-column/enum-base/models/mapping-config.ts
+++ packages/nimble-components/src/table-column/enum-base/models/mapping-config.ts
@@ -1,7 +1,6 @@
 /**
  * Common state shared across Mapping Config
  */
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export abstract class MappingConfig {
     protected constructor(public readonly text: string) {}
 }
--- packages/nimble-components/src/table-column/enum-text/cell-view/index.ts
+++ packages/nimble-components/src/table-column/enum-text/cell-view/index.ts
@@ -37,12 +37,7 @@ TableColumnEnumColumnConfig
         }
 
         const config = this.columnConfig?.mappingConfigs.get(value);
-        if (config) {
-            this.text = (config as MappingTextConfig).text;
-            return;
-        }
-
-        this.text = '';
+        this.text = config ? (config as MappingTextConfig).text : '';
     }
 }
 
