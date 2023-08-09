20e90002a5fb273eaa492aad15aafc8a19b370a7
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-08-09 17:16:22 -0500

    Add mapping-text element

---
+++ packages/nimble-blazor/NimbleBlazor/Components/NimbleMappingText.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NimbleBlazor;
+
+public partial class NimbleMappingText : ComponentBase
+{
+    /// <summary>
+    /// Gets or sets the key value being mapped.
+    /// </summary>
+    [Parameter]
+    public string? Key { get; set; }
+
+    /// <summary>
+    /// Gets or sets the text being mapped to.
+    /// </summary>
+    [Parameter]
+    public string? Text { get; set; }
+}
---
+++ packages/nimble-blazor/Tests/NimbleBlazor.Tests/Unit/Components/NimbleMappingTextTests.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Linq.Expressions;
+using Bunit;
+using Xunit;
+
+namespace NimbleBlazor.Tests.Unit.Components;
+
+/// <summary>
+/// Tests for <see cref="NimbleMappingText"/>
+/// </summary>
+public class NimbleMappingTextTests
+{
+    [Fact]
+    public void NimbleMappingText_Rendered_HasMappingTextMarkup()
+    {
+        var context = new TestContext();
+        context.JSInterop.Mode = JSRuntimeMode.Loose;
+        var expectedMarkup = "nimble-mapping-text";
+
+        var anchor = context.RenderComponent<NimbleMappingText>();
+
+        Assert.Contains(expectedMarkup, anchor.Markup);
+    }
+
+    [Fact]
+    public void NimbleMappingTextKeyAttribute_HasCorrectMarkup()
+    {
+        var element = RenderWithPropertySet(x => x.Key, "foo");
+
+        Assert.Contains($"key=\"foo\"", element.Markup);
+    }
+
+    [Fact]
+    public void NimbleMappingTextTextAttribute_HasCorrectMarkup()
+    {
+        var element = RenderWithPropertySet(x => x.Text, "foo");
+
+        Assert.Contains($"text=\"foo\"", element.Markup);
+    }
+
+    private IRenderedComponent<NimbleMappingText> RenderWithPropertySet<TProperty>(Expression<Func<NimbleMappingText, TProperty>> propertyGetter, TProperty propertyValue)
+    {
+        var context = new TestContext();
+        context.JSInterop.Mode = JSRuntimeMode.Loose;
+        return context.RenderComponent<NimbleMappingText>(p => p.Add(propertyGetter, propertyValue));
+    }
+}
