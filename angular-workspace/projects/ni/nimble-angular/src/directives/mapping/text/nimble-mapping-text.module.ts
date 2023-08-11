aee84aa3a23aac7bdd8f673b5b486c49b87383d6
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-08-11 10:00:40 -0500

    Fix import path

--- angular-workspace/projects/ni/nimble-angular/src/directives/mapping/text/nimble-mapping-text.module.ts
+++ angular-workspace/projects/ni/nimble-angular/src/directives/mapping/text/nimble-mapping-text.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NimbleMappingTextDirective } from '../../src/directives/mapping/text/nimble-mapping-text.directive';
+import { NimbleMappingTextDirective } from './nimble-mapping-text.directive';
 
 import '@ni/nimble-components/dist/esm/mapping/text';
 
