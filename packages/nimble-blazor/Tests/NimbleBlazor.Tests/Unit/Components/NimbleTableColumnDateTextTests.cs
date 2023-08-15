0383f9073c3c76b7bbd93bc7def171bcd318647c
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-08-15 14:47:04 -0500

    Test all enum values

--- packages/nimble-blazor/Tests/NimbleBlazor.Tests/Unit/Components/NimbleTableColumnDateTextTests.cs
+++ packages/nimble-blazor/Tests/NimbleBlazor.Tests/Unit/Components/NimbleTableColumnDateTextTests.cs
@@ -28,112 +28,121 @@ public class NimbleTableColumnDateTextTests
         Assert.Contains(expectedMarkup, table.Markup);
     }
 
-    [Fact]
-    public void NimbleTableColumnDateText_WithFormatAttribute_HasTableMarkup()
+    [Theory]
+    [InlineData(DateTextFormat.Default, "<nimble-table-column-date-text((?!format).)*>")]
+    [InlineData(DateTextFormat.Custom, @"format=""custom""")]
+    public void NimbleTableColumnDateText_WithFormatAttribute_HasTableMarkup(DateTextFormat value, string expectedMarkupRegEx)
     {
-        var table = RenderWithPropertySet(x => x.Format, DateTextFormat.Custom);
-
-        var expectedMarkup = @"format=""custom""";
-        Assert.Contains(expectedMarkup, table.Markup);
+        var table = RenderWithPropertySet(x => x.Format, value);
+        Assert.Matches(expectedMarkupRegEx, table.Markup);
     }
 
-    [Fact]
-    public void NimbleTableColumnDateText_WithCustomLocaleMatcherAttribute_HasTableMarkup()
+    [Theory]
+    [InlineData(LocaleMatcherAlgorithm.BestFit, @"custom-locale-matcher=""best fit""")]
+    [InlineData(LocaleMatcherAlgorithm.Lookup, @"custom-locale-matcher=""lookup""")]
+    public void NimbleTableColumnDateText_WithCustomLocaleMatcherAttribute_HasTableMarkup(LocaleMatcherAlgorithm value, string expectedMarkupRegEx)
     {
-        var table = RenderWithPropertySet(x => x.CustomLocaleMatcher, LocaleMatcherAlgorithm.Lookup);
-
-        var expectedMarkup = @"custom-locale-matcher=""lookup""";
-        Assert.Contains(expectedMarkup, table.Markup);
+        var table = RenderWithPropertySet(x => x.CustomLocaleMatcher, value);
+        Assert.Matches(expectedMarkupRegEx, table.Markup);
     }
 
-    [Fact]
-    public void NimbleTableColumnDateText_WithCustomWeekdayAttribute_HasTableMarkup()
+    [Theory]
+    [InlineData(WeekdayFormat.Long, @"custom-weekday=""long""")]
+    [InlineData(WeekdayFormat.Short, @"custom-weekday=""short""")]
+    [InlineData(WeekdayFormat.Narrow, @"custom-weekday=""narrow""")]
+    public void NimbleTableColumnDateText_WithCustomWeekdayAttribute_HasTableMarkup(WeekdayFormat value, string expectedMarkupRegEx)
     {
-        var table = RenderWithPropertySet(x => x.CustomWeekday, WeekdayFormat.Narrow);
-
-        var expectedMarkup = @"custom-weekday=""narrow""";
-        Assert.Contains(expectedMarkup, table.Markup);
+        var table = RenderWithPropertySet(x => x.CustomWeekday, value);
+        Assert.Matches(expectedMarkupRegEx, table.Markup);
     }
 
-    [Fact]
-    public void NimbleTableColumnDateText_WithCustomEraAttribute_HasTableMarkup()
+    [Theory]
+    [InlineData(EraFormat.Long, @"custom-era=""long""")]
+    [InlineData(EraFormat.Short, @"custom-era=""short""")]
+    [InlineData(EraFormat.Narrow, @"custom-era=""narrow""")]
+    public void NimbleTableColumnDateText_WithCustomEraAttribute_HasTableMarkup(EraFormat value, string expectedMarkupRegEx)
     {
-        var table = RenderWithPropertySet(x => x.CustomEra, EraFormat.Short);
-
-        var expectedMarkup = @"custom-era=""short""";
-        Assert.Contains(expectedMarkup, table.Markup);
+        var table = RenderWithPropertySet(x => x.CustomEra, value);
+        Assert.Matches(expectedMarkupRegEx, table.Markup);
     }
 
-    [Fact]
-    public void NimbleTableColumnDateText_WithCustomYearAttribute_HasTableMarkup()
+    [Theory]
+    [InlineData(YearFormat.Numeric, @"custom-year=""numeric""")]
+    [InlineData(YearFormat.TwoDigit, @"custom-year=""2-digit""")]
+    public void NimbleTableColumnDateText_WithCustomYearAttribute_HasTableMarkup(YearFormat value, string expectedMarkupRegEx)
     {
-        var table = RenderWithPropertySet(x => x.CustomYear, YearFormat.TwoDigit);
-
-        var expectedMarkup = @"custom-year=""2-digit""";
-        Assert.Contains(expectedMarkup, table.Markup);
+        var table = RenderWithPropertySet(x => x.CustomYear, value);
+        Assert.Matches(expectedMarkupRegEx, table.Markup);
     }
 
-    [Fact]
-    public void NimbleTableColumnDateText_WithCustomMonthAttribute_HasTableMarkup()
+    [Theory]
+    [InlineData(MonthFormat.Numeric, @"custom-month=""numeric""")]
+    [InlineData(MonthFormat.TwoDigit, @"custom-month=""2-digit""")]
+    [InlineData(MonthFormat.Long, @"custom-month=""long""")]
+    [InlineData(MonthFormat.Short, @"custom-month=""short""")]
+    [InlineData(MonthFormat.Narrow, @"custom-month=""narrow""")]
+    public void NimbleTableColumnDateText_WithCustomMonthAttribute_HasTableMarkup(MonthFormat value, string expectedMarkupRegEx)
     {
-        var table = RenderWithPropertySet(x => x.CustomMonth, MonthFormat.Long);
-
-        var expectedMarkup = @"custom-month=""long""";
-        Assert.Contains(expectedMarkup, table.Markup);
+        var table = RenderWithPropertySet(x => x.CustomMonth, value);
+        Assert.Matches(expectedMarkupRegEx, table.Markup);
     }
 
-    [Fact]
-    public void NimbleTableColumnDateText_WithCustomDayAttribute_HasTableMarkup()
+    [Theory]
+    [InlineData(DayFormat.Numeric, @"custom-day=""numeric""")]
+    [InlineData(DayFormat.TwoDigit, @"custom-day=""2-digit""")]
+    public void NimbleTableColumnDateText_WithCustomDayAttribute_HasTableMarkup(DayFormat value, string expectedMarkupRegEx)
     {
-        var table = RenderWithPropertySet(x => x.CustomDay, DayFormat.Numeric);
-
-        var expectedMarkup = @"custom-day=""numeric""";
-        Assert.Contains(expectedMarkup, table.Markup);
+        var table = RenderWithPropertySet(x => x.CustomDay, value);
+        Assert.Matches(expectedMarkupRegEx, table.Markup);
     }
 
-    [Fact]
-    public void NimbleTableColumnDateText_WithCustomHourAttribute_HasTableMarkup()
+    [Theory]
+    [InlineData(HourFormat.Numeric, @"custom-hour=""numeric""")]
+    [InlineData(HourFormat.TwoDigit, @"custom-hour=""2-digit""")]
+    public void NimbleTableColumnDateText_WithCustomHourAttribute_HasTableMarkup(HourFormat value, string expectedMarkupRegEx)
     {
-        var table = RenderWithPropertySet(x => x.CustomHour, HourFormat.Numeric);
-
-        var expectedMarkup = @"custom-hour=""numeric""";
-        Assert.Contains(expectedMarkup, table.Markup);
+        var table = RenderWithPropertySet(x => x.CustomHour, value);
+        Assert.Matches(expectedMarkupRegEx, table.Markup);
     }
 
-    [Fact]
-    public void NimbleTableColumnDateText_WithCustomMinuteAttribute_HasTableMarkup()
+    [Theory]
+    [InlineData(MinuteFormat.Numeric, @"custom-minute=""numeric""")]
+    [InlineData(MinuteFormat.TwoDigit, @"custom-minute=""2-digit""")]
+    public void NimbleTableColumnDateText_WithCustomMinuteAttribute_HasTableMarkup(MinuteFormat value, string expectedMarkupRegEx)
     {
-        var table = RenderWithPropertySet(x => x.CustomMinute, MinuteFormat.TwoDigit);
-
-        var expectedMarkup = @"custom-minute=""2-digit""";
-        Assert.Contains(expectedMarkup, table.Markup);
+        var table = RenderWithPropertySet(x => x.CustomMinute, value);
+        Assert.Matches(expectedMarkupRegEx, table.Markup);
     }
 
-    [Fact]
-    public void NimbleTableColumnDateText_WithCustomSecondAttribute_HasTableMarkup()
+    [Theory]
+    [InlineData(SecondFormat.Numeric, @"custom-second=""numeric""")]
+    [InlineData(SecondFormat.TwoDigit, @"custom-second=""2-digit""")]
+    public void NimbleTableColumnDateText_WithCustomSecondAttribute_HasTableMarkup(SecondFormat value, string expectedMarkupRegEx)
     {
-        var table = RenderWithPropertySet(x => x.CustomSecond, SecondFormat.Numeric);
-
-        var expectedMarkup = @"custom-second=""numeric""";
-        Assert.Contains(expectedMarkup, table.Markup);
+        var table = RenderWithPropertySet(x => x.CustomSecond, value);
+        Assert.Matches(expectedMarkupRegEx, table.Markup);
     }
 
-    [Fact]
-    public void NimbleTableColumnDateText_WithCustomTimeZoneNameAttribute_HasTableMarkup()
+    [Theory]
+    [InlineData(TimeZoneNameFormat.Long, @"custom-time-zone-name=""long""")]
+    [InlineData(TimeZoneNameFormat.Short, @"custom-time-zone-name=""short""")]
+    [InlineData(TimeZoneNameFormat.LongGeneric, @"custom-time-zone-name=""longGeneric""")]
+    [InlineData(TimeZoneNameFormat.LongOffset, @"custom-time-zone-name=""longOffset""")]
+    [InlineData(TimeZoneNameFormat.ShortGeneric, @"custom-time-zone-name=""shortGeneric""")]
+    [InlineData(TimeZoneNameFormat.ShortOffset, @"custom-time-zone-name=""shortOffset""")]
+    public void NimbleTableColumnDateText_WithCustomTimeZoneNameAttribute_HasTableMarkup(TimeZoneNameFormat value, string expectedMarkupRegEx)
     {
-        var table = RenderWithPropertySet(x => x.CustomTimeZoneName, TimeZoneNameFormat.ShortGeneric);
-
-        var expectedMarkup = @"custom-time-zone-name=""shortGeneric""";
-        Assert.Contains(expectedMarkup, table.Markup);
+        var table = RenderWithPropertySet(x => x.CustomTimeZoneName, value);
+        Assert.Matches(expectedMarkupRegEx, table.Markup);
     }
 
-    [Fact]
-    public void NimbleTableColumnDateText_WithCustomFormatMatcherAttribute_HasTableMarkup()
+    [Theory]
+    [InlineData(FormatMatcherAlgorithm.BestFit, @"custom-format-matcher=""best fit""")]
+    [InlineData(FormatMatcherAlgorithm.Basic, @"custom-format-matcher=""basic""")]
+    public void NimbleTableColumnDateText_WithCustomFormatMatcherAttribute_HasTableMarkup(FormatMatcherAlgorithm value, string expectedMarkupRegEx)
     {
-        var table = RenderWithPropertySet(x => x.CustomFormatMatcher, FormatMatcherAlgorithm.Basic);
-
-        var expectedMarkup = @"custom-format-matcher=""basic""";
-        Assert.Contains(expectedMarkup, table.Markup);
+        var table = RenderWithPropertySet(x => x.CustomFormatMatcher, value);
+        Assert.Matches(expectedMarkupRegEx, table.Markup);
     }
 
     [Fact]
@@ -163,13 +172,14 @@ public class NimbleTableColumnDateTextTests
         Assert.Contains(expectedMarkup, table.Markup);
     }
 
-    [Fact]
-    public void NimbleTableColumnDateText_WithCustomDayPeriodAttribute_HasTableMarkup()
+    [Theory]
+    [InlineData(DayPeriodFormat.Long, @"custom-day-period=""long""")]
+    [InlineData(DayPeriodFormat.Short, @"custom-day-period=""short""")]
+    [InlineData(DayPeriodFormat.Narrow, @"custom-day-period=""narrow""")]
+    public void NimbleTableColumnDateText_WithCustomDayPeriodAttribute_HasTableMarkup(DayPeriodFormat value, string expectedMarkupRegEx)
     {
-        var table = RenderWithPropertySet(x => x.CustomDayPeriod, DayPeriodFormat.Narrow);
-
-        var expectedMarkup = @"custom-day-period=""narrow""";
-        Assert.Contains(expectedMarkup, table.Markup);
+        var table = RenderWithPropertySet(x => x.CustomDayPeriod, value);
+        Assert.Matches(expectedMarkupRegEx, table.Markup);
     }
 
     [Fact]
@@ -181,31 +191,37 @@ public class NimbleTableColumnDateTextTests
         Assert.Contains(expectedMarkup, table.Markup);
     }
 
-    [Fact]
-    public void NimbleTableColumnDateText_WithCustomDateStyleAttribute_HasTableMarkup()
+    [Theory]
+    [InlineData(DateStyle.Full, @"custom-date-style=""full""")]
+    [InlineData(DateStyle.Medium, @"custom-date-style=""medium""")]
+    [InlineData(DateStyle.Long, @"custom-date-style=""long""")]
+    [InlineData(DateStyle.Short, @"custom-date-style=""short""")]
+    public void NimbleTableColumnDateText_WithCustomDateStyleAttribute_HasTableMarkup(DateStyle value, string expectedMarkupRegEx)
     {
-        var table = RenderWithPropertySet(x => x.CustomDateStyle, DateStyle.Medium);
-
-        var expectedMarkup = @"custom-date-style=""medium""";
-        Assert.Contains(expectedMarkup, table.Markup);
+        var table = RenderWithPropertySet(x => x.CustomDateStyle, value);
+        Assert.Matches(expectedMarkupRegEx, table.Markup);
     }
 
-    [Fact]
-    public void NimbleTableColumnDateText_WithCustomTimeStyleAttribute_HasTableMarkup()
+    [Theory]
+    [InlineData(TimeStyle.Full, @"custom-time-style=""full""")]
+    [InlineData(TimeStyle.Medium, @"custom-time-style=""medium""")]
+    [InlineData(TimeStyle.Long, @"custom-time-style=""long""")]
+    [InlineData(TimeStyle.Short, @"custom-time-style=""short""")]
+    public void NimbleTableColumnDateText_WithCustomTimeStyleAttribute_HasTableMarkup(TimeStyle value, string expectedMarkupRegEx)
     {
-        var table = RenderWithPropertySet(x => x.CustomTimeStyle, TimeStyle.Long);
-
-        var expectedMarkup = @"custom-time-style=""long""";
-        Assert.Contains(expectedMarkup, table.Markup);
+        var table = RenderWithPropertySet(x => x.CustomTimeStyle, value);
+        Assert.Matches(expectedMarkupRegEx, table.Markup);
     }
 
-    [Fact]
-    public void NimbleTableColumnDateText_WithCustomHourCycleAttribute_HasTableMarkup()
+    [Theory]
+    [InlineData(HourCycleFormat.H11, @"custom-hour-cycle=""h11""")]
+    [InlineData(HourCycleFormat.H12, @"custom-hour-cycle=""h12""")]
+    [InlineData(HourCycleFormat.H23, @"custom-hour-cycle=""h23""")]
+    [InlineData(HourCycleFormat.H24, @"custom-hour-cycle=""h24""")]
+    public void NimbleTableColumnDateText_WithCustomHourCycleAttribute_HasTableMarkup(HourCycleFormat value, string expectedMarkupRegEx)
     {
-        var table = RenderWithPropertySet(x => x.CustomHourCycle, HourCycleFormat.H23);
-
-        var expectedMarkup = @"custom-hour-cycle=""h23""";
-        Assert.Contains(expectedMarkup, table.Markup);
+        var table = RenderWithPropertySet(x => x.CustomHourCycle, value);
+        Assert.Matches(expectedMarkupRegEx, table.Markup);
     }
 
     private IRenderedComponent<NimbleTableColumnDateText> RenderWithPropertySet<TProperty>(Expression<Func<NimbleTableColumnDateText, TProperty>> propertyGetter, TProperty propertyValue)
