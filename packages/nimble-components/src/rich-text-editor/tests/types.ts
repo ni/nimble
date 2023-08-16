748c780076a5e8b565a1ccd7fb9909c1abaaf723
Aagash Raaj <aagash.maridas@solitontech.com>
2023-08-16 15:52:06 +0530

    Remove types.ts file

--- packages/nimble-components/src/rich-text-editor/tests/types.ts
+++
@@ -1,6 +0,0 @@
-export const ExampleDataType = {
-    plainString: 'PlainString',
-    markdownString: 'MarkdownString'
-} as const;
-export type ExampleDataType =
-    (typeof ExampleDataType)[keyof typeof ExampleDataType];
