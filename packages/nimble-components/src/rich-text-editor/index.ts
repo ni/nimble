33778ebfb432bf42fc42c1b60a973366945faeb5
Vivin Krishna <123377523+vivinkrishna-ni@users.noreply.github.com>
2023-08-23 22:43:17 +0530

    Typo correction in the empty property comment in the code
    Co-authored-by: m-akinc <7282195+m-akinc@users.noreply.github.com>

--- packages/nimble-components/src/rich-text-editor/index.ts
+++ packages/nimble-components/src/rich-text-editor/index.ts
@@ -100,7 +100,7 @@ export class RichTextEditor extends FoundationElement implements ErrorPattern {
      * @public
      */
     public get empty(): boolean {
-        // Tiptap [isEmpty](https://tiptap.dev/api/editor#is-empty) returns true even if the editor has white spaces.
+        // Tiptap [isEmpty](https://tiptap.dev/api/editor#is-empty) returns false even if the editor has only whitespace.
         // However, the expectation is to return true if the editor is empty or contains only whitespace.
         // Hence, by retrieving the current text content using Tiptap state docs and then trimming the string to determine whether it is empty or not.
         return this.tiptapEditor.state.doc.textContent.trim().length === 0;
