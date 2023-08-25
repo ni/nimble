46169b217bad938ed54506f3ba640c9586dedd47
mollykreis <20542556+mollykreis@users.noreply.github.com>
2023-08-25 15:41:53 -0500

    Update decimal-formatter.spec.ts

--- packages/nimble-components/src/table-column/number-text/models/tests/decimal-formatter.spec.ts
+++ packages/nimble-components/src/table-column/number-text/models/tests/decimal-formatter.spec.ts
@@ -4,22 +4,25 @@ import { DecimalFormatter } from '../decimal-formatter';
 describe('DecimalFormatter', () => {
     const testCases: readonly {
         name: string,
-        decimalDigits?: number,
+        decimalDigits: number,
         value: number,
         expectedFormattedValue: string
     }[] = [
         {
             name: 'NEGATIVE_INFINITY renders as -∞',
+            decimalDigits: 1,
             value: Number.NEGATIVE_INFINITY,
             expectedFormattedValue: '-∞'
         },
         {
             name: 'POSITIVE_INFINITY renders as ∞',
+            decimalDigits: 1,
             value: Number.POSITIVE_INFINITY,
             expectedFormattedValue: '∞'
         },
         {
             name: 'NaN renders as NaN',
+            decimalDigits: 1,
             value: Number.NaN,
             expectedFormattedValue: 'NaN'
         },
@@ -64,9 +67,7 @@ describe('DecimalFormatter', () => {
             `${testCase.name}`,
             // eslint-disable-next-line @typescript-eslint/no-loop-func
             () => {
-                const formatter = new DecimalFormatter(
-                    testCase.decimalDigits ?? 2
-                );
+                const formatter = new DecimalFormatter(testCase.decimalDigits);
                 const formattedValue = formatter.formatValue(testCase.value);
                 expect(formattedValue).toEqual(testCase.expectedFormattedValue);
             }
