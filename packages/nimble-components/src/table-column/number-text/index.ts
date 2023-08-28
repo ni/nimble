40b952e1910a61a4532821033bfbd2de0e8309d4
mollykreis <20542556+mollykreis@users.noreply.github.com>
2023-08-28 15:47:53 -0500

    Number text decimal format (#1457)
    # Pull Request
    
    ## 🤨 Rationale
    
    This PR is part of #1011. It adds support for making a table column have
    `decimal` formatting, which includes:
    
    - Adding a new `format` value of `decimal`
    - Adding `decimal-digits` attribute
    - Adding validation that `decimal-digits` is in the allowed range
    - Making the default alignment of a `decimal` column `right`
    
    ## 👩‍💻 Implementation
    
    The general approach of these new capabilities is aligned with other
    columns that have been implemented. A few notes that might be of
    interest:
    
    - This PR adds a `TableColumnNumberTextValidator` to validate the
    `decimal-digits`. The column is invalid if its `format` is `decimal` and
    `decimal-digits` is outside of the supported range.
    - Added a new `DecimalFormatter` class that extends `NumberFormatter`.
    It differs slightly from the `RoundToIntegerFormatter` and
    `DecimalFormatter` in that the `formatter` is not static because the
    number of decimals being rendered can vary between columns.
    
    ## 🧪 Testing
    
    - New unit tests
    - Added additional functionality to storybook story & manually verified
    it was correct
    - Added another column to the matrix tests to show the decimal format
    
    ## ✅ Checklist
    
    <!--- Review the list and put an x in the boxes that apply or ~~strike
    through~~ around items that don't (along with an explanation). -->
    
    - [ ] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

--- packages/nimble-components/src/table-column/number-text/index.ts
+++ packages/nimble-components/src/table-column/number-text/index.ts
@@ -1,10 +1,10 @@
 import { DesignSystem } from '@microsoft/fast-foundation';
-import { attr } from '@microsoft/fast-element';
+import { attr, nullableNumberConverter } from '@microsoft/fast-element';
 import { styles } from '../base/styles';
 import { template } from '../base/template';
 import type { TableNumberField } from '../../table/types';
 import { TableColumnTextBase } from '../text-base';
-import { TableColumnSortOperation } from '../base/types';
+import { TableColumnSortOperation, TableColumnValidity } from '../base/types';
 import { tableColumnNumberTextGroupHeaderTag } from './group-header-view';
 import { tableColumnNumberTextCellViewTag } from './cell-view';
 import type { ColumnInternalsOptions } from '../base/models/column-internals';
@@ -12,6 +12,8 @@ import { NumberTextAlignment, NumberTextFormat } from './types';
 import type { NumberFormatter } from './models/number-formatter';
 import { RoundToIntegerFormatter } from './models/round-to-integer-formatter';
 import { DefaultFormatter } from './models/default-formatter';
+import { DecimalFormatter } from './models/decimal-formatter';
+import { TableColumnNumberTextValidator } from './models/table-column-number-text-validitor';
 import { TextCellViewBaseAlignment } from '../text-base/cell-view/types';
 
 export type TableColumnNumberTextCellRecord = TableNumberField<'value'>;
@@ -26,19 +28,31 @@ declare global {
     }
 }
 
+const defaultDecimalDigits = 2;
+
 /**
  * The table column for displaying numbers as text.
  */
 export class TableColumnNumberText extends TableColumnTextBase {
+    /** @internal */
+    public validator = new TableColumnNumberTextValidator(this.columnInternals);
+
     @attr
     public format: NumberTextFormat;
 
     @attr
     public alignment: NumberTextAlignment;
 
+    @attr({ attribute: 'decimal-digits', converter: nullableNumberConverter })
+    public decimalDigits?: number;
+
     public override connectedCallback(): void {
         super.connectedCallback();
         this.updateColumnConfig();
+    }
+
+    public override get validity(): TableColumnValidity {
+        return this.validator.getValidity();
     }
 
     protected override getColumnInternalsOptions(): ColumnInternalsOptions {
@@ -59,18 +73,32 @@ export class TableColumnNumberText extends TableColumnTextBase {
         this.updateColumnConfig();
     }
 
+    private decimalDigitsChanged(): void {
+        this.updateColumnConfig();
+    }
+
     private updateColumnConfig(): void {
-        const columnConfig: TableColumnNumberTextColumnConfig = {
-            formatter: this.createFormatter(),
-            alignment: this.determineCellContentAlignment()
-        };
-        this.columnInternals.columnConfig = columnConfig;
+        this.validator.validateDecimalDigits(this.format, this.decimalDigits);
+
+        if (this.validator.isValid()) {
+            const columnConfig: TableColumnNumberTextColumnConfig = {
+                formatter: this.createFormatter(),
+                alignment: this.determineCellContentAlignment()
+            };
+            this.columnInternals.columnConfig = columnConfig;
+        } else {
+            this.columnInternals.columnConfig = undefined;
+        }
     }
 
     private createFormatter(): NumberFormatter {
         switch (this.format) {
             case NumberTextFormat.roundToInteger:
                 return new RoundToIntegerFormatter();
+            case NumberTextFormat.decimal:
+                return new DecimalFormatter(
+                    this.decimalDigits ?? defaultDecimalDigits
+                );
             default:
                 return new DefaultFormatter();
         }
@@ -86,7 +114,10 @@ export class TableColumnNumberText extends TableColumnTextBase {
         }
 
         // Look at format to determine the default alignment
-        if (this.format === NumberTextFormat.roundToInteger) {
+        if (
+            this.format === NumberTextFormat.roundToInteger
+            || this.format === NumberTextFormat.decimal
+        ) {
             return TextCellViewBaseAlignment.right;
         }
         return TextCellViewBaseAlignment.left;
