31afcf40d43efbbc25acf11a7a1dd8f0ec67778b
Aagash Raaj <123377167+aagash-ni@users.noreply.github.com>
2023-08-30 23:36:34 +0530

    Address PR Comments

--- packages/nimble-components/src/rich-text-editor/tests/rich-text-editor-labels.spec.ts
+++ packages/nimble-components/src/rich-text-editor/tests/rich-text-editor-labels.spec.ts
@@ -79,7 +79,6 @@ describe('Rich Text Editor with LabelProviderRichTextEditor', () => {
 
     for (const value of formattingButtons) {
         const specType = getSpecTypeByNamedList(value, focused, disabled);
-        // eslint-disable-next-line @typescript-eslint/no-loop-func
         specType(
             `uses correct label '${value.label}' for ${value.name} button`,
             // eslint-disable-next-line @typescript-eslint/no-loop-func
