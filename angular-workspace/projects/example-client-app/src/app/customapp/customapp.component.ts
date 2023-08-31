38a495ff56b8fb86e292c6c2e8ea1ee04a7a4155
mollykreis <20542556+mollykreis@users.noreply.github.com>
2023-08-31 18:09:23 -0500

    Angular support for number-text table column (#1468)
    # Pull Request
    
    ## 🤨 Rationale
    
    This is part of #1011.
    
    It adds Angular support for the number-text column.
    
    ## 👩‍💻 Implementation
    
    This PR adds Angular support for the number-text column. The column
    isn't complete yet in `nimble-components`, but this adds Angular support
    for what is implemented so far.
    
    ## 🧪 Testing
    
    - Verified the column works as expected in the Angular example app.
    - Unit tests for the directive.
    - Used locally built package to verify the column can be leveraged in
    SLE
    
    ## ✅ Checklist
    
    <!--- Review the list and put an x in the boxes that apply or ~~strike
    through~~ around items that don't (along with an explanation). -->
    
    - [ ] I have updated the project documentation to reflect my changes or
    determined no changes are needed.
    
    ---------
    
    Co-authored-by: m-akinc <7282195+m-akinc@users.noreply.github.com>

--- angular-workspace/projects/example-client-app/src/app/customapp/customapp.component.ts
+++ angular-workspace/projects/example-client-app/src/app/customapp/customapp.component.ts
@@ -20,6 +20,7 @@ interface SimpleTableRecord extends TableRecord {
     date: number;
     statusCode: number;
     result: string;
+    number: number;
 }
 
 @Component({
@@ -131,7 +132,8 @@ export class CustomAppComponent {
             linkLabel: 'Link',
             date: (tableData.length % 2 === 0) ? new Date(2023, 7, 16, 3, 56, 11).valueOf() : new Date(2022, 2, 7, 20, 28, 41).valueOf(),
             statusCode: (tableData.length % 2 === 0) ? 100 : 101,
-            result: (tableData.length % 2 === 0) ? 'success' : 'unknown'
+            result: (tableData.length % 2 === 0) ? 'success' : 'unknown',
+            number: tableData.length / 10
         });
         this.tableDataSubject.next(tableData);
     }
---
+++ angular-workspace/projects/ni/nimble-angular/table-column/number-text/public-api.ts
@@ -0,0 +1,2 @@
+export * from './nimble-table-column-number-text.directive';
+export * from './nimble-table-column-number-text.module';
