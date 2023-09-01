9d1a1babfb9b5136b4cb12a9a39207e90e054857
Jesse Attas <jattasNI@users.noreply.github.com>
2023-09-01 17:25:55 -0500

    Add mobile toolbar story

--- packages/nimble-components/src/toolbar/tests/toolbar.stories.ts
+++ packages/nimble-components/src/toolbar/tests/toolbar.stories.ts
@@ -3,10 +3,14 @@ import type { Meta, StoryObj } from '@storybook/html';
 import { createUserSelectedThemeStory } from '../../utilities/tests/storybook';
 import { toolbarTag } from '..';
 import { buttonTag } from '../../button';
+import { menuTag } from '../../menu';
+import { menuButtonTag } from '../../menu-button';
+import { menuItemTag } from '../../menu-item';
 import { iconCogTag } from '../../icons/cog';
 import { iconEyeTag } from '../../icons/eye';
 import { iconFilterTag } from '../../icons/filter';
 import { iconPencilTag } from '../../icons/pencil';
+import { iconThreeDotsLineTag } from '../../icons/three-dots-line';
 import { iconTrashTag } from '../../icons/trash';
 
 const overviewText = `Per [W3C](https://w3c.github.io/aria-practices/#toolbar) - A toolbar is a container
@@ -25,14 +29,18 @@ ${toolbarTag}::part(positioning-region) {
 
 const metadata: Meta = {
     title: 'Components/Toolbar',
-    tags: ['autodocs'],
     parameters: {
         docs: {
             description: {
                 component: overviewText
             }
         }
-    },
+    }
+};
+
+export default metadata;
+
+export const toolbar: StoryObj = {
     // prettier-ignore
     render: createUserSelectedThemeStory(html`
         <${toolbarTag}>
@@ -61,6 +69,26 @@ const metadata: Meta = {
     `)
 };
 
-export default metadata;
-
-export const toolbar: StoryObj = {};
+export const mobileToolbar: StoryObj = {
+    // prettier-ignore
+    render: createUserSelectedThemeStory(html`
+        <div style="width: 350px; height: 200px;">
+            <${toolbarTag}>
+                <${buttonTag} appearance="ghost" slot="start">
+                    <${iconEyeTag} slot="start"></${iconEyeTag}>
+                    View
+                </${buttonTag}>
+                <${menuButtonTag} appearance="ghost" slot="end" content-hidden title="More">
+                    More
+                    <${iconThreeDotsLineTag} slot="start"></${iconThreeDotsLineTag}>
+                    <${menuTag} slot="menu">
+                        <${menuItemTag}>Delete</${menuItemTag}>
+                        <${menuItemTag}>Edit</${menuItemTag}>
+                        <${menuItemTag}>Settings</${menuItemTag}>
+                        <${menuItemTag}>Filter</${menuItemTag}>
+                    </${menuTag}>
+                </${menuButtonTag}>
+            </${toolbarTag}>
+        </div>
+    `)
+};
