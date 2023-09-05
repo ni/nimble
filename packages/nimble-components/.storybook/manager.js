a9a71ca3ffb63a3ccdacaa16b279c5795aab341c
Jesse Attas <jattasNI@users.noreply.github.com>
2023-09-05 11:15:27 -0500

    Don't render patterns or internal stories

--- packages/nimble-components/.storybook/manager.js
+++ packages/nimble-components/.storybook/manager.js
@@ -3,5 +3,15 @@ import theme from './theme';
 
 addons.setConfig({
     enableShortcuts: false,
+    sidebar: {
+        filters: {
+            patterns: item => {
+                return (
+                    !item.title.startsWith('Internal/')
+                    && !item.title.startsWith('patterns')
+                );
+            }
+        }
+    },
     theme
 });
