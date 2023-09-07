151fa6bfc2bfc09651bd1a689f655945312a3ee7
rajsite <rajsite@users.noreply.github.com>
2023-09-07 13:14:16 -0500

    Add rollup onwarn handler to suppress d3 warnings

--- packages/nimble-components/rollup.config.js
+++ packages/nimble-components/rollup.config.js
@@ -16,6 +16,28 @@ const umdProductionPlugin = () => replace({
     preventAssignment: true
 });
 
+// d3 has circular dependencies it won't remove:
+// https://github.com/d3/d3-selection/issues/168
+// So ignore just d3's circular dependencies following the pattern from:
+// https://github.com/rollup/rollup/issues/1089#issuecomment-635564942
+// Updated to use current onwarn api:
+// https://rollupjs.org/configuration-options/#onwarn
+const onwarn = (warning, defaultHandler) => {
+    const ignoredWarnings = [
+        {
+            code: 'CIRCULAR_DEPENDENCY',
+            file: 'node_modules/d3-',
+        },
+    ];
+
+    if (!ignoredWarnings.some(({ code, file }) => (
+        warning.code === code
+        && warning.message.includes(file)))
+    ) {
+        defaultHandler(warning);
+    }
+};
+
 // eslint-disable-next-line import/no-default-export
 export default [
     {
@@ -32,7 +54,8 @@ export default [
             resolve(),
             commonJS(),
             json()
-        ]
+        ],
+        onwarn
     },
     {
         input: 'dist/esm/all-components.js',
@@ -55,6 +78,7 @@ export default [
             resolve(),
             commonJS(),
             json()
-        ]
+        ],
+        onwarn
     }
 ];
