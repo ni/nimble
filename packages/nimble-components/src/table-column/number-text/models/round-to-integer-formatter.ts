b49755bcb4948910c5b896b7c11abb64c6aca2cf
mollykreis <20542556+mollykreis@users.noreply.github.com>
2023-09-12 11:58:04 -0500

    Add locale support to number-text column (#1499)
    # Pull Request
    
    ## 🤨 Rationale
    
    This is part of #1011 
    
    This PR adds support for the number-text column using the `lang` token
    on the theme provider to format numbers in a locale-aware way.
    
    ## 👩‍💻 Implementation
    
    I followed the same pattern used in the date-text column to listen for
    changes to the `lang` theme provider token and pass that into the
    formatters for the number-text column.
    
    ## 🧪 Testing
    
    - Manually tested in storybook
    - New unit test for the column that verifies the formatter is updated
    with the `lang` changes
    - New unit tests for each formatter that verify the locale is used when
    formatting
    - Note: I updated the formatter tests to run all the test cases with an
    additional locale. On one hand this potentially seemed like "too many"
    locale tests, but I thought it was the best way to ensure that the
    locale was used in all internal formatters (e.g. the `default` formatter
    uses 3 formatters internally) and that we had coverage for a wide range
    of possible values to be formatted.
    
    ## ✅ Checklist
    
    <!--- Review the list and put an x in the boxes that apply or ~~strike
    through~~ around items that don't (along with an explanation). -->
    
    - [ ] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

--- packages/nimble-components/src/table-column/number-text/models/round-to-integer-formatter.ts
+++ packages/nimble-components/src/table-column/number-text/models/round-to-integer-formatter.ts
@@ -4,16 +4,21 @@ import { NumberFormatter } from './number-formatter';
  * The formatter for a number-text column whose format is configured to be 'roundToInteger'.
  */
 export class RoundToIntegerFormatter extends NumberFormatter {
-    private static readonly formatter = new Intl.NumberFormat(undefined, {
-        maximumFractionDigits: 0,
-        useGrouping: true
-    });
+    private readonly formatter: Intl.NumberFormat;
+
+    public constructor(locale: string) {
+        super();
+        this.formatter = new Intl.NumberFormat(locale, {
+            maximumFractionDigits: 0,
+            useGrouping: true
+        });
+    }
 
     protected format(number: number): string {
         // The NumberFormat option of `signDisplay: "negative"` is not supported in all browsers nimble supports.
         // Because that option cannot be used to avoid rendering "-0", coerce the values that will round to -0 to 0
         // prior to formatting.
         const valueToFormat = Math.round(number) === 0 ? 0 : number;
-        return RoundToIntegerFormatter.formatter.format(valueToFormat);
+        return this.formatter.format(valueToFormat);
     }
 }
