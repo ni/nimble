5f38265dd29aea52058ee9aceeb0e296b96a2e05
Vivin Krishna <123377523+vivinkrishna-ni@users.noreply.github.com>
2023-09-12 19:33:30 +0530

    Rich text editor | Setting height in storybook docs (#1513)
    # Pull Request
    
    ## 🤨 Rationale
    
    <!---
    Provide some background and a description of your work.
    What problem does this change solve?
    
    Include links to issues, work items, or other discussions.
    -->
    1. To provide more spacing and facilitate the exploration of various
    functionalities of the rich text editor, setting a default height in
    storybook docs until enabling the `fit-to-content` attribute.
    2. To make the content, such as the links
    (https://github.com/ni/nimble/pull/1496#discussion_r1322114329), visible
    if any are added as part of the test, height is added for storybook
    tests.
    
    ## 👩‍💻 Implementation
    
    <!---
    Describe how the change addresses the problem. Consider factors such as
    complexity, alternative solutions, performance impact, etc.
    
    Consider listing files with important changes or comment on them
    directly in the pull request.
    -->
    1. Add `height: 160px;` to the rich text editor storybook doc.
    2. Add height for the theme matrix test to isolate the Chromatic test
    difference from the https://github.com/ni/nimble/pull/1496 PR.
    
    ## 🧪 Testing
    
    <!---
    Detail the testing done to ensure this submission meets requirements. 
    
    Include automated/manual test additions or modifications, testing done
    on a local build, private CI run results, and additional testing not
    covered by automatic pull request validation. If any functionality is
    not covered by automated testing, provide justification.
    -->
    Verified manually the differences in the height of the component in
    storybook docs and tests.
    
    ## ✅ Checklist
    
    <!--- Review the list and put an x in the boxes that apply or ~~strike
    through~~ around items that don't (along with an explanation). -->
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

--- packages/nimble-components/src/rich-text/editor/tests/rich-text-editor.stories.ts
+++ packages/nimble-components/src/rich-text/editor/tests/rich-text-editor.stories.ts
@@ -86,6 +86,7 @@ const metadata: Meta<RichTextEditorArgs> = {
     })}
     <${richTextEditorTag}
         ${ref('editorRef')}
+        style="height: 160px;"
         data-unused="${x => x.setMarkdownData(x)}"
         ?disabled="${x => x.disabled}"
         ?footer-hidden="${x => x.footerHidden}"
