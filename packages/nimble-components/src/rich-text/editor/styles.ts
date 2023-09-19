1517aec9453c747243da4391427916fa4d578205
Vivin Krishna <123377523+vivinkrishna-ni@users.noreply.github.com>
2023-09-19 11:36:53 +0530

    Removed pointer-events in slot element

--- packages/nimble-components/src/rich-text/editor/styles.ts
+++ packages/nimble-components/src/rich-text/editor/styles.ts
@@ -250,16 +250,6 @@ export const styles = css`
         gap: ${standardPadding};
         place-items: center;
     }
-    ${
-        /**
-         * Restricting the pointer-events for the slot="start" where the icon is rendered.
-         * This can be removed after the below issue is fixed.
-         * https://github.com/ni/nimble/issues/1422
-         */ ''
-    }
-    nimble-toggle-button::part(start) {
-        pointer-events: none;
-    }
 
     :host([error-visible]) .error-icon {
         display: none;
