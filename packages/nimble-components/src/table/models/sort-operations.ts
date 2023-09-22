520cf8241b8797500025af25384c0f3e5c281416
mollykreis <20542556+mollykreis@users.noreply.github.com>
2023-09-22 11:56:13 -0500

    Fix bug where undefined sorted to wrong position in number-text column

--- packages/nimble-components/src/table/models/sort-operations.ts
+++ packages/nimble-components/src/table/models/sort-operations.ts
@@ -1,6 +1,5 @@
-import {
+import type {
     SortingFnOption as TanStackSortingFnOption,
-    sortingFns as TanStackSortingFns,
     Row as TanStackRow
 } from '@tanstack/table-core';
 import { TableColumnSortOperation } from '../../table-column/base/types';
@@ -14,11 +13,11 @@ export function getTanStackSortingFunction<TData>(
 ): TanStackSortingFnOption<TData> {
     switch (sortOperation) {
         case TableColumnSortOperation.basic:
-            return TanStackSortingFns.basic;
+            return basicSortFunction;
         case TableColumnSortOperation.localeAwareCaseSensitive:
             return localeAwareCaseSensitiveSortFunction;
         default:
-            return TanStackSortingFns.basic;
+            return basicSortFunction;
     }
 }
 
@@ -45,4 +44,36 @@ function localeAwareCaseSensitiveSortFunction<TData>(
         return -1;
     }
     return 1;
+}
+
+/**
+ * A function to perform a basic sort of two rows from TanStack for a given column.
+ * The function sorts `undefined` followed by `null` before all other values that are
+ * ordered using the `>` operator.
+ */
+function basicSortFunction<TData>(
+    rowA: TanStackRow<TData>,
+    rowB: TanStackRow<TData>,
+    columnId: string
+): number {
+    const valueA = rowA.getValue<string | number | boolean | null | undefined>(columnId);
+    const valueB = rowB.getValue<string | number | boolean | null | undefined>(columnId);
+
+    if (valueA === valueB) {
+        return 0;
+    }
+    if (valueA === undefined) {
+        return -1;
+    }
+    if (valueB === undefined) {
+        return 1;
+    }
+    if (valueA === null) {
+        return -1;
+    }
+    if (valueB === null) {
+        return 1;
+    }
+
+    return valueA > valueB ? 1 : -1;
 }
---
+++ packages/nimble-components/src/table/models/tests/sort-operations.spec.ts
@@ -0,0 +1,83 @@
+import type {
+    Row as TanStackRow,
+    SortingFn
+} from '@tanstack/table-core';
+import { TableColumnSortOperation } from '../../../table-column/base/types';
+import { getTanStackSortingFunction } from '../sort-operations';
+import type { TableFieldValue } from '../../types';
+
+describe('Sort Operations', () => {
+    function createFakeRow(value: TableFieldValue): TanStackRow<TableFieldValue> {
+        const row = jasmine.createSpyObj<TanStackRow<TableFieldValue>>('TanStackRow', ['getValue']);
+        row.getValue.and.returnValue(value);
+        return row;
+    }
+
+    const basicNumericSortOrder = [
+        { value: undefined, name: 'undefined' },
+        { value: null, name: 'null' },
+        { value: Number.NEGATIVE_INFINITY, name: '-∞' },
+        { value: Number.MIN_SAFE_INTEGER, name: 'MIN_SAFE_INTEGER' },
+        { value: -9999, name: '-9999' },
+        { value: -3, name: '-3' },
+        { value: 0, name: '0' },
+        { value: Number.EPSILON, name: 'EPSILON' },
+        { value: 3, name: '3' },
+        { value: 9999, name: '9999' },
+        { value: Number.MAX_SAFE_INTEGER, name: 'MAX_SAFE_INTEGER' },
+        { value: Number.POSITIVE_INFINITY, name: '∞' }
+    ] as const;
+    const basicBooleanSortOrder = [
+        { value: undefined, name: 'undefined' },
+        { value: null, name: 'null' },
+        { value: false, name: 'false' },
+        { value: true, name: 'true' }
+    ] as const;
+    const basicStringSortOrder = [
+        { value: undefined, name: 'undefined' },
+        { value: null, name: 'null' },
+        { value: '', name: '\'\'' },
+        { value: 'abc', name: 'abc' },
+        { value: 'zzz', name: 'zzz' }
+    ] as const;
+
+    const localeAwareCaseSensitiveSortOrder = [
+        { value: undefined, name: 'undefined' },
+        { value: null, name: 'null' },
+        { value: '', name: '\'\'' },
+        { value: 'abc', name: 'abc' },
+        { value: 'ABC', name: 'ABC' },
+        { value: 'hello world', name: 'hello WORLD' },
+        { value: 'xyz', name: 'xyz' }
+    ];
+
+    const sortOrderTestCases = [
+        { sortOperation: TableColumnSortOperation.basic, testCases: [basicNumericSortOrder, basicBooleanSortOrder, basicStringSortOrder] },
+        { sortOperation: TableColumnSortOperation.localeAwareCaseSensitive, testCases: [localeAwareCaseSensitiveSortOrder] }
+    ] as const;
+    for (const sortOrderTestCase of sortOrderTestCases) {
+        for (const expectedSortOrder of sortOrderTestCase.testCases) {
+            for (let i = 0; i < expectedSortOrder.length; i++) {
+                for (let j = 0; j < expectedSortOrder.length; j++) {
+                    const sortOrderA = expectedSortOrder[i]!;
+                    const sortOrderB = expectedSortOrder[j]!;
+                    it(`comparing ${sortOrderA.name} to ${sortOrderB.name} returns expected value`, () => {
+                        let expectedSortResult: number;
+                        if (i === j) {
+                            expectedSortResult = 0;
+                        } else if (i > j) {
+                            expectedSortResult = 1;
+                        } else {
+                            expectedSortResult = -1;
+                        }
+
+                        const sortFunction = getTanStackSortingFunction(sortOrderTestCase.sortOperation) as SortingFn<TableFieldValue>;
+                        const rowA = createFakeRow(sortOrderA.value);
+                        const rowB = createFakeRow(sortOrderB.value);
+                        expect(sortFunction(rowA, rowB, '')).toBe(expectedSortResult);
+                    });
+                }
+            }
+        }
+    }
+});
