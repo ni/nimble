aba482ed866abbfd5d74e45a26b5f71fa728d5e4
mollykreis <20542556+mollykreis@users.noreply.github.com>
2023-09-25 16:41:25 -0500

    restructure tests

--- packages/nimble-components/src/table/models/tests/sort-operations.spec.ts
+++ packages/nimble-components/src/table/models/tests/sort-operations.spec.ts
@@ -2,6 +2,7 @@ import type { Row as TanStackRow, SortingFn } from '@tanstack/table-core';
 import { TableColumnSortOperation } from '../../../table-column/base/types';
 import { getTanStackSortingFunction } from '../sort-operations';
 import type { TableFieldValue } from '../../types';
+import { parameterizeNamedList } from '../../../utilities/tests/parameterized';
 
 describe('Sort Operations', () => {
     function createFakeRow(
@@ -15,85 +16,86 @@ describe('Sort Operations', () => {
         return row;
     }
 
-    const basicNumericSortOrder = [
-        { value: undefined, name: 'undefined' },
-        { value: null, name: 'null' },
-        { value: Number.NEGATIVE_INFINITY, name: '-∞' },
-        { value: Number.MIN_SAFE_INTEGER, name: 'MIN_SAFE_INTEGER' },
-        { value: -9999, name: '-9999' },
-        { value: -3, name: '-3' },
-        { value: 0, name: '0' },
-        { value: Number.EPSILON, name: 'EPSILON' },
-        { value: 3, name: '3' },
-        { value: 9999, name: '9999' },
-        { value: Number.MAX_SAFE_INTEGER, name: 'MAX_SAFE_INTEGER' },
-        { value: Number.POSITIVE_INFINITY, name: '∞' }
-    ] as const;
-    const basicBooleanSortOrder = [
-        { value: undefined, name: 'undefined' },
-        { value: null, name: 'null' },
-        { value: false, name: 'false' },
-        { value: true, name: 'true' }
-    ] as const;
-    const basicStringSortOrder = [
-        { value: undefined, name: 'undefined' },
-        { value: null, name: 'null' },
-        { value: '', name: "''" },
-        { value: 'abc', name: 'abc' },
-        { value: 'zzz', name: 'zzz' }
-    ] as const;
-
-    const localeAwareCaseSensitiveSortOrder = [
-        { value: undefined, name: 'undefined' },
-        { value: null, name: 'null' },
-        { value: '', name: "''" },
-        { value: 'abc', name: 'abc' },
-        { value: 'ABC', name: 'ABC' },
-        { value: 'hello world', name: 'hello WORLD' },
-        { value: 'xyz', name: 'xyz' }
-    ];
-
-    const sortOrderTestCases = [
+    const testCases = [
         {
+            name: 'basic sorting with numbers',
             sortOperation: TableColumnSortOperation.basic,
-            testCases: [
-                basicNumericSortOrder,
-                basicBooleanSortOrder,
-                basicStringSortOrder
+            expectedSortOrder: [
+                undefined,
+                null,
+                Number.NEGATIVE_INFINITY,
+                Number.MIN_SAFE_INTEGER,
+                -9999,
+                -3,
+                0,
+                Number.EPSILON,
+                3,
+                9999,
+                Number.MAX_SAFE_INTEGER,
+                Number.POSITIVE_INFINITY
             ]
         },
         {
+            name: 'basic sorting with booleans',
+            sortOperation: TableColumnSortOperation.basic,
+            expectedSortOrder: [
+                undefined,
+                null,
+                false,
+                true
+            ]
+        },
+        {
+            name: 'basic sorting with strings',
+            sortOperation: TableColumnSortOperation.basic,
+            expectedSortOrder: [
+                undefined,
+                null,
+                '',
+                'abc',
+                'zzz'
+            ]
+        },
+        {
+            name: 'locale aware case sensitive sorting',
             sortOperation: TableColumnSortOperation.localeAwareCaseSensitive,
-            testCases: [localeAwareCaseSensitiveSortOrder]
+            expectedSortOrder: [
+                undefined,
+                null,
+                '',
+                'abc',
+                'ABC',
+                'hello world',
+                'xyz'
+            ]
         }
     ] as const;
-    for (const sortOrderTestCase of sortOrderTestCases) {
-        for (const expectedSortOrder of sortOrderTestCase.testCases) {
-            for (let i = 0; i < expectedSortOrder.length; i++) {
-                for (let j = 0; j < expectedSortOrder.length; j++) {
-                    const sortOrderA = expectedSortOrder[i]!;
-                    const sortOrderB = expectedSortOrder[j]!;
-                    it(`comparing ${sortOrderA.name} to ${sortOrderB.name} returns expected value`, () => {
-                        let expectedSortResult: number;
-                        if (i === j) {
-                            expectedSortResult = 0;
-                        } else if (i > j) {
-                            expectedSortResult = 1;
-                        } else {
-                            expectedSortResult = -1;
-                        }
+    parameterizeNamedList(testCases, (spec, name, value) => {
+        spec(name, () => {
+            for (let i = 0; i < value.expectedSortOrder.length; i++) {
+                for (let j = 0; j < value.expectedSortOrder.length; j++) {
+                    const valueA = value.expectedSortOrder[i]!;
+                    const valueB = value.expectedSortOrder[j]!;
 
-                        const sortFunction = getTanStackSortingFunction(
-                            sortOrderTestCase.sortOperation
-                        ) as SortingFn<TableFieldValue>;
-                        const rowA = createFakeRow(sortOrderA.value);
-                        const rowB = createFakeRow(sortOrderB.value);
-                        expect(sortFunction(rowA, rowB, '')).toBe(
-                            expectedSortResult
-                        );
-                    });
+                    let expectedSortResult: number;
+                    if (i === j) {
+                        expectedSortResult = 0;
+                    } else if (i > j) {
+                        expectedSortResult = 1;
+                    } else {
+                        expectedSortResult = -1;
+                    }
+
+                    const sortFunction = getTanStackSortingFunction(
+                        value.sortOperation
+                    ) as SortingFn<TableFieldValue>;
+                    const rowA = createFakeRow(valueA);
+                    const rowB = createFakeRow(valueB);
+                    expect(sortFunction(rowA, rowB, '')).toBe(
+                        expectedSortResult
+                    );
                 }
             }
-        }
-    }
+        });
+    });
 });
