42491203eeb98298795bb28d613a481fc187485c
Malcolm Smith <20709258+msmithNI@users.noreply.github.com>
2023-10-23 14:42:57 -0500

    Angular integration

---
+++ angular-workspace/projects/ni/nimble-angular/pipes/duration.pipe.spec.ts
@@ -0,0 +1,50 @@
+import { DurationPipe } from './duration.pipe';
+
+describe('DurationPipe', () => {
+    describe('basic formatting (English)', () => {
+        let pipe: DurationPipe;
+
+        beforeEach(() => {
+            pipe = new DurationPipe('en');
+        });
+
+        const testCases = [
+            {
+                name: 'null',
+                value: null,
+                expected: ''
+            },
+            {
+                name: 'undefined',
+                value: undefined,
+                expected: ''
+            },
+            {
+                name: '1d 2h 3m 4.56s (number)',
+                value: (4.56 + 3 * 60 + 2 * 3600 + 1 * 24 * 3600) * 1000,
+                expected: '1 day, 2 hr, 3 min, 4.56 sec'
+            },
+            {
+                name: '1d 2h 3m 4.56s (as string "93784560")',
+                value: '93784560',
+                expected: '1 day, 2 hr, 3 min, 4.56 sec'
+            },
+        ];
+
+        testCases.forEach(test => {
+            it(`${test.name} transforms to ${test.expected}`, () => {
+                const result = pipe.transform(test.value);
+                expect(result).toEqual(test.expected);
+            });
+        });
+    });
+
+    describe('localized formatting', () => {
+        it('for "de" locale, transforms "1d 2h 3m 4.56s" correctly', () => {
+            const pipe = new DurationPipe('de');
+            const milliseconds = (4.56 + 3 * 60 + 2 * 3600 + 1 * 24 * 3600) * 1000;
+            const transform = pipe.transform(milliseconds);
+            expect(transform).toEqual('1 Tg., 2 Std., 3 Min., 4,56 Sek.');
+        });
+    });
+});
---
+++ angular-workspace/projects/ni/nimble-angular/pipes/duration.pipe.ts
@@ -0,0 +1,29 @@
+import { Inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
+import { DurationFormatter } from '@ni/nimble-components/dist/esm/table-column/duration-text/models/duration-formatter';
+
+/**
+ * A pipe that transforms a number of milliseconds (string/number) into a string formatted like "1 day, 2 hr, 3 min, 4.567 sec"
+ */
+@Pipe({
+    name: 'duration'
+})
+export class DurationPipe implements PipeTransform {
+    private readonly durationFormatter;
+
+    public constructor(@Inject(LOCALE_ID) locale: string) {
+        this.durationFormatter = new DurationFormatter(locale);
+    }
+
+    public transform(timeInMilliseconds: string | number | null | undefined): string {
+        if (timeInMilliseconds == null) {
+            return '';
+        }
+
+        const milliseconds = this.parseDuration(timeInMilliseconds);
+        return this.durationFormatter.format(milliseconds);
+    }
+
+    private parseDuration(duration: string | number): number {
+        return (typeof duration === 'string') ? parseFloat(duration) : duration;
+    }
+}
