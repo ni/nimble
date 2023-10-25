6b7d6467f6a560d12fd630cf40e329f4290cb00b
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-10-25 15:38:18 -0500

    Blazor components for byte unit and volt unit

---
+++ packages/nimble-blazor/NimbleBlazor/Components/NimbleUnitByte.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NimbleBlazor;
+
+public partial class NimbleUnitByte : ComponentBase
+{
+    /// <summary>
+    /// Gets or sets whether the bytes units are 1024-based (rather than 1000-based).
+    /// </summary>
+    [Parameter]
+    public bool? Binary { get; set; }
+
+    /// <summary>
+    /// Gets or sets a collection of additional attributes that will be applied to the created element.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
+}
---
+++ packages/nimble-blazor/NimbleBlazor/Components/NimbleUnitVolt.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NimbleBlazor;
+
+public partial class NimbleUnitVolt : ComponentBase
+{
+    /// <summary>
+    /// Gets or sets a collection of additional attributes that will be applied to the created element.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
+}
