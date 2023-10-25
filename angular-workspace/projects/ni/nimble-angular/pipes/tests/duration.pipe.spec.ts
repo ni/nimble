edc637a708c5f9e5949715db5d3087a61fd260f2
Malcolm Smith <20709258+msmithNI@users.noreply.github.com>
2023-10-25 18:07:19 -0500

    PR feedback

--- angular-workspace/projects/ni/nimble-angular/pipes/tests/duration.pipe.spec.ts
+++ angular-workspace/projects/ni/nimble-angular/pipes/tests/duration.pipe.spec.ts
@@ -16,6 +16,11 @@ describe('DurationPipe', () => {
                 expected: ''
             },
             {
+                name: 'empty string',
+                value: '',
+                expected: ''
+            },
+            {
                 name: '1d 2h 3m 4.56s (number)',
                 value: (4.56 + 3 * 60 + 2 * 3600 + 1 * 24 * 3600) * 1000,
                 expected: '1 day, 2 hr, 3 min, 4.56 sec'
@@ -24,6 +29,49 @@ describe('DurationPipe', () => {
                 name: '1d 2h 3m 4.56s (as string "93784560")',
                 value: '93784560',
                 expected: '1 day, 2 hr, 3 min, 4.56 sec'
+            },
+            {
+                name: '1.223344 ms',
+                value: 1.223344,
+                expected: '0.001 sec'
+            },
+            {
+                name: '1.223344 ms (string)',
+                value: '1.223344',
+                expected: '0.001 sec'
+            },
+            { name: '0', value: 0, expected: '0 sec' },
+            { name: '0 (string)', value: '0', expected: '0 sec' },
+            { name: '-0', value: -0, expected: '0 sec' },
+            {
+                name: 'MIN_VALUE',
+                value: Number.MIN_VALUE,
+                expected: '0 sec'
+            },
+            {
+                name: 'MIN_VALUE * 1000',
+                value: Number.MIN_VALUE * 1000,
+                expected: '5E-324 sec'
+            },
+            {
+                name: 'MAX_VALUE',
+                value: Number.MAX_VALUE,
+                expected: '1.798E305 sec'
+            },
+            {
+                name: 'Inf',
+                value: Number.POSITIVE_INFINITY,
+                expected: ''
+            },
+            {
+                name: '-Inf',
+                value: Number.NEGATIVE_INFINITY,
+                expected: ''
+            },
+            {
+                name: 'NaN',
+                value: Number.NaN,
+                expected: ''
             },
         ];
 
