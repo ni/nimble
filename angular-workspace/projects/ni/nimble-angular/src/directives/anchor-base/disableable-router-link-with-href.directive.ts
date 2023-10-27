ead3357fcade632c9d34eae1fc2494bda8572545
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-10-27 16:40:51 -0500

    Fix build

--- angular-workspace/projects/ni/nimble-angular/src/directives/anchor-base/disableable-router-link-with-href.directive.ts
+++ angular-workspace/projects/ni/nimble-angular/src/directives/anchor-base/disableable-router-link-with-href.directive.ts
@@ -1,10 +1,11 @@
-import type { ElementRef, Injector } from '@angular/core';
+import { Directive, type ElementRef, type Injector } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 import { ActivatedRoute, Router, RouterLinkWithHref } from '@angular/router';
 
 /**
  * Base class for Nimble router link directives that go on disableable elements
  */
+@Directive()
 export class DisableableRouterLinkWithHrefDirective<T extends { disabled: boolean }> extends RouterLinkWithHref {
     public constructor(injector: Injector, private readonly elementRef: ElementRef<T>) {
         super(injector.get(Router), injector.get(ActivatedRoute), injector.get(LocationStrategy));
