a954b1c75b2c5fae18411815f678d77bfa9bd89f
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-10-30 11:16:28 -0500

    Minor naming change

--- packages/nimble-components/src/table-column/number-text/models/tests/unit-scale-formatter.spec.ts
+++ packages/nimble-components/src/table-column/number-text/models/tests/unit-scale-formatter.spec.ts
@@ -7,79 +7,79 @@ describe('UnitScaleFormatter', () => {
         name: string,
         value: number,
         expectedFormattedValue: string,
-        expectedConvertedValue: number
+        expectedScaledValue: number
     }[] = [
         {
             name: 'NEGATIVE_INFINITY uses base unit',
             value: Number.NEGATIVE_INFINITY,
             expectedFormattedValue: '-Infinity x1',
-            expectedConvertedValue: Number.NEGATIVE_INFINITY
+            expectedScaledValue: Number.NEGATIVE_INFINITY
         },
         {
             name: 'POSITIVE_INFINITY uses base unit',
             value: Number.POSITIVE_INFINITY,
             expectedFormattedValue: 'Infinity x1',
-            expectedConvertedValue: Number.POSITIVE_INFINITY
+            expectedScaledValue: Number.POSITIVE_INFINITY
         },
         {
             name: 'NaN uses base unit',
             value: Number.NaN,
             expectedFormattedValue: 'NaN x1',
-            expectedConvertedValue: Number.NaN
+            expectedScaledValue: Number.NaN
         },
         {
             name: '-0 uses base unit',
             value: -0,
             expectedFormattedValue: '0 x1',
-            expectedConvertedValue: -0
+            expectedScaledValue: -0
         },
         {
             name: '+0 uses base unit',
             value: 0,
             expectedFormattedValue: '0 x1',
-            expectedConvertedValue: 0
+            expectedScaledValue: 0
         },
         {
             name: 'smaller than smallest unit uses smallest unit',
             value: 0.1,
             expectedFormattedValue: '0.2 x0.5',
-            expectedConvertedValue: 0.2
+            expectedScaledValue: 0.2
         },
         {
             name: 'exactly smallest unit uses smallest unit',
             value: 0.5,
             expectedFormattedValue: '1 x0.5',
-            expectedConvertedValue: 1
+            expectedScaledValue: 1
         },
         {
             name: '0.75 uses 0.5 unit',
             value: 0.75,
             expectedFormattedValue: '1.5 x0.5',
-            expectedConvertedValue: 1.5
+            expectedScaledValue: 1.5
         },
         {
             name: '3.5 uses 1 unit',
             value: 3.5,
             expectedFormattedValue: '3.5 x1',
-            expectedConvertedValue: 3.5
+            expectedScaledValue: 3.5
         },
         {
             name: '7.5 uses 5 unit',
             value: 7.5,
             expectedFormattedValue: '1.5 x5',
-            expectedConvertedValue: 1.5
+            expectedScaledValue: 1.5
         },
         {
             name: 'bigger than biggest unit uses biggest unit',
             value: 20,
             expectedFormattedValue: '2 x10',
-            expectedConvertedValue: 2
+            expectedScaledValue: 2
         },
         {
             name: 'negative values pick unit by magnitude',
             value: -20,
             expectedFormattedValue: '-2 x10',
-            expectedConvertedValue: -2
+            expectedScaledValue: -2
         }
     ] as const;
 
@@ -109,8 +109,8 @@ describe('UnitScaleFormatter', () => {
     parameterizeNamedList(testCases, (spec, name, value) => {
         spec(`${name} (getValueForBestUnit)`, () => {
             const formatter = new TestUnitScaleFormatter('', {});
-            const formattedValue = formatter.getScaledNumber(value.value);
-            expect(formattedValue).toEqual(value.expectedConvertedValue);
+            const scaledValue = formatter.getScaledNumber(value.value);
+            expect(scaledValue).toEqual(value.expectedScaledValue);
         });
     });
 
