d013b557fcde58e2cb29bc9307a469c392ecb74d
Aagash Raaj <123377167+aagash-ni@users.noreply.github.com>
2023-11-07 18:28:50 +0530

    Update story book file

--- packages/nimble-components/src/rich-text-mention/mention-users/tests/rich-text-mention-users.stories.ts
+++ packages/nimble-components/src/rich-text-mention/mention-users/tests/rich-text-mention-users.stories.ts
@@ -1,34 +1,30 @@
 import { html } from '@microsoft/fast-element';
 import type { Meta, StoryObj } from '@storybook/html';
-import { withActions } from '@storybook/addon-actions/decorator';
-import {
-    createUserSelectedThemeStory,
-    incubatingWarning
-} from '../../../utilities/tests/storybook';
-import { richTextMentionUsersTag } from '..';
-import { mappingMentionUserTag } from '../../../mapping/mention-user';
-import { richTextEditorTag } from '../../../rich-text/editor';
+import { createUserSelectedThemeStory } from '../../../utilities/tests/storybook';
+import { hiddenWrapper } from '../../../utilities/tests/hidden';
 
 const metadata: Meta = {
-    title: 'Incubating/Rich Text: Mention Users',
-    tags: ['autodocs'],
-    decorators: [withActions],
-    // prettier-ignore
-    render: createUserSelectedThemeStory(html`
-  ${incubatingWarning({
-        componentName: 'rich text mention users',
-        statusLink: 'https://github.com/ni/nimble/issues/1288'
-    })}
-     <${richTextEditorTag} style="height: 160px;">
-        <${richTextMentionUsersTag} pattern="http://users/.*">
-            <${mappingMentionUserTag} mention-href="http://users/user-id-1" display-name="John Doe"></${mappingMentionUserTag}>
-            <${mappingMentionUserTag} mention-href="http://users/user-id-2" display-name="Alice Smith"></${mappingMentionUserTag}>
-            <${mappingMentionUserTag} mention-href="http://users/user-id-3" display-name="Bob Jones"></${mappingMentionUserTag}>
-        </${richTextMentionUsersTag}>
-    </${richTextEditorTag} style="height: 160px;">
-  `)
+    title: 'Internal/Rich Text',
+    parameters: {
+        docs: {
+            description: {
+                component:
+                    'The `nimble-rich-text-mention-users` element will use the `nimble-mapping-mention-user` element as its user list content and utilize the pattern attribute to parse user href links, generating configuration for the mention list. This element will also include various validation checks to ensure the content format and pattern are correctly verified.'
+            }
+        }
+    }
 };
 
 export default metadata;
 
-export const richTextEditor: StoryObj = {};
+export const richTextMentionUsers: StoryObj = {
+    render: createUserSelectedThemeStory(hiddenWrapper(html`<style></style>`)),
+    argTypes: {
+        pattern: {
+            description:
+                'Using this regex, the user ID will be extracted from the user href during the parsing and serializing of comments in the editor.',
+            control: { type: 'none' }
+        }
+    },
+    args: {}
+};
