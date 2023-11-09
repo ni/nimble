fdda01520f5791e9aa7d31d532eb968af313251d
Vivin Krishna <123377523+vivinkrishna-ni@users.noreply.github.com>
2023-11-09 13:53:33 +0530

    Address feedbacks

--- packages/nimble-components/src/listbox/tests/listbox-matrix.stories.ts
+++ packages/nimble-components/src/listbox/tests/listbox-matrix.stories.ts
@@ -4,9 +4,10 @@ import {
     createMatrix,
     sharedMatrixParameters
 } from '../../utilities/tests/matrix';
-import { createMatrixThemeStory } from '../../utilities/tests/storybook';
+import { createMatrixThemeStory, createStory } from '../../utilities/tests/storybook';
 import { listboxTag } from '..';
 import { listOptionTag } from '../../list-option';
+import { hiddenWrapper } from '../../utilities/tests/hidden';
 
 const metadata: Meta = {
     title: 'Tests/Listbox',
@@ -17,16 +18,30 @@ const metadata: Meta = {
 
 export default metadata;
 
+const playFunction = (): void => {
+    const editorNodeList = document.querySelectorAll('nimble-listbox');
+    editorNodeList.forEach(element => element.selectFirstOption());
+};
+
 const component = (): ViewTemplate => html`
     <${listboxTag}>
-        <${listOptionTag} value="1">Option 1</${listOptionTag}>
-        <${listOptionTag} value="2">Option 2</${listOptionTag}>
-        <${listOptionTag} value="3">Option 3</${listOptionTag}>
-        <${listOptionTag} value="4">Option 4</${listOptionTag}>
-        <${listOptionTag} value="5">Option 5</${listOptionTag}>
+        <${listOptionTag} value="1">Loooooooooooooooooooooooong Word</${listOptionTag}>
+        <${listOptionTag} value="2">Word</${listOptionTag}>
+        <${listOptionTag} value="3">Short Word</${listOptionTag}>
+        <${listOptionTag} value="4" disabled>Disabled</${listOptionTag}>
+        <${listOptionTag} value="5">Multiple words in a single option to test the width of the option it gets wrapped</${listOptionTag}>
     </${listboxTag}>
 `;
 
 export const listboxThemeMatrix: StoryFn = createMatrixThemeStory(
     createMatrix(component)
+);
+listboxThemeMatrix.play = playFunction;
+
+export const hiddenListbox: StoryFn = createStory(
+    hiddenWrapper(
+        html`<${listboxTag} hidden>
+            <${listOptionTag} value="1">Option 1</${listOptionTag}>
+        </${listboxTag}>`
+    )
 );
