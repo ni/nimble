57e3aa37966b2b956656b3c4b8242822caa9a096
Vivin Krishna <123377523+vivinkrishna-ni@users.noreply.github.com>
2023-11-10 20:50:03 +0530

    Added comment for the contenteditable workaround

--- packages/nimble-components/src/rich-text-mention/users/view/template.ts
+++ packages/nimble-components/src/rich-text-mention/users/view/template.ts
@@ -1,6 +1,10 @@
 import { html } from '@microsoft/fast-element';
 import type { RichTextMentionUsersView } from '.';
 
+// Setting `contenteditable="true"` on the slot serves as a workaround to address the issue in the Chrome browser
+// that arises when the mention user view is activated at the end of a line in the rich text editor while in edit mode.
+// This can be removed when the below issue is resolved
+// See: https://github.com/ni/nimble/issues/1659
 export const template = html<RichTextMentionUsersView>`<span class="control"
         >@${x => x.mentionLabel}</span
     ><slot contenteditable="true"></slot>`;
