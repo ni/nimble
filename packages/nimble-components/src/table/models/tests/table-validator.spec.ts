2033498065b3853f7ae17c7cc096a9f20fade3c9
Jonathan Meyer <26874831+atmgrifter00@users.noreply.github.com>
2023-11-13 16:39:44 -0600

    Linting

--- packages/nimble-components/src/table/models/tests/table-validator.spec.ts
+++ packages/nimble-components/src/table/models/tests/table-validator.spec.ts
@@ -216,21 +216,9 @@ describe('TableValidator', () => {
         it('ID field name can be an empty string', () => {
             const data = [
                 // eslint-disable-next-line @typescript-eslint/naming-convention
-                {
-                    data: {
-                        stringField: 'value-1',
-                        numberField: 10,
-                        '': 'empty-1'
-                    }
-                },
+                { data: { stringField: 'value-1', numberField: 10, '': 'empty-1' } },
                 // eslint-disable-next-line @typescript-eslint/naming-convention
-                {
-                    data: {
-                        stringField: 'value-2',
-                        numberField: 11,
-                        '': 'empty-2'
-                    }
-                }
+                { data: { stringField: 'value-2', numberField: 11, '': 'empty-2' } }
             ];
 
             const isValid = validator.validateRecordIds(data, '');
@@ -243,21 +231,9 @@ describe('TableValidator', () => {
         it('validation occurs when ID field name is an empty string', () => {
             const data = [
                 // eslint-disable-next-line @typescript-eslint/naming-convention
-                {
-                    data: {
-                        stringField: 'value-1',
-                        numberField: 10,
-                        '': 'empty-1'
-                    }
-                },
+                { data: { stringField: 'value-1', numberField: 10, '': 'empty-1' } },
                 // eslint-disable-next-line @typescript-eslint/naming-convention
-                {
-                    data: {
-                        stringField: 'value-2',
-                        numberField: 11,
-                        '': 'empty-1'
-                    }
-                }
+                { data: { stringField: 'value-2', numberField: 11, '': 'empty-1' } }
             ];
 
             const isValid = validator.validateRecordIds(data, '');
