bf9dfbdd04c95e5fe0c15eaccb89f57a35ddc94a
Vivin Krishna <123377523+vivinkrishna-ni@users.noreply.github.com>
2023-11-25 11:01:41 +0530

    Added a missing type for rich text validity

--- packages/nimble-components/src/rich-text/base/types.ts
+++ packages/nimble-components/src/rich-text/base/types.ts
@@ -2,4 +2,5 @@ import type { ValidityObject } from '../../utilities/models/validator';
 
 export interface RichTextValidity extends ValidityObject {
     readonly invalidMentionConfiguration: boolean;
+    readonly duplicateMentionConfiguration: boolean;
 }
