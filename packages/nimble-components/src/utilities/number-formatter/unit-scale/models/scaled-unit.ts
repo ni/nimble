4b4ec8c88ecd51259c182f05de3e52dedafb0573
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-12-04 09:52:22 -0600

    Prettier fix

--- packages/nimble-components/src/utilities/number-formatter/unit-scale/models/scaled-unit.ts
+++ packages/nimble-components/src/utilities/number-formatter/unit-scale/models/scaled-unit.ts
@@ -5,7 +5,10 @@ export abstract class UnitFormatter {
     public abstract format(value: number): string;
 }
 
-export type UnitFormatterFactoryFunction = (locale: string, numberFormatOptions: Intl.NumberFormatOptions | undefined) => UnitFormatter;
+export type UnitFormatterFactoryFunction = (
+    locale: string,
+    numberFormatOptions: Intl.NumberFormatOptions | undefined
+) => UnitFormatter;
 
 /**
  * A unit that represents a scaled version of a base unit.
