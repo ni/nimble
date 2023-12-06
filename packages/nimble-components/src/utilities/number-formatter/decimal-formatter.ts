0dc280ea941d890190998dcbf7c85fc2eac4b6d9
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2023-12-06 10:50:33 -0600

    fix imports

--- packages/nimble-components/src/utilities/number-formatter/decimal-formatter.ts
+++ packages/nimble-components/src/utilities/number-formatter/decimal-formatter.ts
@@ -1,5 +1,5 @@
 import { NumberFormatter } from './number-formatter';
-import type { UnitFormatter } from './unit-formatter';
+import type { UnitFormatter } from './unit-scale/models/unit-formatter';
 import type { UnitScale } from './unit-scale/unit-scale';
 
 /**
