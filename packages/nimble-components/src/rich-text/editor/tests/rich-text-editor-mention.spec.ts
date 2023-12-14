6c67f01b1f9b6551a5fed09be495e9a09f9695c9
Aagash Raaj <123377167+aagash-ni@users.noreply.github.com>
2023-12-14 16:35:30 +0530

    Minor fix

--- packages/nimble-components/src/rich-text/editor/tests/rich-text-editor-mention.spec.ts
+++ packages/nimble-components/src/rich-text/editor/tests/rich-text-editor-mention.spec.ts
@@ -89,6 +89,23 @@ describe('RichTextEditorMention', () => {
         expect(element.getMarkdown()).toBe('<user:1>');
     });
 
+    it('Should render mention node in between bold and italics content', async () => {
+        element.setMarkdown('***Bold <user:1> Italics***');
+        await appendUserMentionConfiguration(element, [
+            { key: 'user:1', displayName: 'username1' }
+        ]);
+        expect(element.getMarkdown()).toBe('***Bold <user:1> Italics***');
+        expect(pageObject.getMarkdownRenderedTagNames()).toEqual([
+            'P',
+            'STRONG',
+            'EM',
+            RICH_TEXT_MENTION_USERS_VIEW_TAG
+        ]);
+        expect(
+            pageObject.getEditorMentionViewAttributeValues('mention-label')
+        ).toEqual(['username1']);
+    });
+
     describe('user mention dynamic loading', () => {
         it('adding mention configuration converts the absolute link matching the pattern to mention node', async () => {
             element.setMarkdown('<user:1>');
