838f881d4a3e6fb683c28896fdb47641f22b98dc
Vivin Krishna <123377523+vivinkrishna-ni@users.noreply.github.com>
2024-01-02 19:26:20 +0530

    Removing view element for decoration node

--- packages/nimble-components/src/rich-text/editor/models/create-tiptap-editor.ts
+++ packages/nimble-components/src/rich-text/editor/models/create-tiptap-editor.ts
@@ -216,7 +216,10 @@ function createCustomMentionExtension(
     }).configure({
         suggestion: {
             char: config.character,
-            decorationTag: config.viewElement,
+            // When rendering the view element as a decoration tag for suggestions, it leads to the deletion of the entire suggested word in Safari.
+            // Refer to the following issue for commenting out the specified line of code as a workaround
+            // https://github.com/ni/nimble/issues/1716
+            // decorationTag: config.viewElement,
             pluginKey: new PluginKey(config.key),
             allowSpaces: true,
             render: () => {
