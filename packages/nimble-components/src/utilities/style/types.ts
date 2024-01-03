63887210e4e0775c095e8dfa6a02d0408c7b0ece
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2024-01-03 11:10:09 -0600

    Remove unused values

--- packages/nimble-components/src/utilities/style/types.ts
+++ packages/nimble-components/src/utilities/style/types.ts
@@ -1,7 +1,5 @@
 export const ZIndexLevels = {
     zIndex1: '1',
-    zIndex100: '100',
-    zIndex1000: '1000',
-    zIndex10000: '10000'
+    zIndex1000: '1000'
 } as const;
 export type ZIndexLevels = (typeof ZIndexLevels)[keyof typeof ZIndexLevels];
