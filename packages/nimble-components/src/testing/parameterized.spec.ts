48419c619e3187506419b3e20570b9f143372ff7
mollykreis <20542556+mollykreis@users.noreply.github.com>
2024-01-05 10:19:39 -0600

    move parameterized.spec.ts

--- packages/nimble-components/src/testing/parameterized.spec.ts
+++ packages/nimble-components/src/testing/parameterized.spec.ts
@@ -1,7 +1,7 @@
 import {
     parameterize,
     parameterizeNamedList
-} from '../../../testing/parameterized';
+} from './parameterized';
 
 // The following aliases are just to reduce the number
 // of eslint disables in this test file. In normal
