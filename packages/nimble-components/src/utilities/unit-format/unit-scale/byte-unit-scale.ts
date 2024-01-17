9d53e66734d159448803aef596ac397d305cba5f
rajsite <rajsite@users.noreply.github.com>
2024-01-17 16:24:51 -0600

    Make 1000 base byte scale use base in definition

--- packages/nimble-components/src/utilities/unit-format/unit-scale/byte-unit-scale.ts
+++ packages/nimble-components/src/utilities/unit-format/unit-scale/byte-unit-scale.ts
@@ -3,12 +3,12 @@ import { ScaledUnit } from '../scaled-unit/scaled-unit';
 import { UnitScale } from './unit-scale';
 
 const byteUnitScaleOptions = [
-    [10 ** 0, 'byte', 'long'],
-    [10 ** 3, 'kilobyte', 'short'],
-    [10 ** 6, 'megabyte', 'short'],
-    [10 ** 9, 'gigabyte', 'short'],
-    [10 ** 12, 'terabyte', 'short'],
-    [10 ** 15, 'petabyte', 'short']
+    [1000 ** 0, 'byte', 'long'],
+    [1000 ** 1, 'kilobyte', 'short'],
+    [1000 ** 2, 'megabyte', 'short'],
+    [1000 ** 3, 'gigabyte', 'short'],
+    [1000 ** 4, 'terabyte', 'short'],
+    [1000 ** 5, 'petabyte', 'short']
 ] as const;
 
 /**
