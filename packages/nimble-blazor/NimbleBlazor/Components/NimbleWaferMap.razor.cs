e31bba837951de1f8ba0989498022d69cf503535
Natan Muntean <natan.muntean@ni.com>
2024-01-23 19:19:13 +0200

    lint fix

--- packages/nimble-blazor/NimbleBlazor/Components/NimbleWaferMap.razor.cs
+++ packages/nimble-blazor/NimbleBlazor/Components/NimbleWaferMap.razor.cs
@@ -25,56 +25,67 @@ public partial class NimbleWaferMap : ComponentBase
     private IJSRuntime? JSRuntime { get; set; }
 
     /// <summary>
+    /// Represents the starting point and the direction of the two axes, X and Y, which are used for displaying the die grid on the wafer map canvas.
     /// </summary>
     [Parameter]
     public WaferMapOriginLocation? OriginLocation { get; set; }
 
     /// <summary>
+    /// Represents the X coordinate of the minimum corner of the the grid bounding box for rendering the wafer map.
     /// </summary>
     [Parameter]
     public double? GridMinX { get; set; }
 
     /// <summary>
+    /// Represents the Y coordinate of the minimum corner of the the grid bounding box for rendering the wafer map.
     /// </summary>
     [Parameter]
     public double? GridMaxX { get; set; }
 
     /// <summary>
+    /// Represents the X coordinate of the maximum corner of the the grid bounding box for rendering the wafer map.
     /// </summary>
     [Parameter]
     public double? GridMinY { get; set; }
 
     /// <summary>
+    /// Represents the Y coordinate of the maximum corner of the the grid bounding box for rendering the wafer map.
     /// </summary>
     [Parameter]
     public double? GridMaxY { get; set; }
 
     /// <summary>
+    /// Represents the orientation of the notch on the wafer map outline.
     /// </summary>
     [Parameter]
     public WaferMapOrientation? Orientation { get; set; }
 
     /// <summary>
+    /// Represents the number of characters allowed to be displayed within a single die, including the label suffix.
     /// </summary>
     [Parameter]
     public double? MaxCharacters { get; set; }
 
     /// <summary>
+    /// Represents a boolean value that determines if the die labels in the wafer map view are shown or not. Default value is false.
     /// </summary>
     [Parameter]
     public bool? DieLabelsHidden { get; set; }
 
     /// <summary>
+    /// Represents a string that can be added as a label in the end of the each data information in the wafer map dies value.
     /// </summary>
     [Parameter]
     public string? DieLabelsSuffix { get; set; }
 
     /// <summary>
+    /// Represents an Enum value that determent if the colorScale is represent a continues gradient values (linear), or is set categorically (ordinal).
     /// </summary>
     [Parameter]
     public WaferMapColorScaleMode? ColorScaleMode { get; set; }
 
     /// <summary>
+    /// Represents a list of strings of dies that will be highlighted in the wafer map view.
     /// </summary>
     [Parameter]
     public IEnumerable<string>? HighlightedTags
@@ -91,6 +102,7 @@ public partial class NimbleWaferMap : ComponentBase
     }
 
     /// <summary>
+    /// Represents the input data, an array of `WaferMapDie`, which fills the wafer map with content
     /// </summary>
     [Parameter]
     public IEnumerable<WaferMapDie>? Dies
@@ -107,6 +119,7 @@ public partial class NimbleWaferMap : ComponentBase
     }
 
     /// <summary>
+    /// Represents the color spectrum which shows the status of the dies on the wafer.
     /// </summary>
     [Parameter]
     public WaferMapColorScale? ColorScale
@@ -123,12 +136,13 @@ public partial class NimbleWaferMap : ComponentBase
     }
 
     /// <summary>
-    /// Gets or sets a callback that's invoked before 'open' changes on an action menu.
+    /// Will be triggered to inform the user that the state of hovering over a die has changed.
     /// </summary>
     [Parameter]
     public EventCallback<WaferMapHoverDieChangedEventArgs> HoverDieChanged { get; set; }
 
     /// <summary>
+    /// Returns an object of boolean values that represents the validity states that the wafer map's configuration can be in.
     /// </summary>
     public async Task<IWaferMapValidity> GetValidityAsync()
     {
