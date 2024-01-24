0d5601ee19f91a4255c3c5dfa50e6cf895861f6a
Jonathan Meyer <26874831+atmgrifter00@users.noreply.github.com>
2024-01-24 16:42:26 -0600

    Remove fit.

--- packages/nimble-components/src/select/tests/types.spec.ts
+++ packages/nimble-components/src/select/tests/types.spec.ts
@@ -1,7 +1,7 @@
 import type { FilterMode } from '../types';
 
 describe('Select type', () => {
-    fit('FilterMode fails compile if assigning arbitrary string values', () => {
+    it('FilterMode fails compile if assigning arbitrary string values', () => {
         // @ts-expect-error This expect will fail if the enum-like type is missing "as const"
         const filterMode: FilterMode = 'hello';
         expect(filterMode!).toEqual('hello');
