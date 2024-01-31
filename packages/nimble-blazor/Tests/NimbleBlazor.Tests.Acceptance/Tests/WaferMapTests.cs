acbb629e966325e75b974eead2e7dfe318611c26
Natan Muntean <natan.muntean@ni.com>
2024-01-31 23:45:13 +0200

    new canvas color tests

---
+++ packages/nimble-blazor/Tests/NimbleBlazor.Tests.Acceptance/Tests/WaferMapTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Playwright;
+using Xunit;
+
+namespace NimbleBlazor.Tests.Acceptance
+{
+    public class WaferMapTests : AcceptanceTestsBase
+    {
+        public WaferMapTests(PlaywrightFixture playwrightFixture, BlazorServerWebHostFixture blazorServerClassFixture)
+            : base(playwrightFixture, blazorServerClassFixture)
+        {
+        }
+
+        [Fact]
+        public async Task WaferMap_WithDiesAndColorScale_RendersColorsAsync()
+        {
+            await using var pageWrapper = await NewPageForRouteAsync("WaferMapRenderTest");
+            var page = pageWrapper.Page;
+            var color = await page.EvaluateAsync<string>(
+                @"document.getElementsByTagName('nimble-wafer-map')[0].canvas.getContext('2d').getImageData(249, 249, 1, 1).data.toString()"
+            );
+            Assert.Equal(@"85,85,0,255", color);
+        }
+    }
+}
