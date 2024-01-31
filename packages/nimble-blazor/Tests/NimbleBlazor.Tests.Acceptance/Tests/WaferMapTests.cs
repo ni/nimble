678f8e3aab1785a569d40943dc7b9e8d2ffd80fa
Natan Muntean <natan.muntean@ni.com>
2024-02-01 00:11:28 +0200

    lint

--- packages/nimble-blazor/Tests/NimbleBlazor.Tests.Acceptance/Tests/WaferMapTests.cs
+++ packages/nimble-blazor/Tests/NimbleBlazor.Tests.Acceptance/Tests/WaferMapTests.cs
@@ -15,8 +15,7 @@ namespace NimbleBlazor.Tests.Acceptance
             await using var pageWrapper = await NewPageForRouteAsync("WaferMapRenderTest");
             var page = pageWrapper.Page;
             var color = await page.EvaluateAsync<string>(
-                @"document.getElementsByTagName('nimble-wafer-map')[0].canvas.getContext('2d').getImageData(249, 249, 1, 1).data.toString()"
-            );
+                @"document.getElementsByTagName('nimble-wafer-map')[0].canvas.getContext('2d').getImageData(249, 249, 1, 1).data.toString()");
             Assert.Equal(@"85,85,0,255", color);
         }
 
