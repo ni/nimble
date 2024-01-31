38953ba026ac5637efda9044773ac757e92f8282
Natan Muntean <natan.muntean@ni.com>
2024-02-01 00:07:29 +0200

    getValidity check

--- packages/nimble-blazor/Tests/NimbleBlazor.Tests.Acceptance/Tests/WaferMapTests.cs
+++ packages/nimble-blazor/Tests/NimbleBlazor.Tests.Acceptance/Tests/WaferMapTests.cs
@@ -7,8 +7,7 @@ namespace NimbleBlazor.Tests.Acceptance
     {
         public WaferMapTests(PlaywrightFixture playwrightFixture, BlazorServerWebHostFixture blazorServerClassFixture)
             : base(playwrightFixture, blazorServerClassFixture)
-        {
-        }
+        { }
 
         [Fact]
         public async Task WaferMap_WithDiesAndColorScale_RendersColorsAsync()
@@ -19,6 +18,18 @@ namespace NimbleBlazor.Tests.Acceptance
                 @"document.getElementsByTagName('nimble-wafer-map')[0].canvas.getContext('2d').getImageData(249, 249, 1, 1).data.toString()"
             );
             Assert.Equal(@"85,85,0,255", color);
+        }
+
+        [Fact]
+        public async Task WaferMap_WithGridDimensions_IsValidAsync()
+        {
+            await using var pageWrapper = await NewPageForRouteAsync("WaferMapRenderTest");
+            var page = pageWrapper.Page;
+            var validButton = page.Locator("nimble-button");
+            var textField = page.Locator("nimble-text-field");
+
+            await validButton.ClickAsync();
+            await Assertions.Expect(textField).ToHaveAttributeAsync("current-value", "False");
         }
     }
 }
