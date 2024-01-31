ce1a3becfe6ec93b6d2d44df288c9e09104b8a82
Natan Muntean <natan.muntean@ni.com>
2024-01-31 19:46:34 +0200

    removed acceptance and improved unit

--- packages/nimble-blazor/Tests/NimbleBlazor.Tests/Unit/Components/NimbleWaferMapTests.cs
+++ packages/nimble-blazor/Tests/NimbleBlazor.Tests/Unit/Components/NimbleWaferMapTests.cs
@@ -11,6 +11,60 @@ namespace NimbleBlazor.Tests.Unit.Components;
 public class NimbleWaferMapTests
 {
     [Fact]
+    public void NimbleWaferMap_WithGridMinXAttribute_HasMarkup()
+    {
+        var waferMap = RenderWithPropertySet(x => x.GridMinX!, 3);
+
+        var expectedMarkup = "grid-min-x=\"3\"";
+        Assert.Contains(expectedMarkup, waferMap.Markup);
+    }
+
+    [Fact]
+    public void NimbleWaferMap_WithGridMaxXAttribute_HasMarkup()
+    {
+        var waferMap = RenderWithPropertySet(x => x.GridMaxX!, 3);
+
+        var expectedMarkup = "grid-max-x=\"3\"";
+        Assert.Contains(expectedMarkup, waferMap.Markup);
+    }
+
+    [Fact]
+    public void NimbleWaferMap_WithGridMinYAttribute_HasMarkup()
+    {
+        var waferMap = RenderWithPropertySet(x => x.GridMinY!, 3);
+
+        var expectedMarkup = "grid-min-y=\"3\"";
+        Assert.Contains(expectedMarkup, waferMap.Markup);
+    }
+
+    [Fact]
+    public void NimbleWaferMap_WithGridMaxYAttribute_HasMarkup()
+    {
+        var waferMap = RenderWithPropertySet(x => x.GridMaxY!, 3);
+
+        var expectedMarkup = "grid-max-y=\"3\"";
+        Assert.Contains(expectedMarkup, waferMap.Markup);
+    }
+
+    [Fact]
+    public void NimbleWaferMap_WithDieLabelsHiddenAttribute_HasMarkup()
+    {
+        var waferMap = RenderWithPropertySet(x => x.DieLabelsHidden!, true);
+
+        var expectedMarkup = "die-labels-hidden";
+        Assert.Contains(expectedMarkup, waferMap.Markup);
+    }
+
+    [Fact]
+    public void NimbleWaferMap_WithDieLabelsSuffixAttribute_HasMarkup()
+    {
+        var waferMap = RenderWithPropertySet(x => x.DieLabelsSuffix!, "a");
+
+        var expectedMarkup = "die-labels-suffix=\"a\"";
+        Assert.Contains(expectedMarkup, waferMap.Markup);
+    }
+
+    [Fact]
     public void NimbleWaferMap_WithMaxCharactersAttribute_HasMarkup()
     {
         var waferMap = RenderWithPropertySet(x => x.MaxCharacters!, 3);
