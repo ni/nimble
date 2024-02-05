240d4e39006834c5a92f6e85cba292601534bd28
Razvan Nazare <razvan.nazare@ni.com>
2024-02-05 12:02:32 +0200

    deleted garbage code

--- packages/site/.eleventy.js
+++ packages/site/.eleventy.js
@@ -4,8 +4,7 @@ module.exports = eleventyConfig => {
         'landing/dist': './',
         '../../packages/nimble-components/dist/storybook': 'storybook',
         '../../angular-workspace/dist/example-client-app': 'storybook/example-client-app',
-        '../../packages/nimble-blazor/dist/blazor-client-app': 'storybook/blazor-client-app',
-        '../../packages/nimble-components/performance': 'storybook/wafer-performance'
+        '../../packages/nimble-blazor/dist/blazor-client-app': 'storybook/blazor-client-app'
     });
     eleventyConfig.setTemplateFormats([
         'md',
