bb346bf3f588149a8064b775158ccd4526986502
Razvan Nazare <razvan.nazare@ni.com>
2024-02-05 15:00:48 +0200

    Update nimble-wafer-map.module.ts

--- angular-workspace/projects/ni/nimble-angular/wafer-map/nimble-wafer-map.module.ts
+++ angular-workspace/projects/ni/nimble-angular/wafer-map/nimble-wafer-map.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NimbleWaferMapDirective } from './nimble-wafer-map.directive';
 
-import '@ni/nimble-components/dist/esm/unit/volt';
+import '@ni/nimble-components/dist/esm/wafer-map';
 
 @NgModule({
     declarations: [NimbleWaferMapDirective],
