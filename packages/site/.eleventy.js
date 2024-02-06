0e621fffca3a765f9618e71ece2c36d2d81327bd
Razvan Nazare <razvan.nazare@ni.com>
2024-02-06 15:57:18 +0200

    Update .eleventy.js

--- packages/site/.eleventy.js
+++ packages/site/.eleventy.js
@@ -1,11 +1,11 @@
 module.exports = eleventyConfig => {
     eleventyConfig.addPassthroughCopy({
         root: './',
-        'landing/dist': './',
+        // 'landing/dist': './', only for testing purposes
         '../../packages/nimble-components/dist/storybook': 'storybook',
         '../../angular-workspace/dist/example-client-app': 'storybook/example-client-app',
         '../../packages/nimble-blazor/dist/blazor-client-app': 'storybook/blazor-client-app',
-        '../../packages/performance/landing/dist': './'// only for testing purposes
+        '../../packages/performance/landing/dist': './', // only for testing purposes
     });
     eleventyConfig.setTemplateFormats([
         'md',
