658f567e8e99f12d5c911a3e9cd1e79111048c16
Razvan Nazare <razvan.nazare@ni.com>
2024-02-06 15:56:00 +0200

    Update .eleventy.js

--- packages/site/.eleventy.js
+++ packages/site/.eleventy.js
@@ -5,7 +5,7 @@ module.exports = eleventyConfig => {
         '../../packages/nimble-components/dist/storybook': 'storybook',
         '../../angular-workspace/dist/example-client-app': 'storybook/example-client-app',
         '../../packages/nimble-blazor/dist/blazor-client-app': 'storybook/blazor-client-app',
-        '../../packages/performance/landing/dist': 'storybook/performance/wafer-map'
+        '../../packages/performance/landing/dist': './'// only for testing purposes
     });
     eleventyConfig.setTemplateFormats([
         'md',
