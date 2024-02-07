712034c7f2fb25979165f022149c4e31e9b787bd
Razvan-NI <110180309+Razvan1928@users.noreply.github.com>
2024-02-07 12:00:42 +0200

    Performance tests for wafermap (#1786)
    # Pull Request
    
    ## 🤨 Rationale
    
    We wish to create performance tests using lighthouse, for that we are
    going to create most of the infrustructure
    
    ## 👩‍💻 Implementation
    
    The wafer performance tests will appear in a lighthouse report generated
    by the CI after every push, or can be trigger manually using npm run
    performance -w packages/performance.
    
    At runtime, while we create/populate the wafer component we are
    measuring the performance using windows.performance. The result will be
    available at 'User Timing marks and measures'
    
    To avoid confusion the existing lighthouse tests were moved in the
    directory angular-workspace
    
    The performance tests are built using vite
    
    Despite lighthouse the landing site of the performance tests can be find
    in the Chromatic build
    
    The values on the wafer dies are computed simply (i + j) % 100, where i
    and j are the coordinates of the die.
    
    Also some math was used to compute the radius/sideLegth of the wafer.  
    sideLength = Math.sqrt(diesCount / Math.PI) * 2;
    
    it is derived from the circle's aria formula
    A = π r²
    where A in our case is the number of dies.
    
    ## 🧪 Testing
    
    The results can be found in the CI
    
    ## ✅ Checklist
    
    <!--- Review the list and put an x in the boxes that apply or ~~strike
    through~~ around items that don't (along with an explanation). -->
    
    - [ ] I have updated the project documentation to reflect my changes or
    determined no changes are needed.
    
    ---------
    
    Co-authored-by: rajsite <rajsite@users.noreply.github.com>

---
+++ angular-workspace/lighthouserc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    ci: {
+        collect: {
+            staticDistDir: './dist/example-client-app',
+            url: [
+                'http://localhost:58452/#/customapp'
+            ],
+            numberOfRuns: 3,
+        },
+        assert: {
+            assertions: {
+                'categories:performance': ['warn', { minScore: 0.9 }],
+                // TODO: reset to 0.9 as part of https://github.com/ni/nimble/issues/1090
+                // TODO: reset to error as part of https://github.com/ni/nimble/issues/1650
+                'categories:accessibility': ['warn', { minScore: 0.88 }]
+            }
+        },
+        upload: {
+            target: 'temporary-public-storage',
+        },
+    },
+};
---
+++ packages/performance/README.md
@@ -0,0 +1,9 @@
+# Performance
+
+## Overview
+
+This package contains performance tests for nimble components.
+
+## Contributing
+
+Follow the instructions in [CONTRIBUTING.md](/packages/performance/CONTRIBUTING.md) to modify this library.
