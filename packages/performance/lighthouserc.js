57f01dde4e63aa69db44ea4250336b489d0072c0
rajsite <rajsite@users.noreply.github.com>
2024-02-07 11:27:51 -0600

    Update lhci paths

--- packages/performance/lighthouserc.js
+++ packages/performance/lighthouserc.js
@@ -1,9 +1,11 @@
 module.exports = {
     ci: {
         collect: {
-            staticDistDir: './dist/',
+            // Serve outside package so that package name is part of url path
+            // and shows up in GitHub status checks
+            staticDistDir: '../',
             url: [
-                'http://localhost/wafer-map/'
+                'http://localhost/performance/dist/wafer-map/'
             ],
             numberOfRuns: 1,
             settings: {
