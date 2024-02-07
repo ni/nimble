4641e42fa4596473377829c7ee5ce02b59ef7b6c
Natan Muntean <natan.muntean@ni.com>
2024-02-07 12:41:21 +0200

    waiting for canvas to be visible

--- packages/nimble-blazor/Tests/NimbleBlazor.Tests.Acceptance/Tests/WaferMapTests.cs
+++ packages/nimble-blazor/Tests/NimbleBlazor.Tests.Acceptance/Tests/WaferMapTests.cs
@@ -14,8 +14,12 @@ namespace NimbleBlazor.Tests.Acceptance
         {
             await using var pageWrapper = await NewPageForRouteAsync("WaferMapRenderTest");
             var page = pageWrapper.Page;
+            var canvas = page.Locator("canvas");
+
+            await Assertions.Expect(canvas).ToBeVisibleAsync();
             var color = await page.EvaluateAsync<string>(
                 @"document.getElementsByTagName('nimble-wafer-map')[0].canvas.getContext('2d').getImageData(249, 249, 1, 1).data.toString()");
+
             Assert.Equal(@"85,85,0,255", color);
         }
 
