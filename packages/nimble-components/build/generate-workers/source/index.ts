2b367b5f90614c08a28b2a882e5fad3388452475
Natan Muntean <natan.muntean@ni.com>
2024-02-09 11:08:12 +0200

    added to prettierignore

--- packages/nimble-components/build/generate-workers/source/index.ts
+++ packages/nimble-components/build/generate-workers/source/index.ts
@@ -18,8 +18,7 @@ const modulePath = require.resolve(moduleName);
 const sourceCode = fs.readFileSync(modulePath, 'utf-8');
 
 const componentFileContents =
-`// prettier-ignore
-export const workerCode = \`
+`export const workerCode = \`
 ${sourceCode.replaceAll('`','\\`').replaceAll('$','\\$').replaceAll('exports.RenderWorker = RenderWorker;','')}
 
 const renderWorker = new RenderWorker();
