baa6c78f45b0054e41d713adce7b06469f2a4dfc
Razvan Nazare <razvan.nazare@ni.com>
2024-02-13 18:53:54 +0200

    Update index.ts

--- packages/nimble-components/build/generate-workers/source/index.ts
+++ packages/nimble-components/build/generate-workers/source/index.ts
@@ -18,7 +18,8 @@ const modulePath = require.resolve(moduleName);
 const sourceCode = fs.readFileSync(modulePath, 'utf-8');
 
 const componentFileContents =
-`export const workerCode = \`
+`/* eslint-disable no-useless-escape */
+export const workerCode = \`
 ${sourceCode.replaceAll('`','\\`').replaceAll('$','\\$').replaceAll('exports.RenderWorker = RenderWorker;','')}
 \`;`;
 
