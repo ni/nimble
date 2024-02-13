8df4dd20166c5b683a84f57efa6b5a528db9f6d7
Jonathan Meyer <26874831+atmgrifter00@users.noreply.github.com>
2024-02-13 17:36:43 -0600

    Update packages/nimble-components/src/anchored-region/styles.ts
    Co-authored-by: Milan Raj <rajsite@users.noreply.github.com>

--- packages/nimble-components/src/anchored-region/styles.ts
+++ packages/nimble-components/src/anchored-region/styles.ts
@@ -3,7 +3,8 @@ import { display } from '@microsoft/fast-foundation';
 import { ZIndexLevels } from '../utilities/style/types';
 
 export const styles = css`
-    ${display('block')}:host {
+    ${display('block')}
+    :host {
         contain: layout;
         z-index: ${ZIndexLevels.zIndex1000};
     }
