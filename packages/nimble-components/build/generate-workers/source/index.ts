3fbf07ee666cd2dd368f0b6b342ed7c5465c6bfe
Razvan Nazare <razvan.nazare@ni.com>
2024-02-14 16:31:55 +0200

    Update index.ts

--- packages/nimble-components/build/generate-workers/source/index.ts
+++ packages/nimble-components/build/generate-workers/source/index.ts
@@ -22,7 +22,7 @@ const sourceCode = fs.readFileSync(modulePath, 'utf-8');
 
 const combinedFileContents =
 `// eslint-disable-next-line no-template-curly-in-string
-export const workerCode = ${JSON.stringify(healthStatusContent + sourceCode)}`;
+export const workerCode = ${JSON.stringify(healthStatusContent + sourceCode)};`;
 
 const filePath = path.resolve(workersDirectory, 'render-worker.ts');
 console.log(`Writing worker file "${filePath}"`);
