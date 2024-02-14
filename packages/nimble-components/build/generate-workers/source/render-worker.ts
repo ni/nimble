24336ed2593333c1f992c90e8036fa67910409ed
Razvan Nazare <razvan.nazare@ni.com>
2024-02-14 14:47:35 +0200

    removed unused properties

--- packages/nimble-components/build/generate-workers/source/render-worker.ts
+++ packages/nimble-components/build/generate-workers/source/render-worker.ts
@@ -2,18 +2,8 @@ import * as Comlink from 'comlink';
 import { HealthStatus } from './health-status';
 
 export class RenderWorker {
-    private canvas!: OffscreenCanvas;
     private worker!: number;
-    private context!: OffscreenCanvasRenderingContext2D;
     private dieMatrix: Uint8Array = Uint8Array.from([]);
-    private verticalScale: { a: number, b: number } = { a: 0, b: 1 };
-    private horizontalScale: { a: number, b: number } = { a: 0, b: 1 };
-    private margin: { top: number, right: number } = { top: 0, right: 0 };
-    private dieDimensions: { width: number, height: number } = { width: 1, height: 1 };
-    private colorCategories: { color: string, startValue: number, endValue?: number }[] = [];
-    private yLimits: { min: number, max: number } = { min: 0, max: 0 };
-    private xLimits: { min: number, max: number } = { min: 0, max: 0 };
-    private transform: { k: number, x: number, y: number } = { k: 1, x: 0, y: 0 };
     private performanceTest: string | undefined;
     private healthStatus: HealthStatus = HealthStatus.Unknown;
 
