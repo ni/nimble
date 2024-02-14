f9e59d2a2f7552760161b49ee79807077a87702a
Razvan Nazare <razvan.nazare@ni.com>
2024-02-14 12:38:26 +0200

    Update index.ts

--- packages/nimble-components/build/generate-workers/source/index.ts
+++ packages/nimble-components/build/generate-workers/source/index.ts
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const moduleName = './render-worker.js';
+const healthStatusFileName = './health-status.js';
 
 const workersDirectory = path.resolve('./src/wafer-map/workers');
 if (fs.existsSync(workersDirectory)) {
@@ -13,16 +14,20 @@ console.log(`Creating workers directory "${workersDirectory}"`);
 fs.mkdirSync(workersDirectory);
 console.log('Finished creating workers directory');
 
+const healthStatusSourcePath = require.resolve(healthStatusFileName);
+const healthStatusContent = fs.readFileSync(healthStatusSourcePath, 'utf-8');
+
 const modulePath = require.resolve(moduleName);
 const sourceCode = fs.readFileSync(modulePath, 'utf-8');
 
-const componentFileContents =
+const combinedFileContents =
 `/* eslint-disable no-useless-escape */
 export const workerCode = \`
+${healthStatusContent}
 ${sourceCode.replaceAll('`','\\`').replaceAll('$','\\$').replaceAll('exports.RenderWorker = RenderWorker;','')}
 \`;`;
 
 const filePath = path.resolve(workersDirectory, 'render-worker.ts');
 console.log(`Writing worker file "${filePath}"`);
-fs.writeFileSync(filePath, componentFileContents, { encoding: 'utf-8' });
+fs.writeFileSync(filePath, combinedFileContents, { encoding: 'utf-8' });
 console.log('Finished writing worker file');
