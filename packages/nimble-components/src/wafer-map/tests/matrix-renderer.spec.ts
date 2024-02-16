c02e891135c9143368d787c7ba5e4b64ab1bfa50
Razvan Nazare <razvan.nazare@ni.com>
2024-02-16 16:38:49 +0200

    Create matrix-renderer.spec.ts

---
+++ packages/nimble-components/src/wafer-map/tests/matrix-renderer.spec.ts
@@ -0,0 +1,12 @@
+import { WaferMap } from '..';
+import { HealthStatus } from '../../../build/generate-workers/source/health-status';
+import { MatrixRenderer } from '../modules/matrix-renderer';
+
+describe('MatrixRenderer', () => {
+    it('worker should be healthy', async () => {
+        const wafermap = new WaferMap();
+        const renderer = new MatrixRenderer(wafermap);
+        const isWorkerHealthy = await renderer.workerOne.isWorkerHealthy();
+        expect(isWorkerHealthy).toBe(HealthStatus.Healty);
+    });
+});
