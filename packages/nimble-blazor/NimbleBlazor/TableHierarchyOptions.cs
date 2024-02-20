6747068846449f21cf56c3ecce4772b334343707
mollykreis <20542556+mollykreis@users.noreply.github.com>
2024-02-20 15:27:08 -0600

    split classes into different files

--- packages/nimble-blazor/NimbleBlazor/TableHierarchyOptions.cs
+++ packages/nimble-blazor/NimbleBlazor/TableHierarchyOptions.cs
@@ -2,19 +2,6 @@
 
 namespace NimbleBlazor;
 
-public class TableSetRecordHierarchyOptions
-{
-    public TableSetRecordHierarchyOptions(string recordId, TableRecordHierarchyOptions options)
-    {
-        RecordId = recordId;
-        Options = options;
-    }
-
-    public string RecordId { get; set; }
-
-    public TableRecordHierarchyOptions Options { get; set; }
-}
-
 public class TableRecordHierarchyOptions
 {
     public TableRecordHierarchyOptions(TableRecordDelayedHierarchyState delayedHierarchyState)
