b677d6b9ed7451715bb36043b245369060c8511b
Milan Raj <rajsite@users.noreply.github.com>
2024-02-22 17:50:47 -0600

    Card accessibility tweak (#1858)
    # Pull Request
    
    ## 🤨 Rationale
    
    The card was previously disabled after it started resulting in axe-core
    completely failing. After reporting to axe-core their comment in
    https://github.com/dequelabs/axe-core/issues/4335#issuecomment-1959646970
    made me think we can workaround the axe-core issue by wrapping the slot
    in a span and targeting that instead for aria.
    
    While not necessary for runtime behavior or accessibility, the
    workaround adds minimal overhead and complexity. Does not need to be
    removed after the axe-core fix. Fixes #1650
    
    ## 👩‍💻 Implementation
    
    Wraps the slot in a span and use that as the aria target. 
    
    ## 🧪 Testing
    
    Can see the PR has a passing lighthouse score with the card re-enabled.
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

--- packages/nimble-components/src/card/template.ts
+++ packages/nimble-components/src/card/template.ts
@@ -2,11 +2,8 @@ import { html } from '@microsoft/fast-element';
 import type { Card } from '.';
 
 export const template = html<Card>`
-    ${
-    '' /* Explicitly set role to work around Lighthouse error. See https://github.com/ni/nimble/issues/1650. */
-}
-    <section role="region" aria-labelledby="title-slot">
-        <slot name="title" id="title-slot"></slot>
+    <section aria-labelledby="title-slot">
+        <span id="title-slot"><slot name="title"></slot></span>
         <slot></slot>
     </section>
 `;
