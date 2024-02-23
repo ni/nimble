0eab6c2ea6e69b7d80ee662e10c5803c20ec1f01
Malcolm Smith <20709258+msmithNI@users.noreply.github.com>
2024-02-23 16:39:19 -0600

    Fix merge issues

--- packages/nimble-blazor/Examples/Demo.Shared/Pages/ComponentsDemo.razor.cs
+++ packages/nimble-blazor/Examples/Demo.Shared/Pages/ComponentsDemo.razor.cs
@@ -20,29 +20,29 @@ public partial class ComponentsDemo
     private string? DrawerClosedReason { get; set; }
     private string? SelectedRadio { get; set; } = "2";
     private bool BannerOpen { get; set; }
-    private List<PersonTableRecord> _delayedHierarchyTableData = new List<PersonTableRecord>()
+    private readonly List<PersonTableRecord> _delayedHierarchyTableData = new()
     {
         new PersonTableRecord("jacqueline-bouvier", null, "Jacqueline", "Bouvier", 80, true),
         new PersonTableRecord("mona-simpson", null, "Mona", "Simpson", 77, true),
         new PersonTableRecord("agnes-skinner", null, "Agnes", "Skinner", 88, true)
     };
-    private HashSet<string> _recordsLoadingChildren = new HashSet<string>();
-    private HashSet<string> _recordsWithLoadedChildren = new HashSet<string>();
+    private readonly HashSet<string> _recordsLoadingChildren = new();
+    private readonly HashSet<string> _recordsWithLoadedChildren = new();
 
-[NotNull]
-public IEnumerable<SimpleTableRecord> TableData { get; set; } = Enumerable.Empty<SimpleTableRecord>();
-[NotNull]
-public IEnumerable<WaferMapDie> Dies { get; set; } = Enumerable.Empty<WaferMapDie>();
-[NotNull]
-public IEnumerable<string> HighlightedTags { get; set; } = Enumerable.Empty<string>();
-[NotNull]
-public WaferMapColorScale ColorScale { get; set; } = new WaferMapColorScale(new List<string> { "red", "green" }, new List<string> { "0", "100" });
+    [NotNull]
+    public IEnumerable<SimpleTableRecord> TableData { get; set; } = Enumerable.Empty<SimpleTableRecord>();
+    [NotNull]
+    public IEnumerable<WaferMapDie> Dies { get; set; } = Enumerable.Empty<WaferMapDie>();
+    [NotNull]
+    public IEnumerable<string> HighlightedTags { get; set; } = Enumerable.Empty<string>();
+    [NotNull]
+    public WaferMapColorScale ColorScale { get; set; } = new WaferMapColorScale(new List<string> { "red", "green" }, new List<string> { "0", "100" });
 
-public ComponentsDemo()
-{
-    AddTableRows(10);
-    UpdateDies(5);
-}
+    public ComponentsDemo()
+    {
+        AddTableRows(10);
+        UpdateDies(5);
+    }
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
@@ -114,7 +114,7 @@ public ComponentsDemo()
             await _delayedHierarchyTable!.SetDataAsync(_delayedHierarchyTableData);
         }
 
-        List<TableSetRecordHierarchyOptions> options = new List<TableSetRecordHierarchyOptions>();
+        List<TableSetRecordHierarchyOptions> options = new();
         _delayedHierarchyTableData.ForEach(person =>
         {
             if (_recordsLoadingChildren.Contains(person.Id))
@@ -141,140 +141,140 @@ public ComponentsDemo()
         set => _drawerLocation = (DrawerLocation)Enum.Parse(typeof(DrawerLocation), value);
     }
 
-public async Task OpenDialogAsync()
-{
-    var response = await _dialog!.ShowAsync();
-    DialogClosedReason = response.Reason == DialogCloseReason.UserDismissed ? "User dismissed"
-                                                                      : response.Value.ToString();
-}
-
-public async Task CloseDialogAsync(DialogResult reason)
-{
-    await _dialog!.CloseAsync(reason);
-}
-
-public async Task OpenDrawerAsync()
-{
-    var response = await _drawer!.ShowAsync();
-    DrawerClosedReason = response.Reason == DrawerCloseReason.UserDismissed ? "User dismissed"
-                                                                      : response.Value.ToString();
-}
-
-public async Task CloseDrawerAsync(DialogResult reason)
-{
-    await _drawer!.CloseAsync(reason);
-}
-
-public void AddTableRows(int numberOfRowsToAdd)
-{
-    var tableData = new List<SimpleTableRecord>(TableData);
-
-    for (int i = 0; i < numberOfRowsToAdd; i++)
+    public async Task OpenDialogAsync()
     {
-        int rowCount = tableData.Count;
-        string rowCountString = rowCount.ToString(CultureInfo.CurrentCulture);
-
-        tableData.Add(new SimpleTableRecord(
-            rowCountString,
-            tableData.Count >= 4 ? (tableData.Count % 4).ToString(CultureInfo.CurrentCulture) : null,
-            $"new string {rowCountString}",
-            $"bar {rowCountString}",
-            "/",
-            "Link",
-            (rowCount % 2 == 0) ? new DateTime(2023, 8, 16, 2, 56, 11) : new DateTime(2022, 3, 7, 20, 28, 41),
-            (rowCount % 2 == 0) ? 100 : 101,
-            (rowCount % 2 == 0) ? "success" : "unknown",
-            rowCount / 10.0,
-            rowCount * 1000.0 * (1.1 + (2 * 60) + (3 * 3600))));
+        var response = await _dialog!.ShowAsync();
+        DialogClosedReason = response.Reason == DialogCloseReason.UserDismissed ? "User dismissed"
+                                                                          : response.Value.ToString();
     }
 
-    TableData = tableData;
-}
-
-public void UpdateDies(int numberOfDies)
-{
-    if (numberOfDies < 0)
+    public async Task CloseDialogAsync(DialogResult reason)
     {
-        return;
+        await _dialog!.CloseAsync(reason);
     }
-    var dies = new List<WaferMapDie>();
-    int radius = (int)Math.Ceiling(Math.Sqrt(numberOfDies / Math.PI));
-    var centerX = radius;
-    var centerY = radius;
 
-    for (var i = centerY - radius; i <= centerY + radius; i++)
+    public async Task OpenDrawerAsync()
     {
-        for (
-            var j = centerX;
-            ((j - centerX) * (j - centerX)) + ((i - centerY) * (i - centerY))
-            <= radius * radius;
-            j--)
+        var response = await _drawer!.ShowAsync();
+        DrawerClosedReason = response.Reason == DrawerCloseReason.UserDismissed ? "User dismissed"
+                                                                          : response.Value.ToString();
+    }
+
+    public async Task CloseDrawerAsync(DialogResult reason)
+    {
+        await _drawer!.CloseAsync(reason);
+    }
+
+    public void AddTableRows(int numberOfRowsToAdd)
+    {
+        var tableData = new List<SimpleTableRecord>(TableData);
+
+        for (int i = 0; i < numberOfRowsToAdd; i++)
         {
-            var value = (i + j) % 100;
-            dies.Add(new WaferMapDie(i, j, value.ToString(CultureInfo.CurrentCulture)));
+            int rowCount = tableData.Count;
+            string rowCountString = rowCount.ToString(CultureInfo.CurrentCulture);
+
+            tableData.Add(new SimpleTableRecord(
+                rowCountString,
+                tableData.Count >= 4 ? (tableData.Count % 4).ToString(CultureInfo.CurrentCulture) : null,
+                $"new string {rowCountString}",
+                $"bar {rowCountString}",
+                "/",
+                "Link",
+                (rowCount % 2 == 0) ? new DateTime(2023, 8, 16, 2, 56, 11) : new DateTime(2022, 3, 7, 20, 28, 41),
+                (rowCount % 2 == 0) ? 100 : 101,
+                (rowCount % 2 == 0) ? "success" : "unknown",
+                rowCount / 10.0,
+                rowCount * 1000.0 * (1.1 + (2 * 60) + (3 * 3600))));
         }
-        // generate points right of centerX
-        for (
-            var j = centerX + 1;
-            ((j - centerX) * (j - centerX)) + ((i - centerY) * (i - centerY))
-            <= radius * radius;
-            j++)
-        {
-            var value = (i + j) % 100;
-            dies.Add(new WaferMapDie(i, j, value.ToString(CultureInfo.CurrentCulture)));
-        }
+
+        TableData = tableData;
     }
-    Dies = dies;
-}
-public void AddDiesToRadius(int numberOfDies)
-{
-    UpdateDies(Dies.Count() + (int)(numberOfDies * numberOfDies * Math.PI));
-}
-public void RemoveDiesFromRadius(int numberOfDies)
-{
-    UpdateDies(Dies.Count() - (int)(numberOfDies * numberOfDies * Math.PI));
-}
+
+    public void UpdateDies(int numberOfDies)
+    {
+        if (numberOfDies < 0)
+        {
+            return;
+        }
+        var dies = new List<WaferMapDie>();
+        int radius = (int)Math.Ceiling(Math.Sqrt(numberOfDies / Math.PI));
+        var centerX = radius;
+        var centerY = radius;
+
+        for (var i = centerY - radius; i <= centerY + radius; i++)
+        {
+            for (
+                var j = centerX;
+                ((j - centerX) * (j - centerX)) + ((i - centerY) * (i - centerY))
+                <= radius * radius;
+                j--)
+            {
+                var value = (i + j) % 100;
+                dies.Add(new WaferMapDie(i, j, value.ToString(CultureInfo.CurrentCulture)));
+            }
+            // generate points right of centerX
+            for (
+                var j = centerX + 1;
+                ((j - centerX) * (j - centerX)) + ((i - centerY) * (i - centerY))
+                <= radius * radius;
+                j++)
+            {
+                var value = (i + j) % 100;
+                dies.Add(new WaferMapDie(i, j, value.ToString(CultureInfo.CurrentCulture)));
+            }
+        }
+        Dies = dies;
+    }
+    public void AddDiesToRadius(int numberOfDies)
+    {
+        UpdateDies(Dies.Count() + (int)(numberOfDies * numberOfDies * Math.PI));
+    }
+    public void RemoveDiesFromRadius(int numberOfDies)
+    {
+        UpdateDies(Dies.Count() - (int)(numberOfDies * numberOfDies * Math.PI));
+    }
 }
 
 public class SimpleTableRecord
 {
-public SimpleTableRecord(
-    string id,
-    string? parentId,
-    string stringValue1,
-    string stringValue2,
-    string? href,
-    string? linkLabel,
-    DateTime date,
-    int statusCode,
-    string result,
-    double number,
-    double duration)
-{
-    Id = id;
-    ParentId = parentId;
-    StringValue1 = stringValue1;
-    StringValue2 = stringValue2;
-    Href = href;
-    LinkLabel = linkLabel;
-    Date = (ulong)(date - DateTime.UnixEpoch.ToLocalTime()).TotalMilliseconds;
-    StatusCode = statusCode;
-    Result = result;
-    Number = number;
-    Duration = duration;
-}
+    public SimpleTableRecord(
+        string id,
+        string? parentId,
+        string stringValue1,
+        string stringValue2,
+        string? href,
+        string? linkLabel,
+        DateTime date,
+        int statusCode,
+        string result,
+        double number,
+        double duration)
+    {
+        Id = id;
+        ParentId = parentId;
+        StringValue1 = stringValue1;
+        StringValue2 = stringValue2;
+        Href = href;
+        LinkLabel = linkLabel;
+        Date = (ulong)(date - DateTime.UnixEpoch.ToLocalTime()).TotalMilliseconds;
+        StatusCode = statusCode;
+        Result = result;
+        Number = number;
+        Duration = duration;
+    }
 
-public string Id { get; }
-public string? ParentId { get; }
-public string StringValue1 { get; }
-public string StringValue2 { get; }
-public string? Href { get; }
-public string? LinkLabel { get; }
-public ulong Date { get; }
-public int StatusCode { get; }
-public string Result { get; }
-public double Number { get; }
-public double Duration { get; }
+    public string Id { get; }
+    public string? ParentId { get; }
+    public string StringValue1 { get; }
+    public string StringValue2 { get; }
+    public string? Href { get; }
+    public string? LinkLabel { get; }
+    public ulong Date { get; }
+    public int StatusCode { get; }
+    public string Result { get; }
+    public double Number { get; }
+    public double Duration { get; }
 }
 
 public class PersonTableRecord
--- packages/nimble-blazor/Tests/NimbleBlazor.Tests.Acceptance/Tests/TableTests.cs
+++ packages/nimble-blazor/Tests/NimbleBlazor.Tests.Acceptance/Tests/TableTests.cs
@@ -24,67 +24,67 @@ public class TableTests : AcceptanceTestsBase
             var expected = new string[] { "A0", "A1", "A2", "A3", "A4" };
             await Assertions.Expect(rows).ToContainTextAsync(expected);
         }
+    }
 
-        [Theory]
-        [InlineData(0, TableRecordDelayedHierarchyState.CanLoadChildren)]
-        [InlineData(1, TableRecordDelayedHierarchyState.None)]
-        [InlineData(2, TableRecordDelayedHierarchyState.LoadingChildren)]
-        [InlineData(3, TableRecordDelayedHierarchyState.None)]
-        public async Task Table_RendersHierarchyOptionsAsync(int rowIndex, TableRecordDelayedHierarchyState expectedHierarchyState)
+    [Theory]
+    [InlineData(0, TableRecordDelayedHierarchyState.CanLoadChildren)]
+    [InlineData(1, TableRecordDelayedHierarchyState.None)]
+    [InlineData(2, TableRecordDelayedHierarchyState.LoadingChildren)]
+    [InlineData(3, TableRecordDelayedHierarchyState.None)]
+    public async Task Table_RendersHierarchyOptionsAsync(int rowIndex, TableRecordDelayedHierarchyState expectedHierarchyState)
+    {
+        await using (var pageWrapper = await NewPageForRouteAsync("TableSetRecordHierarchyOptionsTest"))
         {
-            await using (var pageWrapper = await NewPageForRouteAsync("TableSetRecordHierarchyOptionsTest"))
+            var page = pageWrapper.Page;
+            var table = page.Locator("nimble-table");
+            await Assertions.Expect(table).ToBeVisibleAsync();
+
+            var rows = table.Locator("nimble-table-row");
+            await Assertions.Expect(rows).ToHaveCountAsync(4);
+
+            var row = rows.Nth(rowIndex);
+            var rowExpandCollapseButton = row.Locator("nimble-button");
+            var rowSpinner = row.Locator("nimble-spinner");
+
+            if (expectedHierarchyState == TableRecordDelayedHierarchyState.CanLoadChildren)
             {
-                var page = pageWrapper.Page;
-                var table = page.Locator("nimble-table");
-                await Assertions.Expect(table).ToBeVisibleAsync();
+                await Assertions.Expect(rowExpandCollapseButton).ToBeVisibleAsync();
+            }
+            else
+            {
+                await Assertions.Expect(rowExpandCollapseButton).Not.ToBeVisibleAsync();
+            }
 
-                var rows = table.Locator("nimble-table-row");
-                await Assertions.Expect(rows).ToHaveCountAsync(4);
-
-                var row = rows.Nth(rowIndex);
-                var rowExpandCollapseButton = row.Locator("nimble-button");
-                var rowSpinner = row.Locator("nimble-spinner");
-
-                if (expectedHierarchyState == TableRecordDelayedHierarchyState.CanLoadChildren)
-                {
-                    await Assertions.Expect(rowExpandCollapseButton).ToBeVisibleAsync();
-                }
-                else
-                {
-                    await Assertions.Expect(rowExpandCollapseButton).Not.ToBeVisibleAsync();
-                }
-
-                if (expectedHierarchyState == TableRecordDelayedHierarchyState.LoadingChildren)
-                {
-                    await Assertions.Expect(rowSpinner).ToBeVisibleAsync();
-                }
-                else
-                {
-                    await Assertions.Expect(rowSpinner).Not.ToBeVisibleAsync();
-                }
+            if (expectedHierarchyState == TableRecordDelayedHierarchyState.LoadingChildren)
+            {
+                await Assertions.Expect(rowSpinner).ToBeVisibleAsync();
+            }
+            else
+            {
+                await Assertions.Expect(rowSpinner).Not.ToBeVisibleAsync();
             }
         }
+    }
 
-        [Fact]
-        public async Task Table_TriggersRowExpandToggleEventAsync()
+    [Fact]
+    public async Task Table_TriggersRowExpandToggleEventAsync()
+    {
+        await using (var pageWrapper = await NewPageForRouteAsync("TableSetRecordHierarchyOptionsTest"))
         {
-            await using (var pageWrapper = await NewPageForRouteAsync("TableSetRecordHierarchyOptionsTest"))
-            {
-                var page = pageWrapper.Page;
-                var table = page.Locator("nimble-table");
-                await Assertions.Expect(table).ToBeVisibleAsync();
-                var textField = page.Locator("nimble-text-field");
+            var page = pageWrapper.Page;
+            var table = page.Locator("nimble-table");
+            await Assertions.Expect(table).ToBeVisibleAsync();
+            var textField = page.Locator("nimble-text-field");
 
-                var rows = table.Locator("nimble-table-row");
-                var expandableRow = rows.Nth(0);
-                var rowExpandCollapseButton = expandableRow.Locator("nimble-button");
+            var rows = table.Locator("nimble-table-row");
+            var expandableRow = rows.Nth(0);
+            var rowExpandCollapseButton = expandableRow.Locator("nimble-button");
 
-                await rowExpandCollapseButton.ClickAsync();
-                await Assertions.Expect(textField).ToHaveAttributeAsync("current-value", "RecordId: 0, OldState: False, NewState: True");
+            await rowExpandCollapseButton.ClickAsync();
+            await Assertions.Expect(textField).ToHaveAttributeAsync("current-value", "RecordId: 0, OldState: False, NewState: True");
 
-                await rowExpandCollapseButton.ClickAsync();
-                await Assertions.Expect(textField).ToHaveAttributeAsync("current-value", "RecordId: 0, OldState: True, NewState: False");
-            }
+            await rowExpandCollapseButton.ClickAsync();
+            await Assertions.Expect(textField).ToHaveAttributeAsync("current-value", "RecordId: 0, OldState: True, NewState: False");
         }
     }
 }
