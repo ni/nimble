c15f71f11b0d959600523366ff638eda20a0ba20
Razvan Nazare <razvan.nazare@ni.com>
2024-02-23 18:52:54 +0200

    Update matrix-renderer.spec.ts

--- packages/nimble-components/src/wafer-map/tests/matrix-renderer.spec.ts
+++ packages/nimble-components/src/wafer-map/tests/matrix-renderer.spec.ts
@@ -2,9 +2,9 @@ import { WaferMap } from '..';
 import { MatrixRenderer } from '../modules/matrix-renderer';
 
 /*
-* Sometimes methods from render worker become bugged after being built, bundled and copied to a different location
-* with these tests we ensure they behave as expected after this process
-*/
+ * Sometimes methods from render worker become bugged after being built, bundled and copied to a different location
+ * with these tests we ensure they behave as expected after this process
+ */
 describe('MatrixRenderer worker:', () => {
     const wafermap = new WaferMap();
     let renderer = new MatrixRenderer(wafermap);
@@ -13,7 +13,9 @@ describe('MatrixRenderer worker:', () => {
     });
 
     it('emptyMatrix() should empty the dieMatrix', async () => {
-        renderer.workerOne.dieMatrix = Promise.resolve(Uint8Array.from([1, 2, 3]));
+        renderer.workerOne.dieMatrix = Promise.resolve(
+            Uint8Array.from([1, 2, 3])
+        );
         await renderer.workerOne.emptyMatrix();
         const resolvedDieMatrix = await renderer.workerOne.dieMatrix;
         expect(resolvedDieMatrix.length).toEqual(0);
@@ -22,6 +24,8 @@ describe('MatrixRenderer worker:', () => {
     it('updateMatrix should update the dieMatrix correctly', async () => {
         const testData: Iterable<number> = [4, 5, 6];
         await renderer.workerOne.updateMatrix(testData);
-        expect(renderer.workerOne.dieMatrix).toEqual(Promise.resolve(Uint8Array.from(testData)));
+        expect(renderer.workerOne.dieMatrix).toEqual(
+            Promise.resolve(Uint8Array.from(testData))
+        );
     });
 });
