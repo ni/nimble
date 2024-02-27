728a0e7b34ccb5bae928a0375f2e52e54240de45
Razvan Nazare <razvan.nazare@ni.com>
2024-02-27 18:31:43 +0200

    Update render-worker.ts

--- packages/nimble-components/build/generate-workers/source/render-worker.ts
+++ packages/nimble-components/build/generate-workers/source/render-worker.ts
@@ -1,5 +1,12 @@
 import { expose } from 'comlink';
 
+/**
+ * RenderWorker class is meant to be used within a Web Worker context, 
+ * using Comlink to facilitate communication between the main thread and the worker. 
+ * The RenderWorker class manages a matrix of dies, once an instance of RenderWorker is created, 
+ * it is exposed to the main thread using Comlink's `expose` method.
+ * This setup is used in the wafer-map component to perform heavy computational duties
+ */
 export class RenderWorker {
     public worker!: number;
     public dieMatrix: Uint8Array = Uint8Array.from([]);
