1616cd9c581c18a1387d2fa883416e350662d558
Razvan Nazare <razvan.nazare@ni.com>
2024-02-27 18:35:55 +0200

    Update matrix-renderer.ts

--- packages/nimble-components/src/wafer-map/modules/matrix-renderer.ts
+++ packages/nimble-components/src/wafer-map/modules/matrix-renderer.ts
@@ -1,4 +1,4 @@
-import * as Comlink from 'comlink';
+import { wrap, Remote } from 'comlink';
 import type { WaferMap } from '..';
 import { workerCode } from '../workers/render-worker';
 import type { RenderWorker } from '../../../build/generate-workers/dist/esm/source/render-worker';
@@ -7,11 +7,11 @@ import type { RenderWorker } from '../../../build/generate-workers/dist/esm/sour
  * Responsible for drawing the dies inside the wafer map, adding dieText and scaling the canvas
  */
 export class MatrixRenderer {
-    public readonly workerOne: Comlink.Remote<RenderWorker>;
+    public readonly workerOne: Remote<RenderWorker>;
 
     public constructor(private readonly wafermap: WaferMap) {
         const blob = new Blob([workerCode], { type: 'text/javascript' });
         const url = URL.createObjectURL(blob);
-        this.workerOne = Comlink.wrap<RenderWorker>(new Worker(url));
+        this.workerOne = wrap<RenderWorker>(new Worker(url));
     }
 }
