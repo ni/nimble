0b49379c61d3a5a21e917726dc658b24cc04dfc6
Razvan Nazare <razvan.nazare@ni.com>
2024-02-28 20:27:02 +0200

    implemented feedback

--- packages/nimble-components/src/wafer-map/modules/worker-renderer.ts
+++ packages/nimble-components/src/wafer-map/modules/worker-renderer.ts
@@ -8,18 +8,19 @@ let url: string;
  * Asynchronously creates and returns a Remote<MatrixRenderer> instance.
  * This function simplifies the process of creating and accessing MatrixRenderer instances.
  */
-export const createMatrixRenderer = async (): Promise<
-Remote<MatrixRenderer>
-> => {
+export const createMatrixRenderer = async (): Promise<{
+    matrixRenderer: Remote<MatrixRenderer>,
+    terminate: () => void
+}> => {
     if (url === undefined) {
         const blob = new Blob([workerCode], { type: 'text/javascript' });
         url = URL.createObjectURL(blob);
     }
-    // eslint-disable-next-line @typescript-eslint/naming-convention
     const worker = new Worker(url);
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     const RemoteMatrixRenderer = wrap<new() => MatrixRenderer>(worker);
     const matrixRenderer = await new RemoteMatrixRenderer();
-    const terminate = () => worker.terminate();
+    const terminate = (): void => worker.terminate();
     return {
         matrixRenderer,
         terminate
--- packages/nimble-components/src/wafer-map/tests/worker-renderer.spec.ts
+++ packages/nimble-components/src/wafer-map/tests/worker-renderer.spec.ts
@@ -3,14 +3,25 @@ import { createMatrixRenderer } from '../modules/worker-renderer';
 import type { MatrixRenderer } from '../../../build/generate-workers/dist/esm/source/matrix-renderer';
 
 describe('MatrixRenderer worker:', () => {
-    let matrixRenderer: Remote<MatrixRenderer>;
+    let matrixRenderer: Remote<MatrixRenderer> | undefined;
+    let terminate: () => void;
 
     beforeEach(async () => {
-        matrixRenderer = await createMatrixRenderer();
+        const result = await createMatrixRenderer();
+        matrixRenderer = result.matrixRenderer;
+        terminate = result.terminate;
+    });
+
+    afterEach(() => {
+        terminate();
+        matrixRenderer = undefined;
     });
 
     it('updateMatrix should update the dieMatrix', async () => {
         const testData = [4, 5, 6];
+        if (matrixRenderer === undefined) {
+            throw new Error('matrixRenderer is undefined');
+        }
         await matrixRenderer.updateMatrix(testData);
         const resolvedDieMatrix = await matrixRenderer.dieMatrix;
         expect(Array.from(resolvedDieMatrix)).toEqual(
@@ -20,6 +31,9 @@ describe('MatrixRenderer worker:', () => {
 
     it('emptyMatrix should empty the dieMatrix', async () => {
         const testData = [4, 5, 6];
+        if (matrixRenderer === undefined) {
+            throw new Error('matrixRenderer is undefined');
+        }
         await matrixRenderer.updateMatrix(testData);
         await matrixRenderer.emptyMatrix();
         const resolvedDieMatrix = await matrixRenderer.dieMatrix;
