5ca52869cf51568ad42c6760de5669f12a3843d2
Razvan Nazare <razvan.nazare@ni.com>
2024-02-28 19:37:24 +0200

    renamed wrapperdWorker into matrixRenderer

--- packages/nimble-components/build/generate-workers/source/tests/matrix-renderer.spec.ts
+++ packages/nimble-components/build/generate-workers/source/tests/matrix-renderer.spec.ts
@@ -2,27 +2,27 @@ import { Remote, expose, wrap } from 'comlink';
 import { MatrixRenderer } from '../matrix-renderer';
 
 describe('MatrixRenderer with MessageChannel', () => {
-    let wrappedWorker: Remote<MatrixRenderer>;
+    let matrixRenderer: Remote<MatrixRenderer>;
 
     beforeEach(async () => {
         const { port1, port2 } = new MessageChannel();
         const worker = new MatrixRenderer();
         expose(worker, port1);
-        wrappedWorker = await wrap<MatrixRenderer>(port2);
+        matrixRenderer = await wrap<MatrixRenderer>(port2);
     });
 
     it('updateMatrix should update the dieMatrix', async () => {
         const testData: Iterable<number> = [4, 5, 6];
-        await wrappedWorker.updateMatrix(testData);
+        await matrixRenderer.updateMatrix(testData);
 
-        const updatedMatrix = await wrappedWorker.dieMatrix;
+        const updatedMatrix = await matrixRenderer.dieMatrix;
         expect(updatedMatrix).toEqual(Uint8Array.from(testData));
     });
 
     it('emptyMatrix should empty the dieMatrix', async () => {
-        await wrappedWorker.emptyMatrix();
+        await matrixRenderer.emptyMatrix();
 
-        const updatedMatrix = await wrappedWorker.dieMatrix;
+        const updatedMatrix = await matrixRenderer.dieMatrix;
         expect(updatedMatrix.length).toEqual(0);
     });
 });
