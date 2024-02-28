916366ad3e89cdca3fb4086eca63450a2a3d55ef
Razvan-NI <110180309+Razvan1928@users.noreply.github.com>
2024-02-28 19:52:19 +0200

    Update packages/nimble-components/src/wafer-map/modules/worker-renderer.ts
    Co-authored-by: Milan Raj <rajsite@users.noreply.github.com>

--- packages/nimble-components/src/wafer-map/modules/worker-renderer.ts
+++ packages/nimble-components/src/wafer-map/modules/worker-renderer.ts
@@ -2,6 +2,9 @@ import { wrap, Remote } from 'comlink';
 import { workerCode } from '../workers/matrix-renderer';
 import type { MatrixRenderer } from '../../../build/generate-workers/dist/esm/source/matrix-renderer';
 
+
+let url: string;
+
 /**
  * Asynchronously creates and returns a Remote<MatrixRenderer> instance.
  * This function simplifies the process of creating and accessing MatrixRenderer instances.
@@ -9,8 +12,10 @@ import type { MatrixRenderer } from '../../../build/generate-workers/dist/esm/so
 export const createMatrixRenderer = async (): Promise<
 Remote<MatrixRenderer>
 > => {
-    const blob = new Blob([workerCode], { type: 'text/javascript' });
-    const url = URL.createObjectURL(blob);
+    if (url === undefined) {
+        const blob = new Blob([workerCode], { type: 'text/javascript' });
+        const url = URL.createObjectURL(blob);
+    }
     // eslint-disable-next-line @typescript-eslint/naming-convention
     const RemoteMatrixRenderer = wrap<new() => MatrixRenderer>(
         new Worker(url));
