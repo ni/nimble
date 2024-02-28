b6b2b0bb31dd3b21572588ca6a4bd271dd5b5afc
mollykreis <20542556+mollykreis@users.noreply.github.com>
2024-02-28 14:57:29 -0600

    PR feedback

--- packages/nimble-components/src/table-column/enum-base/models/mapping-icon-config.ts
+++ packages/nimble-components/src/table-column/enum-base/models/mapping-icon-config.ts
@@ -1,4 +1,4 @@
-import { html, when, type ViewTemplate } from '@microsoft/fast-element';
+import { html, type ViewTemplate } from '@microsoft/fast-element';
 import type { IconSeverity } from '../../../icon-base/types';
 import { MappingConfig } from './mapping-config';
 
@@ -6,23 +6,29 @@ export interface IconView {
     severity: IconSeverity;
     text?: string;
 }
+
+// Create an empty template containing only a space because creating a ViewTemplate
+// with an empty string throws an exception at runtime.
+const emptyTemplate = html<IconView>` `;
+
 const createIconTemplate = (
     icon: string | undefined
-): ViewTemplate<IconView> => html`
-    ${when(
-        () => icon,
-        html<IconView>`
-        <${icon!}
+): ViewTemplate<IconView> => {
+    if (icon === undefined) {
+        return emptyTemplate;
+    }
+
+    return html`
+        <${icon}
             title="${x => x.text}"
             role="img"
             aria-label="${x => x.text}"
             severity="${x => x.severity}"
             class="no-shrink"
         >
-        </${icon!}>
-    `
-    )}
-`;
+        </${icon}>
+    `;
+};
 
 /**
  * Mapping configuration corresponding to a icon mapping
