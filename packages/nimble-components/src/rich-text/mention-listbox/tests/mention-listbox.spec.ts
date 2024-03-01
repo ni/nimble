3405db3c7cf9ce2c63beff4e2e9fedab1a02ee7e
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2024-03-01 12:47:51 -0600

    Mention listbox test

--- packages/nimble-components/src/rich-text/mention-listbox/tests/mention-listbox.spec.ts
+++ packages/nimble-components/src/rich-text/mention-listbox/tests/mention-listbox.spec.ts
@@ -1,4 +1,8 @@
+import { html, repeat } from '@microsoft/fast-element';
 import { RichTextMentionListbox, richTextMentionListboxTag } from '..';
+import { waitForUpdatesAsync } from '../../../testing/async-helpers';
+import { type Fixture, fixture } from '../../../utilities/tests/fixture';
+import { listOptionTag } from '../../../list-option';
 
 describe('RichTextMentionListbox', () => {
     it('should export its tag', () => {
@@ -11,5 +15,42 @@ describe('RichTextMentionListbox', () => {
         expect(
             document.createElement('nimble-rich-text-mention-listbox')
         ).toBeInstanceOf(RichTextMentionListbox);
+    });
+
+    async function setup500Options(): Promise<Fixture<RichTextMentionListbox>> {
+        // prettier-ignore
+        const viewTemplate = html`
+            <${richTextMentionListboxTag}>
+                ${repeat(() => [...Array(500).keys()], html<number>`
+                    <${listOptionTag} value="${x => x}">${x => x}</${listOptionTag}>`)}
+            </${richTextMentionListboxTag}>
+        `;
+        return fixture<RichTextMentionListbox>(viewTemplate);
+    }
+
+    async function waitForSelectionUpdateAsync(): Promise<void> {
+        await waitForUpdatesAsync();
+        await waitForUpdatesAsync(); // second wait is necessary because scrolling is queued with requestAnimationFrame
+    }
+
+    it('should scroll the selected option into view when opened', async () => {
+        const { element, connect, disconnect } = await setup500Options();
+        await connect();
+        element.show({
+            anchorNode: document.documentElement,
+            filter: ''
+        });
+        await waitForSelectionUpdateAsync(); // showing filters the options and modifies the selection
+
+        element.selectedIndex = 300;
+        await waitForSelectionUpdateAsync();
+        const listbox = element.region!.querySelector('.listbox')!;
+        expect(listbox.scrollTop).toBeGreaterThan(8000);
+
+        element.selectedIndex = 0;
+        await waitForSelectionUpdateAsync();
+        expect(listbox.scrollTop).toBeCloseTo(4);
+
+        await disconnect();
     });
 });
