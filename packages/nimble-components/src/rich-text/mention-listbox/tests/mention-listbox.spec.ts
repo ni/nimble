64b4301d2e6891403a289166ad78c35982f04a8a
m-akinc <7282195+m-akinc@users.noreply.github.com>
2024-03-01 16:45:59 -0600

    Disable intermittent test (#1892)
    ## 🤨 Rationale
    
    New test is intermittent. Example failure:
    https://github.com/ni/nimble/actions/runs/8117617770/job/22190199901
    
    ## 👩‍💻 Implementation
    
    Followed intermittent test procedure.
    
    ## 🧪 Testing
    
    N/A
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

--- packages/nimble-components/src/rich-text/mention-listbox/tests/mention-listbox.spec.ts
+++ packages/nimble-components/src/rich-text/mention-listbox/tests/mention-listbox.spec.ts
@@ -35,7 +35,8 @@ describe('RichTextMentionListbox', () => {
         await waitAnimationFrame(); // necessary because scrolling is queued with requestAnimationFrame
     }
 
-    it('should scroll the selected option into view when opened', async () => {
+    // Intermittent test. See tech debt issue: https://github.com/ni/nimble/issues/1891
+    xit('should scroll the selected option into view when opened', async () => {
         const { element, connect, disconnect } = await setup500Options();
         await connect();
         element.show({
