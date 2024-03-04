20dcd92de4e9bcc13d73ef02660d9d5766fc5309
Razvan Nazare <razvan.nazare@ni.com>
2024-03-04 10:51:44 +0200

    Update wafer-map-convertor.spec.ts

--- packages/nimble-components/src/wafer-map/tests/wafer-map-convertor.spec.ts
+++ packages/nimble-components/src/wafer-map/tests/wafer-map-convertor.spec.ts
@@ -13,16 +13,26 @@ describe('WaferMap Convertor', () => {
 
     it('should return the maximum number of tags', () => {
         waferMapConvertor.computeMaximumNumberOfTags();
-        const maxTags = Math.max(...waferMapDies.map((die: WaferMapDie) => die.tags?.length ?? 0));
+        const maxTags = Math.max(
+            ...waferMapDies.map((die: WaferMapDie) => die.tags?.length ?? 0)
+        );
         expect(waferMapConvertor.maxTags).toEqual(maxTags);
     });
 
     it('should populate the wafer layers', () => {
         waferMapConvertor.populateLayers();
-        expect(waferMapConvertor.colIndexLayer).toEqual(waferMapDies.map(die => die.x));
-        expect(waferMapConvertor.rowIndexLayer).toEqual(waferMapDies.map(die => die.y));
-        expect(waferMapConvertor.valuesLayer).toEqual(waferMapDies.map(die => Number(die.value)));
-        expect(waferMapConvertor.tags).toEqual(waferMapDies.map(die => die.tags ?? []));
+        expect(waferMapConvertor.colIndexLayer).toEqual(
+            waferMapDies.map(die => die.x)
+        );
+        expect(waferMapConvertor.rowIndexLayer).toEqual(
+            waferMapDies.map(die => die.y)
+        );
+        expect(waferMapConvertor.valuesLayer).toEqual(
+            waferMapDies.map(die => Number(die.value))
+        );
+        expect(waferMapConvertor.tags).toEqual(
+            waferMapDies.map(die => die.tags ?? [])
+        );
     });
 
     it('should convert wafer map data to apache arrow table', () => {
@@ -37,14 +47,16 @@ describe('WaferMap Convertor', () => {
         arrays = {
             colIndex: new Int32Array(waferMapConvertorForTest.colIndexLayer),
             rowIndex: new Int32Array(waferMapConvertorForTest.rowIndexLayer),
-            value: new Float32Array(waferMapConvertorForTest.valuesLayer),
+            value: new Float32Array(waferMapConvertorForTest.valuesLayer)
         };
 
         for (let i = 0; i < waferMapConvertorForTest.maxTags; i++) {
-            const tagValues = waferMapConvertorForTest.tags.map(tag => tag[i] ?? null);
+            const tagValues = waferMapConvertorForTest.tags.map(
+                tag => tag[i] ?? null
+            );
             arrays = {
                 ...arrays,
-                [`tag${i}`]: tagValues,
+                [`tag${i}`]: tagValues
             };
         }
 
