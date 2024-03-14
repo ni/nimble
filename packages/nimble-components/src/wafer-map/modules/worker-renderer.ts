724afb97fe9d24b82878b7f2d0368bd704c86b2a
Razvan Nazare <razvan.nazare@ni.com>
2024-03-14 13:46:02 +0200

    added some experimental data in storybook

--- packages/nimble-components/src/wafer-map/modules/worker-renderer.ts
+++ packages/nimble-components/src/wafer-map/modules/worker-renderer.ts
@@ -5,7 +5,7 @@ import { HoverDieOpacity } from '../types';
  * Responsible for drawing the dies inside the wafer map, adding dieText and scaling the canvas
  */
 export class WorkerRenderer {
-    public constructor(private readonly wafermap: WaferMap) {}
+    public constructor(private readonly wafermap: WaferMap) { }
 
     public updateSortedDiesAndDrawWafer(): void {
         // redundant function for backwards compatibility
@@ -13,8 +13,25 @@ export class WorkerRenderer {
     }
 
     public drawWafer(): void {
+        const testData = {
+            colIndexes: [0, 100, 100, 100, 200, 200, 200, 200, 200, 300, 300, 300, 400],
+            rowIndexes: [200, 200, 100, 300, 200, 100, 0, 300, 400, 200, 100, 300, 200],
+            values: [
+                14.24, 76.43, 44.63, 67.93, 72.71, 79.04, 26.49, 37.79, 59.82, 52.92,
+                98.53, 20.83, 62.81
+            ]
+        };
+
         // rendering will be implemented in a future PR
-        this.renderHover();
+        this.wafermap.workerOne.setTransform(this.wafermap.transform).then(() => {
+            this.wafermap.workerOne.updateMatrix(
+                testData
+            ).then(() => {
+                this.wafermap.workerOne.drawWafer().then(() => {
+                    this.renderHover();
+                }, () => { });
+            }, () => { });
+        }, () => { });
     }
 
     public renderHover(): void {
