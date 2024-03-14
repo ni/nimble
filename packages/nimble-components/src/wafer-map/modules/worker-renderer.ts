6f5b3f3ca156743f8d6162f39fd9c4264ba833c2
Razvan Nazare <razvan.nazare@ni.com>
2024-03-14 13:47:54 +0200

    put back not needed to be deleted line

--- packages/nimble-components/src/wafer-map/modules/worker-renderer.ts
+++ packages/nimble-components/src/wafer-map/modules/worker-renderer.ts
@@ -14,6 +14,7 @@ export class WorkerRenderer {
 
     public drawWafer(): void {
         // rendering will be implemented in a future PR
+        this.renderHover();
     }
 
     public renderHover(): void {
