805f105919985ac6fda2ee3bbcfb4a18f9488e72
Natan Muntean <natan.muntean@ni.com>
2024-03-18 19:44:17 +0200

    replaced with experimental

--- packages/nimble-components/src/wafer-map/modules/event-coordinator.ts
+++ packages/nimble-components/src/wafer-map/modules/event-coordinator.ts
@@ -2,7 +2,7 @@ import type { WaferMapDie } from '../types';
 import { ZoomHandler } from './zoom-handler';
 import type { WaferMap } from '..';
 import { HoverHandler } from './hover-handler';
-import { HoverHandler as ExpHoverHandler } from './experimental/hover-handler';
+import { HoverHandler as ExperimentalHoverHandler } from './experimental/hover-handler';
 
 export interface EventCoordinatorCallbacks {
     dieSelected: (die: WaferMapDie) => void;
@@ -14,19 +14,19 @@ export interface EventCoordinatorCallbacks {
 export class EventCoordinator {
     private readonly zoomHandler;
     private readonly stableHoverHandler;
-    private readonly expHoverHandler;
-    private hoverHandler: HoverHandler | ExpHoverHandler;
+    private readonly experimentalHoverHandler;
+    private hoverHandler: HoverHandler | ExperimentalHoverHandler;
     public constructor(private readonly wafermap: WaferMap) {
         this.zoomHandler = new ZoomHandler(wafermap);
         this.stableHoverHandler = new HoverHandler(wafermap);
-        this.expHoverHandler = new ExpHoverHandler(wafermap);
+        this.experimentalHoverHandler = new ExperimentalHoverHandler(wafermap);
         this.hoverHandler = this.stableHoverHandler;
     }
 
     public setStrategy(): void {
         this.hoverHandler = this.wafermap.diesTable === undefined
             ? this.stableHoverHandler
-            : this.expHoverHandler;
+            : this.experimentalHoverHandler;
     }
 
     public attachEvents(): void {
