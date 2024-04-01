959686e1227470989c7c06858001e35bbdfd8dda
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2024-04-01 14:47:35 -0500

    Lint fix

--- packages/nimble-components/src/rich-text/viewer/styles.ts
+++ packages/nimble-components/src/rich-text/viewer/styles.ts
@@ -1,10 +1,6 @@
 import { css } from '@microsoft/fast-element';
 import { display } from '@microsoft/fast-foundation';
-import {
-    bodyFont,
-    bodyFontColor,
-    linkFontColor
-} from '../../theme-provider/design-tokens';
+import { bodyFont, bodyFontColor } from '../../theme-provider/design-tokens';
 
 export const styles = css`
     ${display('flex')}
