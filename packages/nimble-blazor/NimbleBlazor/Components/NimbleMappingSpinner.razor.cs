3df43384be04ed9d52d65177dabe305d65ae77b5
mollykreis <20542556+mollykreis@users.noreply.github.com>
2024-04-10 13:42:38 -0500

    angular + blazor

--- packages/nimble-blazor/NimbleBlazor/Components/NimbleMappingSpinner.razor.cs
+++ packages/nimble-blazor/NimbleBlazor/Components/NimbleMappingSpinner.razor.cs
@@ -9,4 +9,10 @@ public partial class NimbleMappingSpinner<TKey> : NimbleMappingBase<TKey>
     /// </summary>
     [Parameter]
     public string? Text { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether or not the mapping's text should be rendered in the table's cells.
+    /// </summary>
+    [Parameter]
+    public bool TextHidden { get; set; }
 }
