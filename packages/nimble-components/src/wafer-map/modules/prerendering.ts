6afe9a06dbb9c668c3f00362bac08868fea4c8f7
Razvan Nazare <razvan.nazare@ni.com>
2024-04-12 16:41:17 +0300

    Update prerendering.ts

--- packages/nimble-components/src/wafer-map/modules/prerendering.ts
+++ packages/nimble-components/src/wafer-map/modules/prerendering.ts
@@ -51,7 +51,7 @@ export class Prerendering {
         const isDieRenderInfo = (
             info: DieRenderInfo | null
         ): info is DieRenderInfo => info !== null;
-        if (!this.wafermap.isExperimentalUpdate()) {
+        if (this.wafermap.diesTable === undefined) {
             this._diesRenderInfo = this.wafermap.dies
                 .map(die => this.computeDieRenderInfo(die))
                 .filter(isDieRenderInfo);
