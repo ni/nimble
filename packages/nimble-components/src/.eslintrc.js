b68cd668b26acb889c9923d4f1a27668b81b6063
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2024-04-17 17:04:52 -0500

    Avoid lint error

--- packages/nimble-components/src/.eslintrc.js
+++ packages/nimble-components/src/.eslintrc.js
@@ -4,5 +4,6 @@ module.exports = {
     parserOptions: {
         project: '../tsconfig.json',
         tsconfigRootDir: __dirname
-    }
+    },
+    ignorePatterns: ['.eslintrc.js'] // Avoids eslint error: https://typescript-eslint.io/linting/troubleshooting#i-get-errors-telling-me-eslint-was-configured-to-run--however-that-tsconfig-does-not--none-of-those-tsconfigs-include-this-file
 };
