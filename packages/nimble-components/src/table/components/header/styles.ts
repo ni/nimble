fd64c39012334e905707a31bf8290fd831ef4898
mollykreis <20542556+mollykreis@users.noreply.github.com>
2024-04-22 12:06:41 -0500

    try this instead

--- packages/nimble-components/src/table/components/header/styles.ts
+++ packages/nimble-components/src/table/components/header/styles.ts
@@ -12,10 +12,10 @@ import {
     defaultMinPixelWidth
 } from '../../../table-column/base/types';
 
-// When the column is narrower than the default minimum width, the sorting and grouping
-// indicators should be hidden. The container query is based on the content of the header,
-// so the left/right padding should not be considered in the width.
-const defaultMinHeaderContentWidth = `${defaultMinPixelWidth - 2 * columnSpacing}px`;
+// When the column becomes more narrow than the default minimum width, the sorting and grouping
+// indicators should be hidden. The container query is based on the content of the header, so
+// the left/right padding should not be considered in the width.
+const defaultMinHeaderContentWidth = `${defaultMinPixelWidth - 2 * columnSpacing - 1}px`;
 
 export const styles = css`
     ${display('flex')}
@@ -36,13 +36,48 @@ export const styles = css`
     .sort-indicator,
     .grouped-indicator {
         flex: 0 0 auto;
-        display: none;
+
     }
 
-    @container column-header (min-width: ${defaultMinHeaderContentWidth}) {
+    @container column-header (max-width: ${defaultMinHeaderContentWidth}) {
         .sort-indicator,
         .grouped-indicator {
-            display: inline-flex;
+            display: none;
         }
     }
 `;
+
+// // When the column is narrower than the default minimum width, the sorting and grouping
+// // indicators should be hidden. The container query is based on the content of the header,
+// // so the left/right padding should not be considered in the width.
+// const defaultMinHeaderContentWidth = `${defaultMinPixelWidth - 2 * columnSpacing}px`;
+
+// export const styles = css`
+//     ${display('flex')}
+
+//     :host {
+//         height: ${controlHeight};
+//         align-items: center;
+//         padding: 0px ${mediumPadding};
+//         font: ${tableHeaderFont};
+//         color: ${tableHeaderFontColor};
+//         ${iconColor.cssCustomProperty}: ${tableHeaderFontColor};
+//         text-transform: uppercase;
+//         gap: ${mediumPadding};
+//         cursor: default;
+//         container: column-header / inline-size;
+//     }
+
+//     .sort-indicator,
+//     .grouped-indicator {
+//         flex: 0 0 auto;
+//         display: none;
+//     }
+
+//     @container column-header (min-width: ${defaultMinHeaderContentWidth}) {
+//         .sort-indicator,
+//         .grouped-indicator {
+//             display: inline-flex;
+//         }
+//     }
+// `;
