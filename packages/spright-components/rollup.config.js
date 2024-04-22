7c529a5f104662679f64bf2b09de9f91fdabc51b
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2024-04-22 12:06:06 -0500

    Add back circular dependency ignoring code

--- packages/spright-components/rollup.config.js
+++ packages/spright-components/rollup.config.js
@@ -16,6 +16,33 @@ const umdProductionPlugin = () => replace({
     preventAssignment: true
 });
 
+// Custom onwarn handler for ignoring specific warnings:
+// https://rollupjs.org/configuration-options/#onwarn
+const onwarn = (warning, defaultHandler) => {
+    const ignoredWarnings = [
+        // d3 has circular dependencies it won't remove:
+        // See https://github.com/d3/d3-selection/issues/168
+        {
+            code: 'CIRCULAR_DEPENDENCY',
+            file: 'node_modules/d3-'
+        },
+        // apache-arrow has circular dependencies:
+        // See https://github.com/apache/arrow/issues/40516
+        {
+            code: 'CIRCULAR_DEPENDENCY',
+            file: 'node_modules/apache-arrow'
+        }
+    ];
+
+    if (
+        !ignoredWarnings.some(
+            ({ code, file }) => warning.code === code && warning.message.includes(file)
+        )
+    ) {
+        defaultHandler(warning);
+    }
+};
+
 // eslint-disable-next-line import/no-default-export
 export default [
     {
@@ -32,7 +59,8 @@ export default [
             resolve(),
             commonJS(),
             json()
-        ]
+        ],
+        onwarn
     },
     {
         input: 'dist/esm/all-components.js',
@@ -55,6 +83,7 @@ export default [
             resolve(),
             commonJS(),
             json()
-        ]
+        ],
+        onwarn
     }
 ];
