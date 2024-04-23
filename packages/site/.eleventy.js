ae56f777e8aa6653d05b887582ccc04cd41ff892
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2024-04-23 16:08:25 -0500

    Revert changes to site package

--- packages/site/.eleventy.js
+++ packages/site/.eleventy.js
@@ -2,10 +2,9 @@ module.exports = eleventyConfig => {
     eleventyConfig.addPassthroughCopy({
         root: './',
         'landing/dist': './',
-        '../../packages/nimble-components/dist/storybook': 'storybook/nimble',
-        '../../angular-workspace/dist/example-client-app': 'storybook/nimble/example-client-app',
-        '../../packages/nimble-blazor/dist/blazor-client-app': 'storybook/nimble/blazor-client-app',
-        '../../packages/spright-components/dist/storybook': 'storybook/spright',
+        '../../packages/nimble-components/dist/storybook': 'storybook',
+        '../../angular-workspace/dist/example-client-app': 'storybook/example-client-app',
+        '../../packages/nimble-blazor/dist/blazor-client-app': 'storybook/blazor-client-app',
         '../../packages/performance/dist': 'storybook/performance'
     });
     eleventyConfig.setTemplateFormats([
