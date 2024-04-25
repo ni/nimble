c56b8518de39d66a1c86837b160348def0ad992d
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2024-04-25 17:40:15 -0500

    Fix output paths

--- packages/nimble-blazor/build/generate-hybrid/rollup.nimble.config.js
+++ packages/nimble-blazor/build/generate-hybrid/rollup.nimble.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 export default {
     input: path.resolve(__dirname, 'source/NimbleBlazor.HybridWorkaround.js'),
     output: {
-        file: path.resolve(__dirname, '../../wwwroot/NimbleBlazor.HybridWorkaround.js'),
+        file: path.resolve(__dirname, '../../NimbleBlazor/wwwroot/NimbleBlazor.HybridWorkaround.js'),
         format: 'iife'
     },
     plugins: [nodeResolve()]
