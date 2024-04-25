affc0edd20a8e0b03c871c0931c9486e2dc89b9f
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2024-04-25 18:07:05 -0500

    Add and exercise testing fixture in spright-components

--- packages/spright-components/src/rectangle/tests/rectangle.spec.ts
+++ packages/spright-components/src/rectangle/tests/rectangle.spec.ts
@@ -1,6 +1,24 @@
+import { html } from '@microsoft/fast-element';
 import { Rectangle, rectangleTag } from '..';
+import { fixture, Fixture } from '../../utilities/tests/fixture';
+
+async function setup(): Promise<Fixture<Rectangle>> {
+    return fixture<Rectangle>(html`<spright-rectangle></spright-rectangle>`);
+}
 
 describe('Rectangle', () => {
+    let element: Rectangle;
+    let connect: () => Promise<void>;
+    let disconnect: () => Promise<void>;
+
+    beforeEach(async () => {
+        ({ element, connect, disconnect } = await setup());
+    });
+
+    afterEach(async () => {
+        await disconnect();
+    });
+
     it('should export its tag', () => {
         expect(rectangleTag).toBe('spright-rectangle');
     });
@@ -9,5 +27,12 @@ describe('Rectangle', () => {
         expect(document.createElement('spright-rectangle')).toBeInstanceOf(
             Rectangle
         );
+    });
+
+    it('should have a slot element in the shadow DOM', async () => {
+        await connect();
+        expect(
+            (element.shadowRoot?.childNodes.item(0) as HTMLElement).tagName
+        ).toBe('SLOT');
     });
 });
