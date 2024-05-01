8df826989df0f1abeb07817295a6b0f1c032a378
rajsite <rajsite@users.noreply.github.com>
2024-05-01 13:16:30 -0500

    Concurrent all the things

---
+++ packages/concurrently-workspace/README.md
@@ -0,0 +1,3 @@
+# Concurrently workspace
+
+A workaround for concurrently to support workspaces. Based on [issue 284](https://github.com/open-cli-tools/concurrently/issues/284#issuecomment-1030102961).
---
+++ packages/concurrently-workspace/index.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+const concurrently = require("concurrently");
+const fs = require('fs');
+const path = require('path');
+
+const workingDirectory = process.cwd();
+console.log(`Working directory: ${workingDirectory}`);
+const workspacePackage = JSON.parse(fs.readFileSync(path.resolve(workingDirectory, 'package.json')));
+const workspaces = workspacePackage.workspaces;
+console.log(`Found workspace entries: ${workspaces.join(', ')}`);
+const script = process.argv[2]
+console.log(`Script to run ${script}`);
+
+concurrently(workspaces.map((workspace) => ({
+    command: `npm run ${script} -w ${workspace} --if-present`,
+    name: `${workspace}:${script}`,
+})), {
+    cwd: workingDirectory,
+    group: true,
+    timings: true
+}).result.catch(e => {
+    console.error('Commands had non-zero exit codes');
+    process.exit(1);
+});
