caf5e11a18d51b90e223e4f8ee8c81f84dfc62c7
mollykreis <20542556+mollykreis@users.noreply.github.com>
2024-05-10 12:27:44 -0500

    minor updates

--- packages/blazor-workspace/NimbleBlazor/Components/NimbleMappingEmpty.razor.cs
+++ packages/blazor-workspace/NimbleBlazor/Components/NimbleMappingEmpty.razor.cs
@@ -5,7 +5,7 @@ namespace NimbleBlazor;
 public partial class NimbleMappingEmpty<TKey> : NimbleMappingBase<TKey>
 {
     /// <summary>
-    /// Gets or sets text for the mapped value, tooltip, and accessible name.
+    /// Gets or sets text for the mapped value.
     /// </summary>
     [Parameter]
     public string? Text { get; set; }
