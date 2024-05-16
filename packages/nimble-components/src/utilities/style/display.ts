cb7b8a49d2f896c30e3756992d14962ee07ecb0f
rajsite <rajsite@users.noreply.github.com>
2024-05-16 12:46:14 -0500

    fn docs

--- packages/nimble-components/src/utilities/style/display.ts
+++ packages/nimble-components/src/utilities/style/display.ts
@@ -4,6 +4,10 @@ import {
     display as foundationDisplay
 } from '@microsoft/fast-foundation';
 
+/**
+ * This utility will generate the appropriate display style, as well as a style rule
+ * to hide the host element when its `hidden` attribute is set.
+ */
 export const display: typeof foundationDisplay = (
     displayValue: CSSDisplayPropertyValue
 ) => `${foundationDisplay(displayValue)}`;
