813e0e2ee0d7c23b84158e4bffefe3b0b0a30b0c
rajsite <rajsite@users.noreply.github.com>
2024-05-22 11:09:45 -0500

    Update eslint rule restrictions

--- packages/storybook/.eslintrc.cjs
+++ packages/storybook/.eslintrc.cjs
@@ -15,7 +15,21 @@ module.exports = {
                     'error',
                     { devDependencies: true }
                 ],
-                'import/no-relative-packages': 'off'
+                // This rule is disabled in order to allow linking directly to nimble-components and spright-components source
+                // Other files in the repo should NOT directly link to source and should resolve with the package identifier instead
+                'import/no-relative-packages': 'off',
+                'no-restricted-imports': [
+                    'error',
+                    {
+                        patterns: [
+                            {
+                                group: ['@ni/nimble-components/*', '@ni/spright-components/*'],
+                                message:
+                                    'For storybook, link directly to the nimble-components or spright-components source path in the monorepo instead.'
+                            },
+                        ]
+                    }
+                ],
             }
         }
     ]
