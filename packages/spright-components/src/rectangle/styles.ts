a912f916336264ef530bc4f61f977786665715ca
rajsite <rajsite@users.noreply.github.com>
2024-05-22 11:10:52 -0500

    Revert "Spright recatangle red"
    This reverts commit c03e5e4e1c792f38ab9c4d7fb8493c02c49f4979.

--- packages/spright-components/src/rectangle/styles.ts
+++ packages/spright-components/src/rectangle/styles.ts
@@ -3,6 +3,7 @@ import {
     bodyDisabledFontColor,
     bodyFont,
     bodyFontColor,
+    borderHoverColor,
     borderRgbPartialColor
 } from '@ni/nimble-components/dist/esm/theme-provider/design-tokens';
 import { display } from '../utilities/style/display';
@@ -11,7 +12,7 @@ export const styles = css`
     ${display('inline-block')}
 
     :host {
-        border: 2px solid red;
+        border: 2px solid ${borderHoverColor};
     }
 
     :host(:hover) {
