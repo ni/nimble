1bf7d77b1e2eece17da344999755b1e256221747
Jesse Attas <jattasNI@users.noreply.github.com>
2024-05-24 14:05:17 -0500

    separate docs for checkbox attribute and property

--- packages/storybook/src/nimble/checkbox/checkbox.stories.ts
+++ packages/storybook/src/nimble/checkbox/checkbox.stories.ts
@@ -7,6 +7,7 @@ import { apiCategory, createUserSelectedThemeStory, disabledDescription, slotted
 interface CheckboxArgs {
     label: string;
     checked: boolean;
+    checkedProperty: undefined;
     indeterminate: boolean;
     disabled: boolean;
     change: undefined;
@@ -36,8 +37,13 @@ const metadata: Meta<CheckboxArgs> = {
             table: { category: apiCategory.slots }
         },
         checked: {
-            description: 'Whether the checkbox is checked.',
+            description: 'Whether the checkbox is initially checked. Setting this attribute after the checkbox initializes will not affect its current state. However, interactively changing the checkbox state does affect this attribute. Note that the `checked` property behaves differently than the `checked` attribute.',
             table: { category: apiCategory.attributes }
+        },
+        checkedProperty: {
+            name: 'checked',
+            description: 'Whether the checkbox is checked. Setting this property affects the current checkbox state. Interactively changing the checkbox state affects this property. Note that the `checked` property behaves differently than the `checked` attribute.',
+            table: { category: apiCategory.nonAttributeProperties }
         },
         indeterminate: {
             description: `Whether the checkbox is in the indeterminate (i.e. partially checked) state. Configured programmatically, not by attribute.
