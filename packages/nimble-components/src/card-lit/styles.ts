11b439ff8355a3384139700e67080927303e18be
Jesse Attas <jattasNI@users.noreply.github.com>
2024-05-29 11:47:44 -0500

    Fix styles for card

--- packages/nimble-components/src/card-lit/styles.ts
+++ packages/nimble-components/src/card-lit/styles.ts
@@ -1,37 +1,37 @@
-import { css } from 'lit';
-// import { display } from '../utilities/style/display';
+import { css, unsafeCSS } from 'lit';
+import { display } from '../utilities/style/display';
 import {
     bodyEmphasizedFontWeight,
     bodyFont,
     bodyFontColor,
     cardBorderColor,
-    // largePadding,
+    largePadding,
     subtitleFontColor,
     subtitlePlus1Font
 } from '../theme-provider/design-tokens';
 
-// TODO: CSS types
-    // ${display('flex')}
-    // :host {
-    //     flex-direction: column;
-    //     gap: ${largePadding};
-    //     padding: ${largePadding};
-    //     border: 2px solid ${cardBorderColor};
-    //     border-radius: 8px;
-    //     font: ${bodyFont};
-    //     color: ${bodyFontColor};
-    // }
-
-    // section {
-    //     display: contents;
-    // }
-
-    // slot[name='title'] {
-    //     font: ${subtitlePlus1Font};
-    //     font-weight: ${bodyEmphasizedFontWeight};
-    //     color: ${subtitleFontColor};
-    // }
 
 export const styles = css`
+    ${unsafeCSS(display('flex'))}
 
+    :host {
+        flex-direction: column;
+        gap: var(${unsafeCSS(largePadding.cssCustomProperty)});
+        padding: var(${unsafeCSS(largePadding.cssCustomProperty)});
+        border: 2px solid var(${unsafeCSS(cardBorderColor.cssCustomProperty)});
+        border-radius: 8px;
+        font: var(${unsafeCSS(bodyFont.cssCustomProperty)});
+        color: var(${unsafeCSS(bodyFontColor.cssCustomProperty)});
+    }
+    section {
+        display: contents;
+    }
+
+    slot[name='title'] {
+        font: var(${unsafeCSS(subtitlePlus1Font.cssCustomProperty)});
+        font-weight: var(
+            ${unsafeCSS(bodyEmphasizedFontWeight.cssCustomProperty)}
+        );
+        color: var(${unsafeCSS(subtitleFontColor.cssCustomProperty)});
+    }
 `;
