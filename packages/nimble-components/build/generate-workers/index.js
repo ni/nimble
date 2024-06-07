647eb3b2aee2ac727f2d5a3b45fd5f0ed8f6cf96
rajsite <rajsite@users.noreply.github.com>
2024-06-07 16:22:14 -0500

    lint

--- packages/nimble-components/build/generate-workers/index.js
+++ packages/nimble-components/build/generate-workers/index.js
@@ -3,15 +3,18 @@ const path = require('path');
 
 const workersDirectory = path.resolve(__dirname, '../../src/wafer-map/workers');
 
-const matrixRendererBundlePath = path.resolve(__dirname, './dist/bundle/matrix-renderer.js');
+const matrixRendererBundlePath = path.resolve(
+    __dirname,
+    './dist/bundle/matrix-renderer.js'
+);
 const matrixRendererBundle = fs.readFileSync(matrixRendererBundlePath, 'utf-8');
 const typesDTSPath = path.resolve(__dirname, './dist/esm/types.d.ts');
 const typesDTS = fs.readFileSync(typesDTSPath, 'utf-8');
-const matrixRendererTypesPath = path.resolve(__dirname, './dist/esm/matrix-renderer.d.ts');
-const matrixRendererTypes = fs.readFileSync(
-    matrixRendererTypesPath,
-    'utf-8'
+const matrixRendererTypesPath = path.resolve(
+    __dirname,
+    './dist/esm/matrix-renderer.d.ts'
 );
+const matrixRendererTypes = fs.readFileSync(matrixRendererTypesPath, 'utf-8');
 
 const workerCode = `export const workerCode = ${JSON.stringify(matrixRendererBundle)};`;
 
