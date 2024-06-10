d2a0eb5611a7ea8a6f3a6d06e8fdea3fea5e31e5
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2024-06-10 15:44:23 -0500

    Update inlined code from most recent

--- packages/nimble-components/karma-webkit-launcher-custom.js
+++ packages/nimble-components/karma-webkit-launcher-custom.js
@@ -18,124 +18,117 @@
  * @author mbordihn@google.com (Markus Bordihn)
  */
 
-const child_process = require('child_process');
-const os = require('os');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
+const child_process = require("child_process");
+const os = require("os");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
-const isCI = require('is-ci');
+const isCI = require("is-ci");
 
 /**
  * @return {string}
  */
 function getTempDir() {
-    return path.join(os.tmpdir(), uuidv4());
+  return path.join(os.tmpdir(), uuidv4());
 }
 
 /**
  * @return {boolean}
  */
 const isPlaywrightAvailable = function () {
-    try {
-        if (require.resolve('playwright')) {
-            return true;
-        }
-    } catch (e) {
-        return false;
+  try {
+    if (require.resolve("playwright")) {
+      return true;
     }
+  } catch (e) {
     return false;
+  }
+  return false;
 };
 
 /**
  * @return {boolean}
  */
 const isPlaywrightCoreAvailable = function () {
-    try {
-        if (require.resolve('playwright-core')) {
-            return true;
-        }
-    } catch (e) {
-        return false;
+  try {
+    if (require.resolve("playwright-core")) {
+      return true;
     }
+  } catch (e) {
     return false;
+  }
+  return false;
 };
 
 /**
  * @return {boolean}
  */
 const isPlaywrightWebkitAvailable = function () {
-    try {
-        if (require.resolve('playwright-webkit')) {
-            return true;
-        }
-    } catch (e) {
-        return false;
+  try {
+    if (require.resolve("playwright-webkit")) {
+      return true;
     }
+  } catch (e) {
     return false;
+  }
+  return false;
 };
 
 /**
  * @return {String}
  */
 const getPlaywrightExecutable = function () {
-    if (isPlaywrightAvailable()) {
-        const playwright = require('playwright');
-        return playwright.webkit.executablePath();
-    } if (isPlaywrightWebkitAvailable()) {
-        const playwright = require('playwright-webkit');
-        return playwright.webkit.executablePath();
-    } if (isPlaywrightCoreAvailable()) {
-        const playwright = require('playwright-core');
-        return playwright.webkit.executablePath();
-    }
+  if (isPlaywrightAvailable()) {
+    const playwright = require("playwright");
+    return playwright.webkit.executablePath();
+  } else if (isPlaywrightWebkitAvailable()) {
+    const playwright = require("playwright-webkit");
+    return playwright.webkit.executablePath();
+  } else if (isPlaywrightCoreAvailable()) {
+    const playwright = require("playwright-core");
+    return playwright.webkit.executablePath();
+  }
 };
 
 /**
  * @return {String}
  */
 const getWebkitExecutable = function () {
-    if (
-        isPlaywrightAvailable()
-        || isPlaywrightWebkitAvailable()
-        || isPlaywrightCoreAvailable()
-    ) {
-        return getPlaywrightExecutable();
-    }
-    /*
-     if (process.platform == 'darwin') {
-        return '/usr/bin/osascript';
-    }
-    */
-    return '';
+  if (
+    isPlaywrightAvailable() ||
+    isPlaywrightWebkitAvailable() ||
+    isPlaywrightCoreAvailable()
+  ) {
+    return getPlaywrightExecutable();
+  } else if (process.platform == "darwin") {
+    return "/usr/bin/osascript";
+  }
+  return "";
 };
 
 /**
  * @return {boolean}
  */
 const hasSafariEnv = function () {
-    return (
-        process.env && process.env.SAFARI_BIN && process.env.SAFARI_BIN != ''
-    );
+  return process.env && process.env.SAFARI_BIN && process.env.SAFARI_BIN != "";
 };
 
 /**
  * @return {boolean}
  */
 const hasWebkitEnv = function () {
-    return (
-        process.env && process.env.WEBKIT_BIN && process.env.WEBKIT_BIN != ''
-    );
+  return process.env && process.env.WEBKIT_BIN && process.env.WEBKIT_BIN != "";
 };
 
 /**
  * @return {boolean}
  */
 const hasWebkitHeadlessEnv = function () {
-    return (
-        process.env
-        && process.env.WEBKIT_HEADLESS_BIN
-        && process.env.WEBKIT_HEADLESS_BIN != ''
-    );
+  return (
+    process.env &&
+    process.env.WEBKIT_HEADLESS_BIN &&
+    process.env.WEBKIT_HEADLESS_BIN != ""
+  );
 };
 
 /**
@@ -144,12 +137,80 @@ const hasWebkitHeadlessEnv = function () {
  * @return {URL}
  */
 const addTestBrowserInformation = function (url, test_browser) {
-    const newURL = new URL(url);
-    newURL.searchParams.append('test_browser', test_browser);
-    return newURL;
+  const newURL = new URL(url);
+  newURL.searchParams.append("test_browser", test_browser);
+  return newURL;
 };
 
-/* omitted: SafariBrowser */
+/**
+ * Safari Browser definition.
+ * @param {*} baseBrowserDecorator
+ * @param {*} args
+ */
+const SafariBrowser = function (baseBrowserDecorator, args) {
+  baseBrowserDecorator(this);
+  let testUrl;
+
+  this._start = (url) => {
+    const flags = args.flags || [];
+    const command = this._getCommand();
+    testUrl = addTestBrowserInformation(url, "Safari");
+    if (command && command.endsWith("osascript")) {
+      if (process.platform != "darwin") {
+        console.warn(
+          `The platform ${process.platform}, is unsupported for SafariBrowser.`
+        );
+      }
+      if (isCI) {
+        console.warn(
+          `Depending on the CI system, it could be that you need to disable SIP to allow the execution of AppleScripts!`
+        );
+      }
+      this._execCommand(
+        this._getCommand(),
+        [path.resolve(__dirname, "scripts/LaunchSafari.scpt"), testUrl].concat(
+          flags
+        )
+      );
+    } else {
+      this._execCommand(
+        this._getCommand(),
+        [testUrl, "--user-data-dir=" + getTempDir()].concat(flags)
+      );
+    }
+  };
+
+  this.on("kill", (done) => {
+    // Close opened tabs if open by osascript.
+    if (
+      process.platform == "darwin" &&
+      this._getCommand().endsWith("osascript")
+    ) {
+      closeSafariTab(testUrl);
+    }
+    done();
+  });
+
+  this.on("done", () => {
+    // Close opened tabs if open by osascript.
+    if (
+      process.platform == "darwin" &&
+      this._getCommand().endsWith("osascript")
+    ) {
+      closeSafariTab(testUrl);
+    }
+  });
+};
+
+SafariBrowser.prototype = {
+  name: "Safari",
+  DEFAULT_CMD: {
+    darwin: !hasSafariEnv() ? "/usr/bin/osascript" : "",
+  },
+  ENV_CMD: "SAFARI_BIN",
+};
+
+SafariBrowser.$inject = ["baseBrowserDecorator", "args"];
 
 /**
  * Webkit Browser definition.
@@ -157,59 +218,67 @@ const addTestBrowserInformation = function (url, test_browser) {
  * @param {*} args
  */
 const WebkitBrowser = function (baseBrowserDecorator, args) {
-    // Automatically switch to Safari, if osascript is used and not headless mode.
-    if (
-        (args && args.flags
-            ? !args.flags.join(' ').includes('--headless')
-            : true)
-        && process.platform == 'darwin'
-        && !hasWebkitEnv()
-        && getWebkitExecutable().endsWith('osascript')
-    ) {
-        SafariBrowser.call(this, baseBrowserDecorator, args);
-        return;
+  // Automatically switch to Safari, if osascript is used and not headless mode.
+  if (
+    (args && args.flags
+      ? !args.flags.join(" ").includes("--headless")
+      : true) &&
+    process.platform == "darwin" &&
+    !hasWebkitEnv() &&
+    getWebkitExecutable().endsWith("osascript")
+  ) {
+    SafariBrowser.call(this, baseBrowserDecorator, args);
+    return;
+  }
+
+  baseBrowserDecorator(this);
+  let testUrl;
+
+  this._start = (url) => {
+    const command = this._getCommand();
+
+    // Add used browser to test url.
+    if (command && command.includes("ms-playwright")) {
+      testUrl = addTestBrowserInformation(url, "Playwright");
+    } else {
+      testUrl = addTestBrowserInformation(url, "Custom");
     }
 
-    baseBrowserDecorator(this);
-    let testUrl;
+    const flags = args.flags || [];
+    this._execCommand(
+      this._getCommand(),
+      [testUrl, "--user-data-dir=" + getTempDir()].concat(flags)
+    );
+  };
 
-    this._start = url => {
-        const command = this._getCommand();
+  this.on("kill", (done) => {
+    // Clean up all remaining processes after 500ms delay on normal clients.
+    //if (!isCI) {
+      childProcessCleanup(this.id, done);
+    //} else {
+    //  done();
+    //}
+  });
 
-        // Add used browser to test url.
-        if (command && command.includes('ms-playwright')) {
-            testUrl = addTestBrowserInformation(url, 'Playwright');
-        } else {
-            testUrl = addTestBrowserInformation(url, 'Custom');
-        }
-
-        const flags = args.flags || [];
-        this._execCommand(
-            this._getCommand(),
-            [testUrl, `--user-data-dir=${getTempDir()}`].concat(flags)
-        );
-    };
-
-    this.on('kill', done => {
-        childProcessCleanup(this.id, done);
-    });
-
-    this.on('done', () => {
-        childProcessCleanup(this.id);
-    });
+  this.on("done", () => {
+    // Clean up all remaining processes after 500ms delay on normal clients.
+    //if (!isCI) {
+      childProcessCleanup(this.id);
+    //}
+  });
 };
 
 WebkitBrowser.prototype = {
-    name: 'Webkit',
-    DEFAULT_CMD: {
-        linux: !hasWebkitEnv() ? getPlaywrightExecutable() : '',
-        darwin: !hasWebkitEnv() ? getWebkitExecutable() : '',
-        win32: !hasWebkitEnv() ? getPlaywrightExecutable() : ''
-    },
-    ENV_CMD: 'WEBKIT_BIN'
+  name: "Webkit",
+  DEFAULT_CMD: {
+    linux: !hasWebkitEnv() ? getPlaywrightExecutable() : "",
+    darwin: !hasWebkitEnv() ? getWebkitExecutable() : "",
+    win32: !hasWebkitEnv() ? getPlaywrightExecutable() : "",
+  },
+  ENV_CMD: "WEBKIT_BIN",
 };
 
-WebkitBrowser.$inject = ['baseBrowserDecorator', 'args'];
+WebkitBrowser.$inject = ["baseBrowserDecorator", "args"];
 
 /**
  * Webkit Headless Browser definition.
@@ -217,33 +286,77 @@ WebkitBrowser.$inject = ['baseBrowserDecorator', 'args'];
  * @param {*} args
  */
 const WebkitHeadlessBrowser = function (baseBrowserDecorator, args) {
-    const headlessFlags = ['--headless'];
-    if (process.platform == 'darwin' || process.platform == 'win32') {
-        headlessFlags.push('--disable-gpu');
-    }
-    if (args && args.flags && args.flags.length > 0) {
-        args.flags = args.flags.concat(headlessFlags);
-    } else {
-        args = {};
-        args.flags = headlessFlags;
-    }
-    WebkitBrowser.call(this, baseBrowserDecorator, args);
+  const headlessFlags = ["--headless"];
+  if (process.platform == "darwin" || process.platform == "win32") {
+    headlessFlags.push("--disable-gpu");
+  }
+  if (args && args.flags && args.flags.length > 0) {
+    args.flags = args.flags.concat(headlessFlags);
+  } else {
+    args = {};
+    args.flags = headlessFlags;
+  }
+  WebkitBrowser.call(this, baseBrowserDecorator, args);
 };
 
 WebkitHeadlessBrowser.prototype = {
-    name: 'WebkitHeadless',
-    DEFAULT_CMD: {
-        linux: !hasWebkitHeadlessEnv() ? getPlaywrightExecutable() : '',
-        darwin: !hasWebkitHeadlessEnv() ? getPlaywrightExecutable() : '',
-        win32: !hasWebkitHeadlessEnv() ? getPlaywrightExecutable() : ''
-    },
-    ENV_CMD: 'WEBKIT_HEADLESS_BIN'
+  name: "WebkitHeadless",
+  DEFAULT_CMD: {
+    linux: !hasWebkitHeadlessEnv() ? getPlaywrightExecutable() : "",
+    darwin: !hasWebkitHeadlessEnv() ? getPlaywrightExecutable() : "",
+    win32: !hasWebkitHeadlessEnv() ? getPlaywrightExecutable() : "",
+  },
+  ENV_CMD: "WEBKIT_HEADLESS_BIN",
 };
 
-WebkitHeadlessBrowser.$inject = ['baseBrowserDecorator', 'args'];
+WebkitHeadlessBrowser.$inject = ["baseBrowserDecorator", "args"];
 
-/* omitted: EpiphanyBrowser */
-/* omitted: closeSafariTab */
+/**
+ * Epiphany Browser definition.
+ * @param {*} baseBrowserDecorator
+ * @param {*} args
+ */
+const EpiphanyBrowser = function (baseBrowserDecorator, args) {
+  baseBrowserDecorator(this);
+  let testUrl;
+
+  this.on("start", (url) => {
+    testUrl = addTestBrowserInformation(url, "Epiphany");
+    const flags = args.flags || [];
+    this._execCommand(
+      this._getCommand(),
+      [testUrl, "--profile=" + getTempDir()].concat(flags)
+    );
+  });
+};
+
+EpiphanyBrowser.prototype = {
+  name: "Epiphany",
+  DEFAULT_CMD: {
+    linux: "/usr/bin/epiphany",
+  },
+  ENV_CMD: "EPIPHANY_BIN",
+};
+
+EpiphanyBrowser.$inject = ["baseBrowserDecorator", "args"];
+
+/**
+ * @param {string} url
+ */
+const closeSafariTab = function (url) {
+  if (!url || url == "") {
+    return;
+  }
+  const findChildProcesses = `osascript ${path.resolve(
+    __dirname,
+    "scripts/CloseSafariTab.scpt"
+  )} "${url}"`;
+  child_process.exec(findChildProcesses, (error) => {
+    if (error && error.signal != "SIGHUP") {
+      throw error;
+    }
+  });
+};
 
 /**
  * Cleans up child processes related to the Playwright task ID.
@@ -251,85 +364,85 @@ WebkitHeadlessBrowser.$inject = ['baseBrowserDecorator', 'args'];
  * @param {function} callback - An optional callback function to execute after cleanup.
  */
 const childProcessCleanup = function (task_id, callback) {
-    const isCallbackDefined = callback && typeof callback === 'function';
+  const isCallbackDefined = callback && typeof callback === "function";
 
-    if (process.platform !== 'darwin' && process.platform !== 'linux') {
-        if (isCallbackDefined) {
-            callback();
-        }
-        return;
+  if (process.platform !== "darwin") {
+    if (isCallbackDefined) {
+      callback();
+    }
+    return;
+  }
+
+  // Find all related child process for playwright based on the task id.
+  console.log('Looking for Playwright child processes');
+  const findChildProcesses = `ps | grep -i "playwright" | grep -i "id=${task_id}"`;
+  child_process.exec(findChildProcesses, (error, stdout) => {
+    // Ignore error from killed karma processes.
+    if (error && error.signal != "SIGHUP") {
+      throw error;
     }
 
-    // Find all related child process for playwright based on the task id.
-    console.log('Looking for Playwright child processes');
-    const findChildProcesses = `ps | grep -i "playwright" | grep -i "id=${task_id}"`;
-    child_process.exec(findChildProcesses, (error, stdout) => {
-        // Ignore error from killed karma processes.
-        if (error && error.signal != 'SIGHUP') {
-            throw error;
-        }
+    // Check process list for relevant entries.
+    if (
+      stdout &&
+      stdout.toLowerCase().includes("playwright") &&
+      stdout.includes(task_id)
+    ) {
+      // Extract relevant child process ids.
+      const childProcessIds = stdout.match(/^\s?(\d)+\s?/gm);
+      if (childProcessIds && childProcessIds.length > 0) {
+        killChildProcesses(childProcessIds, task_id);
 
-        // Check process list for relevant entries.
-        if (
-            stdout
-            && stdout.toLowerCase().includes('playwright')
-            && stdout.includes(task_id)
-        ) {
-            // Extract relevant child process ids.
-            const childProcessIds = stdout.match(/^\s?(\d)+\s?/gm);
-            if (childProcessIds && childProcessIds.length > 0) {
-                killChildProcesses(childProcessIds, task_id);
-
-                // Allow 500ms for the processes to close before calling the callback.
-                if (isCallbackDefined) {
-                    setTimeout(callback, 500);
-                }
-            } else if (isCallbackDefined) {
-                callback();
-            }
-        } else if (isCallbackDefined) {
-            callback();
+        // Allow 500ms for the processes to close before calling the callback.
+        if (isCallbackDefined) {
+          setTimeout(callback, 500);
         }
-    });
+      } else if (isCallbackDefined) {
+        callback();
+      }
+    } else if (isCallbackDefined) {
+      callback();
+    }
+  });
 };
 
 /**
  * @param {Array} childProcessIds
  * @param {String} task_id
  */
-const killChildProcesses = function (childProcessIds, task_id = 'unknown') {
-    if (!childProcessIds || childProcessIds.length <= 0) {
-        return;
+const killChildProcesses = function (childProcessIds, task_id = "unknown") {
+  if (!childProcessIds || childProcessIds.length <= 0) {
+    return;
+  }
+
+  childProcessIds.forEach((childProcessId) => {
+    // Check if the process is still valid with a 0 kill signal.
+    try {
+      process.kill(childProcessId, 0);
+    } catch (error) {
+      if (error.code === "EPERM") {
+        console.error(
+          `No permission to kill child process ${childProcessId} for karma-task ${task_id}`
+        );
+      }
+      return;
     }
 
-    console.log('childProcessIds', JSON.stringify(childProcessIds));
-
-    childProcessIds.forEach(childProcessId => {
-        // Check if the process is still valid with a 0 kill signal.
-        try {
-            process.kill(childProcessId, 0);
-        } catch (error) {
-            if (error.code === 'EPERM') {
-                console.error(
-                    `No permission to kill child process ${childProcessId} for karma-task ${task_id}`
-                );
-            }
-            return;
-        }
-
-        // Killing child process, if there are no permission error.
-        try {
-            process.kill(childProcessId, 'SIGHUP');
-        } catch (killError) {
-            // Ignore errors if process is already killed.
-            if (killError.code != 'ESRCH') {
-                throw killError;
-            }
-        }
-    });
+    // Killing child process, if there are no permission error.
+    try {
+      process.kill(childProcessId, "SIGHUP");
+    } catch (killError) {
+      // Ignore errors if process is already killed.
+      if (killError.code != "ESRCH") {
+        throw killError;
+      }
+    }
+  });
 };
 
 module.exports = {
-    'launcher:CustomWebkit': ['type', WebkitBrowser],
-    'launcher:CustomWebkitHeadless': ['type', WebkitHeadlessBrowser]
+  "launcher:Epiphany": ["type", EpiphanyBrowser],
+  "launcher:Safari": ["type", SafariBrowser],
+  "launcher:CustomWebkit": ["type", WebkitBrowser],
+  "launcher:CustomWebkitHeadless": ["type", WebkitHeadlessBrowser],
 };
