1fbe84f50ac4c316d1697070d4dffe11db58f2fc
Jesse Attas <jattasNI@users.noreply.github.com>
2024-06-15 16:12:29 -0500

    Remove punycode

--- packages/spright-components/rollup.config.js
+++ packages/spright-components/rollup.config.js
@@ -4,7 +4,6 @@ import sourcemaps from 'rollup-plugin-sourcemaps';
 import terser from '@rollup/plugin-terser';
 import replace from '@rollup/plugin-replace';
 import json from '@rollup/plugin-json';
-import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 const umdDevelopmentPlugin = () => replace({
     'process.env.NODE_ENV': JSON.stringify('development'),
@@ -54,7 +53,6 @@ export default [
         },
         plugins: [
             umdDevelopmentPlugin(),
-            nodePolyfills(),
             sourcemaps(),
             resolve(),
             commonJS(),
@@ -78,7 +76,6 @@ export default [
         },
         plugins: [
             umdProductionPlugin(),
-            nodePolyfills(),
             sourcemaps(),
             resolve(),
             commonJS(),
