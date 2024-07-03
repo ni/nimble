338649365934dde18397226f674ecb93e8e3d23e
Natan Muntean <natan.muntean@ni.com>
2024-07-03 16:30:08 +0300

    duplicated tests

--- packages/blazor-workspace/Tests/NimbleBlazor.Tests.Acceptance/Tests/WaferMapTests.cs
+++ packages/blazor-workspace/Tests/NimbleBlazor.Tests.Acceptance/Tests/WaferMapTests.cs
@@ -11,14 +11,29 @@ public class WaferMapTests : NimbleAcceptanceTestsBase
     { }
 
     [Fact]
+    public async Task WaferMap_WithDiesAndColorScale_RendersColorsAsync()
+    {
+        await using var pageWrapper = await NewPageForRouteAsync("WaferMapRenderTest");
+        var page = pageWrapper.Page;
+        var wafer = page.Locator("nimble-wafer-map");
+        var canvas = page.Locator(".main-wafer");
+
+        await Assertions.Expect(wafer).ToBeVisibleAsync();
+        await Assertions.Expect(canvas).ToBeVisibleAsync();
+        var color = await page.EvaluateAsync<string>(
+            @"document.getElementsByTagName('nimble-wafer-map')[0].canvas.getContext('2d').getImageData(249, 249, 1, 1).data.toString()");
+
+        Assert.Equal(@"85,85,0,255", color);
+    }
+
+    [Fact]
     public async Task WaferMap_WithGridDimensions_IsValidAsync()
     {
         await using var pageWrapper = await NewPageForRouteAsync("WaferMapRenderTest");
         var page = pageWrapper.Page;
         var wafer = page.Locator("nimble-wafer-map");
         var validButton = page.Locator("nimble-button");
-        var renderTextField = page.Locator(".render-text-field");
-        var textField = page.Locator(".test-text-field");
+        var textField = page.Locator("nimble-text-field");
 
         await Assertions.Expect(wafer).ToBeVisibleAsync();
         await validButton.ClickAsync();
@@ -32,8 +47,7 @@ public class WaferMapTests : NimbleAcceptanceTestsBase
         await using var pageWrapper = await NewPageForRouteAsync("WaferMapRenderTest");
         var page = pageWrapper.Page;
         var wafer = page.Locator("nimble-wafer-map");
-        var renderTextField = page.Locator(".render-text-field");
-        var textField = page.Locator(".test-text-field");
+        var textField = page.Locator("nimble-text-field");
 
         await Assertions.Expect(wafer).ToBeVisibleAsync();
         await wafer.HoverAsync();
