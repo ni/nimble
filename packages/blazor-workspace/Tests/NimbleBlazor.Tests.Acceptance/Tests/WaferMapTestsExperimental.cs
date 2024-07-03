2214283df7d144fdb3fa2fa33f40d744aaac16b6
Natan Muntean <33986780+munteannatan@users.noreply.github.com>
2024-07-03 23:21:33 +0300

    Updated Blazor Wafer Map to use dies table input (#2185)
    # Pull Request
    
    ## 🤨 Rationale
    
    The existing dies input for the blazor wafer map uses json conversion to
    transmit data to the wafer map component.
    This has caused some issues detailed here
    https://dev.azure.com/ni/DevCentral/_workitems/edit/2687395
    
    ## 👩‍💻 Implementation
    
    created a new input for apache arrow table called dies table on the
    blazor wafer map
    this input sends the data to the component as a binary
    created an internal input on the wafer map component to receive the
    binary from blazor and convert it to apache table
    
    created a render finished event which is emitted when the wafer
    component has rendered images on the canvas and not when hovering
    
    ## 🧪 Testing
    
    updated the tests to reflect the changes
    
    ## ✅ Checklist
    
    <!--- Review the list and put an x in the boxes that apply or ~~strike
    through~~ around items that don't (along with an explanation). -->
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.
    
    ---------
    
    Co-authored-by: Jesse Attas <jattasNI@users.noreply.github.com>

---
+++ packages/blazor-workspace/Tests/NimbleBlazor.Tests.Acceptance/Tests/WaferMapTestsExperimental.cs
@@ -0,0 +1,43 @@
+﻿using BlazorWorkspace.Testing.Acceptance;
+using Microsoft.Playwright;
+using Xunit;
+
+namespace NimbleBlazor.Tests.Acceptance;
+
+public class WaferMapTestsExperimental : NimbleAcceptanceTestsBase
+{
+    public WaferMapTestsExperimental(PlaywrightFixture playwrightFixture, NimbleBlazorWebHostServerFixture blazorServerClassFixture)
+        : base(playwrightFixture, blazorServerClassFixture)
+    { }
+
+    [Fact]
+    public async Task WaferMap_WithGridDimensions_IsValidAsync()
+    {
+        await using var pageWrapper = await NewPageForRouteAsync("WaferMapRenderTestExperimental");
+        var page = pageWrapper.Page;
+        var wafer = page.Locator("nimble-wafer-map");
+        var validButton = page.Locator("nimble-button");
+        var renderTextField = page.Locator(".render-text-field");
+        var textField = page.Locator(".test-text-field");
+
+        await Assertions.Expect(wafer).ToBeVisibleAsync();
+        await validButton.ClickAsync();
+
+        await Assertions.Expect(textField).ToHaveAttributeAsync("current-value", "False");
+    }
+
+    [Fact]
+    public async Task WaferMap_WithHoverEvent_TriggersDieChangeEventAsync()
+    {
+        await using var pageWrapper = await NewPageForRouteAsync("WaferMapRenderTestExperimental");
+        var page = pageWrapper.Page;
+        var wafer = page.Locator("nimble-wafer-map");
+        var renderTextField = page.Locator(".render-text-field");
+        var textField = page.Locator(".test-text-field");
+
+        await Assertions.Expect(wafer).ToBeVisibleAsync();
+        await wafer.HoverAsync();
+
+        await Assertions.Expect(textField).ToHaveAttributeAsync("current-value", "4");
+    }
+}
