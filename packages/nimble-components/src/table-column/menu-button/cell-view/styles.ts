3165257cb2f224227c4f8dfa348a3dc090feb8ff
mollykreis <20542556+mollykreis@users.noreply.github.com>
2024-07-08 16:22:30 -0500

    Update menu button styles

--- packages/nimble-components/src/table-column/menu-button/cell-view/styles.ts
+++ packages/nimble-components/src/table-column/menu-button/cell-view/styles.ts
@@ -1,5 +1,10 @@
 import { css } from '@microsoft/fast-element';
-import { controlSlimHeight } from '../../../theme-provider/design-tokens';
+import {
+    bodyFont,
+    buttonLabelFont,
+    mediumPadding,
+    standardPadding
+} from '../../../theme-provider/design-tokens';
 
 export const styles = css`
     :host {
@@ -8,8 +13,9 @@ export const styles = css`
     }
 
     nimble-menu-button {
-        height: ${controlSlimHeight};
         width: 100%;
+        ${standardPadding.cssCustomProperty}: ${mediumPadding};
+        ${buttonLabelFont.cssCustomProperty}: ${bodyFont};
     }
 
     .value-label {
