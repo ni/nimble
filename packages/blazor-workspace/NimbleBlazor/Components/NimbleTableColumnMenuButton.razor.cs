89209e0eb79256123d0d4519e680d95411989f5a
mollykreis <20542556+mollykreis@users.noreply.github.com>
2024-07-16 16:35:28 -0500

    Add Blazor support for the menu button table column

---
+++ packages/blazor-workspace/NimbleBlazor/Components/NimbleTableColumnMenuButton.razor.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Components;
+
+namespace NimbleBlazor;
+
+public partial class NimbleTableColumnMenuButton : NimbleTableColumn, IFractionalWidthColumn
+{
+    /// <summary>
+    /// Gets or sets the field in the element representing a row of data in a <see cref="NimbleTable{TData}"/>to display
+    /// </summary>
+    [Parameter]
+    [DisallowNull]
+    public string FieldName { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the name of the slot within the <see cref="NimbleTable{TData}"/> where the menu associated with the
+    /// column's menu button will be provided.
+    /// </summary>
+    [Parameter]
+    [DisallowNull]
+    public string MenuSlot { get; set; } = null!;
+
+    /// <summary>
+    /// The fractional/proportional width to use for this column
+    /// </summary>
+    [Parameter]
+    public double FractionalWidth { get; set; } = 1;
+
+    /// <summary>
+    /// The minimum width (in pixels) for this column
+    /// </summary>
+    [Parameter]
+    public double? MinPixelWidth { get; set; }
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+}
