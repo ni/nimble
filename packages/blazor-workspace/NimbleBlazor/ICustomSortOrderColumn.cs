e8b120ffd9f96987f47c8cd93fa8849e4228741a
mollykreis <20542556+mollykreis@users.noreply.github.com>
2024-07-17 14:02:34 -0500

    Add Blazor support for SortByFieldName on anchor and text columns (#2279)
    # Pull Request
    
    ## 🤨 Rationale
    
    Adds support in Blazor for the `SortByFieldName` property on the anchor
    and text columns.
    
    ## 👩‍💻 Implementation
    
    Followed existing patterns to add the `SortByFieldName` property to the
    anchor and text column components.
    
    ## 🧪 Testing
    
    New tests for the property
    
    ## ✅ Checklist
    
    <!--- Review the list and put an x in the boxes that apply or ~~strike
    through~~ around items that don't (along with an explanation). -->
    
    - [ ] I have updated the project documentation to reflect my changes or
    determined no changes are needed.
    
    ---------
    
    Co-authored-by: Milan Raj <rajsite@users.noreply.github.com>

---
+++ packages/blazor-workspace/NimbleBlazor/ICustomSortOrderColumn.cs
@@ -0,0 +1,9 @@
+﻿namespace NimbleBlazor;
+
+/// <summary>
+/// API for table columns supporting custom sort orders
+/// </summary>
+public interface ICustomSortOrderColumn
+{
+    public string? SortByFieldName { get; set; }
+}
