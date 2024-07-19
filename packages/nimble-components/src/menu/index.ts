c2df92008b2003a94d57d685b943d2280e984631
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2024-07-19 18:18:42 -0500

    Fix menu item indentation bug with dynamically slotted icons

--- packages/nimble-components/src/menu/index.ts
+++ packages/nimble-components/src/menu/index.ts
@@ -1,6 +1,7 @@
 import {
     DesignSystem,
     Menu as FoundationMenu,
+    MenuItemRole,
     menuTemplate as template
 } from '@microsoft/fast-foundation';
 import { styles } from './styles';
@@ -11,10 +12,51 @@ declare global {
     }
 }
 
+declare type ItemsChangeHandler = (
+    oldValue: HTMLElement[] | undefined,
+    newValue: HTMLElement[]
+) => void;
+
 /**
  * A nimble-styled menu
  */
-export class Menu extends FoundationMenu {}
+export class Menu extends FoundationMenu {
+    private readonly baseItemsChanged: ItemsChangeHandler;
+
+    private readonly mutationObserver: MutationObserver = new MutationObserver(
+        mutations => {
+            for (const mutation of mutations) {
+                if (mutation.type === 'childList') {
+                    // eslint-disable-next-line @typescript-eslint/dot-notation
+                    (this['setItems'] as () => void)();
+                }
+            }
+        }
+    );
+
+    public constructor() {
+        super();
+        // Need to override the itemsChanged method to handle dynamically slotted icons in menu items.
+        // Preferring to work around private members than fork entire class from FAST.
+        /* eslint-disable @typescript-eslint/dot-notation */
+        this.baseItemsChanged = this['itemsChanged'] as ItemsChangeHandler;
+        this['itemsChanged'] = this.itemsChangedOverride;
+        /* eslint-enable @typescript-eslint/dot-notation */
+    }
+
+    private readonly itemsChangedOverride = (
+        oldValue: HTMLElement[] | undefined,
+        newValue: HTMLElement[]
+    ): void => {
+        this.baseItemsChanged(oldValue, newValue);
+        this.mutationObserver.disconnect();
+        newValue
+            .filter(x => x.role === MenuItemRole.menuitem)
+            .forEach(x => {
+                this.mutationObserver.observe(x, { childList: true });
+            });
+    };
+}
 
 /**
  * A function that returns a nimble-menu registration for configuring the component with a DesignSystem.
--- packages/nimble-components/src/menu/tests/menu.spec.ts
+++ packages/nimble-components/src/menu/tests/menu.spec.ts
@@ -1,11 +1,55 @@
+import { html } from '@microsoft/fast-element';
 import { Menu, menuTag } from '..';
+import { MenuItem, menuItemTag } from '../../menu-item';
+import { Fixture, fixture } from '../../utilities/tests/fixture';
+import { iconCheckTag } from '../../icons/check';
+import { waitForUpdatesAsync } from '../../testing/async-helpers';
 
 describe('Menu', () => {
+    async function setup(): Promise<Fixture<Menu>> {
+        return fixture<Menu>(html`
+            <${menuTag}>
+                <${menuItemTag}>Item 1</${menuItemTag}>
+                <${menuItemTag}>Item 2</${menuItemTag}>
+            </${menuTag}>
+        `);
+    }
+
     it('should export its tag', () => {
         expect(menuTag).toBe('nimble-menu');
     });
 
     it('can construct an element instance', () => {
         expect(document.createElement('nimble-menu')).toBeInstanceOf(Menu);
+    });
+
+    it('properly indents items when an icon is slotted or removed after connecting', async () => {
+        const { element, connect, disconnect } = await setup();
+        await connect();
+        await waitForUpdatesAsync();
+
+        const items = element.querySelectorAll(menuItemTag);
+        const item1 = items[0]!;
+        const item2 = items[1]!;
+        expect(item1.classList.contains('indent-0')).toBeTrue();
+        expect(item2.classList.contains('indent-0')).toBeTrue();
+
+        const icon = document.createElement(iconCheckTag);
+        icon.slot = 'start';
+        item2.appendChild(icon);
+        await waitForUpdatesAsync();
+
+        expect(item1.classList.contains('indent-1')).toBeTrue();
+        expect(item2.classList.contains('indent-1')).toBeTrue();
+
+        item2.removeChild(icon);
+        await waitForUpdatesAsync();
+
+        expect(item1.classList.contains('indent-0')).toBeTrue();
+        expect(item1.classList.contains('indent-1')).toBeFalse();
+        expect(item2.classList.contains('indent-0')).toBeTrue();
+        expect(item2.classList.contains('indent-1')).toBeFalse();
+
+        await disconnect();
     });
 });
