82a3efe59e657f550d652d2b19c3ce8fa78b494d
Jesse Attas <jattasNI@users.noreply.github.com>
2024-07-24 23:04:58 -0500

    Attempt to depend on nimble-react from example app

--- packages/react-example-app/src/App.js
+++ packages/react-example-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NimbleButton from '@ni/nimble-react/dist/esm/components/Button/Button.react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <NimbleButton>Click Me</NimbleButton>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
