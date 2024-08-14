e835630efdf1f906777eb118b3036192f46dc06d
Jesse Attas <jattasNI@users.noreply.github.com>
2024-08-14 15:25:38 -0500

    Update lighthouse accessibility minScore back to .9

--- packages/angular-workspace/lighthouserc.js
+++ packages/angular-workspace/lighthouserc.js
@@ -17,7 +17,7 @@ module.exports = {
         assert: {
             assertions: {
                 'categories:performance': ['error', { minScore: 0.8 }],
-                'categories:accessibility': ['error', { minScore: 0.8 }]
+                'categories:accessibility': ['error', { minScore: 0.9 }]
             }
         },
         upload: {
