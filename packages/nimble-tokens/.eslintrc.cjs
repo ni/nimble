71d1faf0439d2e7fe0701b48ee7e70f90142ab4e
rajsite <rajsite@users.noreply.github.com>
2024-08-20 09:32:37 -0500

    Switch tokens to module

--- packages/nimble-tokens/.eslintrc.cjs
+++ packages/nimble-tokens/.eslintrc.cjs
@@ -7,11 +7,11 @@ module.exports = {
     ],
     overrides: [
         {
-            files: ['*.js'],
+            files: ['*.js', '*.cjs'],
             extends: ['@ni-private/eslint-config-nimble/javascript'],
         },
         {
-            files: ['source/styledictionary/*.js'],
+            files: ['source/styledictionary/*.cjs'],
             rules: {
                 // Build scripts will not be in published package and are allowed to use devDependencies
                 'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
