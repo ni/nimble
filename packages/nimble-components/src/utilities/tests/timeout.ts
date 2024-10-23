60cada38499c8c426557035c7a27cf5769659bef
Jonathan Meyer <jonathan.meyer@ni.com>
2024-10-23 10:13:47 -0500

    Update packages/nimble-components/src/utilities/tests/timeout.ts
    Co-authored-by: Milan Raj <rajsite@users.noreply.github.com>

--- packages/nimble-components/src/utilities/tests/timeout.ts
+++ packages/nimble-components/src/utilities/tests/timeout.ts
@@ -2,10 +2,7 @@
  * Timeout for use in async tets.
  */
 export async function timeout(ms = 0): Promise<void> {
-    await new Promise((resolve, _reject) => {
-        window.setTimeout(() => {
-            // eslint-disable-next-line no-void
-            resolve(void 0);
-        }, ms);
+    await new Promise((resolve) => {
+        window.setTimeout(() => resolve(), ms);
     });
 }
