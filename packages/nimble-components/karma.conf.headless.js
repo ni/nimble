5194f4cb51ab738b1fa4ce46299cb4d765728a0c
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2024-10-29 18:21:00 -0500

    Should be back to the original now

--- packages/nimble-components/karma.conf.headless.js
+++ packages/nimble-components/karma.conf.headless.js
@@ -10,7 +10,7 @@ module.exports = config => {
         plugins: [...config.plugins, 'karma-spec-reporter'],
         reporters: ['spec'],
         specReporter: {
-            suppressPassed: false,
+            suppressPassed: true,
             suppressSkipped: false,
             showSpecTiming: true
         }
