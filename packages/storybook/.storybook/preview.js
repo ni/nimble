bd1894c6aec64c1f8f830ed3fbe4a77c54080905
Milan Raj <rajsite@users.noreply.github.com>
2024-11-18 14:59:54 -0600

    NPM Dependency Update Nov 2024 (#2474)
    # Pull Request
    
    ## 🤨 Rationale
    
    - Updated unpinned dependencies (including major)
    - Rebuilt lock file
    
    ## 👩‍💻 Implementation
    
    See above.
    
    ## 🧪 Testing
    
    Rely on CI.
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

--- packages/storybook/.storybook/preview.js
+++ packages/storybook/.storybook/preview.js
@@ -105,5 +105,5 @@ configureActions({
     depth: 1
 });
 
-// Update the GUID on this line to trigger a turbosnap full rebuild: d3f8a1b2-4c5d-4e7f-8a9e-1b2c3d4e5f6a
+// Update the GUID on this line to trigger a turbosnap full rebuild: d3f8a1b2-4c5d-4e7f-8a9e-1b2c3d4e5f6b
 // See https://www.chromatic.com/docs/turbosnap/#full-rebuilds
