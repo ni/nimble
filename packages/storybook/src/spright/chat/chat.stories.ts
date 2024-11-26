2837ba359ea3cbefb83d2a505ee6b1b93ebc1f1d
Jesse Attas <jattasNI@users.noreply.github.com>
2024-11-26 11:29:21 -0600

    Markdown example

--- packages/storybook/src/spright/chat/chat.stories.ts
+++ packages/storybook/src/spright/chat/chat.stories.ts
@@ -1,17 +1,23 @@
 import type { Meta, StoryObj } from '@storybook/html';
 import { html } from '@microsoft/fast-element';
 import { chatMessageTag } from '../../../../spright-components/src/chat-message';
+import { richTextViewerTag } from '../../../../nimble-components/src/rich-text/viewer';
 import {
     apiCategory,
     createUserSelectedThemeStory,
 } from '../../utilities/storybook';
 
-interface ChatMessageArgs {
+const metadata: Meta<ChatMessageTextArgs> = {
+    title: 'Spright/Chat'
+};
+
+export default metadata;
+
+interface ChatMessageTextArgs {
     text: string;
 }
 
-const metadata: Meta<ChatMessageArgs> = {
-    title: 'Spright/Chat',
+export const chatMessageText: StoryObj<ChatMessageTextArgs> = {
     parameters: {
         actions: {}
     },
@@ -22,15 +28,43 @@ const metadata: Meta<ChatMessageArgs> = {
     `),
     argTypes: {
         text: {
-            description: 'The text to display in the chat text bubble.',
+            description: 'The text to display in the chat message.',
             table: { category: apiCategory.slots }
         },
     },
     args: {
-        text: 'Hi, how are you?',
+        text: 'How do I choose which version of Python to execute my script?',
     }
 };
 
-export default metadata;
+interface ChatMessageRichTextArgs {
+    markdown: string;
+}
+export const chatMessageRichText: StoryObj<ChatMessageRichTextArgs> = {
+    parameters: {
+        actions: {}
+    },
+    render: createUserSelectedThemeStory(html`
+        <${chatMessageTag}>
+            <${richTextViewerTag} :markdown="${x => x.markdown}"></${richTextViewerTag}>
+        </${chatMessageTag}>
+    `),
+    argTypes: {
+        markdown: {
+            description: 'Markdown text for the rich text viewer',
+            table: { category: apiCategory.slots }
+        },
+    },
+    args: {
+        markdown:
+    `To configure your Python version:
 
-export const chatMessage: StoryObj<ChatMessageArgs> = {};
+    1. Select **Adapters...** from the **Configure** menu.
+    
+    2. Configure the Python adapter.
+    
+    3. Choose the desired version from the **Version** dropdown.
+    
+    You can also specify a Python version for a specific module call in the **Advanced Settings** of the Python adapter.`,
+    }
+};
