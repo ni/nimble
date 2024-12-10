c01a3e1b8e91510a28e22d9c8e02f9cdb1416955
rajsite <rajsite@users.noreply.github.com>
2024-12-09 23:16:12 -0600

    Landing and angular tweaks Blazor partially addressed by still need .net 9 changes for full nimble support: https://github.com/dotnet/aspnetcore/issues/58322#issuecomment-2428812743

---
+++ packages/site/vite.config.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'vite';
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+    base: './'
+});
