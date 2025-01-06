1a25eeadda1142935ba12c4fa633db39e43fe6cf
mollykreis <20542556+mollykreis@users.noreply.github.com>
2025-01-06 10:42:56 -0600

    Add comment for createRequiredVisibleLabelTemplate

--- packages/nimble-components/src/patterns/required-visible/template.ts
+++ packages/nimble-components/src/patterns/required-visible/template.ts
@@ -2,6 +2,12 @@ import { ViewTemplate, html, when } from '@microsoft/fast-element';
 import { iconAsteriskTag } from '../../icons/asterisk';
 import type { RequiredVisiblePattern } from './types';
 
+/**
+ * Given the template for a control label, creates a new template that includes
+ * an icon next to the label to indicate whether or not the control is required.
+ *
+ * This function is intended to be used with components leveraging `mixinRequiredVisiblePattern`.
+ */
 /* eslint-disable @typescript-eslint/indent */
 export function createRequiredVisibleLabelTemplate(
     labelTemplate: ViewTemplate<RequiredVisiblePattern>
