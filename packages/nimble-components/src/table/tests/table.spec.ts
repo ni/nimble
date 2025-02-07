07ef60d00c3470366e1b779dc701e54eaeb9f6b8
rajsite <rajsite@users.noreply.github.com>
2025-02-07 00:28:35 -0600

    Disable test in webkit

--- packages/nimble-components/src/table/tests/table.spec.ts
+++ packages/nimble-components/src/table/tests/table.spec.ts
@@ -501,7 +501,7 @@ describe('Table', () => {
             expect(pageObject.isRowHoverStylingEnabled()).toBeFalse();
         });
 
-        it('re-enables row hover styling after scrolling ends', async () => {
+        it('re-enables row hover styling after scrolling ends #SkipWebkit', async () => {
             await connect();
             await element.setData(simpleTableData);
             column1.groupIndex = 0;
