1a8e5833478edd072100c32868f3f704c98adb44
rajsite <rajsite@users.noreply.github.com>
2025-02-13 08:23:50 -0600

    Drop lighthouse performance check

--- packages/angular-workspace/lighthouserc.js
+++ packages/angular-workspace/lighthouserc.js
@@ -16,7 +16,7 @@ module.exports = {
         },
         assert: {
             assertions: {
-                'categories:performance': ['error', { minScore: 0.8 }],
+                'categories:performance': ['error', { minScore: 0.7 }],
                 'categories:accessibility': ['error', { minScore: 0.9 }]
             }
         },
