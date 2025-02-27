73e5993b902af630f187e9b2bc8984ca0ec7d146
Jose A. Hernandez <107654185+joseahdz@users.noreply.github.com>
2025-02-27 10:39:38 -0600

    Removed inheritance from NimbleInputBase<string?>

--- packages/blazor-workspace/NimbleBlazor/Components/NimbleRichTextViewer.razor.cs
+++ packages/blazor-workspace/NimbleBlazor/Components/NimbleRichTextViewer.razor.cs
@@ -3,18 +3,23 @@ using Microsoft.AspNetCore.Components;
 
 namespace NimbleBlazor;
 
-public partial class NimbleRichTextViewer : NimbleInputBase<string?>
+public partial class NimbleRichTextViewer : ComponentBase
 {
+    /// <summary>
+    /// The markdown content to render.
+    /// </summary>
     [Parameter]
     public string? Markdown { get; set; }
 
+    /// <summary>
+    /// The child content of the element.
+    /// </summary>
     [Parameter]
     public RenderFragment? ChildContent { get; set; }
 
-    protected override bool TryParseValueFromString(string? value, out string? result, [NotNullWhen(false)] out string? validationErrorMessage)
-    {
-        result = value;
-        validationErrorMessage = null;
-        return true;
-    }
+    /// <summary>
+    /// Any additional attributes that did not match known properties.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object>? AdditionalAttributes { get; set; }
 }
