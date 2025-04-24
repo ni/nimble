bedb59346d795307e76a9fd7c85b81117645565e
Jesse Attas <jattasNI@users.noreply.github.com>
2025-04-24 14:38:02 -0500

    Input and button

--- packages/spright-components/src/chat/input/styles.ts
+++ packages/spright-components/src/chat/input/styles.ts
@@ -1,7 +1,7 @@
 import { css } from '@ni/fast-element';
 import {
-    applicationBackgroundColor,
-    mediumPadding
+    bodyFont,
+    bodyFontColor,
 } from '@ni/nimble-components/dist/esm/theme-provider/design-tokens';
 import { display } from '../../utilities/style/display';
 
@@ -9,11 +9,31 @@ export const styles = css`
     ${display('flex')}
 
     :host {
-        flex-direction: column;
-        justify-content: flex-start;
-        row-gap: 32px;
-        padding: ${mediumPadding};
-        background: ${applicationBackgroundColor};
-        overflow-y: auto;
+        width: 100%;
+        height: auto;
+    }
+
+    .container {
+        display: grid;
+        grid-template-rows: auto 1fr;
+        grid-template-columns: 1fr auto;
+        width: 100%;
+        height: 100%;
+    }
+
+    textarea {
+        grid-row: 1;
+        grid-column: 1 / 3;
+
+        font: ${bodyFont};
+        color: ${bodyFontColor};
+        width: 100%;
+        resize: none;
+    }
+
+    .send-button {
+        grid-row: 2;
+        grid-column: 2;
+        width: 80px;
     }
 `;
--- packages/spright-components/src/chat/input/template.ts
+++ packages/spright-components/src/chat/input/template.ts
@@ -1,7 +1,25 @@
 import { html } from '@ni/fast-element';
+import { buttonTag } from '@ni/nimble-components/dist/esm/button';
+import { iconPaperPlaneTag } from '@ni/nimble-components/dist/esm/icons/paper-plane';
 import type { ChatInput } from '.';
 
 /* eslint-disable @typescript-eslint/indent */
 // prettier-ignore
-export const template = html<ChatInput>`<slot></slot>`;
+export const template = html<ChatInput>`
+<div class="container">
+    <textarea
+        class="input-control"
+        @ref="_textArea"
+        placeholder="Ask Nigel"
+        rows="1"
+        @bind="InputPrompt.Text" @bind:event="oninput"></textarea>
+    <${buttonTag}
+        class="send-button"
+        appearance="block"
+        appearance-variant="accent"
+    >
+        Send
+        <${iconPaperPlaneTag} slot="start"><${iconPaperPlaneTag}/>
+    </${buttonTag}>    
+</div>`;
 /* eslint-enable @typescript-eslint/indent */
