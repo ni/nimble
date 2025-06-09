2bc167dd378cc5802aad47f496a4b80a599713ca
Fred Visser <1458528+fredvisser@users.noreply.github.com>
2025-06-09 14:01:17 -0500

    improve vite config

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,6 +1,7 @@
 import { dirname, join } from 'path';
 import remarkGfm from 'remark-gfm';
 import circleDependency from 'vite-plugin-circular-dependency';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // All files participating in storybook should be in src
 // so that TypeScript and linters can track them correctly
@@ -28,13 +29,18 @@ export const core = {
 export async function viteFinal(config, { configType }) {
     const { mergeConfig } = await import('vite');
 
-    // Disabling minification to resolve issues with icons.stories.ts metaphor lookup
-    config.build.minify = false;
+    config.build.minify = 'terser';
+    config.build.terserOptions = {
+        keep_classnames: true,
+        keep_fnames: true
+    };
 
     config.plugins.push(circleDependency({
         exclude: /node_modules/,
         circleImportThrowErr: configType === 'PRODUCTION'
     }));
+
+    config.plugins.push(tsconfigPaths());
 
     return mergeConfig(config);
 }
