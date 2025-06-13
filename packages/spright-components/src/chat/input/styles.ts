d5d0201fdebfc1e5cd88c0ac219cb61414ac3dc1
Jesse Attas <jattasNI@users.noreply.github.com>
2025-06-13 17:14:56 -0500

    Revert "Incomplete focus styling"
    This reverts commit bb0debfc0eb1751951a5a84de5b91280adff17c3.

--- packages/spright-components/src/chat/input/styles.ts
+++ packages/spright-components/src/chat/input/styles.ts
@@ -3,16 +3,13 @@ import {
     applicationBackgroundColor,
     bodyFont,
     bodyFontColor,
-    borderHoverColor,
     borderWidth,
     controlLabelFontColor,
     elevation2BoxShadow,
     mediumPadding,
-    popupBorderColor,
-    smallDelay
+    popupBorderColor
 } from '@ni/nimble-components/dist/esm/theme-provider/design-tokens';
 import { display } from '../../utilities/style/display';
-import { focusVisible } from '../../utilities/style/focus';
 
 export const styles = css`
     ${display('flex')}
@@ -32,31 +29,6 @@ export const styles = css`
         background-color: ${applicationBackgroundColor};
         border: ${borderWidth} solid ${popupBorderColor};
         box-shadow: ${elevation2BoxShadow};
-    }
-
-    .container:focus-within {
-        border-bottom-color: ${borderHoverColor};
-    }
-
-    .container::after {
-        content: ' ';
-        position: absolute;
-        bottom: calc(-1 * ${borderWidth});
-        width: 0px;
-        height: 0px;
-        border-bottom: ${borderHoverColor} var(--ni-private-hover-indicator-width) solid;
-        transition: width ${smallDelay} ease-in;
-    }
-
-    @media (prefers-reduced-motion) {
-        .container::after {
-            transition-duration: 0s;
-        }
-    }
-
-    :host(:hover) .container::after,
-    :host(${focusVisible}) .container::after {
-        width: 100%;
     }
 
     textarea {
