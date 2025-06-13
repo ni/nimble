a353f4a3b7ad72a9909973fda6a178aa74176e7d
Jesse Attas <jattasNI@users.noreply.github.com>
2025-06-13 17:25:28 -0500

    fix event name in angular

--- packages/angular-workspace/spright-angular/chat/input/spright-chat-input.directive.ts
+++ packages/angular-workspace/spright-angular/chat/input/spright-chat-input.directive.ts
@@ -1,9 +1,9 @@
 import { Directive, ElementRef, EventEmitter, HostListener, Output, Renderer2 } from '@angular/core';
 import { type ChatInput, chatInputTag } from '@ni/spright-components/dist/esm/chat/input';
-import { type ChatInputSubmitEventDetail } from '@ni/spright-components/dist/esm/chat/input/types';
+import { type ChatInputSendEventDetail } from '@ni/spright-components/dist/esm/chat/input/types';
 
 export type { ChatInput };
-export type { ChatInputSubmitEventDetail };
+export type { ChatInputSendEventDetail };
 export { chatInputTag };
 
 /**
@@ -13,13 +13,13 @@ export { chatInputTag };
     selector: 'spright-chat-input'
 })
 export class SprightChatInputDirective {
-    @Output() public submitEvent = new EventEmitter<string>();
+    @Output() public sendEvent = new EventEmitter<string>();
     public constructor(private readonly renderer: Renderer2, private readonly elementRef: ElementRef<ChatInput>) {}
 
     @HostListener('submit', ['$event'])
-    public onSubmit($event: CustomEvent<ChatInputSubmitEventDetail>): void {
+    public onSubmit($event: CustomEvent<ChatInputSendEventDetail>): void {
         if ($event.target === this.elementRef.nativeElement) {
-            this.submitEvent.emit($event.detail.text);
+            this.sendEvent.emit($event.detail.text);
         }
     }
 }
