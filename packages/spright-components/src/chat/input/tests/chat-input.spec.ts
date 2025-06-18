897ab71556f0d58f289afd961794a99a088bcd11
Jesse Attas <jattasNI@users.noreply.github.com>
2025-06-18 17:27:33 -0500

    Some passing tests

--- packages/spright-components/src/chat/input/tests/chat-input.spec.ts
+++ packages/spright-components/src/chat/input/tests/chat-input.spec.ts
@@ -1,18 +1,26 @@
 import { html } from '@ni/fast-element';
 import { ChatInput, chatInputTag } from '..';
 import { fixture, type Fixture } from '../../../utilities/tests/fixture';
+import { ChatInputPageObject } from '../testing/chat-input.pageobject';
+import type { ChatInputSendEventDetail } from '../types';
 
 async function setup(): Promise<Fixture<ChatInput>> {
     return await fixture<ChatInput>(html`<${chatInputTag}></${chatInputTag}>`);
 }
 
-describe('ChatInput', () => {
+type ChatInputSendEventHandler = (
+    evt: CustomEvent<ChatInputSendEventDetail>
+) => void;
+
+fdescribe('ChatInput', () => {
     let element: ChatInput;
+    let page: ChatInputPageObject;
     let connect: () => Promise<void>;
     let disconnect: () => Promise<void>;
 
     beforeEach(async () => {
         ({ element, connect, disconnect } = await setup());
+        page = new ChatInputPageObject(element);
     });
 
     afterEach(async () => {
@@ -28,10 +36,18 @@ describe('ChatInput', () => {
             await connect();
         });
 
-        it('has no rendered text content', async () => {
+        it('has no rendered text content', () => {
+            expect(page.getRenderedText()).toEqual('');
         });
 
-        it('has disabled send button', async () => {
+        it('shows placeholder', () => {
+            const placeholder = 'My placeholder';
+            element.placeholder = placeholder;
+            expect(page.getPlaceholder()).toEqual(placeholder);
+        });
+
+        it('has disabled send button', () => {
+            expect(page.isSendButtonEnabled()).toBeFalse();
         });
     });
 
@@ -40,19 +56,32 @@ describe('ChatInput', () => {
             await connect();
         });
 
-        it('is reflected in value property get', async () => {
+        it('is reflected in value property get', () => {
+            element.value = 'new value';
+            expect(element.value).toEqual('new value');
         });
 
-        it('affects rendered text', async () => {
+        it('affects rendered text', () => {
+            element.value = 'new value';
+            expect(page.getRenderedText()).toEqual('new value');
         });
 
         it('before connect affects rendered text', async () => {
+            await disconnect();
+            element.value = 'new value';
+            await connect();
+            expect(page.getRenderedText()).toEqual('new value');
         });
 
-        it('to non-empty value enables send button', async () => {
+        it('to non-empty value enables send button', () => {
+            element.value = 'new value';
+            expect(page.isSendButtonEnabled()).toBeTrue();
         });
 
-        it('to empty value disables send button', async () => {
+        it('to empty value disables send button', () => {
+            element.value = 'new value';
+            element.value = '';
+            expect(page.isSendButtonEnabled()).toBeFalse();
         });
     });
 
@@ -61,61 +90,92 @@ describe('ChatInput', () => {
             await connect();
         });
 
-        it('is reflected in value property get', async () => {
+        it('is reflected in value property get', () => {
+            page.setText('new value');
+            expect(element.value).toEqual('new value');
         });
 
-        it('affects rendered text', async () => {
+        it('affects rendered text', () => {
+            page.setText('new value');
+            expect(page.getRenderedText()).toEqual('new value');
         });
 
-        it('to non-empty value enables send button', async () => {
+        it('to non-empty value enables send button', () => {
+            page.setText('new value');
+            expect(page.isSendButtonEnabled()).toBeTrue();
         });
 
-        it('to empty value disables send button', async () => {
+        it('to empty value disables send button', () => {
+            page.setText('new value');
+            page.setText('');
+            expect(page.isSendButtonEnabled()).toBeFalse();
         });
 
-        it('Enter doesn\'t modify value', async () => {
+        it('Enter doesn\'t modify value', () => {
+            page.setText('new value');
+            page.pressEnterKey();
+            expect(element.value).toEqual('new value');
         });
 
-        it('Shift-Enter adds newline and doesn\'t trigger send event', async () => {
+        it('Shift-Enter adds newline', () => {
+            page.setText('new value');
+            page.pressShiftEnterKey();
+            expect(element.value).toEqual('new value\n');
         });
     });
 
-    describe('send', () => {
-        beforeEach(async () => {
-            await connect();
-        });
+    // describe('send', () => {
+    //     const spy = jasmine.createSpy<ChatInputSendEventHandler>();
+    //     const sendListener = waitForEvent(element, 'send', spy);
+    //     beforeEach(async () => {
+    //         await connect();
+    //         spy.calls.reset();
+    //     });
 
-        it('via button click triggers send event with value as data', async () => {
-        });
+    //     it('via button click triggers send event with value as data', async () => {
+    //         element.value = 'new value';
+    //         page.clickSendButton();
+    //         await sendListener;
+    //         expect(spy).toHaveBeenCalledTimes(1);
+    //         expect(spy.calls.mostRecent().args[0].detail.text).toEqual('new value');
+    //     });
 
-        it('via Enter triggers send event with value as data', async () => {
-        });
+    //     it('via button click with no text triggers no send event', () => {
+    //     });
 
-        it('via Enter with no text triggers no send event', async () => {
-        });
-    });
+    //     it('via Enter triggers send event with value as data', () => {
+    //     });
 
-    describe('reset method', () => {
-        beforeEach(async () => {
-            await connect();
-        });
+    //     it('via Enter with no text triggers no send event', () => {
+    //     });
 
-        it('clears the input contents, sets focus, and disables the button', async () => {
-        });
+    //     it('Shift-Enter triggers no send event', () => {
+    //         page.setText('new value');
+    //         page.pressShiftEnterKey();
+    //     });
+    // });
 
-        it('can be called from send event handler', async () => {
-        });
-    });
+    // describe('reset method', () => {
+    //     beforeEach(async () => {
+    //         await connect();
+    //     });
 
-    describe('sendButtonLabel', () => {
-        beforeEach(async () => {
-            await connect();
-        });
+    //     it('clears the input contents, sets focus, and disables the button', () => {
+    //     });
 
-        it('defaults to empty', async () => {
-        });
+    //     it('can be called from send event handler', () => {
+    //     });
+    // });
 
-        it('affects button title and ARIA', async () => {
-        });
-    });
+    // describe('sendButtonLabel', () => {
+    //     beforeEach(async () => {
+    //         await connect();
+    //     });
+
+    //     it('defaults to empty', () => {
+    //     });
+
+    //     it('affects button title and ARIA', () => {
+    //     });
+    // });
 });
