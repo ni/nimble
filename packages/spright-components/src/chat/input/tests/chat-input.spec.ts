b30734c8faa4ceab83150a46178e7a1b507a6d8e
Jesse Attas <jattasNI@users.noreply.github.com>
2025-06-18 16:31:16 -0500

    describe tests

--- packages/spright-components/src/chat/input/tests/chat-input.spec.ts
+++ packages/spright-components/src/chat/input/tests/chat-input.spec.ts
@@ -23,13 +23,99 @@ describe('ChatInput', () => {
         expect(document.createElement(chatInputTag)).toBeInstanceOf(ChatInput);
     });
 
-    it('TODO', async () => {
-        await connect();
-        expect(element.shadowRoot).not.toBeNull();
+    describe('initial state', () => {
+        beforeEach(async () => {
+            await connect();
+        });
+
+        it('has no rendered text content', async () => {
+        });
+
+        it('has disabled send button', async () => {
+        });
     });
 
-    // TODO: test cases
-    // if text empty: send is disabled, enter doesn't send, enter doesn't modify text
-    // text starts out empty and button starts out disabled
-    // after send, text is empty and button is disabled and enter doesn't submit
+    describe('value property set', () => {
+        beforeEach(async () => {
+            await connect();
+        });
+
+        it('is reflected in value property get', async () => {
+        });
+
+        it('affects rendered text', async () => {
+        });
+
+        it('before connect affects rendered text', async () => {
+        });
+
+        it('to non-empty value enables send button', async () => {
+        });
+
+        it('to empty value disables send button', async () => {
+        });
+    });
+
+    describe('user input', () => {
+        beforeEach(async () => {
+            await connect();
+        });
+
+        it('is reflected in value property get', async () => {
+        });
+
+        it('affects rendered text', async () => {
+        });
+
+        it('to non-empty value enables send button', async () => {
+        });
+
+        it('to empty value disables send button', async () => {
+        });
+
+        it('Enter doesn\'t modify value', async () => {
+        });
+
+        it('Shift-Enter adds newline and doesn\'t trigger send event', async () => {
+        });
+    });
+
+    describe('send', () => {
+        beforeEach(async () => {
+            await connect();
+        });
+
+        it('via button click triggers send event with value as data', async () => {
+        });
+
+        it('via Enter triggers send event with value as data', async () => {
+        });
+
+        it('via Enter with no text triggers no send event', async () => {
+        });
+    });
+
+    describe('reset method', () => {
+        beforeEach(async () => {
+            await connect();
+        });
+
+        it('clears the input contents, sets focus, and disables the button', async () => {
+        });
+
+        it('can be called from send event handler', async () => {
+        });
+    });
+
+    describe('sendButtonLabel', () => {
+        beforeEach(async () => {
+            await connect();
+        });
+
+        it('defaults to empty', async () => {
+        });
+
+        it('affects button title and ARIA', async () => {
+        });
+    });
 });
