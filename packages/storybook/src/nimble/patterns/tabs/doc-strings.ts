7a8d274d663cd7892df61c1be6242f3ee1a413e7
Jonathan Meyer <jonathan.meyer@emerson.com>
2025-06-20 16:04:13 -0500

    Update packages/storybook/src/nimble/patterns/tabs/doc-strings.ts
    Co-authored-by: Jesse Attas <jattasNI@users.noreply.github.com>

--- packages/storybook/src/nimble/patterns/tabs/doc-strings.ts
+++ packages/storybook/src/nimble/patterns/tabs/doc-strings.ts
@@ -1,4 +1,4 @@
 import { tabsToolbarTag } from '@ni/nimble-components/dist/esm/tabs-toolbar';
 
-export const defaultSlotDescription = `Slot to display elements on the left side of the \`${tabsToolbarTag}\`. Elements that do not specify a slot will be placed in this slot.`;
-export const endSlotDescription = `Slot to display elements on the right side of the \`${tabsToolbarTag}\`. If slotted content is only in the \`end\` slot, the separator will be hidden.`;
+export const defaultSlotDescription = `Slot to display elements on the left side of the \`${tabsToolbarTag}\`. If this slot is empty, the separator will be hidden.`;`;
+export const endSlotDescription = `Slot to display elements on the right side of the \`${tabsToolbarTag}\`.
