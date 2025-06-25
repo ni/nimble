ddeb012ca538ee92c3096d1723d96d5e8b8d00d0
Jesse Attas <jattasNI@users.noreply.github.com>
2025-06-25 12:37:42 -0500

    React event

--- packages/react-workspace/react-client-app/src/components/App.tsx
+++ packages/react-workspace/react-client-app/src/components/App.tsx
@@ -56,6 +56,7 @@ import { NimbleRichTextMentionUsers } from '@ni/nimble-react/dist/esm/rich-text-
 import { NimbleMappingUser } from '@ni/nimble-react/dist/esm/mapping/user';
 import { NimbleRichTextViewer } from '@ni/nimble-react/dist/esm/rich-text/viewer';
 import { SprightChatConversation } from '@ni/spright-react/dist/esm/chat/conversation';
+import { SprightChatInput, type ChatInputSendEvent } from '@ni/spright-react/dist/esm/chat/input';
 import { SprightChatMessage } from '@ni/spright-react/dist/esm/chat/message';
 import { SprightRectangle } from '@ni/spright-react/dist/esm/rectangle';
 import { NimbleIconCopyText } from '@ni/nimble-react/dist/esm/icons/copy-text';
@@ -680,6 +681,14 @@ export function App(): JSX.Element {
                                 <NimbleButton slot="end" appearance="block">Order a tab</NimbleButton>
                                 <NimbleButton slot="end" appearance="block">Check core temperature</NimbleButton>
                             </SprightChatMessage>
+                            <SprightChatInput
+                                slot="input"
+                                placeholder="Type here"
+                                onSend={e => {
+                                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                                    const unused = (e as ChatInputSendEvent).detail.text;
+                                }}
+                            ></SprightChatInput>
                         </SprightChatConversation>
                     </div>
                     <div className="sub-container">
--- packages/react-workspace/spright-react/src/chat/input/index.ts
+++ packages/react-workspace/spright-react/src/chat/input/index.ts
@@ -1,4 +1,13 @@
 import { ChatInput } from '@ni/spright-components/dist/esm/chat/input';
+import type { ChatInputSendEventDetail } from '@ni/spright-components/dist/esm/chat/input/types';
 import { wrap } from '../../utilities/react-wrapper';
 
-export const SprightChatInput = wrap(ChatInput);
+export { type ChatInput };
+export const SprightChatInput = wrap(ChatInput, {
+    events: {
+        onSend: 'send',
+    }
+});
+export interface ChatInputSendEvent extends CustomEvent<ChatInputSendEventDetail> {
+    target: ChatInput;
+}
