82f2ff11c63b6d25b2efafb228245aa6e8824403
Jesse Attas <jattasNI@users.noreply.github.com>
2025-06-30 13:25:45 -0500

    Brandon's new divider color

--- packages/nimble-components/src/theme-provider/design-tokens.ts
+++ packages/nimble-components/src/theme-provider/design-tokens.ts
@@ -146,7 +146,7 @@ export const sectionBackgroundColor = DesignToken.create<string>(
 
 export const dividerBackgroundColor = DesignToken.create<string>(
     styleNameFromTokenName(tokenNames.dividerBackgroundColor)
-).withDefault((element: HTMLElement) => getColorForTheme(element, Black15, Black80, ForestGreen));
+).withDefault((element: HTMLElement) => getColorForTheme(element, hexToRgbaCssColor(Black91, 0.2), hexToRgbaCssColor(Black15, 0.2), hexToRgbaCssColor(White, 0.2)));
 
 export const fillSelectedColor = DesignToken.create<string>(
     styleNameFromTokenName(tokenNames.fillSelectedColor)
