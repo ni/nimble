9f905d3430d1e715254b9225b799b6486a290bf3
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2025-07-01 13:56:19 -0500

    Reduce size of containing div for two existing select visual tests

--- packages/storybook/src/nimble/select/select-matrix.stories.ts
+++ packages/storybook/src/nimble/select/select-matrix.stories.ts
@@ -213,7 +213,7 @@ const playFunction = async (): Promise<void> => {
 
 export const navigateToDifferentOption: StoryFn = createStory(
     html`
-    <div style="height: 250px;">
+    <div style="height: 120px; width: 250px;">
         <${selectTag} open style="width: 250px;">
             <${listOptionTag} value="1" selected>Option 1</${listOptionTag}>
             <${listOptionTag}>Option 2</${listOptionTag}>
@@ -225,7 +225,7 @@ navigateToDifferentOption.play = playFunction;
 
 export const navigateToDifferentOptionWithGroups: StoryFn = createStory(
     html`
-    <div style="height: 250px;">
+    <div style="height: 120px; width: 250px;">
         <${selectTag} open style="width: 250px;">
             <${listOptionGroupTag} label="Group 1">
                 <${listOptionTag} value="1" selected>Option 1</${listOptionTag}>
