42a11e239749d5bf6255e3e48603d7cdd47feef1
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2025-07-28 16:08:08 -0500

    Collapse breadcrumbs to dropdown when won't fit

--- packages/nimble-components/src/breadcrumb/types.ts
+++ packages/nimble-components/src/breadcrumb/types.ts
@@ -8,3 +8,13 @@ export const BreadcrumbAppearance = {
 } as const;
 export type BreadcrumbAppearance =
     (typeof BreadcrumbAppearance)[keyof typeof BreadcrumbAppearance];
+
+/**
+ * @internal
+ */
+export const CollapseState = {
+    none: 'collapse-none',
+    collapseMiddle: 'collapse-middle',
+    collapseExceptCurrent: 'collapse-except-current'
+} as const;
+export type CollapseState = (typeof CollapseState)[keyof typeof CollapseState];
