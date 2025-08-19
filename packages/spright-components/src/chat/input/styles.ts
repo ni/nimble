e3664e49ce5ff92b954d3c5ed9c4c3724e9954da
Jonathan Meyer <26874831+atmgrifter00@users.noreply.github.com>
2025-08-19 14:46:38 -0500

    Adding styling for focus.

--- packages/spright-components/src/chat/input/styles.ts
+++ packages/spright-components/src/chat/input/styles.ts
@@ -19,18 +19,44 @@ export const styles = css`
     :host {
         width: 100%;
         height: auto;
+        outline: none;
         --ni-private-hover-indicator-width: calc(${borderWidth} + 1px);
+        --ni-private-focus-indicator-width: 1px;
+        --ni-private-indicator-lines-gap: 2px;
     }
 
     .container {
         display: flex;
         flex-direction: column;
+        position: relative;
         width: 100%;
         height: 100%;
 
         background-color: ${applicationBackgroundColor};
         border: ${borderWidth} solid ${popupBorderColor};
         box-shadow: ${elevation2BoxShadow};
+    }
+
+    .container::before {
+        content: '';
+        position: absolute;
+        bottom: calc(${borderWidth} + var(--ni-private-indicator-lines-gap));
+        width: 0px;
+        height: 0px;
+        align-self: center;
+        border-bottom: ${borderHoverColor}
+            var(--ni-private-focus-indicator-width) solid;
+        transition: width ${smallDelay} ease-in;
+    }
+
+    @media (prefers-reduced-motion) {
+        :host::before {
+            transition-duration: 0s;
+        }
+    }
+
+    :host(:focus-within) .container::before {
+        width: calc(100% - 8px);
     }
 
     .container::after {
@@ -45,8 +71,15 @@ export const styles = css`
         transition: width ${smallDelay} ease-in;
     }
 
-    :host(:hover) .container::after {
+    :host(:hover) .container::after,
+    :host(:focus-within) .container::after {
         width: 100%;
+    }
+
+    @media (prefers-reduced-motion) {
+        :host::after {
+            transition-duration: 0s;
+        }
     }
 
     textarea {
