aca3945242df5304d287556b26cf0b9ea011f1e7
rajsite <rajsite@users.noreply.github.com>
2025-09-04 17:37:23 -0500

    Prototype behavior

--- packages/nimble-components/src/breadcrumb/styles.ts
+++ packages/nimble-components/src/breadcrumb/styles.ts
@@ -3,15 +3,35 @@ import { display } from '../utilities/style/display';
 import {
     bodyEmphasizedFont,
     linkFontColor,
-    linkProminentFontColor
+    linkProminentFontColor,
+    smallPadding
 } from '../theme-provider/design-tokens';
 
 export const styles = css`
-    ${display('inline-block')}
+    ${display('flex')}
 
+    :host {
+        flex-direction: row;
+    }
+
+    .scroll-button.left {
+        margin-right: ${smallPadding};
+    }
     .list {
         display: flex;
-        flex-wrap: wrap;
+        max-width: 100%;
+        width: max-content;
+        align-self: end;
+        overflow-x: scroll;
+        scrollbar-width: none;
+    }
+    .scroll-button.right {
+        margin-left: ${smallPadding};
+    }
+
+    ::slotted(*) {
+        flex-shrink: 0;
+        white-space: nowrap;
     }
 
     ::slotted(:last-child) {
