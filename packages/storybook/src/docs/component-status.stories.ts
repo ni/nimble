ffbe3572b108a06c854d1bbc410a3d87ba4db00b
Milan Raj <rajsite@users.noreply.github.com>
2025-09-09 09:45:45 -0500

    Pork barrel a status table fix

--- packages/storybook/src/docs/component-status.stories.ts
+++ packages/storybook/src/docs/component-status.stories.ts
@@ -138,8 +138,8 @@ const components = [
         blazorStatus: ComponentFrameworkStatus.ready
     },
     {
-        componentName: 'Chat',
-        componentHref: './?path=/docs/spright-chat--docs',
+        componentName: 'Chat Conversation',
+        componentHref: './?path=/docs/spright-chat-conversation--docs',
         designHref:
             'https://www.figma.com/design/PO9mFOu5BCl8aJvFchEeuN/Nimble_Components?node-id=12342-81782&node-type=canvas&t=L5GvLaC3injrqWrR-0',
         designLabel: 'Figma',
