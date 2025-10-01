853e186871678080ae0cfb476fbcf8de8c73c7db
Milan Raj <rajsite@users.noreply.github.com>
2025-09-30 23:57:23 -0500

    Scrollable breadcrumb (#2695)
    # Pull Request
    
    ## 🤨 Rationale
    
    This PR resolves https://github.com/ni/nimble/issues/2186 by
    implementing overflow behavior similar to the tab control.
    Notably this PR does not implement the design discussed in
    https://github.com/ni/nimble/issues/2213 and is instead a shorter term
    workaround.
    
    ## 👩‍💻 Implementation
    
    - Fork the foundation breadcrumb template and tests
    - Duplicate the tab scroll implementation and tests tweaked for the
    breadcrumb
    - Notable behavior difference is that for breadcrumb each
    breadcrumb-item is a tab stop instead of having a single tab stop like
    tabs and using roving tabs between each tab.
    - Corresponding SystemLink changes can be seen in [AZDO
    1063821](https://dev.azure.com/ni/DevCentral/_git/Skyline/pullrequest/1063821)
    - Opting to duplicate scrollable tabs / breadcrumb-items pattern for
    now, can consider de-duplicating it if another usage is found
    
    ## 🧪 Testing
    
    - Duplicates test patterns of scrollable tabs
    - Manual validation in SystemLinkShared sl-header storybook and
    TestInsights app
    - Example [stratus-dev sl-header
    storybook](https://62431f379ac921003aaca05a-vmhuqvbrqg.chromatic.com/?path=/story/components-header--default&args=itemsType:many)
    build
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

--- packages/nimble-components/src/breadcrumb/tests/breadcrumb.spec.ts
+++ packages/nimble-components/src/breadcrumb/tests/breadcrumb.spec.ts
@@ -1,9 +1,156 @@
+import { html } from '@ni/fast-element';
 import { Breadcrumb, breadcrumbTag } from '..';
+import { breadcrumbItemTag } from '../../breadcrumb-item';
+import { BreadcrumbPageObject } from '../testing/breadcrumb.pageobject';
+import { fixture, type Fixture } from '../../utilities/tests/fixture';
+
+async function setup(): Promise<Fixture<Breadcrumb>> {
+    const viewTemplate = html`
+        <${breadcrumbTag}>
+            <${breadcrumbItemTag}>Item 1</${breadcrumbItemTag}>
+            <${breadcrumbItemTag}>Item 2</${breadcrumbItemTag}>
+            <${breadcrumbItemTag}>Item 3</${breadcrumbItemTag}>
+            <${breadcrumbItemTag}>Item 4</${breadcrumbItemTag}>
+            <${breadcrumbItemTag}>Item 5</${breadcrumbItemTag}>
+            <${breadcrumbItemTag}>Item 6</${breadcrumbItemTag}>
+        </${breadcrumbTag}>
+    `;
+
+    return await fixture<Breadcrumb>(viewTemplate);
+}
+
+async function setupInstance(): Promise<
+Fixture<Breadcrumb> & { breadcrumbPageObject: BreadcrumbPageObject }
+> {
+    const fixtureResult = await setup();
+    await fixtureResult.connect();
+    const breadcrumbPageObject = new BreadcrumbPageObject(
+        fixtureResult.element
+    );
+    return {
+        ...fixtureResult,
+        breadcrumbPageObject
+    };
+}
 
 describe('Breadcrumb', () => {
     it('can construct an element instance', () => {
         expect(document.createElement(breadcrumbTag)).toBeInstanceOf(
             Breadcrumb
         );
+    });
+
+    describe('Scroll buttons', () => {
+        it('should not show scroll buttons when the Breadcrumb fit within the container', async () => {
+            const { breadcrumbPageObject, disconnect } = await setupInstance();
+            expect(breadcrumbPageObject.areScrollButtonsVisible()).toBeFalse();
+            await disconnect();
+        });
+
+        it('should show scroll buttons when the Breadcrumb overflow the container', async () => {
+            const { breadcrumbPageObject, disconnect } = await setupInstance();
+            await breadcrumbPageObject.setBreadcrumbWidth(300);
+            expect(breadcrumbPageObject.areScrollButtonsVisible()).toBeTrue();
+            await disconnect();
+        });
+
+        it('should hide scroll buttons when the Breadcrumb no longer overflow the container', async () => {
+            const { breadcrumbPageObject, disconnect } = await setupInstance();
+            await breadcrumbPageObject.setBreadcrumbWidth(300); // first make the Breadcrumb overflow
+            await breadcrumbPageObject.setBreadcrumbWidth(1000); // then make the Breadcrumb fit
+            expect(breadcrumbPageObject.areScrollButtonsVisible()).toBeFalse();
+            await disconnect();
+        });
+
+        it('should scroll left when the left scroll button is clicked', async () => {
+            const { breadcrumbPageObject, disconnect } = await setupInstance();
+            await breadcrumbPageObject.setBreadcrumbWidth(300);
+            await breadcrumbPageObject.scrollBreadcrumbItemIntoViewByIndex(5);
+            const currentScrollOffset = breadcrumbPageObject.getBreadcrumbViewScrollOffset();
+            await breadcrumbPageObject.clickScrollLeftButton();
+            expect(
+                breadcrumbPageObject.getBreadcrumbViewScrollOffset()
+            ).toBeLessThan(currentScrollOffset);
+            await disconnect();
+        });
+
+        it('should not scroll left when the left scroll button is clicked and the first breadcrumb item is active', async () => {
+            const { breadcrumbPageObject, disconnect } = await setupInstance();
+            await breadcrumbPageObject.setBreadcrumbWidth(300);
+            await breadcrumbPageObject.clickScrollLeftButton();
+            expect(breadcrumbPageObject.getBreadcrumbViewScrollOffset()).toBe(
+                0
+            );
+            await disconnect();
+        });
+
+        it('should scroll right when the right scroll button is clicked', async () => {
+            const { breadcrumbPageObject, disconnect } = await setupInstance();
+            await breadcrumbPageObject.setBreadcrumbWidth(300);
+            await breadcrumbPageObject.clickScrollRightButton();
+            expect(
+                breadcrumbPageObject.getBreadcrumbViewScrollOffset()
+            ).toBeGreaterThan(0);
+            await disconnect();
+        });
+
+        it('should not scroll right when the right scroll button is clicked and the last breadcrumb item is active', async () => {
+            const { breadcrumbPageObject, disconnect } = await setupInstance();
+            await breadcrumbPageObject.setBreadcrumbWidth(300);
+            await breadcrumbPageObject.scrollBreadcrumbItemIntoViewByIndex(5);
+            const currentScrollOffset = breadcrumbPageObject.getBreadcrumbViewScrollOffset();
+            await breadcrumbPageObject.clickScrollRightButton();
+            expect(breadcrumbPageObject.getBreadcrumbViewScrollOffset()).toBe(
+                currentScrollOffset
+            );
+            await disconnect();
+        });
+
+        it('should show scroll buttons when new breadcrumb item is added and Breadcrumb overflow the container', async () => {
+            const { breadcrumbPageObject, disconnect } = await setupInstance();
+            await breadcrumbPageObject.setBreadcrumbWidth(450);
+            expect(breadcrumbPageObject.areScrollButtonsVisible()).toBeFalse();
+            await breadcrumbPageObject.addBreadcrumbItem(
+                'New Item With Extremely Long Name'
+            );
+            expect(breadcrumbPageObject.areScrollButtonsVisible()).toBeTrue();
+            await disconnect();
+        });
+
+        it('should hide scroll buttons when breadcrumb item is removed and Breadcrumb no longer overflow the container', async () => {
+            const { breadcrumbPageObject, disconnect } = await setupInstance();
+            await breadcrumbPageObject.setBreadcrumbWidth(500);
+            await breadcrumbPageObject.addBreadcrumbItem(
+                'New Item With Extremely Long Name'
+            );
+            expect(breadcrumbPageObject.areScrollButtonsVisible()).toBeTrue();
+            await breadcrumbPageObject.removeBreadcrumbItemByIndex(6);
+            expect(breadcrumbPageObject.areScrollButtonsVisible()).toBeFalse();
+            await disconnect();
+        });
+
+        it('should show scroll buttons when breadcrumb item label is updated and Breadcrumb overflow the container', async () => {
+            const { breadcrumbPageObject, disconnect } = await setupInstance();
+            await breadcrumbPageObject.setBreadcrumbWidth(450);
+            expect(breadcrumbPageObject.areScrollButtonsVisible()).toBeFalse();
+            await breadcrumbPageObject.updateBreadcrumbItemLabel(
+                0,
+                'New Item With Extremely Long Name'
+            );
+            expect(breadcrumbPageObject.areScrollButtonsVisible()).toBeTrue();
+            await disconnect();
+        });
+
+        it('should hide scroll buttons when breadcrumb item label is updated and Breadcrumb no longer overflow the container', async () => {
+            const { breadcrumbPageObject, disconnect } = await setupInstance();
+            await breadcrumbPageObject.setBreadcrumbWidth(550);
+            await breadcrumbPageObject.addBreadcrumbItem(
+                'New Item With Extremely Long Name'
+            );
+            expect(breadcrumbPageObject.areScrollButtonsVisible()).toBeTrue();
+            await breadcrumbPageObject.updateBreadcrumbItemLabel(6, 'Item 6');
+            expect(breadcrumbPageObject.areScrollButtonsVisible()).toBeFalse();
+            await disconnect();
+        });
     });
 });
