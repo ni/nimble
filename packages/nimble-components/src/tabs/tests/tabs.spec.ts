348042d959bd5b1043e91c7e521fabd6cf3a0af6
Milan Raj <rajsite@users.noreply.github.com>
2025-10-01 18:00:54 -0500

    Jasmine retry helper with beforeEach and afterEach support (#2715)
    # Pull Request
    
    ## 🤨 Rationale
    
    - Adds `beforeEach` and `afterEach` execution for the helper
    - Adds `this` context support during function execution which fixed
    Angular usage
    
    Partially addresses https://github.com/ni/nimble/issues/2702
    
    ## 👩‍💻 Implementation
    
    See above
    
    ## 🧪 Testing
    
    Additional test cases added for beforeEach execution.
    
    Looks like the CI caught and recovered an intermittency! [See
    build](https://github.com/ni/nimble/actions/runs/18174187006/job/51736032162?pr=2715#step:17:602).
    Excerpt:
    
    ```
    [test-concurrent:nimble-components] [test-webkit] LOG LOG: 'Test "Table detaching and reattaching maintains scroll position if data length is increased while not attached #SkipFirefox" failed, attempting retry 1 cleanup'
    [test-concurrent:nimble-components] [test-webkit] LOG LOG: 'Test "Table detaching and reattaching maintains scroll position if data length is increased while not attached #SkipFirefox" failed, attempting retry 1 setup'
    [test-concurrent:nimble-components] [test-webkit] LOG LOG: 'Test "Table detaching and reattaching maintains scroll position if data length is increased while not attached #SkipFirefox" failed, attempting retry 1 run in 1000ms'
    ```
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

--- packages/nimble-components/src/tabs/tests/tabs.spec.ts
+++ packages/nimble-components/src/tabs/tests/tabs.spec.ts
@@ -26,67 +26,57 @@ async function setup(): Promise<Fixture<Tabs>> {
     return await fixture<Tabs>(viewTemplate);
 }
 
-async function setupInstance(): Promise<
-Fixture<Tabs> & { tabsPageObject: TabsPageObject }
-> {
-    const fixtureResult = await setup();
-    await fixtureResult.connect();
-    const tabsPageObject = new TabsPageObject(fixtureResult.element);
-    return {
-        ...fixtureResult,
-        tabsPageObject
-    };
-}
-
 describe('Tabs', () => {
+    let tabsPageObject: TabsPageObject;
+    let element: Tabs;
+    let connect: () => Promise<void>;
+    let disconnect: () => Promise<void>;
+
+    beforeEach(async () => {
+        ({ element, connect, disconnect } = await setup());
+        await connect();
+        tabsPageObject = new TabsPageObject(element);
+    });
+
+    afterEach(async () => {
+        await disconnect();
+    });
+
     it('can construct an element instance', () => {
         expect(document.createElement(tabsTag)).toBeInstanceOf(Tabs);
     });
 
     it('setting activeid should scroll the active tab into view', async () => {
-        const { element, tabsPageObject, disconnect } = await setupInstance();
         await tabsPageObject.setTabsWidth(300);
         element.activeid = '6'; // scrolls to the last tab
         await waitForUpdatesAsync();
         expect(tabsPageObject.getTabsViewScrollOffset()).toBeGreaterThan(0);
-
-        await disconnect();
     });
 
     it('clicking on a tab that is completely in view should not scroll the tablist', async () => {
-        const { tabsPageObject, disconnect } = await setupInstance();
         await tabsPageObject.setTabsWidth(300);
         await tabsPageObject.clickTab(2); // clicks the third tab
         await waitForUpdatesAsync();
         expect(tabsPageObject.getTabsViewScrollOffset()).toBe(0);
-
-        await disconnect();
     });
 
     describe('Scroll buttons', () => {
-        it('should not show scroll buttons when the tabs fit within the container', async () => {
-            const { tabsPageObject, disconnect } = await setupInstance();
+        it('should not show scroll buttons when the tabs fit within the container', () => {
             expect(tabsPageObject.areScrollButtonsVisible()).toBeFalse();
-            await disconnect();
         });
 
         it('should show scroll buttons when the tabs overflow the container', async () => {
-            const { tabsPageObject, disconnect } = await setupInstance();
             await tabsPageObject.setTabsWidth(300);
             expect(tabsPageObject.areScrollButtonsVisible()).toBeTrue();
-            await disconnect();
         });
 
         it('should hide scroll buttons when the tabs no longer overflow the container', async () => {
-            const { tabsPageObject, disconnect } = await setupInstance();
             await tabsPageObject.setTabsWidth(300); // first make the tabs overflow
             await tabsPageObject.setTabsWidth(1000); // then make the tabs fit
             expect(tabsPageObject.areScrollButtonsVisible()).toBeFalse();
-            await disconnect();
         });
 
         it('should scroll left when the left scroll button is clicked', async () => {
-            const { element, tabsPageObject, disconnect } = await setupInstance();
             await tabsPageObject.setTabsWidth(300);
             element.activeid = '6'; // scrolls to the last tab
             const currentScrollOffset = tabsPageObject.getTabsViewScrollOffset();
@@ -94,27 +84,21 @@ describe('Tabs', () => {
             expect(tabsPageObject.getTabsViewScrollOffset()).toBeLessThan(
                 currentScrollOffset
             );
-            await disconnect();
         });
 
         it('should not scroll left when the left scroll button is clicked and the first tab is active', async () => {
-            const { tabsPageObject, disconnect } = await setupInstance();
             await tabsPageObject.setTabsWidth(300);
             await tabsPageObject.clickScrollLeftButton();
             expect(tabsPageObject.getTabsViewScrollOffset()).toBe(0);
-            await disconnect();
         });
 
         it('should scroll right when the right scroll button is clicked', async () => {
-            const { tabsPageObject, disconnect } = await setupInstance();
             await tabsPageObject.setTabsWidth(300);
             await tabsPageObject.clickScrollRightButton();
             expect(tabsPageObject.getTabsViewScrollOffset()).toBeGreaterThan(0);
-            await disconnect();
         });
 
         it('should not scroll right when the right scroll button is clicked and the last tab is active', async () => {
-            const { element, tabsPageObject, disconnect } = await setupInstance();
             await tabsPageObject.setTabsWidth(300);
             element.activeid = '6'; // scrolls to the last tab
             const currentScrollOffset = tabsPageObject.getTabsViewScrollOffset();
@@ -122,30 +106,24 @@ describe('Tabs', () => {
             expect(tabsPageObject.getTabsViewScrollOffset()).toBe(
                 currentScrollOffset
             );
-            await disconnect();
         });
 
         it('should show scroll buttons when new tab is added and tabs overflow the container', async () => {
-            const { tabsPageObject, disconnect } = await setupInstance();
             await tabsPageObject.setTabsWidth(450);
             expect(tabsPageObject.areScrollButtonsVisible()).toBeFalse();
             await tabsPageObject.addTab('New Tab With Extremely Long Name');
             expect(tabsPageObject.areScrollButtonsVisible()).toBeTrue();
-            await disconnect();
         });
 
         it('should hide scroll buttons when tab is removed and tabs no longer overflow the container', async () => {
-            const { tabsPageObject, disconnect } = await setupInstance();
             await tabsPageObject.setTabsWidth(500);
             await tabsPageObject.addTab('New Tab With Extremely Long Name');
             expect(tabsPageObject.areScrollButtonsVisible()).toBeTrue();
             await tabsPageObject.removeTab(6);
             expect(tabsPageObject.areScrollButtonsVisible()).toBeFalse();
-            await disconnect();
         });
 
         it('should show scroll buttons when tab label is updated and tabs overflow the container', async () => {
-            const { tabsPageObject, disconnect } = await setupInstance();
             await tabsPageObject.setTabsWidth(450);
             expect(tabsPageObject.areScrollButtonsVisible()).toBeFalse();
             await tabsPageObject.updateTabLabel(
@@ -153,17 +131,14 @@ describe('Tabs', () => {
                 'New Tab With Extremely Long Name'
             );
             expect(tabsPageObject.areScrollButtonsVisible()).toBeTrue();
-            await disconnect();
         });
 
         it('should hide scroll buttons when tab label is updated and tabs no longer overflow the container', async () => {
-            const { tabsPageObject, disconnect } = await setupInstance();
             await tabsPageObject.setTabsWidth(550);
             await tabsPageObject.addTab('New Tab With Extremely Long Name');
             expect(tabsPageObject.areScrollButtonsVisible()).toBeTrue();
             await tabsPageObject.updateTabLabel(6, 'Tab 6');
             expect(tabsPageObject.areScrollButtonsVisible()).toBeFalse();
-            await disconnect();
         });
     });
 });
