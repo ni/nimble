c9bfc8f9f31c3635b3deda5f371d75cf77958b20
rajsite <rajsite@users.noreply.github.com>
2025-10-10 21:47:42 -0500

    Add token tests

--- packages/storybook/src/nimble/theme-provider/tokens.stories.ts
+++ packages/storybook/src/nimble/theme-provider/tokens.stories.ts
@@ -27,7 +27,6 @@ const graphTokenNames = tokenNames.filter(x => x.startsWith('graph'));
 const calendarTokenNames = tokenNames.filter(x => x.startsWith('calendar'));
 
 interface TokenArgs {
-    metaTitle: string;
     tokenNames: TokenName[];
     propertyFormat: PropertyFormat;
 }
@@ -131,31 +130,7 @@ const templateForTokenName = (
     return template;
 };
 
-// prettier-ignore
-const metadata: Meta<TokenArgs> = {
-    title: 'Tokens/Theme-aware Tokens',
-    parameters: {
-        docs: {
-            source: {
-                code: null
-            }
-        },
-        controls: { hideNoControlsWarning: true }
-    },
-    args: {
-        propertyFormat: PropertyFormat.scss
-    },
-    argTypes: {
-        propertyFormat: {
-            options: Object.values(PropertyFormat),
-            control: { type: 'radio' },
-            name: 'Property Format'
-        },
-        tokenNames: {
-            table: { disable: true }
-        }
-    },
-    render: createUserSelectedThemeStory(html`
+export const component = html<TokenArgs>`
         <style>
             table {
                 font: var(${bodyFont.cssCustomProperty});
@@ -196,7 +171,33 @@ const metadata: Meta<TokenArgs> = {
             `)}
             </tbody>
         </table>
-    `),
+`;
+
+// prettier-ignore
+const metadata: Meta<TokenArgs> = {
+    title: 'Tokens/Theme-aware Tokens',
+    parameters: {
+        docs: {
+            source: {
+                code: null
+            }
+        },
+        controls: { hideNoControlsWarning: true }
+    },
+    args: {
+        propertyFormat: PropertyFormat.scss
+    },
+    argTypes: {
+        propertyFormat: {
+            options: Object.values(PropertyFormat),
+            control: { type: 'radio' },
+            name: 'Property Format'
+        },
+        tokenNames: {
+            table: { disable: true }
+        }
+    },
+    render: createUserSelectedThemeStory(component),
     // Setting token default values is done as part of the FAST render queue so it needs to be cleared before reading them
     // https://github.com/microsoft/fast/blob/bbf4e532cf9263727ef1bd8afbc30d79d1104c03/packages/web-components/fast-foundation/src/design-token/custom-property-manager.ts#LL154C3-L154C3
     // This uses Storybook's "loaders" feature to await the queue. https://storybook.js.org/docs/html/writing-stories/loaders
