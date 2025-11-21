1d0b782ef91b2f632f637e528f99a272297d6cb1
Milan Raj <rajsite@users.noreply.github.com>
2025-11-21 16:51:13 -0600

    Update packages/nimble-components/src/table/components/row/index.ts

--- packages/nimble-components/src/table/components/row/index.ts
+++ packages/nimble-components/src/table/components/row/index.ts
@@ -215,6 +215,8 @@ export class TableRow<
         column: TableColumn
     ): void {
         this.menuOpen = event.detail.newState;
+        // Workaround for Firefox issue when action menus opened on different rows
+        // See: https://github.com/ni/nimble/issues/2744
         if (!event.detail.newState) {
             this.currentActionMenuColumn = undefined;
         }
