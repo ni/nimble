932db5586f30763e1109e430da8b829dd97a9058
Valerie Gleason <5265744+hellovolcano@users.noreply.github.com>
2025-12-05 10:32:07 -0600

    fix blazor event handling

--- packages/blazor-workspace/NimbleBlazor/EventHandlers.cs
+++ packages/blazor-workspace/NimbleBlazor/EventHandlers.cs
@@ -31,6 +31,7 @@ public class TableActionMenuToggleEventArgs : EventArgs
     public bool OldState { get; set; }
     public IEnumerable<string>? RecordIds { get; set; }
     public string? ColumnId { get; set; }
+    public string? OperatingRecordId { get; set; }
 }
 
 public class TableRowSelectionEventArgs : EventArgs
