87a9d9986bbc2804838c96073d5cc45f224368af
rajsite <rajsite@users.noreply.github.com>
2025-12-21 17:06:47 -0600

    update images

--- packages/site/src/index.ts
+++ packages/site/src/index.ts
@@ -2,5 +2,7 @@ import '@ni/nimble-components/dist/esm/theme-provider/design-tokens';
 import '@ni/nimble-components/dist/esm/theme-provider';
 
 import { encircle } from './circle';
+import { themify } from './theme';
 
 encircle();
+themify();
