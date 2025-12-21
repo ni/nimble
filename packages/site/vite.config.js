e424a214429e3f85fb8380848c9b147040377068
Milan Raj <rajsite@users.noreply.github.com>
2025-12-21 17:52:15 -0600

    Dark mode on landing page (#2801)
    # Pull Request
    
    ## 🤨 Rationale
    
    Add dark mode to the nimble landing page
    
    ## 👩‍💻 Implementation
    
    - Add a theme-provider
    - Went for adding separate brand icons for light and dark mode and
    switching in css as background images
    - Added script to respond to user's `prefers-color-scheme` setting
    - Removed 11ty and just rely on vite for site (wasn't using any 11ty
    templating features for a single page site)
    
    ## 🧪 Testing
    
    - Testing in Chrome and Firefox
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

--- packages/site/vite.config.js
+++ packages/site/vite.config.js
@@ -1,7 +1,42 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'vite';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+process.env.DEBUG = 'vite:plugin-static-copy';
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
-    base: './'
+    base: './',
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: '../../packages/storybook/dist/storybook/*',
+                    dest: 'storybook/',
+                    overwrite: 'error',
+                },
+                {
+                    src: '../../packages/angular-workspace/dist/example-client-app/*',
+                    dest: 'storybook/example-client-app/',
+                    overwrite: 'error',
+                },
+                {
+                    src: '../../packages/blazor-workspace/dist/blazor-client-app/*',
+                    dest: 'storybook/blazor-client-app/',
+                    overwrite: 'error',
+                },
+                {
+                    src: '../../packages/react-workspace/react-client-app/dist/*',
+                    dest: 'storybook/react-client-app/',
+                    overwrite: 'error',
+                },
+                {
+                    src: '../../packages/performance/dist/*',
+                    dest: 'storybook/performance/',
+                    overwrite: 'error',
+                },
+            ],
+        }),
+    ],
 });
