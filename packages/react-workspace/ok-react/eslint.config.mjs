31e02d44aeffe22bb68861b8010a08ac4983c34e
rajsite <rajsite@users.noreply.github.com>
2026-01-01 15:26:05 -0600

    Switch to package exports

--- packages/react-workspace/ok-react/eslint.config.mjs
+++ packages/react-workspace/ok-react/eslint.config.mjs
@@ -1,17 +1,9 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
-import { lintNimbleConfig, javascriptNimbleConfig, typescriptNimbleConfig } from '@ni-private/eslint-config-nimble';
+import { lintNimbleConfig, typescriptNimbleConfig } from '@ni-private/eslint-config-nimble';
 
 export default defineConfig([
     globalIgnores(['**/dist/']),
     lintNimbleConfig,
-    {
-        files: ['**/*.js', '**/*.cjs'],
-        extends: javascriptNimbleConfig,
-        rules: {
-            // Configuration scripts will not be in published package and are allowed to use devDependencies
-            'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-        }
-    },
     {
         files: ['**/*.ts'],
         extends: typescriptNimbleConfig,
