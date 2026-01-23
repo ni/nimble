ea48f0fb1ca901fa9bf2bfe4a82ae04a44b2ab63
Milan Raj <rajsite@users.noreply.github.com>
2026-01-23 14:54:03 -0600

    Refactor Icon base class to be extendable (#2814)
    # Pull Request
    
    ## 🤨 Rationale
    
    Refactor the Icon base class so new types of icons can extend the class
    manually. Should be a non-breaking change for users as existing icon api
    and paths don't change. Intention is to support custom icon types such
    as multicolor icons (#327) and dynamic icons (#2813).
    
    ## 👩‍💻 Implementation
    
    - Refactor the Icon base class to be abstract
    - Make a new SVGIcon base class that implements the current
    infrastructure for nimble icons
    
    ## 🧪 Testing
    
    - Rely on existing tests
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.
    
    ---------
    
    Co-authored-by: rajsite <1588923+rajsite@users.noreply.github.com>

--- packages/nimble-components/src/icon-svg/template.ts
+++ packages/nimble-components/src/icon-svg/template.ts
@@ -1,8 +1,8 @@
 import { html } from '@ni/fast-element';
-import type { Icon } from '.';
+import type { IconSvg } from '.';
 
 // Avoiding any whitespace in the template because this is an inline element
-export const template = html<Icon>`<div
+export const template = html<IconSvg>`<div
     class="icon"
     aria-hidden="true"
     :innerHTML=${x => x.icon.data}
