d90f08da8d7437b7237203d54d592e32ddf6f224
rajsite <1588923+rajsite@users.noreply.github.com>
2026-01-23 12:26:11 -0600

    Add icon implementor requirements in comment

--- packages/nimble-components/src/icon-base/index.ts
+++ packages/nimble-components/src/icon-base/index.ts
@@ -3,7 +3,11 @@ import { FoundationElement } from '@ni/fast-foundation';
 import type { IconSeverity } from './types';
 
 /**
- * The base class for icon components
+ * The base class for icon components. Implementors:
+ * - Should not increase the API surface area, consumers of icons would not expect additional attributes to configure
+ * - Should not add interactive components to the template, expect to be visual only
+ * - Should respond well to sizing of the element
+ * - Should respond to configuration of the severity attribute and other tokens such as theme
  */
 export abstract class Icon extends FoundationElement {
     /**
