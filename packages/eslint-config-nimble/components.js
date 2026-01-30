1a668dacf87ddc7b79a1f14b3fd393b86eefef0a
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2026-01-29 18:24:54 -0600

    Re-enable rules

--- packages/eslint-config-nimble/components.js
+++ packages/eslint-config-nimble/components.js
@@ -27,12 +27,6 @@ export const componentsNimbleConfig = defineConfig([
                     paths: [...restrictedFastImports()],
                 },
             ],
-
-            // Strict rules components have not adopted yet
-            // See: https://github.com/ni/nimble/issues/2777
-            '@typescript-eslint/no-unnecessary-condition': 'off',
-            '@typescript-eslint/strict-boolean-expressions': 'off',
-            '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'off'
         },
     },
     {
