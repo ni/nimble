e112fa4178d010f7c314190cbdfce142ea998132
Milan Raj <rajsite@users.noreply.github.com>
2026-01-30 16:53:40 -0600

    Storybook capture story source content before attaching to DOM (#2839)
    # Pull Request
    
    ## 🤨 Rationale
    
    Fixes #2706
    
    Historically the `createStorybook` helpers, etc would render string
    results that better reflected the initial story html string rendering.
    However, we switched to returning instanced HTML Elements so we can do
    fancier data binding patterns like [grabbing a ref to a
    dialog](https://github.com/ni/nimble/blob/df77cf29ba4d9b9283d12196b486886784541b93/packages/storybook/src/nimble/dialog/dialog.stories.ts#L119-L120)
    so we need the actual element instance to be live.
    
    However, it looks like storybook attaches the element to the DOM and at
    some point when trying to render the source [calls `outerHTML` on the
    element](https://github.com/storybookjs/storybook/blob/v10.1.11/code/renderers/html/src/docs/sourceDecorator.ts#L40).
    
    ## 👩‍💻 Implementation
    
    In order to get the best of both worlds, this workaround captures the
    `outerHTML` of the element before it is attached to the DOM and then
    overrides the `outerHTML` property to always return that cached value.
    As referenced in the issue I'm not sure we have a convincing argument to
    make for an upstream issue so haven't tried to make one yet.
    
    ## 🧪 Testing
    
    Played around in storybook
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.
    
    ---------
    
    Co-authored-by: rajsite <1588923+rajsite@users.noreply.github.com>

--- packages/storybook/src/utilities/storybook.ts
+++ packages/storybook/src/utilities/storybook.ts
@@ -23,15 +23,24 @@ export const fastParameters = () => ({
 /**
  * Renders a ViewTemplate as elements in a DocumentFragment.
  * Bindings, such as event binding, will be active.
+ * The first element child of the fragment will be returned.
  */
 export const renderViewTemplate = <TSource>(
     viewTemplate: ViewTemplate<TSource>,
     source: TSource
-): DocumentFragment => {
+): Element => {
     const template = document.createElement('template');
     const fragment = template.content;
     viewTemplate.render(source, fragment);
-    return fragment;
+    const content = fragment.firstElementChild!;
+    // Capture the outerHTML content before the node is attached to the DOM
+    // to workaround outerHTML being called after the element is attached to the DOM
+    // https://github.com/ni/nimble/issues/2706
+    const value = content.outerHTML;
+    Object.defineProperty(content, 'outerHTML', {
+        value,
+    });
+    return content;
 };
 
 /**
@@ -44,8 +53,7 @@ export const createStory = <TSource>(
         const wrappedViewTemplate = html<TSource>`
             <div class="code-hide-top-container">${viewTemplate}</div>
         `;
-        const fragment = renderViewTemplate(wrappedViewTemplate, source);
-        const content = fragment.firstElementChild!;
+        const content = renderViewTemplate(wrappedViewTemplate, source);
         return content;
     };
 };
@@ -76,8 +84,7 @@ export const createUserSelectedThemeStory = <TSource>(
                 ${viewTemplate}
             </${themeProviderTag}>
         `;
-        const fragment = renderViewTemplate(wrappedViewTemplate, source);
-        const content = fragment.firstElementChild!;
+        const content = renderViewTemplate(wrappedViewTemplate, source);
         return content;
     };
 };
@@ -113,8 +120,7 @@ export const createFixedThemeStory = <TSource>(
                 </div>
             </${themeProviderTag}>
         `;
-        const fragment = renderViewTemplate(wrappedViewTemplate, source);
-        const content = fragment.firstElementChild!;
+        const content = renderViewTemplate(wrappedViewTemplate, source);
         return content;
     };
 };
