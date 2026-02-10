86a5684f6ecb628d5da31d9b599a91ee9028967b
Valerie Gleason <5265744+hellovolcano@users.noreply.github.com>
2026-02-10 10:45:41 -0600

    added matrix with static image

---
+++ packages/storybook/src/ok/icon-dynamic/icon-dynamic.matrix.stories.ts
@@ -0,0 +1,32 @@
+import type { StoryFn, Meta } from '@storybook/html-vite';
+import { html, type ViewTemplate } from '@ni/fast-element';
+import { IconDynamic } from '@ni/ok-components/dist/esm/icon-dynamic';
+import {
+    createMatrix,
+    sharedMatrixParameters,
+    createMatrixThemeStory
+} from '../../utilities/matrix';
+
+const tagName = 'ok-icon-dynamic-awesome';
+const url = 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><circle cx="8" cy="8" r="6" fill="black"/></svg>';
+
+const metadata: Meta = {
+    title: 'Tests Ok/Icon Dynamic',
+    parameters: {
+        ...sharedMatrixParameters()
+    }
+};
+
+export default metadata;
+
+const component = (): ViewTemplate => {
+    if (!customElements.get(tagName)) {
+        IconDynamic.registerIconDynamic(tagName, url);
+    }
+
+    return html`<${tagName}></${tagName}>`;
+};
+
+export const themeMatrix: StoryFn = createMatrixThemeStory(
+    createMatrix(component)
+);
