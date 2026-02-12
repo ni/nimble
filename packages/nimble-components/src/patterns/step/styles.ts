8021ebadb56afd47e9000bd420e792bad3105818
rajsite <1588923+rajsite@users.noreply.github.com>
2026-02-12 11:41:27 -0600

    severity text placement

--- packages/nimble-components/src/patterns/step/styles.ts
+++ packages/nimble-components/src/patterns/step/styles.ts
@@ -36,6 +36,7 @@ export const styles = css`
             display: inline-flex;
             width: 100%;
             height: 100%;
+            position: relative;
         }
 
         .control { 
