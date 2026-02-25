39c04cb7ab1dea927d5ada9357b24b702619d2ed
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2026-02-25 17:28:14 -0600

    Fix for when TEST_RETRY_COUNT isn't defined

--- packages/jasmine-extensions/src/index.ts
+++ packages/jasmine-extensions/src/index.ts
@@ -3,7 +3,8 @@ import { retryFailedTests } from './retry-failed-tests.js';
 declare const TEST_RETRY_COUNT: string | undefined;
 const defaultRetryCount = 4;
 
-const retriesString = TEST_RETRY_COUNT ?? '-1';
+// Need to check type (rather than just the value), in case the global doesn't exist at all
+const retriesString = typeof TEST_RETRY_COUNT !== 'undefined' ? TEST_RETRY_COUNT : '-1';
 const retries = parseInt(retriesString, 10);
 
 export function applyExtensions(): void {
