b67eff72c2f8a878b7cbfdd2f36391a43b966b88
Milan Raj <rajsite@users.noreply.github.com>
2026-02-26 11:09:41 -0800

    Jasmine extensions karma plugin (#2874)
    # Pull Request
    
    ## 🤨 Rationale
    
    - Update jasmine-extensions to behave like a karma plugin (to be used
    from karma config) and library (to be used from a jasmine node helper).
    - Updated the component library configs to only load the plugin in the
    headless karma configs
    
    ## 👩‍💻 Implementation
    
    - Changed the library main import to be a karma config and moved the
    `applyExtensions` export to a secondary entrypoint
    - As part of the karma plugin added a rollup build step for the browser
    js file that is loaded in the browser by the plugin
    
    ## 🧪 Testing
    
    - Rely on existing tests
    - Manually verified locally that reties happen with test commands but
    not test debugger commands in components libraries. Angular libraries
    not updated yet.
    
    ## ✅ Checklist
    
    - [x] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

--- packages/spright-components/karma.conf.headless.cjs
+++ packages/spright-components/karma.conf.headless.cjs
@@ -3,12 +3,14 @@
  */
 
 const karmaSpecReporter = require('karma-spec-reporter');
+const jasmineExtensions = require('@ni-private/jasmine-extensions');
 const originalConfigFunction = require('./karma.conf.cjs');
 
 module.exports = config => {
     originalConfigFunction(config);
     const options = {
-        plugins: [...config.plugins, karmaSpecReporter],
+        plugins: [jasmineExtensions, ...config.plugins, karmaSpecReporter],
+        frameworks: ['jasmine-extensions', ...config.frameworks],
         reporters: ['spec'],
         specReporter: {
             suppressPassed: true,
