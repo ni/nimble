717c2078c136122b1cbfd74c5c53b802460e2696
Claudia Micu <163188334+Alexia-Claudia-Micu@users.noreply.github.com>
2026-03-03 15:45:57 +0200

    Remove matrix tests

--- packages/storybook/src/spright/chat/input/chat-input-matrix.stories.ts
+++ packages/storybook/src/spright/chat/input/chat-input-matrix.stories.ts
@@ -43,19 +43,6 @@ const errorStates = [
 ] as const;
 type ErrorState = (typeof errorStates)[number];
 
-const showCounterStates = [
-    ['', false],
-    ['counter', true]
-] as const;
-type ShowCounterState = (typeof showCounterStates)[number];
-
-const showCounterMaxLengthStates = [
-    ['no counter', false, 'no limit', -1],
-    ['counter', true, 'no limit', -1],
-    ['counter', true, 'limit 100', 100]
-] as const;
-type ShowCounterMaxLengthState = (typeof showCounterMaxLengthStates)[number];
-
 const metadata: Meta = {
     title: 'Tests Spright/Chat Input',
     parameters: {
@@ -69,8 +56,7 @@ const component = (
     [valueLabel, value]: ValueState,
     [placeholderLabel, placeholder]: PlaceholderState,
     [processingLabel, processing]: ProcessingState,
-    [errorLabel, error]: ErrorState,
-    [showCounterLabel, showCounter, maxLengthLabel, maxLength]: ShowCounterMaxLengthState
+    [errorLabel, error]: ErrorState
 ): ViewTemplate => html`
     <p 
         style="
@@ -79,36 +65,20 @@ const component = (
         margin-bottom: 0px;
         "
     >    
-        ${valueLabel} value, ${placeholderLabel} placeholder, ${processingLabel}, ${errorLabel} error, ${showCounterLabel}, ${maxLengthLabel}
+        ${valueLabel} value, ${placeholderLabel} placeholder, ${processingLabel}, ${errorLabel} error
     </p>
-    ${showCounter
-        ? html`
-            <${chatInputTag}
-                placeholder="${placeholder}"
-                value="${value}"
-                processing="${processing}"
-                error-visible="${error !== ''}"
-                error-text="${error}"
-                show-counter
-                maxlength="${maxLength}"
-            >
-            </${chatInputTag}>
-        `
-        : html`
-            <${chatInputTag}
-                placeholder="${placeholder}"
-                value="${value}"
-                processing="${processing}"
-                error-visible="${error !== ''}"
-                error-text="${error}"
-                maxlength="${maxLength}"
-            >
-            </${chatInputTag}>
-        `}
+    <${chatInputTag}
+        placeholder="${placeholder}"
+        value="${value}"
+        processing="${processing}"
+        error-visible="${error !== ''}"
+        error-text="${error}"
+    >
+    </${chatInputTag}>
 `;
 
 export const themeMatrix: StoryFn = createMatrixThemeStory(
-    createMatrix(component, [valueStates, placeholderStates, processingStates, errorStates, showCounterMaxLengthStates])
+    createMatrix(component, [valueStates, placeholderStates, processingStates, errorStates])
 );
 
 export const hidden: StoryFn = createStory(
@@ -122,8 +92,7 @@ export const textCustomized: StoryFn = createMatrixThemeStory(
 );
 
 const componentWithPlaceholder = (
-    [_, placeholder]: PlaceholderState,
-    [__, showCounter]: ShowCounterState
+    [_, placeholder]: PlaceholderState
 ): ViewTemplate => html`
     <p 
         style="
@@ -133,23 +102,13 @@ const componentWithPlaceholder = (
         "
     >    
     </p>
-    ${showCounter
-        ? html`
-            <${chatInputTag}
-                placeholder="${placeholder}"
-                show-counter
-            >
-            </${chatInputTag}>
-        `
-        : html`
-            <${chatInputTag}
-                placeholder="${placeholder}"
-            >
-            </${chatInputTag}>
-        `}
+    <${chatInputTag}
+        placeholder="${placeholder}"
+    >
+    </${chatInputTag}>
 `;
 
-const interactionStates = cartesianProduct([placeholderStates, showCounterStates] as const);
+const interactionStates = cartesianProduct([placeholderStates] as const);
 
 export const interactionsThemeMatrix: StoryFn = createMatrixThemeStory(
     createMatrixInteractionsFromStates(componentWithPlaceholder, {
