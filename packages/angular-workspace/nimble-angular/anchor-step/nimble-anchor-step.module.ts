cf994f6468d901746f0675af92cce9ba6e9815f2
rajsite <1588923+rajsite@users.noreply.github.com>
2026-04-13 11:50:05 -0500

    Revert step routerLink support

--- packages/angular-workspace/nimble-angular/anchor-step/nimble-anchor-step.module.ts
+++ packages/angular-workspace/nimble-angular/anchor-step/nimble-anchor-step.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NimbleAnchorStepDirective } from './nimble-anchor-step.directive';
-import { NimbleAnchorStepRouterLinkDirective } from './nimble-anchor-step-router-link.directive';
-import { NimbleAnchorStepRouterLinkWithHrefDirective } from './nimble-anchor-step-router-link-with-href.directive';
 
 import '@ni/nimble-components/dist/esm/anchor-step';
 
 @NgModule({
-    declarations: [NimbleAnchorStepDirective, NimbleAnchorStepRouterLinkDirective, NimbleAnchorStepRouterLinkWithHrefDirective],
+    declarations: [NimbleAnchorStepDirective],
     imports: [CommonModule],
-    exports: [NimbleAnchorStepDirective, NimbleAnchorStepRouterLinkDirective, NimbleAnchorStepRouterLinkWithHrefDirective]
+    exports: [NimbleAnchorStepDirective]
 })
 export class NimbleAnchorStepModule { }
