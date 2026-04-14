61ae182390e3806740418709cefb8c2dd5fd1370
Valerie Gleason <5265744+hellovolcano@users.noreply.github.com>
2026-04-14 13:58:19 -0500

    updates for example app refactor

---
+++ packages/react-workspace/react-client-app/src/components/ChipSection.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { NimbleChip } from '@ni/nimble-react/chip';
+import { NimbleCheckbox } from '@ni/nimble-react/checkbox';
+import { SubContainer } from './SubContainer';
+
+export function ChipSection(): React.JSX.Element {
+
+    function onChipRemove(): void {
+        alert('Chip removed');
+    }
+    
+    return (
+        <SubContainer label="Chip">
+            <NimbleChip>Outline Chip</NimbleChip>
+            <NimbleChip appearance="block">Block Chip</NimbleChip>
+            <NimbleChip removable onRemove={onChipRemove}>Removable Chip</NimbleChip>
+            <NimbleChip disabled>Disabled Chip</NimbleChip>
+        </SubContainer>
+    );
+}
