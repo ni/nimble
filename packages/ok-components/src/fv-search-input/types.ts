5c1e70c3ce16698271219216d65ab752a286eb9c
Fred Visser <1458528+fredvisser@users.noreply.github.com>
2026-04-20 09:18:19 -0500

    lint fix

--- packages/ok-components/src/fv-search-input/types.ts
+++ packages/ok-components/src/fv-search-input/types.ts
@@ -1,8 +1,8 @@
 export const FvSearchInputAppearance = {
-	block: 'block',
-	outline: 'outline',
-	ghost: 'ghost',
-	superGhost: 'super-ghost'
+    block: 'block',
+    outline: 'outline',
+    ghost: 'ghost',
+    superGhost: 'super-ghost'
 } as const;
 
 export type FvSearchInputAppearance = (typeof FvSearchInputAppearance)[keyof typeof FvSearchInputAppearance];
