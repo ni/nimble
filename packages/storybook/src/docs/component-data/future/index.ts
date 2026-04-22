3132fc2482a7bd1c000f4a4bb51e723ac9f0dfe3
rajsite <1588923+rajsite@users.noreply.github.com>
2026-04-22 13:46:05 -0500

    Split component status table

---
+++ packages/storybook/src/docs/component-data/future/index.ts
@@ -0,0 +1,172 @@
+import { ComponentFrameworkStatus } from '../../types';
+
+export const componentDataFuture = [
+    {
+        componentName: 'Accordion',
+        designHref:
+            'https://www.figma.com/file/PO9mFOu5BCl8aJvFchEeuN/Nimble_Components?node-id=1295%3A89225&mode=dev',
+        designLabel: 'Figma',
+        issueHref: 'https://github.com/ni/nimble/issues/533',
+        issueLabel: '#533',
+        library: 'future',
+        componentStatus: ComponentFrameworkStatus.doesNotExist,
+        angularStatus: ComponentFrameworkStatus.doesNotExist,
+        blazorStatus: ComponentFrameworkStatus.doesNotExist,
+        reactStatus: ComponentFrameworkStatus.doesNotExist
+    },
+    {
+        componentName: 'Badge',
+        issueHref: 'https://github.com/ni/nimble/issues/1428',
+        issueLabel: '#1428',
+        library: 'future',
+        componentStatus: ComponentFrameworkStatus.doesNotExist,
+        angularStatus: ComponentFrameworkStatus.doesNotExist,
+        blazorStatus: ComponentFrameworkStatus.doesNotExist,
+        reactStatus: ComponentFrameworkStatus.doesNotExist
+    },
+    {
+        componentName: 'Date picker',
+        issueHref: 'https://github.com/ni/nimble/issues/342',
+        issueLabel: '#342',
+        library: 'future',
+        componentStatus: ComponentFrameworkStatus.doesNotExist,
+        angularStatus: ComponentFrameworkStatus.doesNotExist,
+        blazorStatus: ComponentFrameworkStatus.doesNotExist,
+        reactStatus: ComponentFrameworkStatus.doesNotExist
+    },
+    {
+        componentName: 'Date Time Text',
+        issueHref: 'https://github.com/ni/nimble/issues/294',
+        issueLabel: '#294',
+        library: 'future',
+        componentStatus: ComponentFrameworkStatus.doesNotExist,
+        angularStatus: ComponentFrameworkStatus.doesNotExist,
+        blazorStatus: ComponentFrameworkStatus.doesNotExist,
+        reactStatus: ComponentFrameworkStatus.doesNotExist
+    },
+    {
+        componentName: 'Filter Builder (Query Builder)',
+        issueHref: 'https://github.com/ni/nimble/issues/310',
+        issueLabel: '#310',
+        library: 'future',
+        componentStatus: ComponentFrameworkStatus.doesNotExist,
+        angularStatus: ComponentFrameworkStatus.doesNotExist,
+        blazorStatus: ComponentFrameworkStatus.doesNotExist,
+        reactStatus: ComponentFrameworkStatus.doesNotExist
+    },
+    {
+        componentName: 'Label',
+        issueHref: 'https://github.com/ni/nimble/issues/312',
+        issueLabel: '#312',
+        library: 'future',
+        componentStatus: ComponentFrameworkStatus.doesNotExist,
+        angularStatus: ComponentFrameworkStatus.doesNotExist,
+        blazorStatus: ComponentFrameworkStatus.doesNotExist,
+        reactStatus: ComponentFrameworkStatus.doesNotExist
+    },
+    {
+        componentName: 'Picker (Chip)',
+        designHref:
+            'https://www.figma.com/design/PO9mFOu5BCl8aJvFchEeuN/Nimble_Components?node-id=2227-78839&p=f&m=dev',
+        designLabel: 'Figma',
+        issueHref: 'https://github.com/ni/nimble/issues/458',
+        issueLabel: '#458',
+        library: 'future',
+        componentStatus: ComponentFrameworkStatus.doesNotExist,
+        angularStatus: ComponentFrameworkStatus.doesNotExist,
+        blazorStatus: ComponentFrameworkStatus.doesNotExist,
+        reactStatus: ComponentFrameworkStatus.doesNotExist
+    },
+    {
+        componentName: 'Progress Bar',
+        issueHref: 'https://github.com/ni/nimble/issues/304',
+        issueLabel: '#304',
+        library: 'future',
+        componentStatus: ComponentFrameworkStatus.doesNotExist,
+        angularStatus: ComponentFrameworkStatus.doesNotExist,
+        blazorStatus: ComponentFrameworkStatus.doesNotExist,
+        reactStatus: ComponentFrameworkStatus.doesNotExist
+    },
+    {
+        componentName: 'Query Builder',
+        issueHref: 'https://github.com/ni/nimble/issues/506',
+        issueLabel: '#506',
+        library: 'future',
+        componentStatus: ComponentFrameworkStatus.doesNotExist,
+        angularStatus: ComponentFrameworkStatus.doesNotExist,
+        blazorStatus: ComponentFrameworkStatus.doesNotExist,
+        reactStatus: ComponentFrameworkStatus.doesNotExist
+    },
+    {
+        componentName: 'Search Field',
+        designHref:
+            'https://www.figma.com/file/PO9mFOu5BCl8aJvFchEeuN/Nimble_Components?node-id=1507%3A44702&mode=dev',
+        designLabel: 'Figma',
+        issueHref: 'https://github.com/ni/nimble/issues/299',
+        issueLabel: '#299',
+        library: 'future',
+        componentStatus: ComponentFrameworkStatus.doesNotExist,
+        angularStatus: ComponentFrameworkStatus.doesNotExist,
+        blazorStatus: ComponentFrameworkStatus.doesNotExist,
+        reactStatus: ComponentFrameworkStatus.doesNotExist
+    },
+    {
+        componentName: 'Skeleton',
+        issueHref: 'https://github.com/ni/nimble/issues/762',
+        issueLabel: '#762',
+        library: 'future',
+        componentStatus: ComponentFrameworkStatus.doesNotExist,
+        angularStatus: ComponentFrameworkStatus.doesNotExist,
+        blazorStatus: ComponentFrameworkStatus.doesNotExist,
+        reactStatus: ComponentFrameworkStatus.doesNotExist
+    },
+    {
+        componentName: 'Slider',
+        designHref:
+            'https://xd.adobe.com/view/33ffad4a-eb2c-4241-b8c5-ebfff1faf6f6-66ac/screen/29d3a5f7-9eed-498c-9957-2cd495458e3b',
+        designLabel: 'XD',
+        issueHref: 'https://github.com/ni/nimble/issues/295',
+        issueLabel: '#295',
+        library: 'future',
+        componentStatus: ComponentFrameworkStatus.doesNotExist,
+        angularStatus: ComponentFrameworkStatus.doesNotExist,
+        blazorStatus: ComponentFrameworkStatus.doesNotExist,
+        reactStatus: ComponentFrameworkStatus.doesNotExist
+    },
+    {
+        componentName: 'Split Button',
+        issueHref: 'https://github.com/ni/nimble/issues/1523',
+        issueLabel: '#1523',
+        library: 'future',
+        componentStatus: ComponentFrameworkStatus.doesNotExist,
+        angularStatus: ComponentFrameworkStatus.doesNotExist,
+        blazorStatus: ComponentFrameworkStatus.doesNotExist,
+        reactStatus: ComponentFrameworkStatus.doesNotExist
+    },
+    {
+        componentName: 'Toast',
+        designHref:
+            'https://www.figma.com/file/PO9mFOu5BCl8aJvFchEeuN/Nimble_Components?node-id=1295%3A68679&mode=dev',
+        designLabel: 'Figma',
+        issueHref: 'https://github.com/ni/nimble/issues/513',
+        issueLabel: '#513',
+        library: 'future',
+        componentStatus: ComponentFrameworkStatus.doesNotExist,
+        angularStatus: ComponentFrameworkStatus.doesNotExist,
+        blazorStatus: ComponentFrameworkStatus.doesNotExist,
+        reactStatus: ComponentFrameworkStatus.doesNotExist
+    },
+    {
+        componentName: 'Toggle Button Group',
+        designHref:
+            'https://xd.adobe.com/view/33ffad4a-eb2c-4241-b8c5-ebfff1faf6f6-66ac/screen/d022d8af-22f4-4bf2-981c-1dc0c61afece',
+        designLabel: 'XD',
+        issueHref: 'https://github.com/ni/nimble/issues/298',
+        issueLabel: '#298',
+        library: 'future',
+        componentStatus: ComponentFrameworkStatus.doesNotExist,
+        angularStatus: ComponentFrameworkStatus.doesNotExist,
+        blazorStatus: ComponentFrameworkStatus.doesNotExist,
+        reactStatus: ComponentFrameworkStatus.doesNotExist
+    }
+] as const;
