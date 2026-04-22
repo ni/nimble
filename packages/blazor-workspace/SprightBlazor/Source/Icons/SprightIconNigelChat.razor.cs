f6ec68d32a688a4ded23b8ba7cca0fcea014ac9a
Claudia Micu <163188334+Alexia-Claudia-Micu@users.noreply.github.com>
2026-04-22 11:20:08 +0300

    Add react and blazor wrapper for nigel chat icon

---
+++ packages/blazor-workspace/SprightBlazor/Source/Icons/SprightIconNigelChat.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
+namespace SprightBlazor;
+
+public partial class SprightIconNigelChat : ComponentBase
+{
+    /// <summary>
+    /// Gets or sets a collection of additional attributes that will be applied to the created element.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
+}
---
+++ packages/react-workspace/spright-react/src/icons/nigel-chat/index.ts
@@ -0,0 +1,4 @@
+import { IconNigelChat } from '@ni/spright-components/dist/esm/icons/nigel-chat';
+import { wrap } from '../../utilities/react-wrapper';
+
+export const SprightIconNigelChat = wrap(IconNigelChat);
