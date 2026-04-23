9f6c8ee156880eb2cf988e34195d8c3073c1748a
Alexia Claudia Micu <163188334+Alexia-Claudia-Micu@users.noreply.github.com>
2026-04-23 18:46:07 +0300

    Add react and blazor wrapper for nigel chat icon (#2940)
    # Pull Request
    
    ## 🤨 Rationale
    
    https://dev.azure.com/ni/DevCentral/_workitems/edit/3835544
    
    ## 👩‍💻 Implementation
    
    Add react and blazor wrapper for nigel chat icon
    
    ## 🧪 Testing
    
    <!---
    Detail the testing done to ensure this submission meets requirements. 
    
    Include automated/manual test additions or modifications, testing done
    on a local build, private CI run results, and additional testing not
    covered by automatic pull request validation. If any functionality is
    not covered by automated testing, provide justification.
    -->
    
    ## ✅ Checklist
    
    <!--- Review the list and put an x in the boxes that apply or ~~strike
    through~~ around items that don't (along with an explanation). -->
    
    - [ ] I have updated the project documentation to reflect my changes or
    determined no changes are needed.

---
+++ packages/react-workspace/spright-react/src/icons/nigel-chat/index.ts
@@ -0,0 +1,6 @@
+import { IconNigelChat, iconNigelChatTag } from '@ni/spright-components/dist/esm/icons/nigel-chat';
+import { wrap } from '../../utilities/react-wrapper';
+
+export type { IconNigelChat };
+export { iconNigelChatTag };
+export const SprightIconNigelChat = wrap(IconNigelChat);
