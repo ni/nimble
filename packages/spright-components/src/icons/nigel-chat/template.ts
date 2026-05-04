861a21d7b1e0bd025c15bd93fa0df8202ea08ea9
Jesse Attas <jattasNI@users.noreply.github.com>
2026-05-04 17:51:43 -0500

    Use dark mode icon for light mode

--- packages/spright-components/src/icons/nigel-chat/template.ts
+++ packages/spright-components/src/icons/nigel-chat/template.ts
@@ -1,8 +1,10 @@
 import { html } from '@ni/fast-element';
 import type { IconNigelChat } from '.';
-import { nigelChatLightData, nigelChatDarkData } from './icon-data';
+import { nigelChatDarkData } from './icon-data';
 
+// Visual Design is iterating on this icon for NI Connect 2026; it's likely we will get different
+// icons for dark and light mode in future but for now they like the dark icon even on light background
 export const template = html<IconNigelChat>`
-    <div class="icon light-icon" aria-hidden="true" :innerHTML="${() => nigelChatLightData}"></div>
+    <div class="icon light-icon" aria-hidden="true" :innerHTML="${() => nigelChatDarkData}"></div>
     <div class="icon dark-icon" aria-hidden="true" :innerHTML="${() => nigelChatDarkData}"></div>
 `;
