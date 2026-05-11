a4a9c0798472f2d7b088027cd89e77e8652f0f37
Fred Visser <1458528+fredvisser@users.noreply.github.com>
2026-05-11 16:07:15 -0500

    Refine FV i18n ignore attributes

--- packages/eslint-config-nimble/ok/fv/ignore-attributes.js
+++ packages/eslint-config-nimble/ok/fv/ignore-attributes.js
@@ -1,19 +1,26 @@
 // Fv angular attributes that SHOULD NOT ever be localized need to be added to ignoreAttributeSets
 // See: https://github.com/ni/javascript-styleguide/blob/main/packages/eslint-config-angular/lib/ok/fv/ignore-attributes.js
 
-// Fv angular attributes that SHOULD be localized in production, but we don't want to localize
-// in example apps should be added to the following list:
-export const fvIgnoreAttributes = [
-    'appearance-variant',
+const exampleAppOnlyIgnoreAttributes = [
+    // Should be localized in production, but are intentionally not localized in example apps.
     'card-title',
-    'count',
     'description',
     'edit-items-button-label',
     'header',
-    'initials',
-    'interaction-mode',
     'options',
     'selected-values',
     'subtitle',
     'trigger-label',
+];
+
+const temporaryAlwaysIgnoreAttributes = [
+    // Should not be localized. Keep these here until they move into the shared ignoreAttributeSets.
+    'count',
+    'initials',
+    'interaction-mode',
+];
+
+export const fvIgnoreAttributes = [
+    ...exampleAppOnlyIgnoreAttributes,
+    ...temporaryAlwaysIgnoreAttributes,
 ];
