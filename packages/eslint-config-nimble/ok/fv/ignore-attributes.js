6dde2dfb09c1bb39026da9644b3c370a414e5cb1
rajsite <1588923+rajsite@users.noreply.github.com>
2026-05-11 13:57:25 -0500

    Update fv specific path

--- packages/eslint-config-nimble/ok/fv/ignore-attributes.js
+++ packages/eslint-config-nimble/ok/fv/ignore-attributes.js
@@ -1,7 +1,7 @@
 // Fv angular attributes that SHOULD NOT ever be localized need to be added to ignoreAttributeSets
-// See: https://github.com/ni/javascript-styleguide/blob/main/packages/eslint-config-angular/lib/template-options.js
+// See: https://github.com/ni/javascript-styleguide/blob/main/packages/eslint-config-angular/lib/ok/fv/ignore-attributes.js
 
-// Fv angular attributes that SHOULD be localized in production, but we don't want to
+// Fv angular attributes that SHOULD be localized in production, but we don't want to localize
 // in example apps should be added to the following list:
 export const fvIgnoreAttributes = [
 ];
