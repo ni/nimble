32dc35f0242ef4872395dc43bf7645afc059ffa5
Fred Visser <1458528+fredvisser@users.noreply.github.com>
2026-07-10 11:08:55 -0400

    Add OK React component wrappers

---
+++ packages/react-workspace/ok-react/src/fv/master-detail-list-item/index.ts
@@ -0,0 +1,11 @@
+'use client';
+
+import {
+    FvMasterDetailListItem,
+    fvMasterDetailListItemTag
+} from '@ni/ok-components/dist/esm/fv/master-detail-list-item';
+import { wrap } from '../../utilities/react-wrapper';
+
+export { fvMasterDetailListItemTag };
+export { type FvMasterDetailListItem };
+export const OkFvMasterDetailListItem = wrap(FvMasterDetailListItem);
