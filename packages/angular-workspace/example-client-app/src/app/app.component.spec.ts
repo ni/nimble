74fea765fbe244553b328fc7b18353348f3e9fa0
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2026-08-14 17:54:46 -0500

    Fix build and add test command for app

--- packages/angular-workspace/example-client-app/src/app/app.component.spec.ts
+++ packages/angular-workspace/example-client-app/src/app/app.component.spec.ts
@@ -1,17 +1,13 @@
 import { provideZoneChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { NimbleNumberFieldModule, NimbleTextFieldModule } from '@ni/nimble-angular';
+import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [
-                AppComponent
-            ],
             imports: [
-                NimbleTextFieldModule,
-                NimbleNumberFieldModule
+                AppModule
             ],
             providers: [provideZoneChangeDetection()]
         });
