e84422f83eb8c6325f9076dce9a0cf9db0b2bcec
Fred Visser <1458528+fredvisser@users.noreply.github.com>
2026-09-08 17:36:06 -0500

    Keep FV splitter divider visible in color theme

--- packages/ok-components/src/fv/splitter/styles.ts
+++ packages/ok-components/src/fv/splitter/styles.ts
@@ -1,7 +1,7 @@
 import { css } from '@ni/fast-element';
 import {
     borderHoverColor,
-    dividerBackgroundColor
+    borderColor
 } from '@ni/nimble-components/dist/esm/theme-provider/design-tokens';
 import { display } from '../../utilities/style/display';
 
@@ -17,7 +17,7 @@ export const styles = css`
             width: 2px;
             height: 100%;
             min-height: 0;
-            background: ${dividerBackgroundColor};
+            background: ${borderColor};
         }
 
         .splitter {
