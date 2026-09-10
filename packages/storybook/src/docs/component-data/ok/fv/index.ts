73238d7add6d985c7572a6e089574cf46ad7ca55
Fred Visser <1458528+fredvisser@users.noreply.github.com>
2026-09-10 08:39:41 -0500

    fix component status

--- packages/storybook/src/docs/component-data/ok/fv/index.ts
+++ packages/storybook/src/docs/component-data/ok/fv/index.ts
@@ -9,7 +9,7 @@ export const componentDataOkFv = [
         library: 'ok',
         componentStatus: ComponentFrameworkStatus.ready,
         angularStatus: ComponentFrameworkStatus.ready,
-        blazorStatus: ComponentFrameworkStatus.doesNotExist,
+        blazorStatus: ComponentFrameworkStatus.ready,
         reactStatus: ComponentFrameworkStatus.ready
     },
     {
@@ -91,7 +91,7 @@ export const componentDataOkFv = [
         library: 'ok',
         componentStatus: ComponentFrameworkStatus.ready,
         angularStatus: ComponentFrameworkStatus.ready,
-        blazorStatus: ComponentFrameworkStatus.ready,
+        blazorStatus: ComponentFrameworkStatus.doesNotExist,
         reactStatus: ComponentFrameworkStatus.ready
     },
     {
